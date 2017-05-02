beb10c2cf820eb9219e9226fbb0bc704a80d2103
Ralph Peelen <ralph.peelen@deltares.nl>
2017-05-02 07:21:42 +0000

    SOBEK3-986: Coordinate system compare has now fail safe check if source cs is null Former-commit-id: bebdfa7a9505be78dbfbb76dab77650bbc25f573 Former-commit-id: 0acc6b04e3f1f788ba3e9bfaefe2dedbbf615576

--- src/DelftTools.Hydro/Helpers/ICoordinateSystemExtensions.cs
+++ src/DelftTools.Hydro/Helpers/ICoordinateSystemExtensions.cs
@@ -9,7 +9,9 @@ namespace DelftTools.Hydro.Helpers
         /// Only parameters used for coordinate system are used for comparison.
         public static bool EqualsTo(this ICoordinateSystem sourceCoordinateSystem, ICoordinateSystem destinationCoordinateSystem)
         {
+            if (sourceCoordinateSystem == null && destinationCoordinateSystem == null ) return true;
             if (destinationCoordinateSystem == null) return false;
+            if (sourceCoordinateSystem == null) return false;
             if (sourceCoordinateSystem.Name != destinationCoordinateSystem.Name) return false;
             if (sourceCoordinateSystem.Abbreviation != destinationCoordinateSystem.Abbreviation) return false;
             if (sourceCoordinateSystem.Authority != destinationCoordinateSystem.Authority) return false;
