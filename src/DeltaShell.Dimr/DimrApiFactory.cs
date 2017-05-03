33f492b2e5c20a3bbd417c3c8a16ca261de51f77
Hidde Elzinga <hidde.elzinga@deltares.nl>
2017-05-03 08:11:51 +0000

    Added UML diagram for Dimr project Moved DimrApiDataSet to separate file Made DimrApiDataSet and DimrApiFactory static
    Former-commit-id: 265f0898f7f1104e19faa5941cf7761b58625397
    Former-commit-id: ee2d8bd98f168bf9e1835311cb32f65cdd755c3f

--- src/DeltaShell.Dimr/DimrApiFactory.cs
+++ src/DeltaShell.Dimr/DimrApiFactory.cs
@@ -2,15 +2,16 @@
 
 namespace DeltaShell.Dimr
 {
-    public class DimrApiFactory
+    public static class DimrApiFactory
     {
         public static IDimrApi CreateNew(bool useMessagesBuffering = false, bool runRemote = true)
         {
             if(useMessagesBuffering)
                 return new DimrExe(useMessagesBuffering);
-            return runRemote ? (IDimrApi)new RemoteDimrApi(Environment.Is64BitProcess) : new DimrApi(false);
-            //return new RemoteDimrApi(Environment.Is64BitProcess);
-            //return new DimrApi(false);
+
+            return runRemote 
+                ? (IDimrApi)new RemoteDimrApi(Environment.Is64BitProcess) 
+                : new DimrApi(false);
         }
     }
 }
