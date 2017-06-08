d826d5350965ee96a9889362f292168ae8974b94
brummen <brummen@deltares.nl>
2017-06-08 11:15:58 +0000

    DELFT3DFM-875 Save the network object in the FM model
    Former-commit-id: ca246114e29b60c109babd14272cd9fe78f606cf
    Former-commit-id: ba5801e23f30f630c4f080ebc4b3b219f450adee

--- src/DeltaShell.NGHS.IO/Grid/IUGrid1D.cs
+++ src/DeltaShell.NGHS.IO/Grid/IUGrid1D.cs
@@ -2,7 +2,7 @@
 {
     public interface IUGrid1D : IGrid
     {
-        void Create1DGridInFile(string name, int numberOfNodes, int numberOfBranches, int totalNumberOfGeometryPoints);
+        void Create1DGridInFile(string name, int numberOfNodes, int numberOfBranches, int totalNumberOfGeometryPoints, out int networkId);
 
         void Write1DNetworkNodes(double[] nodesX, double[] nodesY, string[] nodesids, string[] nodeslongNames);
         void Write1DNetworkBranches(int[] sourceNodeId, int[] targetNodeId, double[] branchLengths, int[] nbranchgeometrypoints, string[] branchIds, string[] branchLongnames);
@@ -12,11 +12,9 @@
         int GetNumberOfNetworkBranches();
         int GetNumberOfNetworkGeometryPoints();
 
-        void Create1DMeshInFile(string name, int numberOfMeshPoints, int numberOfMeshEdges);
-        void Write1DMeshDiscretizationPoints(int[] branchIdx, double[] offset);
-        int GetNumberOfMeshDiscretisationPoints();
-        int Read1DNetworkGeometry(out double[] geopointsX, out double[] geopointsY);
-        int Read1DNetworkNodes(out double[] nodesX, out double[] nodesY, out string[] nodesIds, out string[] nodesLongnames);
-        int Read1DNetworkBranches(out int[] sourceNodes, out int[] targetNodes, out double[] branchLengths, out int[] branchGeoPoints, out string[] branchIds, out string[] branchLongnames);
+      
+        void Read1DNetworkGeometry(out double[] geopointsX, out double[] geopointsY);
+        void Read1DNetworkBranches(out int[] sourceNodes, out int[] targetNodes, out double[] branchLengths, out int[] branchGeoPoints, out string[] branchIds, out string[] branchLongnames);
+        void Read1DNetworkNodes(out double[] nodesX, out double[] nodesY, out string[] nodesIds, out string[] nodesLongnames);
     }
 }
