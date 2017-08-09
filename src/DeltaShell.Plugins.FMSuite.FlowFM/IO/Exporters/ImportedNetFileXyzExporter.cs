5c2d6c47f4496152bf56ebf48ac88c1566c91335
kahmann <kahmann@deltares.nl>
2017-08-09 10:06:53 +0000

    DELFT3DFM-1137: Improved the code coverage of ImportedNetFileXyzExporter to 100%. I excluded the two protected methods from code coverage, because they cannot be tested from a test class. Former-commit-id: ad0d6fe1d1a5dfb69d582d209039dffb1416d3ce Former-commit-id: 28cbe4c172578c2e882ac21ea828285b22d5e8b5

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Exporters/ImportedNetFileXyzExporter.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Exporters/ImportedNetFileXyzExporter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using DeltaShell.Plugins.SharpMapGis.ImportExport;
 using GeoAPI.Extensions.Coverages;
 
@@ -12,11 +13,13 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.Exporters
             yield return typeof (ImportedFMNetFile);
         }
 
+        [ExcludeFromCodeCoverage]
         protected override bool CheckObject(object item)
         {
             return item is ImportedFMNetFile;
         }
 
+        [ExcludeFromCodeCoverage]
         protected override IEnumerable<IPointValue> GetPointValues(object item)
         {
             return base.GetPointValues(((ImportedFMNetFile) item).Grid);
