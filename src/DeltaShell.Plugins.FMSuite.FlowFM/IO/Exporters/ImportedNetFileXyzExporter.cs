bc8d1c28ef2c8b578e2b0950e53477dc0b8cfb2a
kahmann <kahmann@deltares.nl>
2017-08-10 08:32:06 +0000

    DELFT3DFM-1137: Included functionality in code coverage again in ImportedNetFileXyzExporter. Former-commit-id: df23559654f80fdb2f410bd4c8c4af22feeb26a1 Former-commit-id: ee526260948d15db4f11c9206b3db804cb0a86a7

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Exporters/ImportedNetFileXyzExporter.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Exporters/ImportedNetFileXyzExporter.cs
@@ -12,14 +12,12 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.Exporters
         {
             yield return typeof (ImportedFMNetFile);
         }
-
-        [ExcludeFromCodeCoverage]
+        
         protected override bool CheckObject(object item)
         {
             return item is ImportedFMNetFile;
         }
-
-        [ExcludeFromCodeCoverage]
+        
         protected override IEnumerable<IPointValue> GetPointValues(object item)
         {
             return base.GetPointValues(((ImportedFMNetFile) item).Grid);
