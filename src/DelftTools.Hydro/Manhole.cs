f5d7f2e8e89d63336f650103af05db0071aaccb8
kahmann <kahmann@deltares.nl>
2017-11-06 12:30:06 +0000

    DELFT3DFM-1206: Created an initial design for a manhole. Also added an ICompartment interface, which still needs to be worked out. Former-commit-id: 8738cb30bbdaa634d22513e4926389b9de977f43 Former-commit-id: c589019052da0fb4fc9130c6ca51a1f13d6659ca

---
+++ src/DelftTools.Hydro/Manhole.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace DelftTools.Hydro
+{
+    public class Manhole : HydroNode
+    {
+        /// <summary>
+        /// The unique manhole Id that is defined in the GWSW files of the sewer system.
+        /// </summary>
+        private long manholeId;
+
+        public Manhole(string name, long id)
+        {
+            Name = name;
+            manholeId = id;
+        }
+
+        /// <summary>
+        /// The shape of the manhole (either square or rectangular).
+        /// </summary>
+        public ManholeShape Shape { get; set; }
+
+        /// <summary>
+        /// The compartments that this manhole contains.
+        /// </summary>
+        public ICollection<ICompartment> Compartments { get; set; }
+
+        /// <summary>
+        /// Length of manhole (mm).
+        /// </summary>
+        public int ManholeLength { get; set; }
+
+        /// <summary>
+        /// Width of manhole (mm).
+        /// </summary>
+        public int ManholeWidth { get; set; }
+
+        /// <summary>
+        /// The area at surface level that this manhole can flood (m2).
+        /// </summary>
+        public double FloodableArea { get; set; }
+
+        /// <summary>
+        /// The bottom level of the manhole compared to Dutch NAP (m).
+        /// </summary>
+        public double BottomLevel { get; set; }
+
+        /// <summary>
+        /// The surface level of the manhole compared to Dutch NAP (m).
+        /// </summary>
+        public double SurfaceLevel { get; set; }
+    }
+
+    public enum ManholeShape
+    {
+        Rectangular,
+        Square
+    }
+}
