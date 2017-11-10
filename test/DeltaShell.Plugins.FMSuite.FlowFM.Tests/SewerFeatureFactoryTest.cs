e1fc449a0134f7095fe59854bd5f871e114a6ecf
Carles Soriano Perez <Carles.SorianoPerez@deltares.nl>
2017-11-10 16:25:00 +0000

    DELFT3DFM-1205 Created test for known attributes of pipe type.
    Former-commit-id: 09af16fc1bab67edd11fd52aa6137238313adfc5
    Former-commit-id: 7cdaef6cf43f44d420281dc301fcb517d75d4c7c

--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/SewerFeatureFactoryTest.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/SewerFeatureFactoryTest.cs
@@ -50,17 +50,6 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests
         }
 
         [Test]
-        public void CreatePipeFromFactoryWithKnownAttributes()
-        {
-            var element = new GwswElement
-            {
-                ElementTypeName = SewerFeatureType.Pipe.ToString()
-            };
-
-            Assert.That(element.GetType(), Is.EqualTo(typeof(Pipe)));
-        }
-
-        [Test]
         public void SewerFeatureTypeCanBeRetrievedWithAStringValue()
         {
             SewerFeatureType testValue;
@@ -97,6 +86,85 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests
 
             var createdPipe = element as Pipe;
             Assert.IsNotNull(createdPipe);
+        }
+
+        [Test]
+        public void CreatePipeFromFactoryWithKnownAttributes()
+        {
+            var startLevel = 30;
+            var endLevel = 100;
+            var length = 200;
+            var crossSectionDef = "crossSectionDef001";
+            var startNode = "node001";
+            var endNode = "node002";
+            var pipeType = PipeType.Open;
+
+            var nodeGwswElement = new GwswElement
+            {
+                ElementTypeName = SewerFeatureType.Pipe.ToString(),
+                GwswAttributeList = new List<GwswAttribute>()
+                {
+                    new GwswAttribute()
+                    {
+                        GwswAttributeType = new GwswAttributeType("testFile", 0, "columnName", "string", "PIPE_TYPE", "unkownDefinition", "mandatoryMaybe", "noRemarks"),
+                        ValueAsString = pipeType.ToString()
+                    },
+                    new GwswAttribute()
+                    {
+                        GwswAttributeType = new GwswAttributeType("testFile", 1, "columnName", "double", "LEVEL_START", "unkownDefinition", "mandatoryMaybe", "noRemarks"),
+                        ValueAsString = startLevel.ToString()
+                    },
+                    new GwswAttribute()
+                    {
+                        GwswAttributeType = new GwswAttributeType("testFile", 2, "columnName", "double", "LEVEL_END", "unkownDefinition", "mandatoryMaybe", "noRemarks"),
+                        ValueAsString = endLevel.ToString()
+                    },
+                    new GwswAttribute()
+                    {
+                        GwswAttributeType = new GwswAttributeType("testFile", 3, "columnName", "double", "LENGTH", "unkownDefinition", "mandatoryMaybe", "noRemarks"),
+                        ValueAsString = length.ToString()
+                    },
+                    new GwswAttribute()
+                    {
+                        GwswAttributeType = new GwswAttributeType("testFile", 4, "columnName", "string", "CROSS_SECTION_DEF", "unkownDefinition", "mandatoryMaybe", "noRemarks"),
+                        ValueAsString = crossSectionDef
+                    },
+                    new GwswAttribute()
+                    {
+                        GwswAttributeType = new GwswAttributeType("testFile", 5, "columnName", "string", "NODE_ID_START", "unkownDefinition", "mandatoryMaybe", "noRemarks"),
+                        ValueAsString = startNode
+                    },
+                    new GwswAttribute()
+                    {
+                        GwswAttributeType = new GwswAttributeType("testFile", 6, "columnName", "string", "NODE_ID_END", "unkownDefinition", "mandatoryMaybe", "noRemarks"),
+                        ValueAsString = endNode
+                    },
+                }
+            };
+
+            var element = SewerFeatureFactory.CreateInstance(nodeGwswElement);
+            Assert.That(element.GetType(), Is.EqualTo(typeof(Pipe)));
+
+            var createdPipe = element as Pipe;
+            Assert.IsNotNull(createdPipe);
+            
+            //Not defined yet
+            Assert.IsNull(createdPipe.Source);
+            Assert.IsNull(createdPipe.Target);
+            Assert.IsNull(createdPipe.CrossSectionShape);
+            
+            //Defined
+            Assert.IsNotNull(createdPipe.LevelSource);
+            Assert.AreEqual(startLevel, createdPipe.LevelSource);
+
+            Assert.IsNotNull(createdPipe.LevelTarget);
+            Assert.AreEqual(endLevel, createdPipe.LevelTarget);
+
+            Assert.IsNotNull(createdPipe.PipeType);
+            Assert.AreEqual(pipeType, createdPipe.PipeType);
+
+            Assert.IsNotNull(createdPipe.Length);
+            Assert.AreEqual(length, createdPipe.Length);
         }
     }
 }
