77ac38919f1837ee00c6d54299a605d7b643d303
Alex Koster <Alex.Koster@deltares.nl>
2017-11-27 15:10:49 +0000

    DELFT3DFM-1200 writing retentions, boundaries and boundary condition files
    Former-commit-id: 9654be876430242b0274327901ce52f8b23480a7
    Former-commit-id: be74bff0ac211a27f5b688cce3ec68e9024529b5

--- test/DeltaShell.Plugins.FMSuite.Common.Tests/test-data/urban/script/TestHyd_GWSW/python/FMwriter.py
+++ test/DeltaShell.Plugins.FMSuite.Common.Tests/test-data/urban/script/TestHyd_GWSW/python/FMwriter.py
@@ -1,4 +1,5 @@
 import os
+from math import pi
 from netCDF4 import Dataset
 from datetime import *
 
@@ -9,40 +10,159 @@ class FMwriter:
     def __init__(self, model):
         self.model = model
 
+    def getSorteddArrayByReadingOrder(self, dict):
+        dictValueArrayLength = len(list(self.model.nodes.values())[0])
+        lst = sorted(self.model.nodes.values(), key=lambda x: x[dictValueArrayLength-1])
+        return lst
+
     def writeAll(self, dirPath):  # write all fm files from GWSW model
-        self.writeNodes(dirPath)
+        self.writeBoundaries(dirPath)
+        self.writeRetentions(dirPath)
         self.writeConnections(dirPath)
         self.writeFMnetwork(dirPath, "test")
         return True
 
-    def writeNodes(self, dirPath):  # write all fm files from GWSW model
-        filePath = os.path.join(dirPath, 'output_inputFM', 'node.ini')
-        # UNIQUE_ID	UNI_IDE
-        # MANHOLE_ID	IDE_KNP
-        # network_node_id
-        # MANHOLE_INDICATOR	KNP_KNM
-        # COMPARTMENT_NUMBER	IDE_COM
-        # SURFACE_LEVEL	MVD_NIV
-        # FLOODABLE_AREA	WOS_ OPP
-        # NODE_SHAPE	KNP_VRM
-        # BOTTOM_LEVEL	KNP_BOK
-        # NODE_WIDTH	KNP_BRE
-        # NODE_LENGTH	KNP_LEN
+    def writeBoundaries(self, dirPath):  # write boundaries ini and bc
+        #UNI_IDE	Unieke identificatie van het knooppunt of de verbinding, een verwijzing naar de bestandsregel-identificatie. De waarde van deze kolom mag slechts één keer voorkomen in zowel Knooppunt.csv als Verbinding.csv. Koppeling tussen Knooppunt.csv of Verbinding.csv met Kunstwerk.csv, BOP.csv, Oppervlak.csv, Debiet.csv.
+        #RST_IDE	Identificatie (naam, nummer, code) van het rioolstelsel
+        #PUT_IDE	Identificatie (naam, nummer, code) van de put of het bouwwerk
+        #KNP_XCO	X-coördinaat knooppunt. Conform coördinatenstelsel EPSG:7415 (x/y conform EPSG:28992 (=RD), z conform EPSG:5709 (=NAP).
+        #KNP_YCO	Y-coördinaat knooppunt. Conform coördinatenstelsel EPSG:7415 (x/y conform EPSG:28992 (=RD), z conform EPSG:5709 (=NAP).
+        #CMP_IDE	Identificatie (naam, nummer, code) van het compartiment
+        #MVD_NIV	Niveau maaiveld t.o.v. NAP
+        #MVD_SCH	Type maaiveldschematisering
+        #WOS_OPP	Oppervlak water op straat
+        #KNP_MAT	Materiaal put
+        #KNP_VRM	Vorm put
+        #KNP_BOK	Niveau binnenonderkant put t.o.v. NAP
+        #KNP_BRE	Breedte/diameter putbodem
+        #KNP_LEN	Lengte putbodem
+        #KNP_TYP	Type knooppunt
+        #INZ_TYP	Type afvalwater dat wordt ingezameld
+        #INI_NIV	Initiële waterstand t.o.v. NAP
+        #STA_OBJ	Status van het object
+        #AAN_MVD	Aanname maaiveldhoogte
+        #ITO_IDE	Definitie infiltratiekarakteristieken. Koppeling tussen ItObject.csv en Verbinding.csv of Knooppunt.csv
+        #ALG_TOE	Toelichting bij deze regel
+
+        defaultWaterlevelOut = '-100.00' # low value needed
+
+        filePathIni = os.path.join(dirPath, 'output_inputFM', 'BoundaryLocations.ini')
+        filePathBc = os.path.join(dirPath, 'output_inputFM', 'BoundaryConditions.bc')
+
+        fileLocs = open(filePathIni, 'w')
+        fileBc = open(filePathBc, 'w')
+
+        #header Ini
+        fileLocs.write('[General]\n')
+        fileLocs.write('majorVersion = 1\n')
+        fileLocs.write('minorVersion = 0\n')
+        fileLocs.write('fileType = boundLocs\n')
+        fileLocs.write('\n')
+
+        #header Bc
+        fileBc.write('[Boundary]\n')
+        fileBc.write('majorVersion = 1\n')
+        fileBc.write('minorVersion = 0\n')
+        fileBc.write('fileType = boundConds\n')
+        fileBc.write('\n')
+
+        for item in self.getSorteddArrayByReadingOrder(self.model.nodes):
+
+            if str(item[14]) != 'UIT': # is not a boundary
+                continue
+
+            #location
+            fileLocs.write('[Boundary]\n')
+            fileLocs.write('nodeId = ' + str(item[0]) + '\n')
+            fileLocs.write('type = 1\n')
+            fileLocs.write('\n')
+
+            #condition
+            fileBc.write('[Boundary]\n')
+            fileBc.write('name = ' + str(item[0]) + '\n')
+            fileBc.write('function = constant\n')
+            fileBc.write('time-interpolation = linear-extrapolate\n')
+            fileBc.write('quantity = water_level\n')
+            fileBc.write('unit = m\n')
+            fileBc.write(defaultWaterlevelOut + '\n')
+            fileBc.write('\n')
+
+        fileLocs.close()
+        fileBc.close()
+
+        return True
+
+
+    def writeRetentions(self, dirPath):  # write all manholes from GWSW model
+        filePath = os.path.join(dirPath, 'output_inputFM', 'Retention.ini')
+        #UNI_IDE	Unieke identificatie van het knooppunt of de verbinding, een verwijzing naar de bestandsregel-identificatie. De waarde van deze kolom mag slechts één keer voorkomen in zowel Knooppunt.csv als Verbinding.csv. Koppeling tussen Knooppunt.csv of Verbinding.csv met Kunstwerk.csv, BOP.csv, Oppervlak.csv, Debiet.csv.
+        #RST_IDE	Identificatie (naam, nummer, code) van het rioolstelsel
+        #PUT_IDE	Identificatie (naam, nummer, code) van de put of het bouwwerk
+        #KNP_XCO	X-coördinaat knooppunt. Conform coördinatenstelsel EPSG:7415 (x/y conform EPSG:28992 (=RD), z conform EPSG:5709 (=NAP).
+        #KNP_YCO	Y-coördinaat knooppunt. Conform coördinatenstelsel EPSG:7415 (x/y conform EPSG:28992 (=RD), z conform EPSG:5709 (=NAP).
+        #CMP_IDE	Identificatie (naam, nummer, code) van het compartiment
+        #MVD_NIV	Niveau maaiveld t.o.v. NAP
+        #MVD_SCH	Type maaiveldschematisering
+        #WOS_OPP	Oppervlak water op straat
+        #KNP_MAT	Materiaal put
+        #KNP_VRM	Vorm put
+        #KNP_BOK	Niveau binnenonderkant put t.o.v. NAP
+        #KNP_BRE	Breedte/diameter putbodem
+        #KNP_LEN	Lengte putbodem
+        #KNP_TYP	Type knooppunt
+        #INZ_TYP	Type afvalwater dat wordt ingezameld
+        #INI_NIV	Initiële waterstand t.o.v. NAP
+        #STA_OBJ	Status van het object
+        #AAN_MVD	Aanname maaiveldhoogte
+        #ITO_IDE	Definitie infiltratiekarakteristieken. Koppeling tussen ItObject.csv en Verbinding.csv of Knooppunt.csv
+        #ALG_TOE	Toelichting bij deze regel
 
         file = open(filePath, 'w')
-        for key, value in sorted(self.model.nodes.items()):
-            file.write('[node]\n')
-            file.write('id=' + str(key) + '\n')
-            file.write('network_node_id=' + str(value[1]) + '\n')
-            file.write('manhole_id=' + str(value[1]) + '\n')
-            file.write('manhole_indicator=' + str(value[4]) + '\n')
-            file.write('compartment_number=\n')
-            file.write('surface_level=' + str(value[5]) + '\n')
-            file.write('floodable_area=' + str(value[7]) + '\n')
-            file.write('node_shape=' + str(value[9]) + '\n')
-            file.write('bottom_level=' + str(value[10]) + '\n')
-            file.write('node_width=' + str(value[11]) + '\n')
-            file.write('node_length=' + str(value[12]) + '\n')
+
+        #header
+        file.write('[General]\n')
+        file.write('majorVersion = 1\n')
+        file.write('minorVersion = 0\n')
+        file.write('\n')
+
+        for item in self.getSorteddArrayByReadingOrder(self.model.nodes):
+
+            if str(item[14]) == 'UIT': # is a boundary
+                continue
+
+            # [Retention]
+            # id=''
+            # name=''
+            # nodeId='' #new is ugrid id
+            # manholeId=' #new is manhole parent
+            # storageType=Closed
+            # useTable=0
+            # bedLevel=0.0
+            # area=0.0
+            # streetLevel=0.0
+            file.write('[Retention]\n')
+            file.write('id = ' + str(item[0]) + '\n')
+            file.write('name = ' + str(item[2]) + '\n')
+            file.write('nodeId = ' + str(item[0]) + '\n')
+            file.write('manholeId = ' + str(item[2]) + '\n')
+            file.write('storageType = Closed\n')
+            file.write('useTable = 0\n')
+            file.write('bedLevel = ' + str(item[11]) + '\n')
+
+            area = 0.0
+            br = (float(item[12]) / 100)
+            l = (float(item[13]) / 100)
+
+            if str(item[11]) == 'RND':
+                area = pi * br**2
+            else:
+                area = br * l
+
+            areaStr  = str("%.2f" % round(area,2))
+
+            file.write('area = ' + areaStr + '\n')
+            file.write('streetLevel = ' + str(item[6]) + '\n')
             file.write('\n')
         file.close()
         return True
