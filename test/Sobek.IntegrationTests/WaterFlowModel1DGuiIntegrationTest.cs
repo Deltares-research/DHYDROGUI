53eb81d510ad70db7fcba8b8f7ae5ff247904716
Carles Soriano Perez <Carles.SorianoPerez@deltares.nl>
2018-01-26 14:58:18 +0000

    Increased test performance time due to have increased the amount of work done in the target function.
    Former-commit-id: 6457f6a85fe250c62ecdcc429ffcdf86e93ff16e
    Former-commit-id: 509dd91b1895a0fa03bec5b00794d4931f32f82d

--- test/Sobek.IntegrationTests/WaterFlowModel1DGuiIntegrationTest.cs
+++ test/Sobek.IntegrationTests/WaterFlowModel1DGuiIntegrationTest.cs
@@ -1053,7 +1053,7 @@ namespace Sobek.IntegrationTests
                 var exporter = new WaterFlowModel1DExporter();
 
                 string filepath = TestHelper.GetTestDataPath(typeof(WaterFlowModel1DGuiIntegrationTest).Assembly, "BridgeExport_SOBEK3-54.md1d"); 
-                TestHelper.AssertIsFasterThan(5000, () => exporter.Export(model, filepath));
+                TestHelper.AssertIsFasterThan(6000, () => exporter.Export(model, filepath));
 
                 Assert.IsTrue(File.Exists(filepath));
                 
