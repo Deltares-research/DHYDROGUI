2c98a3fe4a5ebba88b38f7abcdd3a31ad92b7498
Carles Soriano Perez <Carles.SorianoPerez@deltares.nl>
2018-02-07 09:06:17 +0000

    D3DFMIQ-4 Innocuous tests created to filter new categories 'Acceptance' and 'Systems'.
    Former-commit-id: b4cb1e9b9de1b7cf19d30febe4fc90e40ccd3ded
    Former-commit-id: d4c12349b2e6696dc7565d342c474b7717ccd354

---
+++ test/DeltaShell.Dimr.Tests/DimrSystemsTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace DeltaShell.Dimr.Tests
+{
+    [TestFixture]
+    [Category("Systems")]
+    public class DimrSystemsTests
+    {
+        [Test]
+        public void DimrSystemsTestCategoryTest()
+        {
+            Assert.Pass("Systems tests should be under their own category.");
+        }
+    }
+}
