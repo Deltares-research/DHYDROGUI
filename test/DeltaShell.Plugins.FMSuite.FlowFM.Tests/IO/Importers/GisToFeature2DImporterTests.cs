912b25333eacbf531ba854059dfbec754732dcf4
Alex Koster <Alex.Koster@deltares.nl>
2018-06-01 07:50:50 +0000

    FM1D2D-161 Added setup for tests
    Former-commit-id: 55f093b2707d661a0eb590c44e60cefaa2812e00
    Former-commit-id: 319777322dd39a666834745958fcd56c7e0592d5

---
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/Importers/GisToFeature2DImporterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeltaShell.Plugins.FMSuite.FlowFM.IO.Importers;
+using GeoAPI.Geometries;
+using NetTopologySuite.Extensions.Features;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.IO.Importers
+{
+    [TestFixture]
+    public class GisToFeature2DImporterTests
+    {
+        [Test]
+        public void TestPointShapeImport()
+        {
+            var importer = new GisToFeature2DImporter<IPoint,Feature2D>();
+        }
+
+        [Test]
+        public void TestLineStringShapeImport()
+        {
+            var importer = new GisToFeature2DImporter<ILineString, Feature2D>();
+        }
+
+        [Test]
+        public void TestPolygonShapeImport()
+        {
+            var importer = new GisToFeature2DImporter<IPolygon, Feature2D>();
+        }
+    }
+}
