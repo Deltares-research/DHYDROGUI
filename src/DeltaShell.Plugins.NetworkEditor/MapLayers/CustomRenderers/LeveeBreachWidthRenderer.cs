adfb0baf0e7eb389ccea839f6c7fde717c5a4503
brummen <brummen@deltares.nl>
2018-06-05 12:02:03 +0000

    FM1D2D-153 Refactor the renderer
    Former-commit-id: da714aa3c303412c5d8e7548690ce51ed869161e
    Former-commit-id: 520e09be04c000a592946b8d5be09580c07f7b9c

--- src/DeltaShell.Plugins.NetworkEditor/MapLayers/CustomRenderers/LeveeBreachWidthRenderer.cs
+++ src/DeltaShell.Plugins.NetworkEditor/MapLayers/CustomRenderers/LeveeBreachWidthRenderer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Drawing;
 using System.Linq;
 using DelftTools.Hydro.Structures;
@@ -7,11 +6,11 @@ using GeoAPI.Extensions.Feature;
 using GeoAPI.Geometries;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.LinearReferencing;
+using SharpMap.Api;
 using SharpMap.Api.Layers;
 using SharpMap.CoordinateSystems.Transformations;
 using SharpMap.Layers;
 using SharpMap.Rendering;
-using SharpMap.Styles;
 using Point = NetTopologySuite.Geometries.Point;
 
 namespace DeltaShell.Plugins.NetworkEditor.MapLayers.CustomRenderers
@@ -22,15 +21,12 @@ namespace DeltaShell.Plugins.NetworkEditor.MapLayers.CustomRenderers
 
         public override bool Render(IFeature feature, Graphics g, ILayer layer)
         {
-            // Mostly a copy of FeatureCoverageRenderer. Only the rendering itself will be different
+            // Mostly a copy of FeatureCoverageRenderer. 
             var coverageLayer = layer as FeatureCoverageLayer;
             if (coverageLayer == null) return false;
 
             var coverage = coverageLayer.FeatureCoverageToRender;
             var map = coverageLayer.Map;
-
-            // No theme? Can not render?
-            if (coverageLayer.Theme == null) return false;
 
             // Features to render
             IList featuresToRender;
@@ -58,60 +54,74 @@ namespace DeltaShell.Plugins.NetworkEditor.MapLayers.CustomRenderers
                 var featureToRender = coverageFeatures[i];
                 if (!featuresToRender.Contains(featureToRender)) continue;
 
-                var leveeBreach = featureToRender as LeveeBreach;
-                if (leveeBreach == null) continue;
-                var geometry = leveeBreach.Geometry;
-                var leveeBreachPoint = new Coordinate(leveeBreach.BreachLocationX, leveeBreach.BreachLocationY);
-
-                if (GeometryForFeatureDelegate != null)
-                {
-                    geometry = GeometryForFeatureDelegate(featureToRender);
-                }
-                // TODO Sil Cleanup this method
-                //var style = AreaK //coverageLayer.Theme.GetStyle(values[i]) as VectorStyle;
-                //if (style != null)
-                //{
-                //style.SymbolScale = 0.75F;
-                //style.Line.Width = 4.0F;
-
-                var lengthIndexedLine = new LengthIndexedLine(geometry);
-                var offsetToBreach = lengthIndexedLine.Project(leveeBreachPoint);
-                var breachWidth = values[i];
-
-                // Breach end points
-                var leftRunningBreachOffset = offsetToBreach - breachWidth * 0.5;
-                var rightRunningBreachOffset = offsetToBreach + breachWidth * 0.5;
-
-                var leftRunningBreachLocation = ExtractPoint(lengthIndexedLine, leftRunningBreachOffset);
-                var rightRunningBreachLocation = ExtractPoint(lengthIndexedLine, rightRunningBreachOffset);
-
-                // Breach line
-                var leftCoordinate = new Coordinate(leftRunningBreachLocation.Coordinate.X, leftRunningBreachLocation.Coordinate.Y);
-                var rightCoordinate = new Coordinate(rightRunningBreachLocation.Coordinate.X, rightRunningBreachLocation.Coordinate.Y);
-
-                var leftRunningCoordinateIndex = lengthIndexedLine.IndexOf(leftCoordinate);
-                var rightRunningCoordinateIndex = lengthIndexedLine.IndexOf(rightCoordinate);
-
-                var breachLine = lengthIndexedLine.ExtractLine(leftRunningCoordinateIndex, rightRunningCoordinateIndex);
-
-                // Transform
-                if (layer.CoordinateTransformation != null)
-                {
-
-                    leftRunningBreachLocation = GeometryTransform.TransformGeometry(leftRunningBreachLocation, layer.CoordinateTransformation.MathTransform);
-                    rightRunningBreachLocation = GeometryTransform.TransformGeometry(rightRunningBreachLocation, layer.CoordinateTransformation.MathTransform);
-                    breachLine = GeometryTransform.TransformGeometry(breachLine, layer.CoordinateTransformation.MathTransform);
-                }
-
-                // Render
-                var pointStyle = AreaLayerStyles.BreachWidthPointStyle;
-                var lineStyle = AreaLayerStyles.BreachWidthLineStyle;
-                VectorRenderingHelper.RenderGeometry(g, map, breachLine, lineStyle, VectorLayer.DefaultPointSymbol, coverageLayer.ClippingEnabled);
-                VectorRenderingHelper.RenderGeometry(g, map, leftRunningBreachLocation, pointStyle, VectorLayer.DefaultPointSymbol, coverageLayer.ClippingEnabled);
-                VectorRenderingHelper.RenderGeometry(g, map, rightRunningBreachLocation, pointStyle, VectorLayer.DefaultPointSymbol, coverageLayer.ClippingEnabled);
+                DrawFeatureCoverage(g, featureToRender, values[i], map, coverageLayer);
             }
             
             return true;
+        }
+
+        private void DrawFeatureCoverage(Graphics g, IFeature featureToRender, double value, IMap map, FeatureCoverageLayer coverageLayer)
+        {
+            var leveeBreach = featureToRender as LeveeBreach;
+            if (leveeBreach == null) return;
+            var geometry = leveeBreach.Geometry;
+            var leveeBreachPoint = new Coordinate(leveeBreach.BreachLocationX, leveeBreach.BreachLocationY);
+
+            if (GeometryForFeatureDelegate != null)
+            {
+                geometry = GeometryForFeatureDelegate(featureToRender);
+            }
+
+            var lengthIndexedLine = new LengthIndexedLine(geometry);
+            var offsetToBreach = lengthIndexedLine.Project(leveeBreachPoint);
+            var breachWidth = value;
+
+            // Breach end points
+            var leftRunningBreachOffset = offsetToBreach - breachWidth * 0.5;
+            var rightRunningBreachOffset = offsetToBreach + breachWidth * 0.5;
+
+            var leftRunningBreachLocation = ExtractPoint(lengthIndexedLine, leftRunningBreachOffset);
+            var rightRunningBreachLocation = ExtractPoint(lengthIndexedLine, rightRunningBreachOffset);
+
+            // Breach line
+            var breachLine = CreateBreachLine(leftRunningBreachLocation, rightRunningBreachLocation, lengthIndexedLine);
+
+            // Transform
+            if (coverageLayer.CoordinateTransformation != null)
+            {
+                leftRunningBreachLocation = GeometryTransform.TransformGeometry(leftRunningBreachLocation, coverageLayer.CoordinateTransformation.MathTransform);
+                rightRunningBreachLocation = GeometryTransform.TransformGeometry(rightRunningBreachLocation, coverageLayer.CoordinateTransformation.MathTransform);
+                breachLine = GeometryTransform.TransformGeometry(breachLine, coverageLayer.CoordinateTransformation.MathTransform);
+            }
+
+            // Render
+            RenderBreachLine(g, map, breachLine, coverageLayer);
+            RenderBreachEndPoints(g, map, leftRunningBreachLocation, coverageLayer, rightRunningBreachLocation);
+        }
+
+        private static void RenderBreachEndPoints(Graphics g, IMap map, IGeometry leftRunningBreachLocation, FeatureCoverageLayer coverageLayer, IGeometry rightRunningBreachLocation)
+        {
+            var pointStyle = AreaLayerStyles.BreachWidthPointStyle;
+            VectorRenderingHelper.RenderGeometry(g, map, leftRunningBreachLocation, pointStyle, VectorLayer.DefaultPointSymbol, coverageLayer.ClippingEnabled);
+            VectorRenderingHelper.RenderGeometry(g, map, rightRunningBreachLocation, pointStyle, VectorLayer.DefaultPointSymbol, coverageLayer.ClippingEnabled);
+        }
+
+        private static void RenderBreachLine(Graphics g, IMap map, IGeometry breachLine, FeatureCoverageLayer coverageLayer)
+        {
+            var lineStyle = AreaLayerStyles.BreachWidthLineStyle;
+            VectorRenderingHelper.RenderGeometry(g, map, breachLine, lineStyle, VectorLayer.DefaultPointSymbol, coverageLayer.ClippingEnabled);
+        }
+
+        private static IGeometry CreateBreachLine(IGeometry leftRunningBreachLocation, IGeometry rightRunningBreachLocation, LengthIndexedLine lengthIndexedLine)
+        {
+            var leftCoordinate = new Coordinate(leftRunningBreachLocation.Coordinate.X, leftRunningBreachLocation.Coordinate.Y);
+            var rightCoordinate = new Coordinate(rightRunningBreachLocation.Coordinate.X, rightRunningBreachLocation.Coordinate.Y);
+
+            var leftRunningCoordinateIndex = lengthIndexedLine.IndexOf(leftCoordinate);
+            var rightRunningCoordinateIndex = lengthIndexedLine.IndexOf(rightCoordinate);
+
+            var breachLine = lengthIndexedLine.ExtractLine(leftRunningCoordinateIndex, rightRunningCoordinateIndex);
+            return breachLine;
         }
 
         private static IGeometry ExtractPoint(LengthIndexedLine lengthIndexedLine, double offset)
