4cbc3b630ba87c848ac31764837d73cf5c90959d
Pauline Belt <pauline.belt@deltares.nl>
2018-07-03 07:39:48 +0000

    D3DFMIQ-186 Solved ApwxwyFileReader, so that comments are separated from the values. Also added testcase for this situation.
    Former-commit-id: b182e95c5aec4ff154ed0a041685e425f7346269
    Former-commit-id: aff5393da87452b348a4da2a8cf26b983b6c7ba5

--- src/DeltaShell.Plugins.FMSuite.Common/IO/ApwxwyFileReader.cs
+++ src/DeltaShell.Plugins.FMSuite.Common/IO/ApwxwyFileReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DelftTools.Utils.RegularExpressions;
 using DeltaShell.NGHS.IO;
 using log4net;
 
@@ -9,7 +10,8 @@ namespace DeltaShell.Plugins.FMSuite.Common.IO
         private static readonly ILog Log = LogManager.GetLogger(typeof(ApwxwyFileReader));
         private readonly string filePath;
         private const string GridFileIdentifier = "grid_file";
-        
+        private const string KeyValueCommentPattern = @"^\s*(?<key>[^=\s]+)\s*=\s*(?<value>[^#=]*)(#(?<comment>.*))?$";
+
         public ApwxwyFileReader(string filePath)
         {
             this.filePath = filePath;
@@ -24,7 +26,7 @@ namespace DeltaShell.Plugins.FMSuite.Common.IO
 
                 while ((line = GetNextLine()) != null)
                 {
-                    var fields = line.Split('=');
+                    var fields = GetKeyValueComment(line);
                     if (fields[0].Trim() == GridFileIdentifier)
                     {
                         CloseInputFile();
@@ -38,6 +40,21 @@ namespace DeltaShell.Plugins.FMSuite.Common.IO
                 Log.ErrorFormat("File at '{0}' was not found.", filePath);
             }
             return null;
+        }
+
+        private string[] GetKeyValueComment(string line)
+        {
+            var result = new string[3];
+
+            var matches = RegularExpression.GetMatches(KeyValueCommentPattern, line);
+            if (matches.Count == 0) throw new FormatException(String.Format("Invalid key-value-comment line on line {0} in file {1}",
+                LineNumber, InputFilePath));
+
+            result[0] = matches[0].Groups["key"].Value.Trim();
+            result[1] = matches[0].Groups["value"].Value.Trim();
+            result[2] = matches[0].Groups["comment"].Value.Trim(); // Returns "" if comment group not matched
+
+            return result;
         }
     }
 }
