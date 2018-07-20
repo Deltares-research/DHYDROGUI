c77e6ef1d0e2caf63aea8a50c55cef281a8cf531
kahmann <kahmann@deltares.nl>
2018-07-20 14:49:57 +0000

    FM1D2D-57: Whenever the user changes the source level or target level of a pipe, the slope is recalculated and shown in the pipe view. Former-commit-id: e79ee96b8f4bbb789834536804dfd3a310ffcc3c Former-commit-id: 89238234c7da1c38adf25a1441f55574fcd3e88a

--- src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/SewerFeatureViews/PipeViewModel.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/SewerFeatureViews/PipeViewModel.cs
@@ -1,10 +1,12 @@
-﻿using DelftTools.Hydro.Structures;
-using DelftTools.Utils.Aop;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DelftTools.Hydro.Structures;
+using DelftTools.Utils.Reflection;
 
 namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.SewerFeatureViews
 {
-    [Entity]
-    public class PipeViewModel
+    //[Entity]
+    public class PipeViewModel : INotifyPropertyChanged
     {
         private Pipe pipe;
 
@@ -13,14 +15,41 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.SewerFeatureViews
             get { return pipe; }
             set
             {
+                if (pipe != null)
+                {
+                    ((INotifyPropertyChanged)pipe).PropertyChanged -= OnPropertyChanged;
+                }
                 pipe = value;
+                
+                if (pipe != null)
+                {
+                    ((INotifyPropertyChanged)pipe).PropertyChanged += OnPropertyChanged;
+                }
 
-                if (pipe == null) return;
-
-                PipeSlope = pipe.Slope();
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PipeSlope));
             }
         }
 
-        public double PipeSlope { get; set; }
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == TypeUtils.GetMemberName<Pipe>(p => p.LevelTarget) ||
+                e.PropertyName == TypeUtils.GetMemberName<Pipe>(p => p.LevelSource))
+            {
+                OnPropertyChanged(nameof(PipeSlope));
+            }
+        }
+
+        public double PipeSlope
+        {
+            get { return pipe?.Slope() ?? 0; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
