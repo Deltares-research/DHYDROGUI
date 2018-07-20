e09fab994d1bc7555d993b9d10d9fc19d6989c22
kahmann <kahmann@deltares.nl>
2018-07-20 14:55:51 +0000

    FM1D2D-18: Forgot to add SplitPipeCommand in a former commit. It is now added. Former-commit-id: 1cebdd2f9812c2bce6b5d51277c217e11c14c5af Former-commit-id: 77eb7d40a4a6f0e0bf075a9c27c4836a5581ba11

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Commands/SplitPipeCommand.cs
@@ -0,0 +1,27 @@
+﻿using DeltaShell.Plugins.NetworkEditor.Gui.MapTools;
+using log4net;
+using SharpMap.UI.Tools;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Commands
+{
+    public class SplitPipeCommand : NetworkEditorCommand
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(SplitPipeCommand));
+
+        protected IMapTool CurrentTool
+        {
+            get { return MapView.MapControl.GetToolByName(HydroRegionEditorMapTool.InsertManholeToolName); }
+        }
+
+        protected override void OnExecute(params object[] arguments)
+        {
+            var newLineTool = (NewPointFeatureTool)CurrentTool;
+            MapView.MapControl.ActivateTool(newLineTool);
+        }
+
+        public override bool Checked
+        {
+            get { return (null != MapView) && (null != CurrentTool) && CurrentTool.IsActive; }
+        }
+    }
+}
