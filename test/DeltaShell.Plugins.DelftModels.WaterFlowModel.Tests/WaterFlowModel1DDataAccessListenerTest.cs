8ab289cf82756939197bd24a32cee273724879d5
kahmann <kahmann@deltares.nl>
2018-09-17 19:58:40 +0000

    FM1D2D-174: Due to the move of all Roughness classes to the DelftTools.Hydro/Roughness/ folder, we were not backwards compatible for loading Roughness from older projects. Added the "oldClassName" property to make sure this is going correctly. Also, cleaned up WaterFlowModel1DDataAccessListenerTest a little. Former-commit-id: ce09c8b846fd5451b0f4e1f5f9b1a994482ccf8d Former-commit-id: 3c05974c562d6ee212aeb5ed679104997d04ac16

--- test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/WaterFlowModel1DDataAccessListenerTest.cs
+++ test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/WaterFlowModel1DDataAccessListenerTest.cs
@@ -7,6 +7,7 @@ using DelftTools.Shell.Core.Workflow.DataItems;
 using DelftTools.TestUtils;
 using DelftTools.Units;
 using DelftTools.Utils.Collections.Generic;
+using DelftTools.Utils.IO;
 using DelftTools.Utils.Reflection;
 using DeltaShell.Core;
 using DeltaShell.Plugins.CommonTools;
@@ -24,36 +25,44 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests
     public class WaterFlowModel1DDataAccessListenerTest
     {
         [Test]
-        public void TestOnPostLoad_CrossSectionDefinitionsWithNoSectionsFix() // SOBEK3-1392
+        public void TestOnPostLoad_CrossSectionDefinitionsWithNoSectionsFix()
         {
             var testProjectPath = TestHelper.GetTestFilePath(@"DataAccess\MissingCrossSectionDefinitionSections.dsproj");
-            Assert.IsTrue(File.Exists(testProjectPath));
-            
-            using (var app = new DeltaShellApplication())
+            var testFilePath = TestHelper.CreateLocalCopy(testProjectPath);
+            Assert.IsTrue(File.Exists(testFilePath));
+
+            try
             {
-                app.Plugins.Add(new NHibernateDaoApplicationPlugin());
-                app.Plugins.Add(new CommonToolsApplicationPlugin());
-                app.Plugins.Add(new SharpMapGisApplicationPlugin());
-                app.Plugins.Add(new NetworkEditorApplicationPlugin());
-                app.Plugins.Add(new WaterFlowModel1DApplicationPlugin());
-                app.Plugins.Add(new NetCdfApplicationPlugin());
+                using (var app = new DeltaShellApplication())
+                {
+                    app.Plugins.Add(new NHibernateDaoApplicationPlugin());
+                    app.Plugins.Add(new CommonToolsApplicationPlugin());
+                    app.Plugins.Add(new SharpMapGisApplicationPlugin());
+                    app.Plugins.Add(new NetworkEditorApplicationPlugin());
+                    app.Plugins.Add(new WaterFlowModel1DApplicationPlugin());
+                    app.Plugins.Add(new NetCdfApplicationPlugin());
 
-                app.Run();
+                    app.Run();
 
-                app.OpenProject(testProjectPath);
-                Assert.NotNull(app.Project);
+                    app.OpenProject(testFilePath);
+                    Assert.NotNull(app.Project);
 
-                var flow1DModel = app.Project.RootFolder.Models.OfType<WaterFlowModel1D>().FirstOrDefault();
-                Assert.NotNull(flow1DModel);
+                    var flow1DModel = app.Project.RootFolder.Models.OfType<WaterFlowModel1D>().FirstOrDefault();
+                    Assert.NotNull(flow1DModel);
 
-                var network = flow1DModel.Network;
-                Assert.NotNull(network);
+                    var network = flow1DModel.Network;
+                    Assert.NotNull(network);
 
-                var crossSectionDefinitionsWithNoSections = network.CrossSections.Select(cs => cs.Definition)
-                    .Union(network.SharedCrossSectionDefinitions)
-                    .Where(csd => !csd.Sections.Any());
+                    var crossSectionDefinitionsWithNoSections = network.CrossSections.Select(cs => cs.Definition)
+                        .Union(network.SharedCrossSectionDefinitions)
+                        .Where(csd => !csd.Sections.Any());
 
-                Assert.IsFalse(crossSectionDefinitionsWithNoSections.Any());
+                    Assert.IsFalse(crossSectionDefinitionsWithNoSections.Any());
+                }
+            }
+            finally
+            {
+                FileUtils.DeleteIfExists(testFilePath);
             }
         }
 
@@ -61,46 +70,54 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests
         public void TestOnPostLoad_SyncAggregationOptionsForExistingOutputCoverages_ResultsPumps()
         {
             var testProjectPath = TestHelper.GetTestFilePath(@"DataAccess\OutOfSyncAggregationOptions.dsproj");
-            Assert.IsTrue(File.Exists(testProjectPath));
+            var testFilePath = TestHelper.CreateLocalCopy(testProjectPath);
+            Assert.IsTrue(File.Exists(testFilePath));
 
-            using (var app = new DeltaShellApplication())
+            try
             {
-                app.Plugins.Add(new NHibernateDaoApplicationPlugin());
-                app.Plugins.Add(new CommonToolsApplicationPlugin());
-                app.Plugins.Add(new SharpMapGisApplicationPlugin());
-                app.Plugins.Add(new NetworkEditorApplicationPlugin());
-                app.Plugins.Add(new WaterFlowModel1DApplicationPlugin());
-                app.Plugins.Add(new NetCdfApplicationPlugin());
-
-                app.Run();
-
-                app.OpenProject(testProjectPath);
-                Assert.NotNull(app.Project);
-
-                var flow1DModel = app.Project.RootFolder.Models.OfType<WaterFlowModel1D>().FirstOrDefault();
-                Assert.NotNull(flow1DModel);
-                
-                var existingOutputCoverageDataItems = flow1DModel.DataItems
-                .Where(di => (di.Role & DataItemRole.Output) == DataItemRole.Output && di.Value is IFunction);
-
-                foreach (var dataItem in existingOutputCoverageDataItems)
+                using (var app = new DeltaShellApplication())
                 {
-                    var matchingEngineParameter = flow1DModel.OutputSettings.EngineParameters.FirstOrDefault(ep => ep.Name == dataItem.Tag);
-                    Assert.NotNull(matchingEngineParameter);
-                    Assert.AreEqual(matchingEngineParameter.AggregationOptions, AggregationOptions.Current);
-                } 
+                    app.Plugins.Add(new NHibernateDaoApplicationPlugin());
+                    app.Plugins.Add(new CommonToolsApplicationPlugin());
+                    app.Plugins.Add(new SharpMapGisApplicationPlugin());
+                    app.Plugins.Add(new NetworkEditorApplicationPlugin());
+                    app.Plugins.Add(new WaterFlowModel1DApplicationPlugin());
+                    app.Plugins.Add(new NetCdfApplicationPlugin());
+
+                    app.Run();
+
+                    app.OpenProject(testFilePath);
+                    Assert.NotNull(app.Project);
+
+                    var flow1DModel = app.Project.RootFolder.Models.OfType<WaterFlowModel1D>().FirstOrDefault();
+                    Assert.NotNull(flow1DModel);
+
+                    var existingOutputCoverageDataItems = flow1DModel.DataItems
+                        .Where(di => (di.Role & DataItemRole.Output) == DataItemRole.Output && di.Value is IFunction);
+
+                    foreach (var dataItem in existingOutputCoverageDataItems)
+                    {
+                        var matchingEngineParameter = flow1DModel.OutputSettings.EngineParameters.FirstOrDefault(ep => ep.Name == dataItem.Tag);
+                        Assert.NotNull(matchingEngineParameter);
+                        Assert.AreEqual(matchingEngineParameter.AggregationOptions, AggregationOptions.Current);
+                    }
+                }
+            }
+            finally
+            {
+                FileUtils.DeleteIfExists(testFilePath);
             }
         }
 
         [Test]
         public void TestSyncAggregationOptionsForExistingOutputCoverages_HandlesNoMatchingEngineParameter()
         {
-            var dataItems = new List<DataItem>()
+            var dataItems = new List<DataItem>
             {
                 new DataItem(new FeatureCoverage(), DataItemRole.Output, "ExampleTag"), // simulate existing output coverage without matching engine parameter
             }; 
 
-            var engineParameters = new List<EngineParameter>(); // empty list
+            var engineParameters = new List<EngineParameter>();
 
             Assert.DoesNotThrow(() => 
                 TypeUtils.CallPrivateStaticMethod(typeof(WaterFlowModel1DDataAccessListener),
@@ -110,12 +127,12 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests
         [Test]
         public void TestSyncAggregationOptionsForExistingOutputCoverages_HandlesNoComponentsInOutputCoverage()
         {
-            var dataItems = new List<DataItem>()
+            var dataItems = new List<DataItem>
             {
                 new DataItem(new FeatureCoverage(), DataItemRole.Output, QuantityType.PumpDischarge.ToString()) // simulate existing output coverage
             };
 
-            var engineParameters = new List<EngineParameter>()
+            var engineParameters = new List<EngineParameter>
             {
                 new EngineParameter(QuantityType.PumpDischarge, ElementSet.Pumps, DataItemRole.Output, QuantityType.PumpDischarge.ToString(), new Unit()) // simulate existing engine parameter
             };
@@ -128,17 +145,17 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests
         [Test]
         public void TestSyncAggregationOptionsForExistingOutputCoverages_HandlesNoAggregationOptionAttribute()
         {
-            var dataItems = new List<DataItem>()
+            var dataItems = new List<DataItem>
             {
-                new DataItem(new FeatureCoverage()
-                {
-                    Components = new EventedList<IVariable>() { new Variable<double>()} // simulate existing component without Attributes
+                new DataItem(new FeatureCoverage
+                    {
+                    Components = new EventedList<IVariable> { new Variable<double>()} // simulate existing component without Attributes
                 }, 
                 DataItemRole.Output, 
                 "PumpDischarge") // simulate existing output coverage
             };
 
-            var engineParameters = new List<EngineParameter>()
+            var engineParameters = new List<EngineParameter>
             {
                 new EngineParameter(QuantityType.PumpDischarge, ElementSet.Pumps, DataItemRole.Output, QuantityType.PumpDischarge.ToString(), new Unit()) // simulate existing engine parameter
             };
