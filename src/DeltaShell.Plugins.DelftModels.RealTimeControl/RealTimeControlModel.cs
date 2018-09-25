7cc4b1ef043f1931fe350787b7f06024aba7690b
Pauline Belt <pauline.belt@deltares.nl>
2018-09-25 11:02:40 +0000

    D3DFMIQ-483 DIMR update and revert commit #40757
    Former-commit-id: 1794fbb48ca0373689caf81aea5b0f7c433ca75d
    Former-commit-id: 2d5dd6ab5a55f6ea57c2ee5a1347ae36afe5f4d8

--- src/DeltaShell.Plugins.DelftModels.RealTimeControl/RealTimeControlModel.cs
+++ src/DeltaShell.Plugins.DelftModels.RealTimeControl/RealTimeControlModel.cs
@@ -1034,11 +1034,7 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl
 
         IModelState IStateAwareModelEngine.GetCopyOfCurrentState()
         {
-            var currentState = ModelStateHandler.GetState();
-            var filename = "rtc_" + CurrentTime.ToString("yyyyMMdd") + "_" + CurrentTime.ToString("HHmmss")+".xml";
-            if(File.Exists(Path.Combine(ExplicitWorkingDirectory, filename)))
-                ((ModelStateFilesImpl)currentState).AddFile(filename,RealTimeControlXMLFiles.XmlImportState);
-            return currentState;
+            return ModelStateHandler.GetState();
         }
 
         void IStateAwareModelEngine.SetState(IModelState modelState)
@@ -1150,6 +1146,7 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl
                 if (modelStateHandler == null)
                 {
                     IList<DelftTools.Utils.Tuple<string, string>> outAndInFileNames = new List<DelftTools.Utils.Tuple<string, string>>();
+                    outAndInFileNames.Add(new DelftTools.Utils.Tuple<string, string>(RealTimeControlXMLFiles.XmlExportState, RealTimeControlXMLFiles.XmlImportState));
                     modelStateHandler = new ModelFileBasedStateHandler(Name, outAndInFileNames);
                 }
                 return modelStateHandler;
