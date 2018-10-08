fcb237cdbd8a62d7d293d5ad9ecde841df09702d
Alex Koster <Alex.Koster@deltares.nl>
2018-10-08 14:58:00 +0000

    FM1D2D-189 Added test
    Former-commit-id: 1a70c46506bdb82933a789a5262b1bf5b5358e20
    Former-commit-id: 144ec02dc52186d39a7edc6955a99450b963d228

--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/WaterFlowFMModelWriterReaderTest.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/WaterFlowFMModelWriterReaderTest.cs
@@ -2,15 +2,19 @@
 using System.Linq;
 using DelftTools.Functions;
 using DelftTools.Functions.Generic;
+using DelftTools.Hydro;
 using DelftTools.Hydro.Roughness;
 using DelftTools.Hydro.SewerFeatures;
 using DelftTools.Hydro.Structures;
+using DelftTools.Hydro.Structures.WeirFormula;
 using DelftTools.Hydro.Tests.Helpers;
 using DelftTools.Utils.IO;
 using DeltaShell.Plugins.FMSuite.FlowFM.IO;
 using DeltaShell.Plugins.FMSuite.FlowFM.ModelDefinition;
 using DeltaShell.Plugins.NetworkEditor.Tests.Helpers;
+using GeoAPI.Extensions.Networks;
 using GeoAPI.Geometries;
+using NetTopologySuite.Extensions.Networks;
 using NetTopologySuite.Geometries;
 using NUnit.Framework;
 
@@ -100,6 +104,68 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.IO
             WaterFlowFMModelWriter.Write(fmModel);
             var directory = Path.GetDirectoryName(fmModel.MduFilePath);
             Assert.IsTrue(File.Exists(Path.Combine(directory, "roughness-" + sewerRoughness.Name+".ini")));
+        }
+
+        [Test, Ignore("Issue FM1D2D-189")]
+        public void GivenFmModel_With1DAnd2DStructures_CheckStructureIniFile()
+        {
+            var mduFilePath = Path.Combine(tempDirectory, "myFmModel.mdu");
+
+            var network = new HydroNetwork();
+            var fmModel = new WaterFlowFMModel(mduFilePath)
+            {
+                Network = network
+            };
+
+            //1d pump branch
+            var fromNode = new HydroNode("from") {Geometry = new Point(0, 0)};
+            var toNode = new HydroNode("to") { Geometry = new Point(100, 0) };
+            network.Nodes.Add(fromNode);
+            network.Nodes.Add(toNode);
+            var channel = new Channel("channel",fromNode, toNode);
+            network.Branches.Add(channel);
+            var pump1 = new Pump("pump1");
+            NetworkHelper.AddBranchFeatureToBranch(pump1, channel, 50.0);
+
+            Assert.IsTrue(network.Pumps.Any(p => p.Name.Equals("pump1")));
+
+            //1d pump manhole
+            var manhole = SewerFactory.CreateDefaultManholeAndAddToNetwork(network, new Coordinate(0, 10));
+            //var pump2 = SewerFactory.CreateConnectionWithStructure<Pump>(manhole); why can't i use it? why two IManholes? mmm
+            var fromCompartment = new Compartment("cmp1");
+            var toCompartment = new Compartment("cmp2");
+            manhole.Compartments.Add(fromCompartment);
+            manhole.Compartments.Add(toCompartment);
+            network.Nodes.Add(manhole);
+
+            var pump2 = new Pump("pump2");
+
+            var pumpConnection = new SewerConnection("pumpConnection")
+            {
+                SourceCompartment = fromCompartment,
+                TargetCompartment = toCompartment,
+                LevelSource = 0.0,
+                LevelTarget = 0.0,
+                WaterType = SewerConnectionWaterType.None,
+            };
+            pumpConnection.AddStructureToBranch(pump2);
+            network.Branches.Add(pumpConnection);
+            //Assert.IsTrue(network.Pumps.Any(p => p.Name.Equals("pump2")));
+
+            //2d pump 
+            fmModel.Area.Pumps.Add(new Pump2D("pump3") {Geometry = new LineString(new [] {new Coordinate(0, 20), new Coordinate(100, 20)})});
+            
+            WaterFlowFMModelWriter.Write(fmModel);
+
+            var fileProperty = fmModel.ModelDefinition.GetModelProperty(KnownProperties.StructuresFile);
+            var structureFile = Path.Combine(tempDirectory, fileProperty.GetValueAsString());
+
+            Assert.IsTrue(File.Exists(structureFile));
+            var contentFile = File.ReadAllText(structureFile);
+
+            Assert.IsTrue(contentFile.Contains("pump1"));
+            Assert.IsTrue(contentFile.Contains("pump2"));
+            Assert.IsTrue(contentFile.Contains("pump3"));
         }
     }
 }
