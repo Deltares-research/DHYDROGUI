3c55174b591f961aa31340c556ec91fa38ba6d13
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2018-11-14 14:52:33 +0000

    SOBEK3-1532 Fixed failing unit test.
    Former-commit-id: 6b71c31edfac74331039d706f3a6e74a793100b4
    Former-commit-id: de8c38b1a6a669619e43b19adda7082d52233852

--- test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/ImportExport/WaterFlowModel1DFileReaderTest.cs
+++ test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/ImportExport/WaterFlowModel1DFileReaderTest.cs
@@ -1,17 +1,36 @@
-﻿using DelftTools.TestUtils;
+﻿using System.IO;
+using System.Reflection;
+using DelftTools.TestUtils;
+using DelftTools.Utils.IO;
 using DeltaShell.Plugins.DelftModels.WaterFlowModel.ImportExport;
 using NUnit.Framework;
 
 namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
 {
     [TestFixture]
+    [Category(TestCategory.DataAccess)]
     public class WaterFlowModel1DFileReaderTest
     {
+        private string tempFolderPath;
+        
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+           var testFolder = TestHelper.GetTestDataPath(Assembly.GetExecutingAssembly(), @"Md1dReading");
+           tempFolderPath = TestHelper.CreateLocalCopy(testFolder);
+
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            FileUtils.DeleteIfExists(tempFolderPath);
+        }
         
         [Test]
-        public void GivenAMd1dFile_WhenReadingTheAttachedNetworkDefinitionFile_ThenAModelIsReturned()
+       public void GivenAMd1dFile_WhenReadingTheAttachedNetworkDefinitionFile_ThenAModelIsReturned()
         {
-            var md1dFilePath = TestHelper.GetTestFilePath(@"Md1dReading\Md1dExport.md1d");
+            var md1dFilePath = Path.Combine(tempFolderPath, "Md1dExport.md1d");
 
             var waterFlowModel1D = WaterFlowModel1DFileReader.Read(md1dFilePath);
             Assert.IsNotNull(waterFlowModel1D);
@@ -20,7 +39,7 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
         [Test]
         public void GivenAMd1dFile_WhenReadingTheNetworkDefinitionFileWithABadNode_ThenNullIsReturned()
         {
-            var md1dFilePath = TestHelper.GetTestFilePath(@"Md1dReading\Md1dExportBadNode.md1d");
+            var md1dFilePath = Path.Combine(tempFolderPath, "Md1dExportBadNode.md1d");
 
             var waterFlowModel1D = WaterFlowModel1DFileReader.Read(md1dFilePath);
             Assert.IsNull(waterFlowModel1D);
@@ -29,7 +48,7 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
         [Test]
         public void GivenAMd1dFile_WhenReadingTheNetworkDefinitionFileWithABadBranch_ThenNullIsReturned()
         {
-            var md1dFilePath = TestHelper.GetTestFilePath(@"Md1dReading\Md1dExportBadBranch.md1d");
+            var md1dFilePath = Path.Combine(tempFolderPath, "Md1dExportBadBranch.md1d");
 
             var waterFlowModel1D = WaterFlowModel1DFileReader.Read(md1dFilePath);
             Assert.IsNull(waterFlowModel1D);
@@ -38,7 +57,7 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
         [Test]
         public void GivenAMd1dFile_WhenReadingTheNetworkDefinitionFileWithABadNetworkDiscretization_ThenNullIsReturned()
         {
-            var md1dFilePath = TestHelper.GetTestFilePath(@"Md1dReading\Md1dExportBadNetworkDiscretization.md1d");
+            var md1dFilePath = Path.Combine(tempFolderPath, "Md1dExportBadNetworkDiscretization.md1d");
 
             var waterFlowModel1D = WaterFlowModel1DFileReader.Read(md1dFilePath);
             Assert.IsNull(waterFlowModel1D);
