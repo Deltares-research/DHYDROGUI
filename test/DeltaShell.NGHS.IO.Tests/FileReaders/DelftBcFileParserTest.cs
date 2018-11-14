49212e274dec988f2cf52aab2742fef8192f1caa
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2018-11-14 10:58:21 +0000

    SOBEK3-1535 - Added DelftBcFileParser - Added corresponding test - Fixed GivenAnEmptyFile_WhenParsingFile_ThenAFileReadingExceptionIsThrown
    Former-commit-id: 35f709e2f43ffe8edd292f763e277e2d68fe0143
    Former-commit-id: b86941f71022b7fb3d699a8a4a8f900ea9252809

--- test/DeltaShell.NGHS.IO.Tests/FileReaders/DelftBcFileParserTest.cs
+++ test/DeltaShell.NGHS.IO.Tests/FileReaders/DelftBcFileParserTest.cs
@@ -6,26 +6,26 @@ using NUnit.Framework;
 namespace DeltaShell.NGHS.IO.Tests.FileReaders
 {
     [TestFixture]
-    public class DelftIniFileParserTest
+    public class DelftBcFileParserTest
     {
         [Test]
         [ExpectedException(typeof(FileNotFoundException))]
-        public void GivenNoFile_WhenTryingToExecuteReadFile_ThenAFileNotFoundExceptionIsThrown()
+        public void GivenFilePathToANonExistentFile_WhenExecutingDelftBcFileReaderReadFile_ThenAFileNotFoundExceptionIsThrown()
         {
             const string nonExistingFilePath = @"This/File/Does/Not/Exist";
-            DelftIniFileParser.ReadFile(nonExistingFilePath);
+            DelftBcFileParser.ReadFile(nonExistingFilePath);
         }
 
         [Test]
         [Category(TestCategory.DataAccess)]
         [ExpectedException(typeof(FileReadingException))]
-        public void GivenAnEmptyFile_WhenParsingFile_ThenAFileReadingExceptionIsThrown()
+        public void GivenAnEmptyFile_WhenExecutingDelftBcFileReaderReadFile_ThenAFileReadingExceptionIsThrown()
         {
             TestHelper.PerformActionInTemporaryDirectory(tempDir =>
             {
                 var filePath = Path.Combine(tempDir, "anEmpty.File");
                 using (File.Create(filePath)) { }
-                DelftIniFileParser.ReadFile(filePath);
+                DelftBcFileParser.ReadFile(filePath);
             });
         }
     }
