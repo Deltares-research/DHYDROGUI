212038af86bf4c2bc30c0e95878218df14e6ddec
freijtas <freijtas@deltares.nl>
2018-11-26 08:30:23 +0000

    SOBEK3-1510 ValidatorTests
    Former-commit-id: 74da98fc3c86ec3948590082c29fb320b7aa5259
    Former-commit-id: 324c613692bd5fc0de1358fb7276fe69fadc6810

---
+++ test/DeltaShell.NGHS.IO.Tests/DelftXsdValidatorTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using DeltaShell.Dimr.xsd;
+using NUnit.Framework;
+
+namespace DeltaShell.NGHS.IO.Tests
+{
+    [TestFixture]
+    public class DelftXsdValidatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Serializer cannot be null")]
+        public void ValidateWithNoSerializer()
+        {
+            var errors = new List<string>();
+            DelftXsdValidator.ValidateDataObjectModel(null, errors);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Serializer cannot be null")]
+        public void ValidateWithoutNoList()
+        {
+            var serializer = new XmlSerializer(typeof(dimrXML));
+            DelftXsdValidator.ValidateDataObjectModel(serializer, null);
+        }
+
+
+        [Test]
+        public void ValidateWithDimrSerializer()
+        {
+            var serializer = new XmlSerializer(typeof(dimrXML));
+            var errors = new List<string>();
+            var result = DelftXsdValidator.ValidateDataObjectModel(serializer, errors);
+            serializer.UnknownAttribute += delegate(object sender, XmlAttributeEventArgs e)
+            {
+                errors.Add(e.Attr.Name);
+            };
+
+        }
+    }
+}
