3cedbacbb95600d82a30935d8fdfd3d0efda8440
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2018-11-29 06:41:37 +0000

    SOBEK3-1552 - Add SetOutputProperties to WaterFlowModelPropertySetter - Rename SetProperties to SetTimeProperties - Add documentation to SetTimeProperties
    Former-commit-id: 422b50717435ccff69195a75d68d2098c49a4651
    Former-commit-id: 1a64efd704b3ac9ee4d17a7520573fa5e51a8c4d

--- src/DeltaShell.Plugins.DelftModels.WaterFlowModel/ImportExport/ModelDefinition/WaterFlowModelPropertySetter.cs
+++ src/DeltaShell.Plugins.DelftModels.WaterFlowModel/ImportExport/ModelDefinition/WaterFlowModelPropertySetter.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using DelftTools.Shell.Core.Workflow.DataItems;
 using DeltaShell.NGHS.IO.Helpers;
+using DeltaShell.Plugins.DelftModels.WaterFlowModel.ModelApiControllers.ModelApi;
+
 
 namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.ImportExport.ModelDefinition
 {
+    /// <summary>
+    /// WaterFlowModelPropertySetter provides the methods to set different
+    /// model wide aspects based upon a data access model of the md1d file.
+    /// </summary>
     public static class WaterFlowModelPropertySetter
     {
-        public static void SetProperties(IEnumerable<DelftIniCategory> modelSettingsCategories, WaterFlowModel1D model)
+        /// <summary>
+        /// Set the model time properties as specified in the ModelDefinitionsRegion.TimeHeader
+        /// of the <paramref name="modelSettingsCategories"/>
+        /// </summary>
+        /// <param name="modelSettingsCategories"> A set of DelftIniCategories describing a md1d file. </param>
+        /// <param name="model"> The model whose time variables should be changed. </param>
+        /// <remarks>
+        /// Pre-condition: modelSettingsCategories != null && model != null
+        /// Pre-condition: ModelDefinitionsRegion.TimeHeader In modelSettingsCategories
+        /// </remarks>
+        public static void SetTimeProperties(IEnumerable<DelftIniCategory> modelSettingsCategories, WaterFlowModel1D model)
         {
             var timeCategory = modelSettingsCategories.FirstOrDefault(c => c.Name == ModelDefinitionsRegion.TimeHeader);
             var startTime = timeCategory.ReadProperty<DateTime>(ModelDefinitionsRegion.StartTime.Key);
@@ -23,6 +40,84 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.ImportExport.ModelDefini
             var modelOutputSettings = model.OutputSettings;
             modelOutputSettings.GridOutputTimeStep = TimeSpan.FromSeconds(gridPointsOutputTimeStep);
             modelOutputSettings.StructureOutputTimeStep = TimeSpan.FromSeconds(structuresOutputTimeStep);
+        }
+
+        /// <summary>
+        /// Set the WaterFlowModel1DOutputSettingData engine parameters to the
+        /// values specified in <paramref name="modelSettingsCategories"/>.
+        /// </summary>
+        /// <param name="modelSettingsCategories">A set of DelftIniCategories describing a md1d file. </param>
+        /// <param name="outputSettings"> reference to WaterFlowModel1DOutputSettingData of some WaterFlow1DModel.</param>
+        /// <remarks>
+        /// Pre-condition: modelSettingsCategories != null && outputSettings != null
+        /// </remarks>
+        public static void SetOutputProperties(IEnumerable<DelftIniCategory> modelSettingsCategories,
+                                                                             WaterFlowModel1DOutputSettingData outputSettings)
+        {
+            foreach (var cat in modelSettingsCategories)
+            {
+                // Determine element set
+                ElementSet elementSet;
+                switch (cat.Name)
+                {
+                    case ModelDefinitionsRegion.ResultsNodesHeader:
+                        elementSet = ElementSet.GridpointsOnBranches;
+                        break;
+                    case ModelDefinitionsRegion.ResultsBranchesHeader:
+                        elementSet = ElementSet.ReachSegElmSet;
+                        break;
+                    case ModelDefinitionsRegion.ResultsStructuresHeader:
+                        elementSet = ElementSet.Structures;
+                        break;
+                    case ModelDefinitionsRegion.ResultsPumpsHeader:
+                        elementSet = ElementSet.Pumps;
+                        break;
+                    case ModelDefinitionsRegion.ResultsWaterBalanceHeader:
+                        elementSet = ElementSet.ModelWide;
+                        break;
+                    case ModelDefinitionsRegion.ResultsObservationsPointsHeader:
+                        elementSet = ElementSet.Observations;
+                        break;
+                    case ModelDefinitionsRegion.ResultsLateralsHeader:
+                        elementSet = ElementSet.Laterals;
+                        break;
+                    case ModelDefinitionsRegion.ResultsRetentionsHeader:
+                        elementSet = ElementSet.Retentions;
+                        break;
+                    default:
+                        continue;
+                }
+
+                foreach (var prop in cat.Properties)
+                {
+                    // Determine quantity type
+                    QuantityType qType;
+                    try
+                    {
+                        qType = (QuantityType) Enum.Parse(typeof(QuantityType), prop.Name);
+                    }
+                    catch (ArgumentException _)
+                    {
+                        continue;
+                    }
+
+                    var engineParameter = outputSettings.GetEngineParameter(qType, elementSet, DataItemRole.Output);
+
+                    if (engineParameter == null) continue;
+
+                    // determine AggregateOption
+                    AggregationOptions aggregateOption;
+                    try
+                    {
+                        aggregateOption = (AggregationOptions) Enum.Parse(typeof(AggregationOptions), prop.Value);
+                    }
+                    catch (ArgumentException _)
+                    {
+                        continue;
+                    }
+                    engineParameter.AggregationOptions = aggregateOption;
+                }
+            }
         }
     }
 }
