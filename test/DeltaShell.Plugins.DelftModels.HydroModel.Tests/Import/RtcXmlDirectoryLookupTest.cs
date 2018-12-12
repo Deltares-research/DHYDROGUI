1f76518ff332719157ab64f466b7b27fee40c8d2
freijtas <freijtas@deltares.nl>
2018-12-12 14:15:49 +0000

    SOBEK3-1510 Extra test for Json serializer
    Former-commit-id: 25d964826bc03537d5083c9a2b7a5d4da10049de
    Former-commit-id: 74f84b3ad791d9440f5e54067447f9504fd3fcf8

---
+++ test/DeltaShell.Plugins.DelftModels.HydroModel.Tests/Import/RtcXmlDirectoryLookupTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using DelftTools.TestUtils;
+using DeltaShell.Plugins.DelftModels.HydroModel.Import;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.DelftModels.HydroModel.Tests.Import
+{
+    [TestFixture]
+    public class RtcXmlDirectoryLookupTest
+    {
+        [Test]
+        [Category(TestCategory.DataAccess)]
+        public void ConvertJsonFileToObject()
+        {
+            var pathToFile = Path.Combine(TestHelper.GetDataDir(), "JsonConvert", "settings.json");
+            var file = File.ReadAllText(pathToFile);
+            var fileObject = JsonConvert.DeserializeObject<RtcXmlDirectoryLookup>(file);
+
+            Assert.That(fileObject, Is.TypeOf<RtcXmlDirectoryLookup>());
+            Assert.That(fileObject.XmlDirectory, Is.EqualTo("pathToXml"));
+            Assert.That(fileObject.SchemaDirectory, Is.EqualTo("pathToSchema"));
+        }
+    }
+}
