c7d1fba0b80c1959cc43cbf647b6e442a3ae4124
kahmann <kahmann@deltares.nl>
2018-12-14 14:10:24 +0000

    SOBEK3-1589: Refactoring of tests in RealTimeControlDataConfigXmlReaderTest. Former-commit-id: 8ceaac01bac14bdfcd755c503ef14585d0085875 Former-commit-id: 20d408209ed187d804d512a9e3f45efd05ebcc5b

--- test/DeltaShell.Plugins.DelftModels.RealTimeControl.Tests/ImportExport/RealTimeControlDataConfigXmlReaderTest.cs
+++ test/DeltaShell.Plugins.DelftModels.RealTimeControl.Tests/ImportExport/RealTimeControlDataConfigXmlReaderTest.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using DelftTools.Functions.Generic;
+using DelftTools.Utils.Collections.Generic;
 
 namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
 {
@@ -22,35 +23,36 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
 
             var controlGroups = new List<ControlGroup>();
 
+            IList<ConnectionPoint> connectionPoints = new List<ConnectionPoint>();
             Assert.DoesNotThrow(() =>
             {
                 TestHelper.AssertLogMessageIsGenerated(() =>
                     {
                         // When
-                        var connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, controlGroups);
-                        Assert.IsNull(connectionPoints);
+                        connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, controlGroups);
                     },
                     string.Format(Resources.RealTimeControlDataConfigXmlReader_Read_File___0___does_not_exist_, filePath));
             });
+
+            // Then
+            Assert.IsNull(connectionPoints);
         }
 
         [Test]
         [Category(TestCategory.DataAccess)]
-        public void GivenAnExistingFileAndNullIsGivenAsParameterForControlgroups_WhenReading_ThenMethodIsReturnedAndNothingHappens()
+        public void GivenAnExistingFileAndNullIsGivenAsParameterForControlgroups_WhenReading_ThenNullIsReturnedAndNoExceptionIsThrown()
         {
             // Given
-            var fileName = "rtcDataConfig.xml";
+            const string fileName = "rtcDataConfig.xml";
             var directoryPath = TestHelper.GetTestFilePath(Path.Combine("ImportExport", "DataConfigFiles"));
             var filePath = Path.Combine(directoryPath, fileName);
-
-            Assert.That(Directory.Exists(directoryPath));
+            
             Assert.That(File.Exists(filePath));
 
-            Assert.DoesNotThrow(() =>
-            {
-                var connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, null);
-                Assert.IsNull(connectionPoints);
-            });
+            // When/Then
+            IList<ConnectionPoint> connectionPoints = new List<ConnectionPoint>();
+            Assert.DoesNotThrow(() => connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, null));
+            Assert.IsNull(connectionPoints);
         }
 
         [Test]
@@ -58,11 +60,10 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
         public void GivenAnExistingFileWithValidData_WhenReading_ThenCorrectConnectionPointsAreReturnedAndObjectsAreSetInControlgroups()
         {
             // Given
-            var fileName = "rtcDataConfig.xml";
+            const string fileName = "rtcDataConfig.xml";
             var directoryPath = TestHelper.GetTestFilePath(Path.Combine("ImportExport", "DataConfigFiles"));
             var filePath = Path.Combine(directoryPath, fileName);
-
-            Assert.That(Directory.Exists(directoryPath));
+            
             Assert.That(File.Exists(filePath));
 
             var controlGroups = new List<ControlGroup>();
@@ -71,35 +72,31 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
             var connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, controlGroups);
 
             // Then
-            Assert.NotNull(connectionPoints);
-            Assert.AreEqual(2, connectionPoints.Count);
+            CheckControlGroupValidity(controlGroups);
+            CheckConnectionPointsValidity(connectionPoints);
+        }
 
-            var inputs = connectionPoints.OfType<Input>().ToList();
-            Assert.NotNull(inputs);
-            Assert.AreEqual(1, inputs.Count);
-
-            var input = inputs[0];
-            Assert.AreEqual("[Input]parameter/quantity", input.Name);
-            Assert.AreEqual(null, input.ParameterName);
-            Assert.AreEqual(string.Empty, input.LocationName);
-
-            var outputs = connectionPoints.OfType<Output>().ToList();
-            Assert.NotNull(outputs);
-            Assert.AreEqual(1, outputs.Count);
-
-            var output = outputs[0];
-            Assert.AreEqual("[Output]parameter/quantity", output.Name);
-            Assert.AreEqual(null, output.ParameterName);
-            Assert.AreEqual(string.Empty, output.LocationName);
-
+        private static void CheckControlGroupValidity(IList<ControlGroup> controlGroups)
+        {
             Assert.IsNotEmpty(controlGroups);
             Assert.AreEqual(1, controlGroups.Count);
 
             var controlGroup = controlGroups[0];
+            CheckControlGroupConditionsValidity(controlGroup);
+            CheckControlGroupRulesValidity(controlGroup);
+        }
 
+        private static void CheckControlGroupConditionsValidity(IControlGroup controlGroup)
+        {
             var conditions = controlGroup.Conditions;
             Assert.AreEqual(2, conditions.Count);
 
+            CheckTimeConditionValidity(conditions);
+            CheckStandardConditionValidity(conditions);
+        }
+
+        private static void CheckTimeConditionValidity(IEventedList<ConditionBase> conditions)
+        {
             var timeConditions = conditions.OfType<TimeCondition>().ToList();
             Assert.NotNull(timeConditions);
             Assert.AreEqual(1, timeConditions.Count);
@@ -108,16 +105,28 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
             Assert.AreEqual("time_condition", timeCondition.Name);
             Assert.AreEqual(InterpolationType.Constant, timeCondition.InterpolationOptionsTime);
             Assert.AreEqual(ExtrapolationType.Constant, timeCondition.Extrapolation);
+        }
 
+        private static void CheckStandardConditionValidity(IEventedList<ConditionBase> conditions)
+        {
             var standardConditions = conditions.OfType<StandardCondition>()
                 .Where(c => c.GetType() != typeof(TimeCondition)).ToList();
             Assert.NotNull(standardConditions);
             Assert.AreEqual(1, standardConditions.Count);
             Assert.AreEqual("standard_condition", standardConditions[0].Name);
-    
+        }
+
+        private static void CheckControlGroupRulesValidity(IControlGroup controlGroup)
+        {
             var rules = controlGroup.Rules;
             Assert.AreEqual(2, rules.Count);
 
+            CheckTimeRuleValidity(rules);
+            CheckRelativeTimeRuleValidity(rules);
+        }
+
+        private static void CheckTimeRuleValidity(IEnumerable<RuleBase> rules)
+        {
             var timeRules = rules.OfType<TimeRule>().ToList();
             Assert.NotNull(timeRules);
             Assert.AreEqual(1, timeRules.Count);
@@ -125,7 +134,10 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
             var timeRule = timeRules[0];
             Assert.AreEqual("time_rule", timeRule.Name);
             Assert.AreEqual(InterpolationType.Linear, timeRule.InterpolationOptionsTime);
+        }
 
+        private static void CheckRelativeTimeRuleValidity(IEnumerable<RuleBase> rules)
+        {
             var relativeTimeRules = rules.OfType<RelativeTimeRule>().ToList();
             Assert.NotNull(relativeTimeRules);
             Assert.AreEqual(1, relativeTimeRules.Count);
@@ -135,24 +147,57 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
             Assert.AreEqual(InterpolationType.Linear, relativeTimeRule.Interpolation);
         }
 
+        private static void CheckConnectionPointsValidity(IList<ConnectionPoint> connectionPoints)
+        {
+            Assert.NotNull(connectionPoints);
+            Assert.AreEqual(2, connectionPoints.Count);
+
+            CheckInputConnectionPoint(connectionPoints);
+            CheckOutputConnectionPoint(connectionPoints);
+        }
+
+        private static void CheckInputConnectionPoint(IList<ConnectionPoint> connectionPoints)
+        {
+            var inputs = connectionPoints.OfType<Input>().ToList();
+            Assert.NotNull(inputs);
+            Assert.AreEqual(1, inputs.Count);
+
+            var input = inputs[0];
+            Assert.AreEqual("[Input]parameter/quantity", input.Name);
+            Assert.AreEqual(null, input.ParameterName);
+            Assert.AreEqual(string.Empty, input.LocationName);
+        }
+
+        private static void CheckOutputConnectionPoint(IList<ConnectionPoint> connectionPoints)
+        {
+            var outputs = connectionPoints.OfType<Output>().ToList();
+            Assert.NotNull(outputs);
+            Assert.AreEqual(1, outputs.Count);
+
+            var output = outputs[0];
+            Assert.AreEqual("[Output]parameter/quantity", output.Name);
+            Assert.AreEqual(null, output.ParameterName);
+            Assert.AreEqual(string.Empty, output.LocationName);
+        }
+
         [Test]
         [Category(TestCategory.DataAccess)]
         public void GivenAnExistingFileAndWithoutTimeSeriesElements_WhenReading_ThenExpectedMessageIsGivenAndNullIsReturned()
         {
             // Given
-            var fileName = "rtcDataConfig_noelements.xml";
+            const string fileName = "rtcDataConfig_noelements.xml";
             var directoryPath = TestHelper.GetTestFilePath(Path.Combine("ImportExport", "DataConfigFiles"));
             var filePath = Path.Combine(directoryPath, fileName);
 
-            Assert.That(Directory.Exists(directoryPath));
             Assert.That(File.Exists(filePath));
-
-            var controlGroups = new List<ControlGroup>();
 
             TestHelper.AssertLogMessageIsGenerated(() =>
                 {
                     // When
+                    var controlGroups = new List<ControlGroup>();
                     var connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, controlGroups);
+
+                    // Then
                     Assert.IsNull(connectionPoints);
                 },
                 string.Format(Resources.RealTimeControlDataConfigXmlReader_Read_File___0___seems_to_be_empty_, filePath));
@@ -163,19 +208,19 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
         public void GivenAnExistingFileAndWithoutReadableControlGroupNames_WhenReading_ThenExpectedMessageIsGivenAndNullIsReturned()
         {
             // Given
-            var fileName = "rtcDataConfig_nocontrolgroupnames.xml";
+            const string fileName = "rtcDataConfig_nocontrolgroupnames.xml";
             var directoryPath = TestHelper.GetTestFilePath(Path.Combine("ImportExport", "DataConfigFiles"));
             var filePath = Path.Combine(directoryPath, fileName);
 
-            Assert.That(Directory.Exists(directoryPath));
             Assert.That(File.Exists(filePath));
-
-            var controlGroups = new List<ControlGroup>();
 
             TestHelper.AssertLogMessageIsGenerated(() =>
                 {
                     // When
+                    var controlGroups = new List<ControlGroup>();
                     var connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, controlGroups);
+                    
+                    // Then
                     Assert.IsNull(connectionPoints);
                 },
                 string.Format(Resources.RealTimeControlDataConfigXmlReader_Read_Could_not_read_control_groups_from_file___0___, filePath));
@@ -186,19 +231,19 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
         public void GivenAnExistingFileAndWithoutInputsOrOutputs_WhenReading_ThenExpectedMessageIsGivenAndNullIsReturned()
         {
             // Given
-            var fileName = "rtcDataConfig_noconnectionpoints.xml";
+            const string fileName = "rtcDataConfig_noconnectionpoints.xml";
             var directoryPath = TestHelper.GetTestFilePath(Path.Combine("ImportExport", "DataConfigFiles"));
             var filePath = Path.Combine(directoryPath, fileName);
 
-            Assert.That(Directory.Exists(directoryPath));
             Assert.That(File.Exists(filePath));
-
-            var controlGroups = new List<ControlGroup>();
 
             TestHelper.AssertLogMessageIsGenerated(() =>
                 {
                     // When
+                    var controlGroups = new List<ControlGroup>();
                     var connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, controlGroups);
+
+                    // Then
                     Assert.IsNull(connectionPoints);
                 },
                 string.Format(Resources.RealTimeControlDataConfigXmlReader_Read_Could_not_read_connection_points_from_file___0___, filePath));
@@ -209,28 +254,33 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.ImportExport
         public void GivenAnExistingFileForRmmModel_WhenReading_ThenNoExceptionIsThrown()
         {
             // Given
-            var fileName = "rtcDataConfig.xml";
+            const string fileName = "rtcDataConfig.xml";
             var directoryPath = TestHelper.GetTestFilePath(Path.Combine("ImportExport", "RMM"));
             var filePath = Path.Combine(directoryPath, fileName);
 
-            Assert.That(Directory.Exists(directoryPath));
             Assert.That(File.Exists(filePath));
 
+            // When
             var controlGroups = new List<ControlGroup>();
             IList<ConnectionPoint> connectionPoints = new List<ConnectionPoint>();
-
             Assert.DoesNotThrow(() =>
             {
                connectionPoints = RealTimeControlDataConfigXmlReader.Read(filePath, controlGroups);
             });
 
+            // Then
             Assert.AreEqual(23, controlGroups.Count);
             Assert.AreEqual(23, connectionPoints.OfType<Input>().Count());
             Assert.AreEqual(31, connectionPoints.OfType<Output>().Count());
             Assert.AreEqual(43, controlGroups.SelectMany(c => c.Conditions).Count());
             Assert.AreEqual(53, controlGroups.SelectMany(c => c.Rules).Count());
 
-            // Sample
+            CheckSampleControlGroupValidity(controlGroups);
+        }
+
+        private static void CheckSampleControlGroupValidity(List<ControlGroup> controlGroups)
+        {
+            // We take one sample of the control groups and test its validity
             var controlGroup = controlGroups.FirstOrDefault(cg => cg.Name == "HollandscheIJsselkering");
             Assert.NotNull(controlGroup);
 
