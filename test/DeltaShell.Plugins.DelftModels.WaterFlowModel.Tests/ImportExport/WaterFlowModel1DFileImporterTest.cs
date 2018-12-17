953343fdcead7952c7c3a901353ab3ae1c4e6a6e
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2018-12-17 13:57:37 +0000

    SOBEK3-1576 Added large test to check for all the imported files. Need to add the files written in TODO, will be fixed during next SOBEK sprint.
    Former-commit-id: 564ce982f935636cdfdf8e90744603c96b86f9e1
    Former-commit-id: 9d02c6576d469956e1eeacda8a2144a3caf50493

---
+++ test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/ImportExport/WaterFlowModel1DFileImporterTest.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using DelftTools.TestUtils;
+using DelftTools.Utils.IO;
+using DeltaShell.NGHS.IO.TestUtils;
+using DeltaShell.Plugins.DelftModels.WaterFlowModel.ImportExport;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
+{
+    [TestFixture]
+    public class WaterFlowModel1DFileImporterTest
+    {
+        // TODO: BoundaryConditions.bc, CrossSectionDefinitions.ini, CrossSectionLocations.ini, NetworkDefinition.ini, ObservationPoints.ini & the md1d file
+        [TestCase("BoundaryLocations.ini")]
+        [TestCase("Dispersion.ini")]
+        [TestCase("DispersionF3.ini")]
+        [TestCase("DispersionF4.ini")]
+        [TestCase("InitialDischarge.ini")]
+        [TestCase("InitialSalinity.ini")]
+        [TestCase("InitialTemperature.ini")]
+        [TestCase("InitialWaterLevel.ini")]
+        [TestCase("LateralDischargeLocations.ini")]
+        [TestCase("Retention.ini")]
+        [TestCase("roughness-FloodPlain1 (Reversed).ini")]
+        [TestCase("roughness-FloodPlain1.ini")]
+        [TestCase("roughness-Main (Reversed).ini")]
+        [TestCase("roughness-Main.ini")]
+        [TestCase("Salinity.ini")]
+        [TestCase("sobeksim.fnm")]
+        [TestCase("SobekSim.ini")]
+        [TestCase("Structures.ini")]
+        [TestCase("WindShielding.ini")]
+        [Category(TestCategory.Slow)]
+        public void GivenAnMd1dFile_WhenImportingFileAndExporting_ThenTheInputFilesAreTheSameAsTheOutputFiles11(
+            string spatialIniFile)
+        {
+            var md1dFilePath = TestHelper.GetTestFilePath(@"ImportExportTest\TestExport.md1d");
+            var testDirectory = FileUtils.CreateTempDirectory();
+            var sourceFile = TestHelper.GetTestFilePath($@"ImportSpatialData\{spatialIniFile}");
+            var errorMessage = "Files not equal";
+            var targetFilePath = Path.Combine(testDirectory, "FileWriters");
+
+            try
+            {
+                var importer = new WaterFlowModel1DFileImporter
+                {
+                    ProgressChanged = (name, step, steps) => { }
+                };
+                var model = importer.ImportItem(md1dFilePath) as WaterFlowModel1D;
+                Assert.IsNotNull(model);
+
+                WaterFlowModel1DFileWriter.Write(Path.Combine(targetFilePath, ModelFileNames.ModelDefinitionFilename),
+                    model);
+
+                var iniFilePath = Path.Combine(targetFilePath, spatialIniFile);
+                Assert.IsTrue(FileComparer.Compare(iniFilePath, sourceFile, out errorMessage));
+            }
+            finally
+            {
+                FileUtils.DeleteIfExists(testDirectory);
+            }
+        }
+    }
+}
