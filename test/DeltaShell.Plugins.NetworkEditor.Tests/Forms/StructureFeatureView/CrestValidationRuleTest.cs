462ab5c27a7dc416ba6d541f34ca9bee879d494d
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2018-12-24 12:49:45 +0000

    D3DFMIQ-524 Added CrestValidationRuleTest Class and happy flow unit test.
    Former-commit-id: d29b1ea57a5e6efd5d46e5c8f5a87efe5d1dcd91
    Former-commit-id: c0c92bcd75ae418aa101baf87e5a82eb11fe0903

---
+++ test/DeltaShell.Plugins.NetworkEditor.Tests/Forms/StructureFeatureView/CrestValidationRuleTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using DelftTools.Hydro.Structures;
+using DelftTools.Hydro.Structures.WeirFormula;
+using DeltaShell.Plugins.NetworkEditor.Gui.Forms.StructureFeatureView;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.NetworkEditor.Tests.Forms.StructureFeatureView
+{
+    [TestFixture]
+    public class CrestValidationRuleTest
+    {
+        [Test]
+        public void GivenACorrectWeirViewModel_WhenValidatingCrestProperties_ThenAValidResultIsReturned()
+        {
+            var viewModel = new WeirViewModel
+            {
+                Weir = new Weir(), SelectedWeirType = SelectableWeirFormulaType.SimpleGate
+            };
+
+            Assert.That(viewModel.Weir.WeirFormula is GatedWeirFormula);
+            viewModel.BedLevelStructureCentre = 10.0;
+
+            var validationResult = new CrestValidationRule().Validate(viewModel.BedLevelStructureCentre, CultureInfo.CurrentCulture);
+            Assert.IsTrue(validationResult.IsValid);
+        }
+    }
+}
