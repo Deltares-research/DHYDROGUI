4b7eaa14f11c15bf2ab2b0c86d9fa3976505f157
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2018-12-24 12:24:59 +0000

    D3DFMIQ-524 Crest values are now validated in the view itself. When a validation error occurs, the checkboxes will turn red. When hoovering over the value the validation problem will be pointed out.
    Former-commit-id: 5d21a31e694b5101ef3aafcc30a4ae6fecedbf5d
    Former-commit-id: fb7576ed07ef48e095c4bd73c1a468b9c449352e

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/StructureFeatureView/CrestValidationRule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.StructureFeatureView
+{
+    /// <summary>
+    /// This class is used for validating data from the WeirViewWpf.xaml.
+    /// It only validates the text boxes where this ValidationRule is bound to the Textbox "Text" property.
+    /// </summary>
+    public class CrestValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            double result;
+            try
+            {
+                result = Convert.ToDouble(value);
+            }
+
+            catch (FormatException)
+            {
+                return new ValidationResult(false, "The entered value is not a number, please enter a number greater than 0");
+            }
+            catch (InvalidCastException)
+            {
+                return new ValidationResult(false, "The entered value is not a number, please enter a number greater than 0");
+            }
+            catch (OverflowException)
+            {
+                return new ValidationResult(false, "The entered value is either too large or too small, please enter a number between -1.79769313486232E+308 & 1.79769313486232E+308");
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(false, "An error occured, please enter a number greater than 0");
+            }
+
+            return result > 0.0 ? ValidationResult.ValidResult : new ValidationResult(false, "The entered value must be greater than 0");
+        }
+    }
+}
+
