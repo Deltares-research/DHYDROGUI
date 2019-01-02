3ce010810adaec104caeae2407cec2c60a10d410
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2019-01-02 09:33:45 +0000

    D3DFMIQ-524 Created unit tests to test the possible exceptions.
    Former-commit-id: a25a1d9094445fc2732407f0c67a67d9f0655e4d
    Former-commit-id: d72386829a13b5f62249c486eda0040fee65f3ab

--- test/DeltaShell.Plugins.NetworkEditor.Tests/Forms/StructureFeatureView/CrestValidationRuleTest.cs
+++ test/DeltaShell.Plugins.NetworkEditor.Tests/Forms/StructureFeatureView/CrestValidationRuleTest.cs
@@ -23,5 +23,52 @@ namespace DeltaShell.Plugins.NetworkEditor.Tests.Forms.StructureFeatureView
             var validationResult = new CrestValidationRule().Validate(viewModel.BedLevelStructureCentre, CultureInfo.CurrentCulture);
             Assert.IsTrue(validationResult.IsValid);
         }
+
+        [Test]
+        public void GivenAWeirViewModelWhereACrestPropertyOutsideDoubleBounds_WhenValidatingCrestProperties_ThenAnOverFlownExceptionIsThrown()
+        {
+            var viewModel = new WeirViewModel
+            {
+                Weir = new Weir(),
+                SelectedWeirType = SelectableWeirFormulaType.SimpleGate
+            };
+
+            Assert.That(viewModel.Weir.WeirFormula is GatedWeirFormula);
+
+            var validationResult =
+                new CrestValidationRule().Validate("1.79769313486232E+310", CultureInfo.CurrentCulture);
+            Assert.IsFalse(validationResult.IsValid);
+        }
+        [Test]
+        public void GivenAWeirViewModelWhereACrestPropertyOutsideAString_WhenValidatingCrestProperties_ThenAFormatExceptionIsThrown()
+        {
+            var viewModel = new WeirViewModel
+            {
+                Weir = new Weir(),
+                SelectedWeirType = SelectableWeirFormulaType.SimpleGate
+            };
+
+            Assert.That(viewModel.Weir.WeirFormula is GatedWeirFormula);
+
+            var validationResult =
+                new CrestValidationRule().Validate("ThisIsNotAValidDouble", CultureInfo.CurrentCulture);
+            Assert.IsFalse(validationResult.IsValid);
+        }
+
+        [Test]
+        public void GivenAWeirViewModelWhereACrestPropertyIsAnObject_WhenValidatingCrestProperties_ThenAnInvalidCastExceptionIsThrown()
+        {
+            var viewModel = new WeirViewModel
+            {
+                Weir = new Weir(),
+                SelectedWeirType = SelectableWeirFormulaType.SimpleGate
+            };
+
+            Assert.That(viewModel.Weir.WeirFormula is GatedWeirFormula);
+
+            var validationResult =
+                new CrestValidationRule().Validate(new object(), CultureInfo.CurrentCulture);
+            Assert.IsFalse(validationResult.IsValid);
+        }
     }
 }
