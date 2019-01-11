d15ef2406732d35bf7402fb56eecd2bf8e12aa61
freijtas <freijtas@deltares.nl>
2019-01-11 09:07:02 +0000

    D3DFMIQ-528 Processed review comments
    Former-commit-id: 583856c3cde89247884de748eb2b567068e94376
    Former-commit-id: 91603e027dded855ddd6f3c1eddc9330191b03a2

--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/Gui/FlowFMGuiPluginTest.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/Gui/FlowFMGuiPluginTest.cs
@@ -22,7 +22,7 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.Gui
 
         //[TestCase(@"ReloadGrid\netfile_spherical_assigned.nc", 4326, @"ReloadGrid\netfile_spherical_assigned.nc", 4326)]
         //[TestCase(@"ReloadGrid\netfile_spherical_assigned.nc", 4326, @"ReloadGrid\netfile_projected_unassigned.nc", 0)]
-
+        [Category(TestCategory.DataAccess)]
         public void ReloadGridTest(string originalNetFile, int originalEpsg, string editedNetFile, int expectedEpsg)
         {
             //Given
@@ -101,7 +101,6 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.Gui
         }
 
         [Test]
-        [Category(TestCategory.DataAccess)]
         public void GivenAFlowFMGuiPlugin_WhenGetProjectTreeViewNodePresentersIsCalled_AnEnumerableContainingAnFMClassMapFileFunctionStoreNodePresenterIsReturned()
         {
             // Given
