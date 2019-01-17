a27aba90aa55b083ab06a6e487baf71e9207c36b
kahmann <kahmann@deltares.nl>
2019-01-17 15:14:26 +0000

    D3DFMIQ-576: Moved a performance test to its own performance test class. Former-commit-id: e19e9e20200b90b6e823f2bc7d99da831b2456b3 Former-commit-id: bb4fdfb7993888e60a6b5920da769c21b75fe479

---
+++ test/DeltaShell.Plugins.ImportExport.Sobek.Tests/SobekNetworkImporterPerformanceTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using DelftTools.Hydro;
+using DelftTools.TestUtils;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.ImportExport.Sobek.Tests
+{
+    [TestFixture]
+    public class SobekNetworkImporterPerformanceTest
+    {
+        [Test]
+        [Category(TestCategory.Performance)]
+        [Category(TestCategory.Slow)]
+        public void ReadPoNetwork()
+        {
+            var pathToSobekNetwork = TestHelper.GetDataDir() + @"\POup_GV.lit\7\network.tp";
+            var importer = new SobekNetworkImporter();
+
+            HydroNetwork network = null;
+
+            TestHelper.AssertIsFasterThan(28000, () => network = (HydroNetwork)importer.ImportItem(pathToSobekNetwork));
+
+            var channelsWithoutCrossSections = network.Channels.Where(c => !c.CrossSections.Any());
+
+            //linkagenode -> branches with same order number: interpolation/extrapolation cross-sections over node
+            foreach (var channelWithoutCrossSection in channelsWithoutCrossSections)
+            {
+                Assert.Greater(
+                    network.Channels.Count(c => c.OrderNumber == channelWithoutCrossSection.OrderNumber),
+                    1,
+                    string.Format("Channel {0} should have the same order number as another branch for extra/interpolation of cross-section", channelWithoutCrossSection.Name)
+                );
+            }
+        }
+    }
+}
--- test/DeltaShell.Plugins.ImportExport.Sobek.Tests/SobekNetworkImporterTest.cs
+++ test/DeltaShell.Plugins.ImportExport.Sobek.Tests/SobekNetworkImporterTest.cs
@@ -895,34 +895,7 @@ namespace DeltaShell.Plugins.ImportExport.Sobek.Tests
             var network = (HydroNetwork)importer.ImportItem(path);
         }
 
-
         [Test]
-        [Category(TestCategory.Performance)]
-        [Category(TestCategory.Slow)]
-        public void ReadPoNetwork()
-        {
-            var pathToSobekNetwork = TestHelper.GetDataDir() + @"\POup_GV.lit\7\network.tp";
-            var importer = new SobekNetworkImporter();
-
-            HydroNetwork network = null;
-
-            TestHelper.AssertIsFasterThan(28000, () => network = (HydroNetwork) importer.ImportItem(pathToSobekNetwork));
-
-            var channelsWithoutCrossSections = network.Channels.Where(c => !c.CrossSections.Any());
-            
-            //linkagenode -> branches with same order number: interpolation/extrapolation cross-sections over node
-            foreach (var channelWithoutCrossSection in channelsWithoutCrossSections)
-            {
-                Assert.Greater(
-                                network.Channels.Count(c => c.OrderNumber == channelWithoutCrossSection.OrderNumber),
-                                1, 
-                                string.Format("Channel {0} should have the same order number as another branch for extra/interpolation of cross-section", channelWithoutCrossSection.Name)
-                              );
-            }
-        }
-
-        [Test]
-        [Category(TestCategory.DataAccess)]
         [Category(TestCategory.Slow)]
         public void ReadNetworkJkfl2009()
         {
