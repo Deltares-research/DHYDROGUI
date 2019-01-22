a4d5ec9516b33fd5730d8a9725167c82f7142e4e
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2019-01-22 13:32:17 +0000

    SOBEK3-1576 Processed review remarks by adjusting unit test.
    Former-commit-id: 226ad1bf85cf14205b365087a34bf9a58164b8e3
    Former-commit-id: aa140d7358a6c879e6b56ef154a96b2d997e60b7

--- test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/ImportExport/WaterFlowModel1DFileImporterTest.cs
+++ test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/ImportExport/WaterFlowModel1DFileImporterTest.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using DelftTools.TestUtils;
 using DelftTools.Utils.IO;
 using DeltaShell.NGHS.IO.TestUtils;
@@ -10,36 +12,38 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
     [TestFixture]
     public class WaterFlowModel1DFileImporterTest
     {
-        // TODO: BoundaryConditions.bc, CrossSectionDefinitions.ini, CrossSectionLocations.ini, NetworkDefinition.ini, ObservationPoints.ini & the md1d file
-        [TestCase("BoundaryLocations.ini")]
-        [TestCase("Dispersion.ini")]
-        [TestCase("DispersionF3.ini")]
-        [TestCase("DispersionF4.ini")]
-        [TestCase("InitialDischarge.ini")]
-        [TestCase("InitialSalinity.ini")]
-        [TestCase("InitialTemperature.ini")]
-        [TestCase("InitialWaterLevel.ini")]
-        [TestCase("LateralDischargeLocations.ini")]
-        [TestCase("Retention.ini")]
-        [TestCase("roughness-FloodPlain1 (Reversed).ini")]
-        [TestCase("roughness-FloodPlain1.ini")]
-        [TestCase("roughness-Main (Reversed).ini")]
-        [TestCase("roughness-Main.ini")]
-        [TestCase("Salinity.ini")]
-        [TestCase("sobeksim.fnm")]
-        [TestCase("SobekSim.ini")]
-        [TestCase("Structures.ini")]
-        [TestCase("WindShielding.ini")]
+        [Test]
         [Category(TestCategory.DataAccess)]
         [Category(TestCategory.Slow)]
-        public void GivenAnMd1dFile_WhenImportingFileAndExporting_ThenTheInputFilesAreTheSameAsTheOutputFiles11(
-            string spatialIniFile)
+        public void GivenAnMd1dFile_WhenImportingFileAndExporting_ThenTheInputFilesAreTheSameAsTheOutputFiles11()
         {
+            // TODO: Add BoundaryConditions.bc, CrossSectionDefinitions.ini, CrossSectionLocations.ini, NetworkDefinition.ini, ObservationPoints.ini & the md1d file
             var md1dFilePath = TestHelper.GetTestFilePath(@"ImportSpatialData\water flow 1d.md1d");
             var testDirectory = FileUtils.CreateTempDirectory();
-            var sourceFile = TestHelper.GetTestFilePath($@"ImportSpatialData\{spatialIniFile}");
             var errorMessage = "Files not equal";
             var targetFilePath = Path.Combine(testDirectory, "FileWriters");
+            var fileCollection = new List<string>
+            {
+                "BoundaryLocations.ini",
+                "Dispersion.ini",
+                "DispersionF3.ini",
+                "DispersionF4.ini",
+                "InitialDischarge.ini",
+                "InitialSalinity.ini",
+                "InitialTemperature.ini",
+                "InitialWaterLevel.ini",
+                "LateralDischargeLocations.ini",
+                "Retention.ini",
+                "roughness-FloodPlain1 (Reversed).ini",
+                "roughness-FloodPlain1.ini",
+                "roughness-Main (Reversed).ini",
+                "roughness-Main.ini",
+                "Salinity.ini",
+                "sobeksim.fnm",
+                "SobekSim.ini",
+                "Structures.ini",
+                "WindShielding.ini"
+            };
 
             try
             {
@@ -50,11 +54,14 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
                 var model = importer.ImportItem(md1dFilePath) as WaterFlowModel1D;
                 Assert.IsNotNull(model);
 
-                WaterFlowModel1DFileWriter.Write(Path.Combine(targetFilePath, ModelFileNames.ModelDefinitionFilename),
-                    model);
+                WaterFlowModel1DFileWriter.Write(Path.Combine(targetFilePath, ModelFileNames.ModelDefinitionFilename), model);
 
-                var iniFilePath = Path.Combine(targetFilePath, spatialIniFile);
-                Assert.IsTrue(FileComparer.Compare(iniFilePath, sourceFile, out errorMessage));
+                foreach (var file in fileCollection)
+                {
+                    var iniFilePath = Path.Combine(targetFilePath, file);
+                    Assert.IsTrue(FileComparer.Compare(iniFilePath,
+                        TestHelper.GetTestFilePath($@"ImportSpatialData\{file}"), out errorMessage));
+                }
             }
             finally
             {
