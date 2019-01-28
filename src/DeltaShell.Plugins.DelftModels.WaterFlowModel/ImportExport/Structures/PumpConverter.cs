57ca20e5c37220b678156a114cd4816fff0226eb
kahmann <kahmann@deltares.nl>
2019-01-28 21:46:04 +0000

    SOBEK3-1569: Small refactoring of PumpConverter class. Former-commit-id: 7dbbfbde731bf0d0b53c9e64657b0dd03183112a Former-commit-id: 01277aa61343781a9df409378ec9e3cb9ad320b5

--- src/DeltaShell.Plugins.DelftModels.WaterFlowModel/ImportExport/Structures/PumpConverter.cs
+++ src/DeltaShell.Plugins.DelftModels.WaterFlowModel/ImportExport/Structures/PumpConverter.cs
@@ -26,28 +26,40 @@ namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.ImportExport.Structures
             var pump = new Pump();
             BasicStructuresOperations.ReadCommonRegionElements(category, branch, pump);
 
-            var direction = category.ReadProperty<int>(StructureRegion.Direction.Key);
-            pump.ControlDirection = (PumpControlDirection) Math.Abs(direction);
-            pump.DirectionIsPositive = direction > 0;
-
             pump.Capacity = category.ReadProperty<double>(StructureRegion.Capacity.Key);
+            SetSuctionAndDeliveryTriggerPropertiesSetSuctionAndDeliveryTiggerProperties(category, pump);
+            SetDirectionProperties(category, pump);
+            SetReductionTableValues(category, pump);
+
+            return pump;
+        }
+
+        private static void SetSuctionAndDeliveryTriggerPropertiesSetSuctionAndDeliveryTiggerProperties(IDelftIniCategory category, IPump pump)
+        {
             pump.StartSuction = category.ReadProperty<double>(StructureRegion.StartLevelSuctionSide.Key);
             pump.StopSuction = category.ReadProperty<double>(StructureRegion.StopLevelSuctionSide.Key);
             pump.StartDelivery = category.ReadProperty<double>(StructureRegion.StartLevelDeliverySide.Key);
             pump.StopDelivery = category.ReadProperty<double>(StructureRegion.StopLevelDeliverySide.Key);
+        }
 
+        private static void SetDirectionProperties(IDelftIniCategory category, IPump pump)
+        {
+            var direction = category.ReadProperty<int>(StructureRegion.Direction.Key);
+            pump.ControlDirection = (PumpControlDirection) Math.Abs(direction);
+            pump.DirectionIsPositive = direction > 0;
+        }
+
+        private static void SetReductionTableValues(IDelftIniCategory category, IPump pump)
+        {
             var numberOfFunctionEntries = category.ReadProperty<int>(StructureRegion.ReductionFactorLevels.Key);
-            if (numberOfFunctionEntries > 0)
-            {
-                var pumpHeadValues = TransformToDoubleArray(category.ReadProperty<string>(StructureRegion.Head.Key));
-                var reductionFactorValues = TransformToDoubleArray(category.ReadProperty<string>(StructureRegion.ReductionFactor.Key));
-                for (var i = 0; i < numberOfFunctionEntries; i++)
-                {
-                    pump.ReductionTable[pumpHeadValues[i]] = reductionFactorValues[i];
-                }
-            }
+            if (numberOfFunctionEntries <= 0) return;
 
-            return pump;
+            var pumpHeadValues = TransformToDoubleArray(category.ReadProperty<string>(StructureRegion.Head.Key));
+            var reductionFactorValues = TransformToDoubleArray(category.ReadProperty<string>(StructureRegion.ReductionFactor.Key));
+            for (var i = 0; i < numberOfFunctionEntries; i++)
+            {
+                pump.ReductionTable[pumpHeadValues[i]] = reductionFactorValues[i];
+            }
         }
 
         private static double[] TransformToDoubleArray(string valuesString)
