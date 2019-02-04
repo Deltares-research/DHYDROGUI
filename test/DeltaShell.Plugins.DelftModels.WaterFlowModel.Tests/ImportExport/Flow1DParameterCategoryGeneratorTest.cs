b0767de01260a6152d1bbc370144c0ffa033a410
freijtas <freijtas@deltares.nl>
2019-02-04 11:59:30 +0000

    SOBEK3-1720 Added Flow1DParameterCategoryGeneratorTest class
    Former-commit-id: 2d4a9cd2bcf7c7f93928dd2ff370396f4523d4c1
    Former-commit-id: 24cafe1272bb159b3d797f048d7fd526f0a938f3

---
+++ test/DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests/ImportExport/Flow1DParameterCategoryGeneratorTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace DeltaShell.Plugins.DelftModels.WaterFlowModel.Tests.ImportExport
+{
+    [TestFixture]
+    public class Flow1DParameterCategoryGeneratorTest
+    {
+        [Test]
+        public void
+            GivenAWaterFlowModel1D_WhenGenerateSpecialsValuesIsCalledWithThisModel_ThenACorrectGenerateSpecialsValuesIsGenerated() { }
+    }
+}
