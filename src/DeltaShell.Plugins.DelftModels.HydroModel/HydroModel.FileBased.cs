100057aa30e9315742ddfacbf69d1146a1a7ceda
Pauline Belt <pauline.belt@deltares.nl>
2019-05-01 16:11:22 +0000

    D3DFMIQ-928 Fix for new structures names in old integrated models together with RTC.
    Former-commit-id: 4be76edeb1142d2020825262fb0076bc2ef73934
    Former-commit-id: 70fee9ccee3475ce5872f4bcf84cbb4e45eb1ee7

--- src/DeltaShell.Plugins.DelftModels.HydroModel/HydroModel.FileBased.cs
+++ src/DeltaShell.Plugins.DelftModels.HydroModel/HydroModel.FileBased.cs
@@ -67,7 +67,22 @@ namespace DeltaShell.Plugins.DelftModels.HydroModel
                 foreach (var exchange in exchangeInfo.Exchanges)
                 {
                     var sourceItem = sourceItems.FirstOrDefault(di => Equals(ModelExchange.GetExchangeIdentifier(di), exchange.SourceName));
-                    var targetItem = targetItems.FirstOrDefault(di => Equals(ModelExchange.GetExchangeIdentifier(di), exchange.TargetName));
+                    var targetName = exchange.TargetName;
+                    
+                    if (exchangeInfo.TargetModelName == "FlowFM")
+                    {
+                        targetName = targetName.Replace("levelcenter", "CrestLevel");
+                        targetName = targetName.Replace("sill_level", "CrestLevel");
+                        targetName = targetName.Replace("crest_level", "CrestLevel");
+                        
+                        targetName = targetName.Replace("gateheight", "GateLowerEdgeLevel");
+                        targetName = targetName.Replace("lower_edge_level", "GateLowerEdgeLevel");
+                        
+                        targetName = targetName.Replace("door_opening_width", "GateOpeningWidth");
+                        targetName = targetName.Replace("opening_width", "GateOpeningWidth");
+                    }
+
+                    var targetItem = targetItems.FirstOrDefault(di => Equals(ModelExchange.GetExchangeIdentifier(di), targetName));
 
                     if (sourceItem == null || targetItem == null)
                     {
