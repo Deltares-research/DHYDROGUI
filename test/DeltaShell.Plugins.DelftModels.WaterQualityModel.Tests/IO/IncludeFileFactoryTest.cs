3f86ab367eafb74826147f842f6ae9d854924ec5
freijtas <freijtas@deltares.nl>
2019-05-02 11:57:21 +0000

    D3DFMIQ-889 Processed review comments: Changed assertion style Removed unnecessary WaqInitializationSettings properties in IncludeFileFactoryTest
    Former-commit-id: 724bf43a45e23e024293f15dce408f5de7d53bc3
    Former-commit-id: cd48af8d8e0cbc9f2e473e0574c48fe7b4a555ee

--- test/DeltaShell.Plugins.DelftModels.WaterQualityModel.Tests/IO/IncludeFileFactoryTest.cs
+++ test/DeltaShell.Plugins.DelftModels.WaterQualityModel.Tests/IO/IncludeFileFactoryTest.cs
@@ -824,24 +824,14 @@ namespace DeltaShell.Plugins.DelftModels.WaterQualityModel.Tests.IO
         public void CreateNumericalOptionsIncludeTestDefaultNumberOfCoresIs2()
         {
             // setup
-            var initSettings = new WaqInitializationSettings
-            {
-                Settings =
-                {
-                    NumericalScheme = NumericalScheme.Scheme15,
-                    ClosureErrorCorrection = true,
-                    IterationMaximum = 123,
-                    Tolerance = 1e-6,
-                    WriteIterationReport = false,
-                    DryCellThreshold = 0.01
-                }
-            };
+            var initSettings = new WaqInitializationSettings();
 
             // call
             var numericalOptionsInclude = IncludeFileFactory.CreateNumericalOptionsInclude(initSettings);
 
             // assert
-            Assert.That(numericalOptionsInclude, Contains.Substring("CONSTANTS 'NOTHREADS' DATA 2 ; Number of threads used by delwaq"));
+            var expectedText = "CONSTANTS 'NOTHREADS' DATA 2 ; Number of threads used by delwaq";
+            Assert.IsTrue(numericalOptionsInclude.Contains(expectedText));
         }
 
         [Test]
