21d99967fccacb15fea337c3c0cdd78f1d70fa0b
Scott Willcox <scott.willcox@deltares.nl>
2019-06-13 17:30:34 +0000

    D3DFMIQ-1055, Updated bat file.
    Former-commit-id: fce6665aaa74d6f0a4368cc024b4271628c6e29f
    Former-commit-id: c72671c5dc334b544e93282ed34339288b4d6fb1

--- Build/SonarQubeRun.bat
+++ Build/SonarQubeRun.bat
@@ -1,9 +1,9 @@
 set svnDir=%1
 set msBuildDir=%2
 
-"%svnDir%\build\SonarQube\SonarScanner.MSBuild.exe" begin /k:"DELFT3D_Flexible_Mesh_B&O" /d:sonar.host.url="https://sonarqube.directory.intra" /d:sonar.login="d2492e6373f2bec76f88b20051e86a01c9fbfd16"
+"%svnDir%\build\SonarQube\SonarScanner.MSBuild.exe" begin /k:"DELFT3D_Flexible_Mesh_BenO" /d:sonar.host.url="https://sonarqube.directory.intra" /d:sonar.login="cdef2c31f166b3f27938997b0fc04d637c080600"
 
 %msBuildDir%\MsBuild.exe NGHS.sln /t:Rebuild /t:clean
 
-"%svnDir%\build\SonarQube\SonarScanner.MSBuild.exe" end /d:sonar.login="d2492e6373f2bec76f88b20051e86a01c9fbfd16"
+"%svnDir%\build\SonarQube\SonarScanner.MSBuild.exe" end /d:sonar.login="cdef2c31f166b3f27938997b0fc04d637c080600"
 
