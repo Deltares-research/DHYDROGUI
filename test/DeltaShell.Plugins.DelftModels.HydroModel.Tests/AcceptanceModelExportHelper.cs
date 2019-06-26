274cc1d77c8ff4910a58fffb024eb570a89fcbc6
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-06-26 12:28:08 +0000

    D3DFMIQ-1116 - Add Diagnostic Report support to AcceptanceModelsTest - Add Diagnostic Report python script
    Former-commit-id: a96f6709f2260f2ea7b6d98ac2f54a8817d04a6a
    Former-commit-id: c31d8c47c27a64cb0922c34f8e6a30fd90cfbbba

---
+++ test/DeltaShell.Plugins.DelftModels.HydroModel.Tests/AcceptanceModelExportHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using DelftTools.Utils.IO;
+
+
+namespace DeltaShell.Plugins.DelftModels.HydroModel.Tests
+{
+    /// <summary>
+    /// The AcceptanceModelExportHelper provides functionality for exporting
+    /// the dia and log files used within TeamCity to generate reports.
+    /// </summary>
+    public static class AcceptanceModelExportHelper
+    {
+        /// <summary>
+        /// Export an empty log file based upon the <paramref name="config"/>.
+        /// </summary>
+        /// <param name="config"> The configuration. </param>
+        public static void ExportEmptyLogFile(AcceptanceModelExportResultConfig config)
+        {
+            string content = config.HasValidated ? "No .dia file found for this run."
+                                                 : "** ERROR : Failed to validate the model, no .dia file has been produced.";
+
+            File.WriteAllText(Path.Combine(AcceptanceModelExportResultConfig.Delft3DfmExportDirectory,
+                                           $"{config.OutputName}.dia"),
+                              content);
+        }
+
+        /// <summary>
+        /// Export the log file based up on the <paramref name="config"/>.
+        /// </summary>
+        /// <param name="config"> The configuration. </param>
+        public static void ExportLogFile(AcceptanceModelExportResultConfig config)
+        {
+            string diaPath = Path.Combine(config.WorkingDirectory,
+                                          config.CurrentModelName,
+                                          "dflowfm",
+                                          "output",
+                                          $"{config.CurrentModelName}.dia");
+
+            if (!File.Exists(diaPath))
+            {
+                return;
+            }
+
+            FileUtils.CopyFile(diaPath,
+                               Path.Combine(AcceptanceModelExportResultConfig.Delft3DfmExportDirectory, $"{config.OutputName}.dia"));
+
+            config.HasExportedDiagnostics = true;
+        }
+    }
+}
