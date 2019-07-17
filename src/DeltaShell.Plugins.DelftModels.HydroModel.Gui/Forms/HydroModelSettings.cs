2a5f1c582f5e863a59a683f15d59359372ec0d61
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2019-07-17 14:54:01 +0000

    D3DFMIQ-1189 Removed unused usings DeltaShell.Plugins.DelftModels.HydroModel.Gui project
    Former-commit-id: 81d51fc579a6a866716ad61bca2a09db9a640b01
    Former-commit-id: 447f22a417ec6b992de75500c62d5e8059ed61a2

--- src/DeltaShell.Plugins.DelftModels.HydroModel.Gui/Forms/HydroModelSettings.cs
+++ src/DeltaShell.Plugins.DelftModels.HydroModel.Gui/Forms/HydroModelSettings.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
 using DelftTools.Controls;
 using DelftTools.Shell.Core.Workflow;
 using DelftTools.Shell.Gui;
@@ -11,9 +13,7 @@ using DelftTools.Utils.Collections;
 using DelftTools.Utils.Editing;
 using GeoAPI.Extensions.Feature;
 using SharpMap.Api.Layers;
-using Image = System.Drawing.Image;
 using MessageBox = DelftTools.Controls.Swf.MessageBox;
-using UserControl = System.Windows.Forms.UserControl;
 
 namespace DeltaShell.Plugins.DelftModels.HydroModel.Gui.Forms
 {
--- src/DeltaShell.Plugins.DelftModels.HydroModel.Gui/Views/HydroModelTimeSettingsView.xaml.cs
+++ src/DeltaShell.Plugins.DelftModels.HydroModel.Gui/Views/HydroModelTimeSettingsView.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Windows.Controls;
-using DelftTools.Shell.Core.Workflow;
-
-namespace DeltaShell.Plugins.DelftModels.HydroModel.Gui.Views
+﻿namespace DeltaShell.Plugins.DelftModels.HydroModel.Gui.Views
 {
     /// <summary>
     /// Interaction logic for HydroModelTimeSettingsView.xaml
