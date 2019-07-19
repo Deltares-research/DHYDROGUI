e2ec508a5c8cfcbacc65b3f28b98100526163806
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2019-07-19 05:52:00 +0000

    Reverted revision(s) 45005 from branches/FeatureBranches/NGHS_Delft3D_Beheer_en_Onderhoud: D3DFMIQ-922 Removed unused classes ........
    Former-commit-id: 69eebcac67d7dd046c7c50362c7d916fd5eec8d8
    Former-commit-id: f42bf8469c973693192d833744fb7514c98bf8cf

---
+++ src/DelftTools.Hydro/CrossSections/ICrossSectionLocation.cs
@@ -0,0 +1,17 @@
+﻿using DelftTools.Utils;
+
+namespace DelftTools.Hydro.CrossSections
+{
+    public interface ICrossSectionLocation : INameable
+    {
+        string BranchName { get; set; }
+
+        double Chainage { get; set; }
+
+        double Shift { get; set; }
+
+        string Definition { get; set; }
+
+        string LongName { get; set; }
+    }
+}
