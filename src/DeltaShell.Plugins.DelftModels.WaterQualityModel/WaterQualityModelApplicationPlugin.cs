cdcdc57cc714dcdd889fe034f3e77c419d70aa3f
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2019-07-19 15:09:59 +0000

    D3DFMIQ-1191 Paths of model directory and data table manager updated after sav (waq)
    Former-commit-id: f7fdfc0034e81bb20433612e2b82816ce35b4a8f
    Former-commit-id: 58708d7fcfd5952f28a96f35dc495e9d9c8e9cf5

--- src/DeltaShell.Plugins.DelftModels.WaterQualityModel/WaterQualityModelApplicationPlugin.cs
+++ src/DeltaShell.Plugins.DelftModels.WaterQualityModel/WaterQualityModelApplicationPlugin.cs
@@ -107,9 +107,14 @@ namespace DeltaShell.Plugins.DelftModels.WaterQualityModel
                 return;
             }
 
-            IEnumerable<WaterQualityModel> allWaqModels =
-                Application.GetAllModelsInProject().OfType<WaterQualityModel>();
-            allWaqModels.ForEach(m => m.SetEnableMarkOutputOutOfSync(true));
+            IEnumerable<WaterQualityModel> allWaqModels = Application
+                                                          .GetAllModelsInProject()
+                                                          .OfType<WaterQualityModel>();
+            allWaqModels.ForEach(m =>
+            {
+                m.SetEnableMarkOutputOutOfSync(true);
+                m.SetupModelDataFolderStructure(Application.ProjectDataDirectory);
+            });
         }
 
         private void Application_ProjectSaving(Project obj)
