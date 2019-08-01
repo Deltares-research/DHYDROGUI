435db5bec278a79af33ec4d3439f437b49c5643f
kahmann <kahmann@deltares.nl>
2019-08-01 15:26:04 +0000

    D3DFMIQ-1253: Extended tests to cover all the lines for clearing model output from FM-models. Former-commit-id: 3a2762155ede50170f39d33a6ebcc503f2cc3bf7 Former-commit-id: c0f54937750dd6f12afd55e06318b123a060503e

--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/Model/WaterFlowFMModelTest.Output.Data.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/Model/WaterFlowFMModelTest.Output.Data.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using DelftTools.TestUtils;
 using DelftTools.Utils.IO;
+using DeltaShell.NGHS.IO.TestUtils;
 using DeltaShell.Plugins.FMSuite.FlowFM.Model;
 using NUnit.Framework;
 
@@ -15,19 +16,24 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.Model
         public void ClearOutput_WithFunctionsStores_ThenStoresAreClearedAndRelatedFilesRemoved()
         {
             string testDataDirectory = TestHelper.GetTestFilePath(@"Model\Output\Data\FlowFM");
-            string testDirectory = TestHelper.CreateLocalCopy(testDataDirectory);
-            string mduFilePath = Path.Combine(testDirectory, "input", "FlowFM.mdu");
-            try
+            using (var tempDirectory = new TemporaryDirectory())
             {
+                // Setup
+                FileUtils.CopyDirectory(testDataDirectory, tempDirectory.Path);
+                string mduFilePath = Path.Combine(tempDirectory.Path, "input", "FlowFM.mdu");
+
                 var waterFlowFmModel = new WaterFlowFMModel(mduFilePath);
                 string hisFilePath = waterFlowFmModel.OutputHisFileStore.Path;
                 string mapFilePath = waterFlowFmModel.OutputMapFileStore.Path;
+                string classMapFilePath = waterFlowFmModel.OutputClassMapFileStore.Path;
 
                 // Pre-condition
                 Assert.That(waterFlowFmModel.OutputHisFileStore, Is.Not.Null);
                 Assert.That(File.Exists(hisFilePath), Is.True);
                 Assert.That(waterFlowFmModel.OutputMapFileStore, Is.Not.Null);
                 Assert.That(File.Exists(mapFilePath), Is.True);
+                Assert.That(waterFlowFmModel.OutputClassMapFileStore, Is.Not.Null);
+                Assert.That(File.Exists(classMapFilePath), Is.True);
 
                 // Call
                 waterFlowFmModel.ClearOutput();
@@ -37,10 +43,8 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.Model
                 Assert.That(File.Exists(hisFilePath), Is.False);
                 Assert.That(waterFlowFmModel.OutputMapFileStore, Is.Null);
                 Assert.That(File.Exists(mapFilePath), Is.False);
-            }
-            finally
-            {
-                FileUtils.DeleteIfExists(testDirectory);
+                Assert.That(waterFlowFmModel.OutputClassMapFileStore, Is.Null);
+                Assert.That(File.Exists(classMapFilePath), Is.False);
             }
         }
 
@@ -49,42 +53,47 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.Model
         public void ClearOutput_WithFunctionStoresThatHaveLockedFiles_ThenStoreIsClearedButFilesAreNotRemoved()
         {
             string testDataDirectory = TestHelper.GetTestFilePath(@"Model\Output\Data\FlowFM");
-            string testDirectory = TestHelper.CreateLocalCopy(testDataDirectory);
-            string mduFilePath = Path.Combine(testDirectory, "input", "FlowFM.mdu");
-            try
+            using (var tempDirectory = new TemporaryDirectory())
             {
+                // Setup
+                FileUtils.CopyDirectory(testDataDirectory, tempDirectory.Path);
+                string mduFilePath = Path.Combine(tempDirectory.Path, "input", "FlowFM.mdu");
+
                 var waterFlowFmModel = new WaterFlowFMModel(mduFilePath);
                 string hisFilePath = waterFlowFmModel.OutputHisFileStore.Path;
                 string mapFilePath = waterFlowFmModel.OutputMapFileStore.Path;
+                string classMapFilePath = waterFlowFmModel.OutputClassMapFileStore.Path;
 
                 // Pre-condition
                 Assert.That(waterFlowFmModel.OutputHisFileStore, Is.Not.Null);
                 Assert.That(File.Exists(hisFilePath), Is.True);
                 Assert.That(waterFlowFmModel.OutputMapFileStore, Is.Not.Null);
                 Assert.That(File.Exists(mapFilePath), Is.True);
+                Assert.That(waterFlowFmModel.OutputClassMapFileStore, Is.Not.Null);
+                Assert.That(File.Exists(classMapFilePath), Is.True);
 
                 using (File.Open(hisFilePath, FileMode.Open))
                 using (File.Open(mapFilePath, FileMode.Open))
+                using (File.Open(classMapFilePath, FileMode.Open))
                 {
                     // Call
-                    void Call () => waterFlowFmModel.ClearOutput();
+                    void Call() => waterFlowFmModel.ClearOutput();
 
                     // Assert
-                    string[] logMessages = new[]
+                    string[] logMessages =
                     {
                         $"Unable to remove output file '{hisFilePath}':{Environment.NewLine}{$@"The process cannot access the file '{hisFilePath}' because it is being used by another process."}",
-                        $"Unable to remove output file '{mapFilePath}':{Environment.NewLine}{$@"The process cannot access the file '{mapFilePath}' because it is being used by another process."}"
+                        $"Unable to remove output file '{mapFilePath}':{Environment.NewLine}{$@"The process cannot access the file '{mapFilePath}' because it is being used by another process."}",
+                        $"Unable to remove output file '{classMapFilePath}':{Environment.NewLine}{$@"The process cannot access the file '{classMapFilePath}' because it is being used by another process."}"
                     };
-                    TestHelper.AssertLogMessagesAreGenerated(Call, logMessages, 2);
+                    TestHelper.AssertLogMessagesAreGenerated(Call, logMessages, 3);
                     Assert.That(waterFlowFmModel.OutputHisFileStore, Is.Null);
                     Assert.That(File.Exists(hisFilePath), Is.True);
                     Assert.That(waterFlowFmModel.OutputMapFileStore, Is.Null);
                     Assert.That(File.Exists(mapFilePath), Is.True);
+                    Assert.That(waterFlowFmModel.OutputClassMapFileStore, Is.Null);
+                    Assert.That(File.Exists(classMapFilePath), Is.True);
                 }
-            }
-            finally
-            {
-                FileUtils.DeleteIfExists(testDirectory);
             }
         }
     }
