7f89c2e1f7af55e90f8bded4d2575cf58a3042f0
Carles Soriano Perez <Carles.SorianoPerez@deltares.nl>
2019-08-02 09:08:27 +0000

    DELFT3DFM-1117 Merge from development branch.
    Former-commit-id: b895e12db89e3d931bf8f713c3d9ac5b64019896
    Former-commit-id: 1644a70544635e958bb6274b08efaffd3aa68ea1

--- src/DeltaShell.Plugins.FMSuite.FlowFM/Properties/Resources.Designer.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1315,6 +1315,16 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Properties {
         public static string WaterFlowFMModelDefinitionValidator_Validate_ {
             get {
                 return ResourceManager.GetString("WaterFlowFMModelDefinitionValidator_Validate_", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bed level locations should be set to &apos;faces&apos; when morphology is active..
+        /// </summary>
+        public static string WaterFlowFMModelDefinitionValidator_Validate_Bed_level_locations_should_be_set_to__faces__when_morphology_is_active_ {
+            get {
+                return ResourceManager.GetString("WaterFlowFMModelDefinitionValidator_Validate_Bed_level_locations_should_be_set_to" +
+                        "__faces__when_morphology_is_active_", resourceCulture);
             }
         }
         
