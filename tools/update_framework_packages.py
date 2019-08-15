6644d65ad4836367d44075fc2aee755269978718
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2019-08-15 14:08:35 +0000

    DSFRAME-621 - cleaned up file and fixed encoding bug
    Former-commit-id: 014edc3301858ff4a1f240c1008ffe5b0f69406b
    Former-commit-id: 1641966bb114d8accd49f6f79171c08cc18a8f73

--- tools/update_framework_packages.py
+++ tools/update_framework_packages.py
@@ -3,27 +3,52 @@ from pathlib import Path
 import re
 import argparse
 
-def get_args(): 
+
+def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("svn_root_path", help="Path to the root of the SVN directory")
     parser.add_argument("version_number", help="Version number of the Framework NuGet package")
     return parser.parse_args()
 
-def searchfiles(extension, folder):
+
+def search_files(extension, folder):
     for r, d, f in os.walk(folder):
         for file in f:
             if file.endswith(extension):
                 yield Path(r) / Path(file)
 
-if __name__ == "__main__":
-    args = get_args()
-    root_path = args.svn_root_path
-    version_number = args.version_number
 
-    config_file_paths = list(searchfiles('packages.config', root_path))
+def update_project_files(root):
+    _encoding = "utf-8-sig"
+    project_file_paths = list(search_files('.csproj', root))
+
+    for project_file_path in project_file_paths:
+        print("Updating " + str(project_file_path))
+
+        with project_file_path.open(encoding=_encoding) as f:
+            lines = f.readlines()
+
+        for i in range(len(lines)):
+            lines[i] = re.sub(r'DeltaShell\.Framework\.1\.4\.0\.\d{5}',
+                              "DeltaShell.Framework.1.4.0." + version_number, lines[i])
+            lines[i] = re.sub(r'DeltaShell\.TestProject\.1\.4\.0\.\d{5}',
+                              "DeltaShell.TestProject.1.4.0." + version_number, lines[i])
+            lines[i] = re.sub(r'Version=1\.4\.0\.\d{5}',
+                              "Version=1.4.0." + version_number, lines[i])
+
+        with project_file_path.open(mode='w', encoding=_encoding) as f:
+            f.writelines(lines)
+
+
+def update_config_files(root):
+    config_file_paths = list(search_files('packages.config', root))
+
     for config_file_path in config_file_paths:
+        print("Updating " + str(config_file_path))
+
         with config_file_path.open() as f:
             lines = f.readlines()
+
         for i in range(len(lines)):
             lines[i] = re.sub(r'"DeltaShell\.ApplicationPlugin" version="1\.4\.0\.\d{5}"',
                               '"DeltaShell.ApplicationPlugin" version="1.4.0.' + version_number + '"', lines[i])
@@ -31,20 +56,15 @@ if __name__ == "__main__":
                               '"DeltaShell.Framework" version="1.4.0.' + version_number + '"', lines[i])
             lines[i] = re.sub(r'"DeltaShell\.TestProject" version="1\.4\.0\.\d{5}"',
                               '"DeltaShell.TestProject" version="1.4.0.' + version_number + '"', lines[i])
-            with config_file_path.open(mode='w') as f:
-                f.writelines(lines)
 
-    project_file_paths = list(searchfiles('.csproj', root_path))
-    for project_file_path in project_file_paths:
-        print(project_file_path)
-        with project_file_path.open(encoding="utf-8-sig") as f:
-            lines = f.readlines()
-        for i in range(len(lines)):
-            lines[i] = re.sub(r'DeltaShell\.Framework\.1\.4\.0\.\d{5}',
-                                "DeltaShell.Framework.1.4.0." + version_number, lines[i])
-            lines[i] = re.sub(r'DeltaShell\.TestProject\.1\.4\.0\.\d{5}',
-                                "DeltaShell.TestProject.1.4.0." + version_number, lines[i])
-            lines[i] = re.sub(r'Version=1\.4\.0\.\d{5}',
-                                "Version=1.4.0." + version_number, lines[i])
-        with project_file_path.open(mode='w') as f:
+        with config_file_path.open(mode='w') as f:
             f.writelines(lines)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    root_path = args.svn_root_path
+    version_number = args.version_number
+
+    update_project_files(root_path)
+    update_config_files(root_path)
