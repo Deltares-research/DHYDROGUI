c5379f2a852d0b0332c2fb1a3fd3106274a7dd32
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2019-09-02 06:33:56 +0000

    D3DFMIQ-1337 Fixed test
    Former-commit-id: 337c0075b2ae06851620e46157bc64d9c032964a
    Former-commit-id: 6a049d0650e526053d0367346fac6455fdac55e0

--- test/DeltaShell.Plugins.DelftModels.WaterQualityModel.Tests/IO/InputFileExporterTest.cs
+++ test/DeltaShell.Plugins.DelftModels.WaterQualityModel.Tests/IO/InputFileExporterTest.cs
@@ -1,5 +1,7 @@
-﻿using DelftTools.TestUtils;
+﻿using System.IO;
+using DelftTools.TestUtils;
 using DelftTools.Utils.Reflection;
+using DeltaShell.NGHS.IO.TestUtils;
 using DeltaShell.Plugins.DelftModels.WaterQualityModel.DataObjects.SubstanceProcessLibrary;
 using DeltaShell.Plugins.DelftModels.WaterQualityModel.IO;
 using NUnit.Framework;
@@ -29,8 +31,14 @@ namespace DeltaShell.Plugins.DelftModels.WaterQualityModel.Tests.IO
             // Make model valid by adding a substance to the SubstanceProcessLibrary (at least one needs to be present)
             waqModel.SubstanceProcessLibrary.Substances.Add(new WaterQualitySubstance { Name = "Test substance" });
 
+            using (var tempDirectory = new TemporaryDirectory())
+            {
+                string hydFilePath = Path.Combine(tempDirectory.Path, "file.hyd");
+                File.WriteAllText(hydFilePath, "");
+                hydroData.Stub(d => d.FilePath).Return(hydFilePath);
 
-            TestHelper.AssertLogMessageIsGenerated(() => inputFileExporter.Export(waqModel, "aaa\\test.inc"), "Could not find directory 'aaa'");
+                TestHelper.AssertLogMessageIsGenerated(() => inputFileExporter.Export(waqModel, "aaa\\test.inc"), "Could not find directory 'aaa'");
+            }
         }
     }
 }
