53c19174983fdda264cca51cba8c0e8d6938a0bc
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2019-09-03 08:30:21 +0000

    Corrected version numbers that were changed during a Framework update.
    Former-commit-id: f5c49a6d84690f31ed8586bd530cf3ad83bfa0ea
    Former-commit-id: ecb665a56532952cde85282af4d6e491ed7f9a6d

--- AssemblyInfo.Version.Sobek.cs
+++ AssemblyInfo.Version.Sobek.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.7.17.45647")]
-[assembly: AssemblyFileVersion("3.7.17.45647")]
+[assembly: AssemblyVersion("3.7.17.0")]
+[assembly: AssemblyFileVersion("3.7.17.0")]
