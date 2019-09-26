16a634fda6d760555b30688fff2e316d1774d21c
kahmann <kahmann@deltares.nl>
2019-09-26 09:31:04 +0000

    D3DFMIQ-343: Added unit tests for UnstrucGridOperationApi. Former-commit-id: e997f1f7aaecf070247c7b8a4fb1793ad15eee1d Former-commit-id: 20856f75f904ccff0bcfe21e1c6f122f24eb6bd9

--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/Api/UnstrucGridOperationApiTests.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/Api/UnstrucGridOperationApiTests.cs
@@ -16,6 +16,7 @@ using GeoAPI.Geometries;
 using NetTopologySuite.Extensions.Features;
 using NetTopologySuite.Geometries;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.Api
 {
@@ -483,6 +484,99 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.Api
             }
         }
         #endregion
+
+        [Test]
+        public void SnapsToGrid_WithGeometryNull_ThenReturnFalse()
+        {
+            // Setup
+            using (var fmModel = new WaterFlowFMModel())
+            {
+                var api = new UnstrucGridOperationApi(fmModel);
+
+                // Call
+                bool snapsToGrid = api.SnapsToGrid(null);
+
+                // Assert
+                Assert.That(snapsToGrid, Is.False);
+            }
+        }
+
+        [Test]
+        public void SnapsToGrid_WithFlexibleMeshModelApiReturningGeometryValues_ThenReturnTrue()
+        {
+            // Setup
+            var doubleValues = new double[0];
+            var intValues = new int[0];
+            var meshApi = MockRepository.GenerateMock<IFlexibleMeshModelApi>();
+            meshApi.Expect(a => a.GetSnappedFeature(string.Empty, null, null, ref doubleValues, ref doubleValues, ref intValues))
+                   .IgnoreArguments()
+                   .OutRef(
+                       new[] { 0.0, 1.0, 1.0, -999.0 },
+                       new[] { 0.0, 0.0, 1.0, -999.0 },
+                       new[] { 1, 1, 1, 0 }
+                       )
+                   .Return(true);
+
+            var coordinates = new[]
+            {
+                new Coordinate(0.0, 0.0),
+                new Coordinate(1.0, 1.0)
+            };
+            var geometry = MockRepository.GenerateMock<IGeometry>();
+            geometry.Expect(g => g.Coordinates).Return(coordinates).Repeat.Times(2);
+
+            var api = new UnstrucGridOperationApi(meshApi);
+
+            // Call
+            bool snapsToGrid = api.SnapsToGrid(geometry);
+
+            // Assert
+            Assert.That(snapsToGrid, Is.True);
+        }
+
+        [Test]
+        public void GetGridSnappedGeometry_WithFlexibleMeshModelApiNull_ThenReturnGeometries()
+        {
+            // Setup
+            var geometry = MockRepository.GenerateMock<IGeometry>();
+            var api = new UnstrucGridOperationApi(null);
+
+            // Call
+            IGeometry returnedGeometry = api.GetGridSnappedGeometry(string.Empty, geometry);
+
+            // Assert
+            Assert.That(returnedGeometry, Is.SameAs(geometry));
+        }
+
+        [Test]
+        public void GetGridSnappedGeometry_WithFeatureTypeEqualToNoSnapping_ThenReturnGeometries()
+        {
+            // Setup
+            var meshApi = MockRepository.GenerateMock<IFlexibleMeshModelApi>();
+            var geometry = MockRepository.GenerateMock<IGeometry>();
+            var api = new UnstrucGridOperationApi(meshApi);
+
+            // Call
+            IGeometry returnedGeometry = api.GetGridSnappedGeometry("no_snap", geometry);
+
+            // Assert
+            Assert.That(returnedGeometry, Is.SameAs(geometry));
+        }
+
+        [Test]
+        public void GetGridSnappedGeometry_WithEmptyGeometryCollection_ThenReturnGeometries()
+        {
+            // Setup
+            var meshApi = MockRepository.GenerateMock<IFlexibleMeshModelApi>();
+            var geometry = new List<IGeometry>();
+            var api = new UnstrucGridOperationApi(meshApi);
+
+            // Call
+            IEnumerable<IGeometry> returnedGeometry = api.GetGridSnappedGeometry(string.Empty, geometry);
+
+            // Assert
+            Assert.That(returnedGeometry, Is.SameAs(geometry));
+        }
     }
 }
 
