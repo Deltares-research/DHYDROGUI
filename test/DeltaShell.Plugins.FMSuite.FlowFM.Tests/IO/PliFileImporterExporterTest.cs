d0513961741fd689ab1435bfe3c39f48c8f4c347
Hidde Elzinga <hidde.elzinga@deltares.nl>
2019-10-18 08:21:43 +0000

    Increased time limit to improve stability on buildserver
    Former-commit-id: a8b194abb96ff4bfedee3d2d3fc1f54d4752c21c
    Former-commit-id: 59578fef0c0a669d883957b5fca0b13ed43a1145

--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/PliFileImporterExporterTest.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/PliFileImporterExporterTest.cs
@@ -128,7 +128,7 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.IO
 
                 // import the same set twice to include duplicate checking for all items
                 TestHelper.AssertIsFasterThan(
-                    13000,
+                    20000,
                     () => importer.ImportItem(TestHelper.GetTestFilePath("structures\\testBas2FM_fxw.pliz"),
                                               model.Area.FixedWeirs), rankHddAccess: false, warmUp: true);
                 Assert.AreEqual(19459, model.Area.FixedWeirs.Count);
