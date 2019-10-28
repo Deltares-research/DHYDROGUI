f2b675d196fd6f63987bc568a8680c19a31115fe
kahmann <kahmann@deltares.nl>
2019-10-28 14:43:35 +0000

    D3DFMIQ-1462: Multiple-valued properties that are defined on three or more lines in mdu files (separated by a '\') can now be read with MduDelftIniReader. Former-commit-id: b68cddf5194468fba9b36b01a7f4b4f91ed68a79 Former-commit-id: 51228b407605295bef73b7a55d69e6f6428a5645

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/DelftIniReaders/MduDelftIniReader.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/DelftIniReaders/MduDelftIniReader.cs
@@ -12,14 +12,30 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.Readers
             @"#\s*" +                   // '#'-sign with whitespaces
             @"((?<comment>.*))?\z";     // comment, every character until the end of the line
 
+        private const string ValueSlashPattern =
+            @"^\s*" +
+            @"(?<value>[^(\)]*)" +
+            @"\\+\z";
+
         protected override string[] GetKeyValueComment(string line)
         {
             string[] keyValueComment = base.GetKeyValueComment(line);
             if (keyValueComment[1].EndsWith(@"\"))
             {
-                line = GetNextLine();
-                MatchCollection matches = RegularExpression.GetMatches(ValueCommentPattern, line);
+                while ((line = GetNextLine()) != null)
+                {
+                    MatchCollection matchesValueSlash = RegularExpression.GetMatches(ValueSlashPattern, line);
+                    if (matchesValueSlash.Count > 0)
+                    {
+                        keyValueComment[1] = string.Join(" ", keyValueComment[1].TrimEnd('\\', ' '), matchesValueSlash[0].Groups["value"].Value.TrimEnd('\\', ' '));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
 
+                MatchCollection matches = RegularExpression.GetMatches(ValueCommentPattern, line);
                 if (matches.Count > 0)
                 {
                     keyValueComment[1] = string.Join(" ", keyValueComment[1].TrimEnd('\\', ' '), matches[0].Groups["value"].Value.Trim());
--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/Readers/MduDelftIniReaderTest.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/Readers/MduDelftIniReaderTest.cs
@@ -12,12 +12,39 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.IO.Readers
     public class MduDelftIniReaderTest
     {
         [Test]
-        public void ReadDelftIniFile_WithMultipleValuedPropertyDefinedOnMultipleLines_ThenPropertyIsReadCorrectly()
+        public void ReadDelftIniFile_WithMultipleValuedPropertyDefinedOnTwoLines_ThenPropertyIsReadCorrectly()
         {
             // Setup
             string fileContent = "[output]"
                                  + Environment.NewLine
                                  + @"ObsFile  = obs_1_obs.xyn obs_2_obs.xyn \"
+                                 + Environment.NewLine
+                                 + "obs_3_obs.xyn  # My comment";
+
+            var stream = new MemoryStream(Encoding.ASCII.GetBytes(fileContent));
+            var reader = new MduDelftIniReader();
+
+            // Call
+            DelftIniCategory category = reader.ReadDelftIniFile(stream, "myFilePath").Single();
+
+            // Assert
+            Assert.That(category.Name, Is.EqualTo("output"));
+
+            DelftIniProperty property = category.Properties.Single();
+            Assert.That(property.Name, Is.EqualTo("ObsFile"));
+            Assert.That(property.Value, Is.EqualTo("obs_1_obs.xyn obs_2_obs.xyn obs_3_obs.xyn"));
+            Assert.That(property.Comment, Is.EqualTo("My comment"));
+        }
+
+        [Test]
+        public void ReadDelftIniFile_WithMultipleValuedPropertyDefinedOnThreeLines_ThenPropertyIsReadCorrectly()
+        {
+            // Setup
+            string fileContent = "[output]"
+                                 + Environment.NewLine
+                                 + @"ObsFile  = obs_1_obs.xyn \"
+                                 + Environment.NewLine
+                                 + @"obs_2_obs.xyn \"
                                  + Environment.NewLine
                                  + "obs_3_obs.xyn  # My comment";
 
