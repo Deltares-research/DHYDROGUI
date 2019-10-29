bb14e88ab881554c2ba6d546ea181027f4703640
kahmann <kahmann@deltares.nl>
2019-10-29 14:39:41 +0000

    D3DFMIQ-1466: Removed redundant using statements from BoundaryDataWizardPage and LoadsDataWizard. Former-commit-id: c8b1d4b2150344bddaf1d7586ed1f39ee2e288b5 Former-commit-id: dcae1cd79de2a66eea379c4e56a0d327f1d9d939

--- src/DeltaShell.Plugins.DelftModels.WaterQualityModel.Gui/Forms/WaterQualityModelWizard/LoadsDataWizard.cs
+++ src/DeltaShell.Plugins.DelftModels.WaterQualityModel.Gui/Forms/WaterQualityModelWizard/LoadsDataWizard.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DelftTools.Controls.Swf;
+﻿using DelftTools.Controls.Swf;
 using DelftTools.Shell.Gui;
 using DeltaShell.Plugins.DelftModels.WaterQualityModel.Gui.Properties;
 using DeltaShell.Plugins.DelftModels.WaterQualityModel.IO;
