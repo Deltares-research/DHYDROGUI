2902ecefe9529f806010597eef469d8068b43e85
Ralph Peelen <ralph.peelen@deltares.nl>
2019-10-30 09:54:37 +0000

    FPT-97 FPT-99 Added boundary conditions on nodes and laterals on pipes/manholes in fm.
    Former-commit-id: 0f62ddd55b9ec444a3da712816cd0b0a9099cfd9
    Former-commit-id: f3425e7fa8cd173ecea1ff44cdbf1396b5ec4a40

---
+++ src/DeltaShell.NGHS.IO/DataObjects/Helper1D.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DelftTools.Functions;
+using GeoAPI.Extensions.Networks;
+
+namespace DeltaShell.NGHS.IO.DataObjects
+{
+    public enum BoundaryType
+    {
+        None = 0,
+        Level = 1,
+        Discharge,
+        Salinity,
+        WindVelocity,
+        WindDirection,
+        AirTemperature,
+        RelativeHumidity,
+        Cloudiness
+    }
+    /// <summary>
+    /// Provides xml serialization of WaterFlowModel1d
+    /// </summary>
+    public static class Helper1D
+    {
+        public static Model1DBoundaryNodeData CreateDefaultBoundaryCondition(INode node, bool useSalt, bool useTemperature)
+        {
+            var bc = new Model1DBoundaryNodeData
+            {
+                Feature = node,
+                DataType = Model1DBoundaryNodeDataType.None,
+                UseSalt = useSalt,
+                UseTemperature = useTemperature
+            };
+
+            return bc;
+        }
+
+        public static BoundaryType GetBoundaryType(Model1DBoundaryNodeData boundaryNodeData)
+        {
+            switch (boundaryNodeData.DataType)
+            {
+                case Model1DBoundaryNodeDataType.WaterLevelTimeSeries:
+                    return BoundaryType.Level;
+                case Model1DBoundaryNodeDataType.FlowTimeSeries:
+                    return BoundaryType.Discharge;
+                case Model1DBoundaryNodeDataType.FlowConstant:
+                    return BoundaryType.Discharge;
+                case Model1DBoundaryNodeDataType.WaterLevelConstant:
+                    return BoundaryType.Level;
+                case Model1DBoundaryNodeDataType.FlowWaterLevelTable:
+                    // SOBEK3-1035
+                    if (boundaryNodeData.Data != null &&
+                        boundaryNodeData.Data.Components.Any() &&
+                        boundaryNodeData.Data.Components[0].ValueType == typeof(double) &&
+                        boundaryNodeData.Data.Components[0].Values.Count > 0 &&
+                        (double)boundaryNodeData.Data.Components[0].MinValue <= 0 &&
+                        (double)boundaryNodeData.Data.Components[0].MaxValue <= 0)
+                    {
+                        // if Q <= 0 return WaterLevel type
+                        return BoundaryType.Level;
+                    }
+                    // if Q >= 0 return Discharge type (returned by default)
+                    return BoundaryType.Discharge;
+                default:
+                    throw new ArgumentOutOfRangeException(string.Format("BoundaryNodeDataType {0} is not supported by the ModelApi", boundaryNodeData.DataType));
+            }
+        }
+
+        public static IEnumerable<Model1DBoundaryNodeDataType> GetTimeSeriesDataTypes(IFunction series)
+        {
+            var isDischargeSeries = false;
+
+            foreach (var component in series.Components)
+            {
+                if (component.Attributes != null)
+                {
+                    string dischargeName;
+                    if (component.Attributes.TryGetValue(FunctionAttributes.StandardName,
+                        out dischargeName))
+                    {
+                        isDischargeSeries = (dischargeName == FunctionAttributes.StandardNames.WaterDischarge);
+                    }
+                }
+                yield return isDischargeSeries
+                    ? Model1DBoundaryNodeDataType.FlowTimeSeries
+                    : Model1DBoundaryNodeDataType.WaterLevelTimeSeries;
+            }
+        }
+
+    }
+}
