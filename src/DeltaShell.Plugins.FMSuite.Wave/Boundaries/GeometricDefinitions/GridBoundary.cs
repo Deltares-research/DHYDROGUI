37f4993da4d57d0795b9096fdb689b459d8b440f
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-10-31 13:58:50 +0000

    D3DFMIQ-1477 - Add initial GridBoundary.
    Former-commit-id: 95b6a44ed31e4e2cf9668ffd868a4d5f95aea277
    Former-commit-id: cf2e3aa8571b9c6a15e7c0af2efa0a5f917af755

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/GridBoundary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using GeoAPI.Extensions.Coverages;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
+{
+    /// <summary>
+    /// <see cref="GridBoundary"/> defines the grid boundaries given a grid.
+    /// It further provides several convenience functions to ease the difficulty
+    /// working with the boundaries of a grid.
+    /// </summary>
+    public class GridBoundary
+    {
+        private readonly IDiscreteGridPointCoverage observedGrid;
+        private readonly IDictionary<GridSide, IReadOnlyList<GridCoordinate>> boundaries;
+
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="GridBoundary"/>.
+        /// </summary>
+        /// <param name="grid">The grid.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when any argument is <c>null</c>.
+        /// </exception>
+        public GridBoundary(IDiscreteGridPointCoverage grid)
+        {
+            observedGrid = grid ?? throw new ArgumentNullException(nameof(grid));
+        }
+
+        /// <summary>
+        /// Get the set of <see cref="GridCoordinate"/> specifying the boundary
+        /// of the grid at the specified <paramref name="gridSide"/>.
+        /// </summary>
+        /// <value>
+        /// The <see cref="IReadOnlyList{GridCoordinate}"/> specifying the
+        /// boundary of the grid at the specified <paramref name="gridSide"/>.
+        /// </value>
+        /// <param name="gridSide">The grid side.</param>
+        /// <returns>
+        /// The set of <see cref="GridCoordinate"/> specifying the boundary of
+        /// the grid at the specified <paramref name="gridSide"/>.
+        /// </returns>
+        public IReadOnlyList<GridCoordinate> this[GridSide gridSide] => boundaries[gridSide];
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/GeometricDefinitions/GridBoundaryTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using GeoAPI.Extensions.Coverages;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.GeometricDefinitions
+{
+    [TestFixture]
+    public class GridBoundaryTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var grid = Substitute.For<IDiscreteGridPointCoverage>();
+
+            // Call
+            var gridBoundary = new GridBoundary(grid);
+
+            // Assert
+        }
+
+        [Test]
+        public void Constructor_GridNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new GridBoundary(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception, 
+                        Has.Property("ParamName").EqualTo("grid"));
+            
+        }
+    }
+}
