bba6a9ab3fc3e12ea6ec90b4fb675cc25b5c1c2f
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-10-31 16:22:51 +0000

    D3DFMIQ-1477 - Extend GridBoundary with boundary values.
    Former-commit-id: fb41f42c4bb865fb814c1f07d0175f418bf5cd8d
    Former-commit-id: fe93297ea7b6595463272ad195369016cf7322d6

--- src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/GridBoundary.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/GridBoundary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using GeoAPI.Extensions.Coverages;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
@@ -9,6 +10,19 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
     /// It further provides several convenience functions to ease the difficulty
     /// working with the boundaries of a grid.
     /// </summary>
+    /// <remarks>
+    /// The grid is assumed to be structured as follows.
+    ///
+    ///                    -p
+    ///   ( 0, Grid.NMax ) -- ( Grid.MMax, Grid.NMAX)
+    /// ^   |                          |
+    /// |   |                          | |
+    ///     |                          | v 
+    ///   ( 0,         0 ) -- ( Grid.NMax, 0 )
+    ///                    d-
+    ///
+    /// The coordinates are structured in a clock-wise fashion.
+    /// </remarks>
     public class GridBoundary
     {
         private readonly IDiscreteGridPointCoverage observedGrid;
@@ -22,9 +36,31 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
         /// <exception cref="System.ArgumentNullException">
         /// Thrown when any argument is <c>null</c>.
         /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the <paramref name="grid"/> has a <c>Size1</c> or
+        /// <c>Size2</c> of smaller than 2.
+        /// </exception>
         public GridBoundary(IDiscreteGridPointCoverage grid)
         {
-            observedGrid = grid ?? throw new ArgumentNullException(nameof(grid));
+            if (grid == null)
+            {
+                throw new ArgumentNullException(nameof(grid));
+
+            }
+
+            if (grid.Size1 < 2 || grid.Size2 < 2)
+            {
+                throw new ArgumentException($"{nameof(grid)} should contain at least 2 points in each dimension.");
+            }
+
+            observedGrid = grid;
+            boundaries = new Dictionary<GridSide, IReadOnlyList<GridCoordinate>>
+            {
+                {GridSide.West,  GetBoundaryAtSide(GridSide.West).ToList()  },
+                {GridSide.North, GetBoundaryAtSide(GridSide.North).ToList() },
+                {GridSide.East,  GetBoundaryAtSide(GridSide.East).ToList()  },
+                {GridSide.South, GetBoundaryAtSide(GridSide.South).ToList() },
+            };
         }
 
         /// <summary>
@@ -41,5 +77,47 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
         /// the grid at the specified <paramref name="gridSide"/>.
         /// </returns>
         public IReadOnlyList<GridCoordinate> this[GridSide gridSide] => boundaries[gridSide];
+
+        private IEnumerable<GridCoordinate> GetBoundaryAtSide(GridSide side)
+        {
+            int sizeM = observedGrid.Size1;
+            int sizeN = observedGrid.Size2;
+
+            int mMax = sizeM - 1;
+            int nMax = sizeN - 1;
+
+            IEnumerable<GridCoordinate> allCoordinates;
+            switch (side)
+            {
+                case GridSide.West:
+                    allCoordinates = Enumerable.Range(0, sizeN)
+                                               .Select(n => new GridCoordinate(0, n));
+                    break;
+                case GridSide.North:
+                    allCoordinates = Enumerable.Range(0, sizeM)
+                                               .Select(m => new GridCoordinate(m, nMax));
+                    break;
+                case GridSide.East:
+                    allCoordinates = Enumerable.Range(0, sizeN)
+                                               .Select(n => new GridCoordinate(mMax, nMax - n));
+                    break;
+                case GridSide.South:
+                    allCoordinates = Enumerable.Range(0, sizeM)
+                                               .Select(m => new GridCoordinate(mMax - m, 0));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(side), side, null);
+            }
+
+            return allCoordinates.Where(c => !IsDryPoint(c));
+        }
+
+        private bool IsDryPoint(GridCoordinate gridCoordinate)
+        {
+            double x = observedGrid.X.Values[gridCoordinate.X, gridCoordinate.Y];
+            double y = observedGrid.Y.Values[gridCoordinate.X, gridCoordinate.Y];
+
+            return WaveDomainHelper.IsDryPoint(x, y);
+        }
     }
 }
--- test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/GeometricDefinitions/GridBoundaryTest.cs
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/GeometricDefinitions/GridBoundaryTest.cs
@@ -13,12 +13,48 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.GeometricDefinitions
         public void Constructor_ExpectedValues()
         {
             // Setup
+            const int expectedX = 5;
+            const int expectedY = 6;
+
             var grid = Substitute.For<IDiscreteGridPointCoverage>();
+            grid.Size1.Returns(expectedX);
+            grid.Size2.Returns(expectedY);
 
             // Call
             var gridBoundary = new GridBoundary(grid);
 
             // Assert
+            Assert.That(gridBoundary[GridSide.West], Has.Count.EqualTo(expectedY), 
+                        $"Expected the west boundary to have {expectedY} values");
+            for (var i = 0; i < gridBoundary[GridSide.West].Count; i++)
+            {
+                Assert.That(gridBoundary[GridSide.West][i].X, Is.EqualTo(0));
+                Assert.That(gridBoundary[GridSide.West][i].Y, Is.EqualTo(i));
+            }
+
+            Assert.That(gridBoundary[GridSide.North], Has.Count.EqualTo(expectedX), 
+                        $"Expected the west boundary to have {expectedX} values");
+            for (var i = 0; i < gridBoundary[GridSide.North].Count; i++)
+            {
+                Assert.That(gridBoundary[GridSide.North][i].X, Is.EqualTo(i));
+                Assert.That(gridBoundary[GridSide.North][i].Y, Is.EqualTo(expectedY - 1));
+            }
+
+            Assert.That(gridBoundary[GridSide.East], Has.Count.EqualTo(expectedY), 
+                        $"Expected the west boundary to have {expectedY} values");
+            for (var i = 0; i < gridBoundary[GridSide.East].Count; i++)
+            {
+                Assert.That(gridBoundary[GridSide.East][i].X, Is.EqualTo(expectedX - 1));
+                Assert.That(gridBoundary[GridSide.East][i].Y, Is.EqualTo(expectedY - 1 - i));
+            }
+
+            Assert.That(gridBoundary[GridSide.South], Has.Count.EqualTo(expectedX), 
+                        $"Expected the west boundary to have {expectedX} values");
+            for (var i = 0; i < gridBoundary[GridSide.South].Count; i++)
+            {
+                Assert.That(gridBoundary[GridSide.South][i].X, Is.EqualTo(expectedX - 1 - i));
+                Assert.That(gridBoundary[GridSide.South][i].Y, Is.EqualTo(0));
+            }
         }
 
         [Test]
@@ -32,6 +68,29 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.GeometricDefinitions
             Assert.That(exception, 
                         Has.Property("ParamName").EqualTo("grid"));
             
+        }
+
+        [Test]
+        [TestCase(1, 2)]
+        [TestCase(2, 1)]
+        [TestCase(0, 2)]
+        [TestCase(2, 0)]
+        [TestCase(-1, 2)]
+        [TestCase(2, -1)]
+        public void Constructor_GridWithInvalidDimensions_ThrowsArgumentException(int xVal, int yVal)
+        {
+            // Setup
+            var grid = Substitute.For<IDiscreteGridPointCoverage>();
+            grid.Size1.Returns(xVal);
+            grid.Size2.Returns(yVal);
+
+            // Call
+            void Call() => new GridBoundary(grid);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentException>(Call);
+            Assert.That(exception, 
+                        Has.Message.EqualTo("grid should contain at least 2 points in each dimension."));
         }
     }
 }
