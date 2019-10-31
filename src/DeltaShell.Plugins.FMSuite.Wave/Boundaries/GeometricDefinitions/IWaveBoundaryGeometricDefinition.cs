0f594c7a7f314237c79246f67c49a9b988baeb87
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-10-31 12:22:09 +0000

    D3DFMIQ-1477 - Add IWaveBoundaryGeometricDefinition.
    Former-commit-id: d9aca0537329debcacb123cc48ac2c2edf294769
    Former-commit-id: f0b1d9b76cdab828098c887ca1f29f6b78ebb028

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/IWaveBoundaryGeometricDefinition.cs
@@ -0,0 +1,29 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
+{
+    /// <summary>
+    /// <see cref="IWaveBoundaryGeometricDefinition"/> defines the geometric
+    /// attributes of a <see cref="IWaveBoundary"/>.
+    /// </summary>
+    public interface IWaveBoundaryGeometricDefinition
+    {
+        /// <summary>
+        /// Get or set the index of the first <see cref="GridCoordinate"/>
+        /// of this <see cref="IWaveBoundary"/>.
+        /// </summary>
+        /// <value>
+        /// The index of the first <see cref="GridCoordinate"/> of this
+        /// <see cref="IWaveBoundary"/>.
+        /// </value>
+        int StartingIndex { get; set; }
+
+        /// <summary>
+        /// Get or set the index of the last <see cref="GridCoordinate"/>
+        /// of this <see cref="IWaveBoundary"/>.
+        /// </summary>
+        /// <value>
+        /// The index of the last <see cref="GridCoordinate"/> of this
+        /// <see cref="IWaveBoundary"/>.
+        /// </value>
+        int EndingIndex { get; set; }
+    }
+}
