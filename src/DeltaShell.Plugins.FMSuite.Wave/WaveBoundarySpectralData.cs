5c45ef3209b091e0794971956f8dfd89e13fabb3
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-10-31 10:58:53 +0000

    D3DFMIQ-1477 - Separate WavePeriodType from other classes. - Move WavePeriodType to appropriate location. - Update corresponding tests and usages.
    Former-commit-id: 729c49c64df443adcf82775a591d4faf98fc39e1
    Former-commit-id: b9466a2353445cb70439df7be049eb1897486ab6

--- src/DeltaShell.Plugins.FMSuite.Wave/WaveBoundarySpectralData.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/WaveBoundarySpectralData.cs
@@ -4,15 +4,6 @@ namespace DeltaShell.Plugins.FMSuite.Wave
 {
 
 
-    public enum WavePeriodType
-    {
-        [Description("Peak")]
-        Peak,
-
-        [Description("Mean")]
-        Mean
-    }
-
     public enum WaveDirectionalSpreadingType
     {
         [Description("Power")]
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/SpectralData/WavePeriodTypeTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using DeltaShell.NGHS.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.SpectralData;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.SpectralData
+{
+    [TestFixture]
+    public class WavePeriodTypeTest : EnumValuesTestFixture<WavePeriodType>
+    {
+        protected override IDictionary<WavePeriodType, int> ExpectedValueForEnumValues =>
+            new Dictionary<WavePeriodType, int>
+            {
+                { WavePeriodType.Peak, 0 },
+                { WavePeriodType.Mean, 1 },
+            };
+    }
+}
