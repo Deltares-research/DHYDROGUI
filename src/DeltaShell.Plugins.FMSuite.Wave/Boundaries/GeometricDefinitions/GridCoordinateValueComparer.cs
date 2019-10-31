c138084a21e46b779c6ab3c2c2d5691a07efeacd
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-10-31 20:20:59 +0000

    D3DFMIQ-1477 - Add GridCoordinateValueComparer.
    Former-commit-id: f68afa9e9a853d6ef29b5ac3b2b22b4e3a2781dd
    Former-commit-id: b4f4c025026b575d750c85f953425bafc106f83a

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/GridCoordinateValueComparer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
+{
+    /// <summary>
+    /// <see cref="GridCoordinateValueComparer"/> is used to compare grid coordinates
+    /// on equality of their X and Y members.
+    /// </summary>
+    /// <seealso cref="EqualityComparer{GridCoordinate}" />
+    public class GridCoordinateValueComparer : EqualityComparer<GridCoordinate>
+    {
+        public override bool Equals(GridCoordinate c1, GridCoordinate c2)
+        {
+            if (c1 == null && c2 == null)
+            {
+                return true;
+            }
+
+            if (c1 == null || c2 == null)
+            {
+                return false;
+            }
+
+            return c1.X == c2.X && c1.Y == c2.Y;
+        }
+
+        public override int GetHashCode(GridCoordinate coordinate)
+        {
+            if (coordinate == null)
+            {
+                return 0;
+            }
+
+            int hCode = coordinate.X ^ coordinate.Y;
+            return hCode.GetHashCode();
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/GeometricDefinitions/GridCoordinateValueComparerTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.GeometricDefinitions
+{
+    [TestFixture]
+    public class GridCoordinateValueComparerTest
+    {
+        [Test]
+        [TestCaseSource(nameof(EqualsValues))]
+        public void Equals_ReturnsCorrectValue(GridCoordinate coordinate1, GridCoordinate coordinate2, bool expectedValue)
+        {
+            // Setup
+            var equalityComparer = new GridCoordinateValueComparer();
+
+            // Call
+            bool result = equalityComparer.Equals(coordinate1, 
+                                                  coordinate2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedValue),
+                        $"Expected the comparison of {coordinate1} and {coordinate2} to be different:");
+        }
+
+        private static IEnumerable<TestCaseData> EqualsValues =>
+            new[]
+            {
+                new TestCaseData(null, null, true), 
+                new TestCaseData(new GridCoordinate(0, 0), null, false), 
+                new TestCaseData(null, new GridCoordinate(0, 0), false), 
+                new TestCaseData(new GridCoordinate(2, 2), new GridCoordinate(2, 2), true), 
+                new TestCaseData(new GridCoordinate(1, 2), new GridCoordinate(2, 2), false), 
+                new TestCaseData(new GridCoordinate(2, 1), new GridCoordinate(2, 2), false), 
+                new TestCaseData(new GridCoordinate(2, 2), new GridCoordinate(1, 2), false), 
+                new TestCaseData(new GridCoordinate(2, 2), new GridCoordinate(2, 1), false), 
+                new TestCaseData(new GridCoordinate(1, 1), new GridCoordinate(2, 2), false), 
+                new TestCaseData(new GridCoordinate(1, 2), new GridCoordinate(2, 1), false), 
+                new TestCaseData(new GridCoordinate(1, 1), new GridCoordinate(2, 1), false), 
+                new TestCaseData(new GridCoordinate(1, 1), new GridCoordinate(1, 2), false), 
+            };
+
+        [Test]
+        public void GetHashCode_ReturnsCorrectValue()
+        {
+            // Setup
+            var equalityComparer = new GridCoordinateValueComparer();
+            var coordinate = new GridCoordinate(5, 10);
+            const int expectedValue = 5 ^ 10;
+
+            // Call
+            int hashValue = equalityComparer.GetHashCode(coordinate);
+
+            // Assert
+            Assert.That(hashValue, Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        public void GetHashCode_NullValue_ReturnsZero()
+        {
+            // Setup
+            var equalityComparer = new GridCoordinateValueComparer();
+            
+            // Call
+            int hashValue = equalityComparer.GetHashCode(null);
+
+            // Assert
+            Assert.That(hashValue, Is.EqualTo(0));
+        }
+    }
+}
