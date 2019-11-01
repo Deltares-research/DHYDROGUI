418d379e200c07056609af63cdc7b9f54ad5bb5c
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-11-01 09:41:03 +0000

    D3DFMIQ-1482 - Add WaveLayerFactory - Move Creation of the model group layer to WaveLayerFactory
    Former-commit-id: 064f37163952f54439ed47a5538de0bdf81486be
    Former-commit-id: 63caed47575e2898fb80fb019b39f9d55b78a5d7

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Layers/WaveLayerFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Common.Layers;
+using SharpMap.Api.Layers;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Layers
+{
+    /// <summary>
+    /// <see cref="WaveLayerFactory"/> provides the methods to construct the
+    /// different layers of the wave model.
+    /// </summary>
+    public static class WaveLayerFactory
+    {
+        /// <summary>
+        /// Create a new model layer from the given <paramref name="waveModel"/>.
+        /// </summary>
+        /// <param name="waveModel">The wave model.</param>
+        /// <returns>A new <see cref="ILayer"/> containing teh model.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Throw when <paramref name="waveModel"/> is <c>null</c>.
+        /// </exception>
+        public static ILayer CreateModelGroupLayer(WaveModel waveModel)
+        {
+            if (waveModel == null)
+            {
+                throw new ArgumentNullException(nameof(waveModel));
+            }
+
+            return new ModelGroupLayer
+            {
+                Name = waveModel.Name,
+                Model = waveModel,
+            };
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Layers/WaveLayerFactoryTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Common.Layers;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Layers;
+using NUnit.Framework;
+using SharpMap.Api.Layers;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Layers
+{
+    [TestFixture]
+    public class WaveLayerFactoryTest
+    {
+        [Test]
+        public void CreateModelGroupLayer_ValidModel_ReturnsCorrectResults()
+        {
+            // Setup
+            var waveModel = new WaveModel();
+
+            // Call
+            ILayer layer = WaveLayerFactory.CreateModelGroupLayer(waveModel);
+
+            // Assert
+            Assert.That(layer, Is.InstanceOf<ModelGroupLayer>(), 
+                        $"Expected the result to be an instance of {nameof(ModelGroupLayer)}");
+
+            var modelGroupLayer = (ModelGroupLayer) layer;
+            Assert.That(modelGroupLayer.Model, Is.SameAs(waveModel),
+                        "Expected the model of the layer to the same as the provided model.");
+            Assert.That(modelGroupLayer.Name, Is.EqualTo(waveModel.Name),
+                        "Expected the name of the layer to the equal as the provided model name.");
+        }
+
+        [Test]
+        public void CreateModelGroupLayer_WaveModelNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => WaveLayerFactory.CreateModelGroupLayer(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception, Has.Property("ParamName").EqualTo("waveModel"));
+        }
+    }
+}
