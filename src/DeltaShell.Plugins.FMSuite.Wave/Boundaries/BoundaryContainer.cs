ccbf3a21bf7baa950434c048dd860456b782df9f
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-11-01 06:11:48 +0000

    D3DFMIQ-1477 - Add initial BoundaryContainer
    Former-commit-id: e4675c28ef0e18c46e66db9d52dbf65f06944271
    Former-commit-id: 66fc632c32f1c3e338464dc7c99235f3c538e941

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/BoundaryContainer.cs
@@ -0,0 +1,23 @@
+﻿using DelftTools.Utils.Collections.Generic;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries
+{
+    /// <summary>
+    /// <see cref="BoundaryContainer"/> is responsible for managing a set of
+    /// boundaries linked to the current outer grid.
+    /// </summary>
+    public class BoundaryContainer
+    {
+        /// <summary>
+        /// Get the boundaries.
+        /// </summary>
+        /// <value>
+        /// The boundaries.
+        /// </value>
+        /// <remarks>
+        /// This class is the logical owner of the <see cref="IWaveBoundary"/>
+        /// within the model it is part of.
+        /// </remarks>
+        public IEventedList<IWaveBoundary> Boundaries { get; } = new EventedList<IWaveBoundary>();
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/BoundaryContainerTest.cs
@@ -0,0 +1,20 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries
+{
+    [TestFixture]
+    public class BoundaryContainerTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var container = new BoundaryContainer();
+
+            // Assert
+            Assert.That(container.Boundaries, Is.Not.Null);
+            Assert.That(container.Boundaries, Is.Empty);
+        }
+    }
+}
