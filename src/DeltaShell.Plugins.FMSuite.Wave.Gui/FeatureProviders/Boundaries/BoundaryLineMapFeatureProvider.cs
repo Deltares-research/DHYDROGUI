51f0c5aeec10d4aa9dba59851f4bb1b1adaecd71
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-11-06 06:16:22 +0000

    D3DFMIQ-1482 - Add skeleton for BoundaryLineMapFeatureProvider
    Former-commit-id: de0bddca1f3d494cd5faec5f9d7f295853c982a2
    Former-commit-id: cff1d134804966567497aa1b7246083d1b66445c

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/BoundaryLineMapFeatureProvider.cs
@@ -0,0 +1,25 @@
+﻿using SharpMap.Data.Providers;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries
+{
+    /// <summary>
+    /// <see cref="BoundaryLineMapFeatureProvider"/> is responsible for showing
+    /// the appropriate boundary line features given a boundary container.
+    /// It provides the appropriate methods such that these features and their
+    /// underlying data can be created through the Map.
+    /// </summary>
+    /// <remarks>
+    /// This class leverages the <see cref="MultiIEventedListAdapter{TObserved,TDisplayed}"/>
+    /// to create the appropriate lists. These classes are necessary to play
+    /// nice with the framework, and ensure a good separation of concerns between
+    /// view and data. As such, this feature provider can be seen as a view model
+    /// for the line data for the Map.
+    /// </remarks>
+    public class BoundaryLineMapFeatureProvider : Feature2DCollection
+    {
+        public BoundaryLineMapFeatureProvider()
+        {
+
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/FeatureProviders/Boundaries/BoundaryLineMapFeatureProviderTest.cs
@@ -0,0 +1,16 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.FeatureProviders.Boundaries
+{
+    [TestFixture]
+    public class BoundaryLineMapFeatureProviderTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var featureProvider = new BoundaryLineMapFeatureProvider();
+        }
+    }
+}
