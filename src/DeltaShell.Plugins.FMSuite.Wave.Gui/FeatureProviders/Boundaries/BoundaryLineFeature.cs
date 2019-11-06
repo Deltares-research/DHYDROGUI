0df27714b3d8e4751aee5fc0ccde0cbb104584b8
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-11-06 08:09:27 +0000

    D3DFMIQ-1482 - Add skeleton for BoundaryLineFeature
    Former-commit-id: 2a009eba4895b8ea4e71a88c0ddea7a7207bc196
    Former-commit-id: 120d641dc76b4ad0f2339b5c0acd3319c2dfa32d

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/BoundaryLineFeature.cs
@@ -0,0 +1,28 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using GeoAPI.Geometries;
+using NetTopologySuite.Extensions.Features;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries
+{
+    /// <summary>
+    /// <see cref="BoundaryLineFeature"/> describes the line of a <see cref="IWaveBoundary"/>
+    /// as a <see cref="Feature2D"/>.
+    /// </summary>
+    public class BoundaryLineFeature : Feature2D
+    {
+        /// <summary>
+        /// Gets or sets the observed <see cref="IWaveBoundary"/>.
+        /// </summary>
+        public IWaveBoundary ObservedWaveBoundary
+        {
+            get;
+            set;
+        }
+
+        public override IGeometry Geometry
+        {
+            get;
+            set;
+        }
+    }
+}
