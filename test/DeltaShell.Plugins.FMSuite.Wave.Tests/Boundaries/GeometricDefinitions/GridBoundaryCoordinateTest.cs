ce822a4a92c2ca5a5a5923114bcb1925547fa036
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-11-19 10:24:26 +0000

    D3DFMIQ-1477 - Add GridBoundaryCoordinate - Add corresponding tests
    Former-commit-id: 44980f6843869463ec18319ea6f6aa006aa237da
    Former-commit-id: a027b7cfd50d5c8df5ae6a7712cec224bf71c27f

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/GeometricDefinitions/GridBoundaryCoordinateTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using DeltaShell.NGHS.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.GeometricDefinitions
+{
+    [TestFixture]
+    public class GridBoundaryCoordinateTest
+    {
+        private readonly Random random = new Random();
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var expectedSide = random.NextEnumValue<GridSide>();
+            int expectedIndex = random.Next();
+
+            // Call
+            var gridBoundaryCoordinate = new GridBoundaryCoordinate(expectedSide,
+                                                                    expectedIndex);
+
+            // Assert
+            Assert.That(gridBoundaryCoordinate.GridSide, Is.EqualTo(expectedSide));
+            Assert.That(gridBoundaryCoordinate.Index, Is.EqualTo(expectedIndex));
+        }
+
+        [Test]
+        public void Constructor_InvalidGridSide_ThrowsInvalidEnumArgumentException()
+        {
+            // Call
+            void Call() => new GridBoundaryCoordinate((GridSide)0, random.Next());
+
+            // Assert
+            Assert.Throws<InvalidEnumArgumentException>(Call);
+        }
+
+        [Test]
+        public void Constructor_InvalidIndex_ThrowsArgumentOutOfRangeException()
+        {
+            // Call
+            void Call() => new GridBoundaryCoordinate(random.NextEnumValue<GridSide>(),
+                                                      random.Next() * -1);
+
+            // Assert
+            Assert.Throws<ArgumentOutOfRangeException>(Call);
+        }
+    }
+}
