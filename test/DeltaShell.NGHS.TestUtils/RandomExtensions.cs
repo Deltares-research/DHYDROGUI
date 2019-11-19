d4dd9f259dd3a8e3a66f2aa817a8f80a51d92485
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-11-19 10:21:44 +0000

    D3DFMIQ-1477 - Add NextBoolean and NextEnumValue as RandomExtensions
    Former-commit-id: 8275d5131e97ec5b6613b20ac188432ca5d22ae9
    Former-commit-id: d265a61f8b9d5147d5fa69f48d26220ca896f731

---
+++ test/DeltaShell.NGHS.TestUtils/RandomExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DeltaShell.NGHS.TestUtils
+{
+    /// <summary>
+    /// Extension methods for <see cref="Random"/>.
+    /// </summary>
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random value of <typeparamref name="TEnum"/>.
+        /// </summary>
+        /// <typeparam name="TEnum">The <see cref="Enum"/> to use.</typeparam>
+        /// <param name="random">A pseudo-random number generator.</param>
+        /// <returns>A new random value of type <typeparamref name="TEnum"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="random"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <typeparamref name="TEnum"/> is not an <see cref="Enum"/>.</exception>
+        public static TEnum NextEnumValue<TEnum>(this Random random)
+        {
+            if (random == null)
+            {
+                throw new ArgumentNullException(nameof(random));
+            }
+
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException($"'{typeof(TEnum).Name}' is not an enum.");
+            }
+
+            var enumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
+            return enumValues[random.Next(enumValues.Length)];
+        }
+
+        /// <summary>
+        /// Returns a random boolean value.
+        /// </summary>
+        /// <param name="random">A pseudo-random number generator.</param>
+        /// <returns>A new random boolean value.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="random"/> is <c>null</c>.</exception>
+        public static bool NextBoolean(this Random random)
+        {
+            if (random == null)
+            {
+                throw new ArgumentNullException(nameof(random));
+            }
+
+            return Convert.ToBoolean(random.Next(0, 2));
+        }
+    }
+}
