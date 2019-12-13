a505aa7c36b6312c308ac4a1be113824f5caa858
Hidde Elzinga <hidde.elzinga@deltares.nl>
2019-12-13 13:50:56 +0000

    Small improvement for gathering 1D mesh information
    Former-commit-id: 172149e94c164db6e09bba6ff585bd2432c0035b
    Former-commit-id: 599366cbc85a6f97c0b72ba244ee65140ae6f7e6

---
+++ src/DeltaShell.NGHS.IO/Helpers/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DeltaShell.NGHS.IO.Helpers
+{
+    public static class ListExtensions
+    {
+        public static IDictionary<T, int> ToIndexDictionary<T>(this IList<T> list)
+        {
+            var dictionary = new Dictionary<T, int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                dictionary.Add(list[i], i);
+            }
+
+            return dictionary;
+        }
+    }
+}
