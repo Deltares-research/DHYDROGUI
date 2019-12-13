60dcf55d8d403593faab0b5a0d4b3e089a0069c7
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-13 11:56:26 +0000

    D3DFMIQ-1477 - Add WaveBoundaryGeometricDefinition - Update IWaveBoundaryGeometricDefinition - Add corresponding tests
    Former-commit-id: b3e44f12d016205014094e2d1b631bf095e1b3b0
    Former-commit-id: cee2bcac1adaeb672f1f39a077e304f02ff49278

--- src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/IWaveBoundaryGeometricDefinition.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/IWaveBoundaryGeometricDefinition.cs
@@ -1,9 +1,18 @@
-﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
+﻿using System;
+using System.ComponentModel;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
 {
     /// <summary>
     /// <see cref="IWaveBoundaryGeometricDefinition"/> defines the geometric
     /// attributes of a <see cref="IWaveBoundary"/>.
     /// </summary>
+    /// <remarks>
+    /// The following invariants should be enforced:
+    /// * <see cref="StartingIndex"/> >= 0;
+    /// * <see cref="EndingIndex"/> > <see cref="StartingIndex"/>;
+    /// * <see cref="GridSide"/> is a valid enum value;
+    /// </remarks>
     public interface IWaveBoundaryGeometricDefinition
     {
         /// <summary>
@@ -14,6 +23,10 @@
         /// The index of the first <see cref="GridCoordinate"/> of this
         /// <see cref="IWaveBoundary"/>.
         /// </value>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="value"/> is smaller than zero.
+        /// Thrown when <paramref name="value"/> is greater or equal to <see cref="EndingIndex"/>.
+        /// </exception>
         int StartingIndex { get; set; }
 
         /// <summary>
@@ -24,11 +37,17 @@
         /// The index of the last <see cref="GridCoordinate"/> of this
         /// <see cref="IWaveBoundary"/>.
         /// </value>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="value"/> is smaller or equal to <see cref="StartingIndex"/>.
+        /// </exception>
         int EndingIndex { get; set; }
 
         /// <summary>
         /// The side of the grid this <see cref="IWaveBoundary"/> is located on.
         /// </summary>
+        /// <exception cref="InvalidEnumArgumentException">
+        /// Thrown when <see cref="GridSide"/> is set to an invalid enum value.
+        /// </exception>
         GridSide GridSide { get; set; }
     }
 }
---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/WaveBoundaryGeometricDefinition.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
+{
+    /// <summary>
+    /// <see cref="WaveBoundaryGeometricDefinition"/> implements the geometric
+    /// attributes of a <see cref="IWaveBoundary"/>.
+    /// </summary>
+    /// <seealso cref="IWaveBoundaryGeometricDefinition" />
+    public class WaveBoundaryGeometricDefinition : IWaveBoundaryGeometricDefinition
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="WaveBoundaryGeometricDefinition"/>.
+        /// </summary>
+        /// <param name="startingIndex">Index of the starting.</param>
+        /// <param name="endingIndex">Index of the ending.</param>
+        /// <param name="gridSide">The grid side.</param>
+        /// <exception cref="InvalidEnumArgumentException">
+        /// Thrown when <see cref="GridSide"/> is an invalid enum value.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="startingIndex"/> is smaller than zero
+        /// or larger than or equal to <paramref name="endingIndex"/>.
+        /// </exception>
+        public WaveBoundaryGeometricDefinition(int startingIndex,
+                                               int endingIndex,
+                                               GridSide gridSide)
+        {
+            if (startingIndex < 0)
+            {
+                throw new ArgumentException($"StartingIndex: '{startingIndex}' should be larger or equal to zero.");
+            }
+
+            if (startingIndex >= endingIndex)
+            {
+                throw new ArgumentException($"StartingIndex: '{startingIndex}' should be smaller than EndingIndex: {endingIndex}.");
+            }
+
+            GridSide = gridSide;
+            this.startingIndex = startingIndex;
+            this.endingIndex = endingIndex;
+        }
+
+        public int StartingIndex
+        {
+            get => startingIndex;
+            set
+            {
+                ValidateStartingIndex(value);
+                startingIndex = value;
+            }
+        }
+
+        private int startingIndex;
+
+        private void ValidateStartingIndex(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException($"{nameof(StartingIndex)} should be greater or equal to zero.");
+            }
+
+            if (value >= EndingIndex)
+            {
+                throw new ArgumentException($"{nameof(StartingIndex)} should be smaller than {nameof(EndingIndex)}.");
+            }
+        }
+
+        public int EndingIndex
+        {
+            get => endingIndex;
+            set
+            {
+                ValidateEndingIndex(value);
+                endingIndex = value;
+            }
+        }
+
+        private void ValidateEndingIndex(int value)
+        {
+            if (value <= StartingIndex)
+            {
+                throw new ArgumentException($"{nameof(EndingIndex)} should be greater than {nameof(StartingIndex)}.");
+            }
+        }
+
+        private int endingIndex;
+
+        public GridSide GridSide
+        {
+            get => gridSide;
+            set
+            {
+                if (!Enum.IsDefined(typeof(GridSide), value))
+                {
+                    throw new InvalidEnumArgumentException($"Value '{value}' is not a defined GridSide enum.");
+                }
+
+                gridSide = value;
+            }
+        }
+
+        private GridSide gridSide = GridSide.North;
+    }
+}
