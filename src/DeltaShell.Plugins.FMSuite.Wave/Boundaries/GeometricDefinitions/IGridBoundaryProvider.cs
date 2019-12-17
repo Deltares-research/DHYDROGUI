75eeb8830cf898b0938c2640941242e667d4ce13
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-17 12:16:53 +0000

    D3DFMIQ-1477 - Add IGridBoundaryProvider
    Former-commit-id: 233a42998913a32f660f501a3790f1b3cf4b12af
    Former-commit-id: 3107ad766368935251574959fd01326fd56a707d

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/IGridBoundaryProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
+{
+    /// <summary>
+    /// <see cref="IGridBoundaryProvider"/> defines the method required to obtain a
+    /// <see cref="GridBoundary"/>. This <see cref="GridBoundary"/> should not be
+    /// cached, instead this method should be used to retrieve the latest version of
+    /// the <see cref="GridBoundary"/>.
+    /// </summary>
+    public interface IGridBoundaryProvider
+    {
+        /// <summary>
+        /// Gets the grid boundary.
+        /// </summary>
+        /// <returns>
+        /// The latest version of the <see cref="GridBoundary"/>, if it exists;
+        /// Otherwise <c>null</c>.
+        /// </returns>
+        GridBoundary GetGridBoundary();
+    }
+}
