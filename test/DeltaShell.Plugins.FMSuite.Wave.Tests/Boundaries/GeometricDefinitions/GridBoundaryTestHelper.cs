e5e4d856a7a503339fd4d5671cfcb72a39c51136
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-17 12:42:44 +0000

    D3DFMIQ-1477 - Add GridBoundaryTestHelper for creating Gridboundaries
    Former-commit-id: 245a419fb0975a49d6dcbd2e9797015be3b8eda7
    Former-commit-id: f4ef802a68bee6172d09e99a7981b4e202aa63d2

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/GeometricDefinitions/GridBoundaryTestHelper.cs
@@ -0,0 +1,43 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using GeoAPI.Extensions.Coverages;
+using NSubstitute;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.GeometricDefinitions
+{
+    /// <summary>
+    /// A test helper for working with the <see cref="GridBoundary"/> class in tests.
+    /// </summary>
+    public static class GridBoundaryTestHelper
+    {
+        /// <summary>
+        /// Gets a <see cref="GridBoundary"/> with a mocked grid backing it.
+        /// </summary>
+        /// <param name="x">The x dimension.</param>
+        /// <param name="y">The y dimension.</param>
+        /// <param name="grid">The mocked grid.</param>
+        /// <returns>
+        /// A <see cref="Gridboundary"/> with <paramref name="grid"/> backing it.
+        /// </returns>
+        public static GridBoundary GetGridBoundaryWithMockedGrid(int x, int y, out IDiscreteGridPointCoverage grid)
+        {
+            grid = Substitute.For<IDiscreteGridPointCoverage>();
+            grid.Size1.Returns(x);
+            grid.Size2.Returns(y);
+
+           return new GridBoundary(grid);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="GridBoundary"/> with a mocked grid backing it.
+        /// </summary>
+        /// <param name="x">The x dimension.</param>
+        /// <param name="y">The y dimension.</param>
+        /// <returns>
+        /// A <see cref="Gridboundary"/> with a mocked grid.
+        /// </returns>
+        public static GridBoundary GetGridBoundaryWithMockedGrid(int x, int y)
+        {
+            return GetGridBoundaryWithMockedGrid(x, y, out IDiscreteGridPointCoverage _);
+        }
+    }
+}
