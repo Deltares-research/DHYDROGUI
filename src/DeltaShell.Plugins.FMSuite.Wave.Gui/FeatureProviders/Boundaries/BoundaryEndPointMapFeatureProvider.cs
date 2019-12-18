d9a05a21fa1fbae5708cb2d5768dd058dd2812fe
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-18 13:41:26 +0000

    D3DFMIQ-1482 - Add Boundaries/BoundaryEndPointMapFeatureProvider - Add corresponding tests
    Former-commit-id: 437c7d2b3f2e2df3170484972f6eaccc5b5c4fcb
    Former-commit-id: f14418bd0e6a392337a940cd41e2dd96c90deb0e

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/BoundaryEndPointMapFeatureProvider.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using GeoAPI.Extensions.CoordinateSystems;
+using GeoAPI.Extensions.Feature;
+using GeoAPI.Geometries;
+using NetTopologySuite.Extensions.Features;
+using SharpMap.Data.Providers;
+using IGeometryFactory = DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Factories.IGeometryFactory;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries
+{
+    /// <summary>
+    /// <see cref="BoundaryEndPointMapFeatureProvider"/> is responsible for
+    /// generating the features corresponding with the endpoints of all
+    /// <see cref="IWaveBoundary"/>.
+    /// </summary>
+    /// <remarks>
+    /// Several assumptions are made, which might be invalidated in future
+    /// implementations, this will most likely require this class to be
+    /// rewritten.
+    ///
+    /// * It is assumed that <see cref="IWaveBoundary"/> are static, once placed.
+    ///   Follow up issues will most likely invalidate this invariant. In this
+    ///   case, <see cref="BoundaryEndPointMapFeatureProvider"/> will need to be
+    ///   extended or rewritten, to allow for operations on EndPoints.
+    /// * Currently it assumed no explicit EndPoints are added, instead we assume
+    ///   that once a <see cref="IWaveBoundary"/> is added through the
+    ///   <see cref="BoundaryLineMapFeatureProvider"/>, a refresh is triggered,
+    ///   which will generate the EndPoints anew. This is not an ideal solution,
+    ///   but given the minimal amount of endpoints which will exist within a
+    ///   model it is sufficient.
+    /// </remarks>
+    /// <seealso cref="FeatureCollection"/>
+    public class BoundaryEndPointMapFeatureProvider : FeatureCollection
+    {
+        private readonly IBoundaryContainer boundaryContainer;
+        private readonly IGeometryFactory geometryFactory;
+
+        /// <summary>
+        /// Creates a new <see cref="BoundaryEndPointMapFeatureProvider"/>.
+        /// </summary>
+        /// <param name="boundaryContainer">The boundary container.</param>
+        /// <param name="coordinateSystem">The coordinate system.</param>
+        /// <param name="geometryFactory">The geometry factory.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="boundaryContainer"/> or
+        /// <paramref name="geometryFactory"/> is <c>null</c>.
+        /// </exception>
+        public BoundaryEndPointMapFeatureProvider(IBoundaryContainer boundaryContainer,
+                                                  ICoordinateSystem coordinateSystem, 
+                                                  IGeometryFactory geometryFactory)
+        {
+            this.boundaryContainer = boundaryContainer ?? 
+                                     throw new ArgumentNullException(nameof(boundaryContainer));
+            this.geometryFactory = geometryFactory ??
+                                   throw new ArgumentNullException(nameof(geometryFactory));
+
+            CoordinateSystem = coordinateSystem;
+            FeatureType = typeof(Feature2DPoint);
+        }
+
+        public override IList Features
+        {
+            get => boundaryContainer.Boundaries
+                                    .SelectMany(boundary => geometryFactory.ConstructBoundaryEndPoints(boundary))
+                                    .Select(p => new Feature2DPoint {Geometry = p})
+                                    .ToList();
+            set => throw new NotSupportedException("This is currently not supported, implement when needed.");
+        }
+
+        public override IFeature Add(IGeometry geometry) => 
+            throw new NotSupportedException("This is currently not supported, implement when needed.");
+        public override bool Add(IFeature feature) => 
+            throw new NotSupportedException("This is currently not supported, implement when needed.");
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/FeatureProviders/Boundaries/BoundaryEndPointMapFeatureProviderTest.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries;
+using GeoAPI.Extensions.CoordinateSystems;
+using GeoAPI.Extensions.Feature;
+using GeoAPI.Geometries;
+using NetTopologySuite.Extensions.Features;
+using NetTopologySuite.Geometries;
+using NSubstitute;
+using NUnit.Framework;
+using SharpMap.Data.Providers;
+using IGeometryFactory = DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Factories.IGeometryFactory;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.FeatureProviders.Boundaries
+{
+    [TestFixture]
+    public class BoundaryEndPointMapFeatureProviderTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var boundaryContainer = Substitute.For<IBoundaryContainer>();
+            var coordinateSystem = Substitute.For<ICoordinateSystem>();
+            var geometryFactory = Substitute.For<IGeometryFactory>();
+
+            // Call
+            using (var featureProvider = new BoundaryEndPointMapFeatureProvider(boundaryContainer, 
+                                                                                coordinateSystem,
+                                                                                geometryFactory))
+            {
+                // Assert
+                Assert.That(featureProvider, Is.InstanceOf(typeof(FeatureCollection)));
+                Assert.That(featureProvider.FeatureType, Is.EqualTo(typeof(Feature2DPoint)));
+                Assert.That(featureProvider.Features, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void Constructor_BoundaryContainerNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var coordinateSystem = Substitute.For<ICoordinateSystem>();
+            var geometryFactory = Substitute.For<IGeometryFactory>();
+
+            // Call
+            void Call() => new BoundaryEndPointMapFeatureProvider(null, 
+                                                                  coordinateSystem, 
+                                                                  geometryFactory);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("boundaryContainer"));
+        }
+
+        [Test]
+        public void Constructor_GeometryFactory_ThrowsArgumentNullException()
+        {
+            // Setup
+            var boundaryContainer = Substitute.For<IBoundaryContainer>();
+            var coordinateSystem = Substitute.For<ICoordinateSystem>();
+
+            // Call
+            void Call() => new BoundaryEndPointMapFeatureProvider(boundaryContainer, 
+                                                                  coordinateSystem, 
+                                                                  null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("geometryFactory"));
+        }
+
+        [Test]
+        public void GivenABoundaryEndPointMapFeatureProviderWithBoundaries_WhenFeaturesAreRetrieved_ThenTheCorrectEndPointsAreGenerated()
+        {
+            // Setup
+            var boundaryContainer = Substitute.For<IBoundaryContainer>();
+            var coordinateSystem = Substitute.For<ICoordinateSystem>();
+            var geometryFactory = Substitute.For<IGeometryFactory>();
+
+            var boundaries = new EventedList<IWaveBoundary>
+            {
+                Substitute.For<IWaveBoundary>(),
+                Substitute.For<IWaveBoundary>(),
+                Substitute.For<IWaveBoundary>(),
+            };
+
+            boundaryContainer.Boundaries.Returns(boundaries);
+
+            List<Point> endPoints = Enumerable.Range(0, 6)
+                                              .Select(x => new Point(new Coordinate(x + 0.5, -x + 0.5)))
+                                              .ToList();
+
+            for (var i = 0; i < boundaries.Count; i++)
+            {
+                geometryFactory.ConstructBoundaryEndPoints(boundaries[i])
+                               .Returns(endPoints.Skip(i * 2).Take(2));
+            }
+
+            using (var featureProvider = new BoundaryEndPointMapFeatureProvider(boundaryContainer,
+                                                                                coordinateSystem,
+                                                                                geometryFactory))
+            {
+                // Call
+                List<Feature2DPoint> endPointFeatures = featureProvider.Features.Cast<Feature2DPoint>().ToList();
+
+                // Assert
+                Assert.That(endPointFeatures, Has.Count.EqualTo(endPoints.Count));
+                Assert.That(endPointFeatures.Distinct().Count(), Is.EqualTo(endPointFeatures.Count));
+
+                foreach (var feat in endPointFeatures)
+                {
+                    Assert.That(endPoints.Contains(feat.Geometry),
+                                $"Expected {feat.Geometry} to be contained in endPoints.");
+                }
+            }
+        }
+
+        [Test]
+        public void Features_Set_ThrowsNotSupportedException()
+        {
+            // Setup
+            var boundaryContainer = Substitute.For<IBoundaryContainer>();
+            var coordinateSystem = Substitute.For<ICoordinateSystem>();
+            var geometryFactory = Substitute.For<IGeometryFactory>();
+
+            // Call
+            using (var featureProvider = new BoundaryEndPointMapFeatureProvider(boundaryContainer,
+                                                                                coordinateSystem,
+                                                                                geometryFactory))
+            {
+                void Call() => featureProvider.Features = Substitute.For<IList>();
+
+                Assert.Throws<NotSupportedException>(Call);
+            }
+        }
+
+        [Test]
+        public void Add_Geometry_ThrowsNotSupportedException()
+        {
+            // Setup
+            var boundaryContainer = Substitute.For<IBoundaryContainer>();
+            var coordinateSystem = Substitute.For<ICoordinateSystem>();
+            var geometryFactory = Substitute.For<IGeometryFactory>();
+
+            // Call
+            using (var featureProvider = new BoundaryEndPointMapFeatureProvider(boundaryContainer,
+                                                                                coordinateSystem,
+                                                                                geometryFactory))
+            {
+                void Call() => featureProvider.Add(Substitute.For<IGeometry>());
+
+                Assert.Throws<NotSupportedException>(Call);
+            }
+        }
+
+        [Test]
+        public void Add_Feature_ThrowsNotSupportedException()
+        {
+            // Setup
+            var boundaryContainer = Substitute.For<IBoundaryContainer>();
+            var coordinateSystem = Substitute.For<ICoordinateSystem>();
+            var geometryFactory = Substitute.For<IGeometryFactory>();
+
+            // Call
+            using (var featureProvider = new BoundaryEndPointMapFeatureProvider(boundaryContainer,
+                                                                                coordinateSystem,
+                                                                                geometryFactory))
+            {
+                void Call() => featureProvider.Add(Substitute.For<IFeature>());
+
+                Assert.Throws<NotSupportedException>(Call);
+            }
+        }
+    }
+}
