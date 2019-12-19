e2af22f06cea83bb751b9835c6e704ef4e6d3a33
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2019-12-19 10:28:41 +0000

    D3DFMIQ-1590 - Add SupportPoint - Add corresponding tests
    Former-commit-id: f4c354b7f852669959bc042c5e47789743fc3259
    Former-commit-id: 47fee4d72b8062ab29a763a77815bd2a7e73ecd9

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/GeometricDefinitions/SupportPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions
+{
+    /// <summary>
+    /// Represents a support point defined on a <see cref="IWaveBoundary"/>.
+    /// </summary>
+    public class SupportPoint
+    {
+        public double Distance { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SupportPoint"/> class.
+        /// </summary>
+        /// <param name="distance">The distance from the start index of the <see cref="IWaveBoundaryGeometricDefinition"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when <paramref name="distance"/> is smaller than 0.
+        /// </exception>
+        public SupportPoint(double distance)
+        {
+            if (distance < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(distance));
+            }
+
+            Distance = distance;
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/GeometricDefinitions/SupportPointTest.cs
@@ -0,0 +1,36 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using NUnit.Framework;
+using System;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.GeometricDefinitions
+{
+    [TestFixture]
+    public class SupportPointTest
+    {
+        private readonly Random random = new Random();
+
+        [Test]
+        public void Constructor_InvalidDistance_ThrowsArgumentOutOfRangeException()
+        {
+            // Call
+            void Call() => new SupportPoint(random.NextDouble() * -1);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("distance"));
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            double expectedDistance = random.NextDouble();
+
+            // Call
+            var supportPoint = new SupportPoint(expectedDistance);
+
+            // Assert
+            Assert.That(supportPoint.Distance, Is.EqualTo(expectedDistance));
+        }
+    }
+}
