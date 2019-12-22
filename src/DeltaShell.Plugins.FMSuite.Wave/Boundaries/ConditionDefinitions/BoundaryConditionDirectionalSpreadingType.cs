7e7d77b672a5cd8176dfdf45775c5d486c4d2ac1
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-22 18:01:40 +0000

    D3DFMIQ-1589 - Add BoundaryConditionDirectionalSpreadingType - Add corresponding test
    Former-commit-id: ab1cc692f44af53d907ada669cf03cbc4a0f211b
    Former-commit-id: e46bb24d0d4e307816f730de3f0920fa7b36e76d

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/BoundaryConditionDirectionalSpreadingType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions
+{
+    /// <summary>
+    /// <see cref="BoundaryConditionDirectionalSpreadingType"/> defines the
+    /// possible options of directional spreading types.
+    /// </summary>
+    public enum BoundaryConditionDirectionalSpreadingType
+    {
+        [Description("Power")]
+        Power = 1,
+
+        [Description("Degrees")]
+        Degrees = 2,
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/BoundaryConditionDirectionalSpreadingTypeTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using DeltaShell.NGHS.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions
+{
+    [TestFixture]
+    public class BoundaryConditionDirectionalSpreadingTypeTest : 
+        EnumValuesTestFixture<BoundaryConditionDirectionalSpreadingType>
+    {
+        protected override IDictionary<BoundaryConditionDirectionalSpreadingType, int> ExpectedValueForEnumValues =>
+            new Dictionary<BoundaryConditionDirectionalSpreadingType, int>
+            {
+                {BoundaryConditionDirectionalSpreadingType.Power, 1},
+                {BoundaryConditionDirectionalSpreadingType.Degrees, 2}
+            };
+        
+    }
+}
