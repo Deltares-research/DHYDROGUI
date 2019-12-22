2e8389ff9a5388c4a5580a9997b3076ba336cab1
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-22 17:40:01 +0000

    D3DFMIQ-1589 - Add PiersonMoskowitzShape - Add corresponding test
    Former-commit-id: d3ae4bb4eb253792fdddf32e89abd421163cd16d
    Former-commit-id: 7d2543b705760340118eb99b01ad7df891471fc4

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Shapes/PiersonMoskowitzShape.cs
@@ -0,0 +1,11 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes
+{
+    /// <summary>
+    /// <see cref="PiersonMoskowitzShape"/> defines the Pierson-Moskowitz <see cref="IBoundaryConditionShape"/>.
+    /// </summary>
+    /// <seealso cref="IBoundaryConditionShape" />
+    /// <remarks>
+    /// No data is associated with this <see cref="IBoundaryConditionShape"/>.
+    /// </remarks>
+    public class PiersonMoskowitzShape : IBoundaryConditionShape { }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/Shapes/PiersonMoskowitzShapeTest.cs
@@ -0,0 +1,19 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions.Shapes
+{
+    [TestFixture]
+    public class PiersonMoskowitzShapeTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var shape = new PiersonMoskowitzShape();
+
+            // Assert
+            Assert.That(shape, Is.InstanceOf(typeof(IBoundaryConditionShape)));
+        }
+    }
+}
