ab598d1946ea2292fc5dc10f651dd74ed3a2cf48
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-22 17:43:39 +0000

    D3DFMIQ-1589 - Add IWaveBoundaryConditionDefinition
    Former-commit-id: 9c6a550b19625e83aa3ea9f8bc0b3f54ea816c85
    Former-commit-id: 18c02b710491b20adf2ccb2d47f88407653b572d

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/IWaveBoundaryConditionDefinition.cs
@@ -0,0 +1,20 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions
+{
+    /// <summary>
+    /// <see cref="IWaveBoundaryConditionDefinition"/> defines the condition 
+    /// attributes of a <see cref="IWaveBoundary"/>.
+    /// </summary>
+    public interface IWaveBoundaryConditionDefinition
+    {
+        /// <summary>
+        /// Gets or sets the boundary condition shape.
+        /// </summary>
+        /// <value>
+        /// The BoundaryConditionShape.
+        /// </value>
+        IBoundaryConditionShape BoundaryConditionShape { get; set; }
+        
+    }
+}
