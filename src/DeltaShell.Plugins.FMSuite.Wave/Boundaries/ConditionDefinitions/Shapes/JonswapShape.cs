cf7f1c1ffb60897ed34055e967ad04a92eac26ca
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-22 17:29:43 +0000

    D3DFMIQ-1589 - Add JonswapShape - Add corresponding test
    Former-commit-id: 59fc8ba13c5e55cce8183cadaca36d9da6224468
    Former-commit-id: b27748085c4e9b5295faf174b29c5ec04f4735b8

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Shapes/JonswapShape.cs
@@ -0,0 +1,18 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes
+{
+    /// <summary>
+    /// <see cref="JonswapShape"/> defines the Jonswap type of
+    /// <see cref="IBoundaryConditionShape"/> with a <see cref="PeakEnhancementFactor"/>.
+    /// </summary>
+    /// <seealso cref="IBoundaryConditionShape" />
+    public class JonswapShape : IBoundaryConditionShape
+    {
+        /// <summary>
+        /// Gets the peak enhancement factor of this <see cref="JonswapShape"/>.
+        /// </summary>
+        /// <value>
+        /// The peak enhancement factor.
+        /// </value>
+        public double PeakEnhancementFactor { get; set; }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/Shapes/JonswapShapeTest.cs
@@ -0,0 +1,19 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions.Shapes
+{
+    [TestFixture]
+    public class JonswapShapeTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var shape = new JonswapShape();
+
+            // Assert
+            Assert.That(shape, Is.InstanceOf(typeof(JonswapShape)));
+        }
+    }
+}
