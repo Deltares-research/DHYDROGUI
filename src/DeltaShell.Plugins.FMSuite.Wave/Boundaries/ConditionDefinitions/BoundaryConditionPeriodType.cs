0b4066aeea7e8cb0f56ee0bc885991b9188be284
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-22 17:54:34 +0000

    D3DFMIQ-1589 - Add BoundaryConditionPeriodType - Add corresponding test
    Former-commit-id: 6ffe61be03b89ae1809ac54a1b29bd04751dc4c2
    Former-commit-id: 854cc328f110fe7cba307e899fead53334288e0c

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/BoundaryConditionPeriodType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.SpectralData;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions
+{
+    /// <summary>
+    /// <see cref="WavePeriodType"/> defines the possible options of
+    /// wave period types.
+    /// </summary>
+    public enum BoundaryConditionPeriodType
+    {
+        [Description("Peak")]
+        Peak = 1,
+
+        [Description("Mean")]
+        Mean = 2,
+    }
+}
