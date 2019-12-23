04bbd37ddecffcafc7872dfb816baec851fb371c
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-23 06:41:14 +0000

    D3DFMIQ-1589 - Add IBoundaryConditionParameters
    Former-commit-id: 0ab95e6d1df37ae66af6a4d35a3f1c5c10215f17
    Former-commit-id: 25e1fcd81670810ba3efc371facbcf392f8dfad0

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Parameters/IBoundaryConditionParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters
+{
+    /// <summary>
+    /// <see cref="IBoundaryConditionParameters"/> defines the different boundary
+    /// condition parameters used within the <see cref="IWaveBoundaryConditionDefinition"/>.
+    ///
+    /// Currently, the following are defined:
+    ///
+    /// * <see cref="ConstantParameters"/>
+    /// * <see cref="TimeDependentParameters"/>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This interface acts as a discriminated union for its implementing types.
+    /// As such, this interface is empty. Other classes will use it to type
+    /// cast to the implementing types.
+    /// </remarks>
+    // TODO: Extend description with where the right things should be updated.
+    public interface IBoundaryConditionParameters { }
+}
