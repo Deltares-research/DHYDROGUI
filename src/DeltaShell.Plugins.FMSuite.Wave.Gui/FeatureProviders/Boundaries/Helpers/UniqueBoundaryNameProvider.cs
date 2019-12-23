1bce1675974515d9125a7fbe967d5bef5d0dea05
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-23 19:54:56 +0000

    D3DFMIQ-1479 - Add IUniqueBoundaryNameProvider - Add UniqueBoundaryNameProvider - Add corresponding tests
    Former-commit-id: 7d28b35fe834a5445733127b4ffd77b7aa2a71d8
    Former-commit-id: 304ba1da0b08db15c381dd2062c4dfafecd47a81

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/Helpers/UniqueBoundaryNameProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Helpers
+{
+    /// <summary>
+    /// <see cref="IUniqueBoundaryNameProvider"/> implements the method to obtain a unique
+    /// boundary name.
+    /// </summary>
+    /// <seealso cref="IUniqueBoundaryNameProvider" />
+    public class UniqueBoundaryNameProvider : IUniqueBoundaryNameProvider
+    {
+        private readonly IBoundaryContainer boundaryContainer;
+        public const string defaultBoundaryName = "Boundary";
+
+        /// <summary>
+        /// Creates a new <see cref="UniqueBoundaryNameProvider"/> with the given
+        /// <paramref name="boundaryContainer"/>.
+        /// </summary>
+        /// <param name="boundaryContainer">The boundary container.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="boundaryContainer"/> is <c>null</c>.
+        /// </exception>
+        public UniqueBoundaryNameProvider(IBoundaryContainer boundaryContainer)
+        {
+            this.boundaryContainer = boundaryContainer ?? 
+                                     throw new ArgumentNullException(nameof(boundaryContainer));
+        }
+
+        public string GetUniqueName()
+        {
+            // TODO: NamingHelper exists, however it makes use of a List instead of
+            // TODO a HashSet, furthermore it requires the implementation of INameable.
+            // TODO: Verify whether we want to adjust this.
+            return boundaryContainer.Boundaries.Any() ? GenerateUniqueBoundaryName() 
+                                                      : defaultBoundaryName;
+        }
+
+        private string GenerateUniqueBoundaryName()
+        {
+            var names = new HashSet<string>(boundaryContainer.Boundaries.Select(x => x.Name));
+
+            if (!names.Contains(defaultBoundaryName))
+            {
+                return defaultBoundaryName;
+            }
+
+            const string newNameTemplate = defaultBoundaryName + " ({0})";
+            var i = 1;
+
+            string newName;
+            do
+            {
+                newName = string.Format(newNameTemplate, i);
+                i += 1;
+            } while (names.Contains(newName));
+
+            return newName;
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/FeatureProviders/Boundaries/Helpers/UniqueBoundaryNameProviderTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Helpers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.FeatureProviders.Boundaries.Helpers
+{
+    [TestFixture]
+    public class UniqueBoundaryNameProviderTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var boundaryContainer = Substitute.For<IBoundaryContainer>();
+
+            // Call
+            var nameProvider = new UniqueBoundaryNameProvider(boundaryContainer);
+
+            // Assert
+            Assert.That(nameProvider, Is.InstanceOf<IUniqueBoundaryNameProvider>());
+        }
+
+        [Test]
+        public void Constructor_BoundaryContainerNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new UniqueBoundaryNameProvider(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("boundaryContainer"), 
+                        "Expected a different ParamName:");
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetUniqueBoundaryNameTestData))]
+        public void GivenAnUniqueBoundaryNameProviderWithABoundaryContainer_WhenGetUniqueNameIsCalled_ThenTheExpectedResultIsReturned(IBoundaryContainer boundaryContainer, string expectedResult)
+        {
+            // Given
+            var provider = new UniqueBoundaryNameProvider(boundaryContainer);
+
+            // When
+            string result = provider.GetUniqueName();
+
+            // Then
+            Assert.That(result, Is.EqualTo(expectedResult), "Expected a different result.");
+        }
+
+        private static IEnumerable<TestCaseData> GetUniqueBoundaryNameTestData()
+        {
+
+            yield return new TestCaseData(EmptyContainer, UniqueBoundaryNameProvider.defaultBoundaryName);
+            yield return new TestCaseData(NoDefaultContainer, UniqueBoundaryNameProvider.defaultBoundaryName);
+            yield return new TestCaseData(GetContainerWithNElements(5),  "Boundary (5)");
+            yield return new TestCaseData(GetContainerWithNElements(12), "Boundary (12)");
+            yield return new TestCaseData(GetContainerWithNElements(53), "Boundary (53)");
+        }
+
+        private static IBoundaryContainer EmptyContainer
+        {
+            get
+            {
+                var containerEmpty = Substitute.For<IBoundaryContainer>();
+                containerEmpty.Boundaries.Returns(new EventedList<IWaveBoundary>());
+
+                return containerEmpty;
+            }
+        }
+
+        private static IBoundaryContainer NoDefaultContainer
+        {
+            get
+            { 
+                var containerNoDefault = Substitute.For<IBoundaryContainer>(); 
+                var boundariesNoDefault = new EventedList<IWaveBoundary> 
+                {
+                    GetBoundaryMockWithName("a1"), 
+                    GetBoundaryMockWithName("b2"), 
+                    GetBoundaryMockWithName("c3"),
+                };
+
+                containerNoDefault.Boundaries.Returns(boundariesNoDefault);
+                return containerNoDefault;
+            }
+        }
+
+        private static IBoundaryContainer GetContainerWithNElements(int nElements)
+        {
+            var container= Substitute.For<IBoundaryContainer>();
+            var boundaries = new EventedList<IWaveBoundary>
+            {
+                GetBoundaryMockWithName(UniqueBoundaryNameProvider.defaultBoundaryName),
+            };
+
+            const string template = UniqueBoundaryNameProvider.defaultBoundaryName + " ({0})";
+
+            for (var i = 1; i < nElements; i++)
+            {
+                boundaries.Add(GetBoundaryMockWithName(string.Format(template, i)));
+            }
+
+            container.Boundaries.Returns(boundaries);
+            return container;
+        }
+
+        private static IWaveBoundary GetBoundaryMockWithName(string name)
+        {
+            var boundary = Substitute.For<IWaveBoundary>();
+            boundary.Name.Returns(name);
+
+            return boundary;
+        }
+    }
+}
