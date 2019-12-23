2c5a81ee36a015405f33b5a13f350552fbaa54ea
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-23 06:13:01 +0000

    D3DFMIQ-1589 - Add BoundaryConditionParameters - Add corresponding test
    Former-commit-id: f8cde8b545a1a6dbb46416e3d450c05daaaced02
    Former-commit-id: 2f564588f4894ca887e0c85b8a40050c4b0fc140

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/BoundaryConditionParameters.cs
@@ -0,0 +1,53 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions
+{
+    /// <summary>
+    /// <see cref="BoundaryConditionParameters"/> provides the parameters
+    /// associated with a <see cref="IWaveBoundaryConditionDefinition"/>
+    /// in the case of uniform data, or the parameters associated with a
+    /// <see cref="SupportPoint"/> in the case of a spatially variant
+    /// <see cref="IWaveBoundaryConditionDefinition"/>.
+    /// </summary>
+    public class BoundaryConditionParameters
+    {
+        /// <summary>
+        /// Creates a new <see cref="BoundaryConditionParameters"/>.
+        /// </summary>
+        /// <param name="height">The height.</param>
+        /// <param name="period">The period.</param>
+        /// <param name="direction">The direction.</param>
+        /// <param name="spreading">The spreading.</param>
+        public BoundaryConditionParameters(double height,
+                                           double period,
+                                           double direction,
+                                           double spreading)
+        {
+            // TODO (MWT) verify what the correct values are for doubles in here
+            Height = height;
+            Period = period;
+            Direction = direction;
+            Spreading = spreading;
+        }
+
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
+        public double Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the period.
+        /// </summary>
+        public double Period { get; set; }
+
+        /// <summary>
+        /// Gets or sets the direction.
+        /// </summary>
+        public double Direction { get; set; }
+
+        /// <summary>
+        /// Gets or sets the spreading.
+        /// </summary>
+        public double Spreading { get; set; }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/BoundaryConditionParametersTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions
+{
+    [TestFixture]
+    public class BoundaryConditionParametersTest
+    {
+        private readonly Random random = new Random(37);
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            double expectedHeight = random.NextDouble() * 1000.0;
+            double expectedPeriod = random.NextDouble() * 1000.0;
+            double expectedDirection = random.NextDouble() * 1000.0;
+            double expectedSpreading = random.NextDouble() * 1000.0;
+
+            // Call
+            var boundaryConditionParameters = new BoundaryConditionParameters(expectedHeight, 
+                                                                              expectedPeriod, 
+                                                                              expectedDirection, 
+                                                                              expectedSpreading);
+
+            // Assert
+            Assert.That(boundaryConditionParameters.Height, Is.EqualTo(expectedHeight),
+                        "Expected a different Height:");
+            Assert.That(boundaryConditionParameters.Period, Is.EqualTo(expectedPeriod),
+                        "Expected a different Period:");
+            Assert.That(boundaryConditionParameters.Direction, Is.EqualTo(expectedDirection),
+                        "Expected a different Direction:");
+            Assert.That(boundaryConditionParameters.Spreading, Is.EqualTo(expectedSpreading),
+                        "Expected a different Spreading:");
+        }
+    }
+}
