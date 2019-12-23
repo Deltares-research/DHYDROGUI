30f20f18eeb91f2d5fbd510e18e390f70a2abf15
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-23 08:08:02 +0000

    D3DFMIQ-1589 - Add UniformDataComponent - Add corresponding tests
    Former-commit-id: fad711c16191a377b09e40ac84baa466560aef27
    Former-commit-id: acca228f1b306b160e327369084cbe6c19aaeeea

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/DataComponents/UniformDataComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.DataComponents
+{
+    /// <summary>
+    /// <see cref="UniformDataComponent"/> defines a data component consisting
+    /// of a uniform data object for all support points.
+    /// </summary>
+    /// <seealso cref="IBoundaryConditionDataComponent" />
+    public class UniformDataComponent : IBoundaryConditionDataComponent
+    {
+        /// <summary>
+        /// Creates a new <see cref="UniformDataComponent"/>.
+        /// </summary>
+        /// <param name="data">The data of this .</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="data"/> is <c>null</c>.
+        /// </exception>
+        // TODO (MWT) Verify whether we should add an Extension method to verify the IBoundaryConditionParameters as being valid
+        public UniformDataComponent(IBoundaryConditionParameters data)
+        {
+            Data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        public IBoundaryConditionParameters Data
+        {
+            get => data;
+            set => data = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        private IBoundaryConditionParameters data;
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/DataComponents/UniformDataTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.DataComponents;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions.DataComponents
+{
+    [TestFixture]
+    public class UniformDataTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var data = Substitute.For<IBoundaryConditionParameters>();
+
+            // Call
+            var uniformDataComponent = new UniformDataComponent(data);
+
+            // Assert
+            Assert.That(uniformDataComponent, Is.InstanceOf<IBoundaryConditionDataComponent>());
+            Assert.That(uniformDataComponent.Data, Is.SameAs(data),
+                        "Expected a different Data object:");
+        }
+
+        [Test]
+        public void Constructor_DataNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new UniformDataComponent(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            
+            Assert.That(exception.ParamName, Is.EqualTo("data"),
+                        "Expected a different ParamName:");
+        }
+
+        [Test]
+        public void SetData_ValueNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var data = Substitute.For<IBoundaryConditionParameters>();
+            var uniformDataComponent = new UniformDataComponent(data);
+
+            // Call
+            void Call() => uniformDataComponent.Data = null;
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            
+            Assert.That(exception.ParamName, Is.EqualTo("value"),
+                        "Expected a different ParamName:");
+        }
+    }
+}
