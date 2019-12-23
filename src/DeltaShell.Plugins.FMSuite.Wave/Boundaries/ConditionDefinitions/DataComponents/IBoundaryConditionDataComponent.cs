0ade1e2d633d8c8ac2351137dd09785fe076c915
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2019-12-23 06:46:58 +0000

    D3DFMIQ-1589 - Add IBoundaryConditionDataComponent
    Former-commit-id: c6d92538eaa40105ec3ec98d73811fda73c221f0
    Former-commit-id: a6947850c5ac3e02bee1f69b8bbdf5e7b0ffcd1a

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/DataComponents/IBoundaryConditionDataComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.DataComponents
+{
+    /// <summary>
+    /// <see cref="IBoundaryConditionDataComponent"/> defines the different boundary
+    /// condition data components used within the <see cref="IWaveBoundaryConditionDefinition"/>.
+    ///
+    /// Currently, the following are defined:
+    ///
+    /// * <see cref="UniformDataComponent"/>
+    /// * <see cref="SpatiallyVaryingDataComponent"/>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This interface acts as a discriminated union for its implementing types.
+    /// As such, this interface is empty. Other classes will use it to type
+    /// cast to the implementing types.
+    /// </remarks>
+    public interface IBoundaryConditionDataComponent { }
+}
