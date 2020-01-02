33446d8ac658878dab85debc397921a8d6d46fe3
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-02 08:26:19 +0000

    D3DFMIQ-1479 - Add skeleton WaveBoundaryConditionEditorView
    Former-commit-id: 4c27c26e12dd392c7399f232f08c165cc73e535c
    Former-commit-id: a55b984b9ebb087870314c887b498a0dbc3b4220

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/WaveBoundaryConditionEditorView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+using DelftTools.Controls;
+using Image = System.Drawing.Image;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views
+{
+    /// <summary>
+    /// Interaction logic for WaveBoundaryConditionEditorView.xaml
+    /// </summary>
+    public partial class WaveBoundaryConditionEditorView : UserControl, IView
+    {
+        public WaveBoundaryConditionEditorView()
+        {
+            InitializeComponent();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void EnsureVisible(object item)
+        {
+        }
+
+        public object Data { get; set; }
+        public string Text { get; set; }
+        public Image Image { get; set; }
+        public ViewInfo ViewInfo { get; set; }
+    }
+}
