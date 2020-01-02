4a7032e601155948810974597a1bf8eda8af4db0
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-02 12:40:10 +0000

    D3DFMIQ-1479 - Add placeholder BoundaryGeometryView - Connect BoundaryGeometryView in WaveBoundaryConditionEditor
    Former-commit-id: 3658b525d386c9b21dfbcf39cf67a506f56dbca4
    Former-commit-id: 85460b74e59eaa04517cd9bc474d9ecf7646b906

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/WaveBoundaryConditionEditor/BoundaryGeometryView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.WaveBoundaryConditionEditor
+{
+    /// <summary>
+    /// Interaction logic for BoundaryGeometryView.xaml
+    /// </summary>
+    public partial class BoundaryGeometryView : UserControl
+    {
+        public BoundaryGeometryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
