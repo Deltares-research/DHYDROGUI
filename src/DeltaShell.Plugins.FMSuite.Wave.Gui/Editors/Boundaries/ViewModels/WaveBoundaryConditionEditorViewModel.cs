566e9c87ef25a0f62f15af188cac70f0cde278e5
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-02 10:37:21 +0000

    D3DFMIQ-1479 - Add skeleton WaveBoundaryConditionEditorViewModel - Add corresponding tests
    Former-commit-id: 8fd105113d81dc5da4c99187e0e225a609e09005
    Former-commit-id: 16d70596f04ecff71bfb53632c146ad447687d73

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels
+{
+    public class WaveBoundaryConditionEditorViewModel
+    {
+        private readonly IWaveBoundary observedBoundary;
+
+        public WaveBoundaryConditionEditorViewModel(IWaveBoundary observedBoundary)
+        { 
+            this.observedBoundary = observedBoundary ?? throw new ArgumentNullException(nameof(observedBoundary));
+        }
+
+        public string Name => observedBoundary.Name;
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditorViewModelTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels
+{
+    [TestFixture]
+    public class WaveBoundaryConditionEditorViewModelTest
+    {
+        [Test]
+        public void Constructor_ObservedBoundaryValid_SetsCorrectValues()
+        {
+            // Setup
+            var boundary = Substitute.For<IWaveBoundary>();
+            boundary.Name = "A Boundary Name";
+
+            // Call
+            var viewModel = new WaveBoundaryConditionEditorViewModel(boundary);
+
+            // Assert
+            Assert.That(viewModel.Name, Is.EqualTo(boundary.Name), "Expected a different Name:");
+        }
+
+        [Test]
+        public void Constructor_ObservedBoundaryNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new WaveBoundaryConditionEditorViewModel(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("observedBoundary"));
+        }
+    }
+}
