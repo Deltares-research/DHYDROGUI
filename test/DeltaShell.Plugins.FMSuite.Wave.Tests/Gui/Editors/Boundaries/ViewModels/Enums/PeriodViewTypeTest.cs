8d566e4c6e0d1930efc49bd504c1e564bbb2e126
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-07 12:04:58 +0000

    D3DFMIQ-1479 - Fix namespace PeriodViewType in test
    Former-commit-id: 4b009574547a08bb5d8ab8949a0286d599f03431
    Former-commit-id: ce32633206955089bd25dd3c93bf579a082b3cf5

--- test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/Enums/PeriodViewTypeTest.cs
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/Enums/PeriodViewTypeTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using DeltaShell.NGHS.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Enums;
 using NUnit.Framework;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.Enums
