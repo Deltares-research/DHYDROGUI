7ff391945a0987329b85f4197848fd323ed8d782
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-07 10:26:39 +0000

    D3DFMIQ-1479 - Add BoundaryDescriptionViewModel - Update BoundaryDescriptionView with new Name binding - Add corresponding tests
    Former-commit-id: 05f01a59db11d795700b343a314e867b54ae9370
    Former-commit-id: 846b50e51d0d8438c3cbf1aafddf439c159bbfc3

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryDescriptionViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    /// <summary>
+    /// <see cref="BoundaryDescriptionViewModel"/> defines the view model for the boundary description view.
+    /// </summary>
+    /// <seealso cref="INotifyPropertyChanged" />
+    public class BoundaryDescriptionViewModel : INotifyPropertyChanged
+    {
+        private readonly IWaveBoundary observedBoundary;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="BoundaryDescriptionViewModel"/>.
+        /// </summary>
+        /// <param name="observedBoundary">The observed boundary.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="observedBoundary"/> is <c>null</c>.
+        /// </exception>
+        public BoundaryDescriptionViewModel(IWaveBoundary observedBoundary)
+        {
+            this.observedBoundary = observedBoundary ?? 
+                                    throw new ArgumentNullException(nameof(observedBoundary));
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name
+        {
+            get => observedBoundary.Name;
+            set
+            {
+                if (observedBoundary.Name == value)
+                {
+                    return;
+                }
+
+                observedBoundary.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryDescriptionViewModelTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    [TestFixture]
+    public class BoundaryDescriptionViewModelTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            const string expectedName = "aBoundaryName";
+
+            var boundary = Substitute.For<IWaveBoundary>();
+            boundary.Name = expectedName;
+
+            // Call
+            var viewModel = new BoundaryDescriptionViewModel(boundary);
+
+            // Assert
+            Assert.That(viewModel.Name, Is.EqualTo(expectedName), 
+                        "Expected a different Name:");
+        }
+
+        [Test]
+        public void Constructor_ObservedBoundaryNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new BoundaryDescriptionViewModel(null);
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+
+            // Assert
+            Assert.That(exception.ParamName, Is.EqualTo("observedBoundary"),
+                        "Expected a different ParamName:");
+        }
+
+        [Test]
+        public void GivenABoundaryDescriptionViewModelWithABoundary_WhenNameIsSet_ThenTheNameInTheBoundaryIsAdjusted()
+        {
+            // Setup
+            const string expectedName = "aBoundaryName";
+            
+            var boundary = Substitute.For<IWaveBoundary>();
+            var viewModel = new BoundaryDescriptionViewModel(boundary);
+
+            // Call
+            viewModel.Name = expectedName;
+
+
+            // Assert
+            boundary.Received(1).Name = expectedName;
+        }
+    }
+}
