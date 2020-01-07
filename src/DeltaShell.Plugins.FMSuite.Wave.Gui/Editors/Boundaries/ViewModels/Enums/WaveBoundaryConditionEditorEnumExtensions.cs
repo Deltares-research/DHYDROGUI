6f432600656a8b13b15360773cbef86d5d70f4a3
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-07 14:04:18 +0000

    D3DFMIQ-1479 - Add WaveBoundaryConditionEditorEnumExtensions - Add corresponding tests
    Former-commit-id: f723437740e3364dc8d2bc7670c8f83a26ef1ad4
    Former-commit-id: 2b72efd0065a036e225258753940c6ba298dc5d1

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/Enums/WaveBoundaryConditionEditorEnumExtensions.cs
@@ -0,0 +1,63 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.Enums;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Enums
+{
+    /// <summary>
+    /// <see cref="WaveBoundaryConditionEditorEnumExtension"/> provides several conversion extensions
+    /// to transform the ViewTypes to their corresponding model layer types.
+    /// </summary>
+    public static class WaveBoundaryConditionEditorEnumExtension
+    {
+        // Note these methods work because the Enums have been defined with the same values. Keep in mind that changing either of the enums, 
+        // will potentially break this functionality.
+
+        /// <summary>
+        /// Converts the <see cref="BoundaryConditionPeriodType"/> to its corresponding <see cref="PeriodViewType"/>.
+        /// </summary>
+        /// <param name="periodType"> The enum to transform.</param>
+        /// <returns>
+        /// The corresponding <see cref="PeriodViewType"/>.
+        /// </returns>
+        public static PeriodViewType ConvertToPeriodViewType(this BoundaryConditionPeriodType periodType)
+        {
+            return (PeriodViewType)periodType;
+        }
+
+        /// <summary>
+        /// Converts the <see cref="PeriodViewType"/> to its corresponding <see cref="BoundaryConditionPeriodType"/>.
+        /// </summary>
+        /// <param name="periodViewType"> The enum to transform.</param>
+        /// <returns>
+        /// The corresponding <see cref="BoundaryConditionPeriodType"/>.
+        /// </returns>
+        public static BoundaryConditionPeriodType ConvertToBoundaryConditionPeriodType(this PeriodViewType periodViewType)
+        {
+            return (BoundaryConditionPeriodType)periodViewType;
+        }
+
+        /// <summary>
+        /// Converts the <see cref="BoundaryConditionDirectionalSpreadingType"/> to its corresponding <see cref="DirectionalSpreadingViewType"/>.
+        /// </summary>
+        /// <param name="spreadingType"> The enum to transform.</param>
+        /// <returns>
+        /// The corresponding <see cref="DirectionalSpreadingViewType"/>.
+        /// </returns>
+        public static DirectionalSpreadingViewType ConvertToDirectionalSpreadingViewType(this BoundaryConditionDirectionalSpreadingType spreadingType)
+        {
+            return (DirectionalSpreadingViewType)spreadingType;
+        }
+
+        /// <summary>
+        /// Converts the <see cref="DirectionalSpreadingViewType"/> to its corresponding <see cref="BoundaryConditionDirectionalSpreadingType"/>.
+        /// </summary>
+        /// <param name="spreadingViewType"> The enum to transform.</param>
+        /// <returns>
+        /// The corresponding <see cref="BoundaryConditionDirectionalSpreadingType"/>.
+        /// </returns>
+        public static BoundaryConditionDirectionalSpreadingType ConvertToBoundaryConditionDirectionalSpreadingType(this DirectionalSpreadingViewType spreadingViewType)
+        {
+            return (BoundaryConditionDirectionalSpreadingType)spreadingViewType;
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/Enums/WaveBoundaryConditionEditorEnumExtensionsTest.cs
@@ -0,0 +1,62 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Enums;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.Enums
+{
+    [TestFixture]
+    public class WaveBoundaryConditionEditorEnumExtensionsTest
+    {
+        [Test]
+        [TestCase(BoundaryConditionPeriodType.Peak, PeriodViewType.Peak)]
+        [TestCase(BoundaryConditionPeriodType.Mean, PeriodViewType.Mean)]
+        public void ConvertToPeriodViewType_ReturnsCorrectValues(BoundaryConditionPeriodType input, PeriodViewType expectedOutput)
+        {
+            // Call
+            PeriodViewType result = input.ConvertToPeriodViewType();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        [TestCase(PeriodViewType.Peak, BoundaryConditionPeriodType.Peak)]
+        [TestCase(PeriodViewType.Mean, BoundaryConditionPeriodType.Mean)]
+        public void ConvertToBoundaryConditionPeriodType_ReturnsCorrectValues(PeriodViewType input, BoundaryConditionPeriodType expectedOutput)
+        {
+            // Call
+            BoundaryConditionPeriodType result = input.ConvertToBoundaryConditionPeriodType();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedOutput));
+            
+        }
+
+        [Test]
+        [TestCase(BoundaryConditionDirectionalSpreadingType.Degrees, DirectionalSpreadingViewType.Degrees)]
+        [TestCase(BoundaryConditionDirectionalSpreadingType.Power, DirectionalSpreadingViewType.Power)]
+        public void ConvertToDirectionalSpreadingViewType_ReturnsCorrectValues(BoundaryConditionDirectionalSpreadingType input, 
+                                                                               DirectionalSpreadingViewType expectedOutput)
+        {
+            // Call
+            DirectionalSpreadingViewType result = input.ConvertToDirectionalSpreadingViewType();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedOutput));
+            
+        }
+
+        [Test]
+        [TestCase(DirectionalSpreadingViewType.Degrees, BoundaryConditionDirectionalSpreadingType.Degrees)]
+        [TestCase(DirectionalSpreadingViewType.Power, BoundaryConditionDirectionalSpreadingType.Power)]
+        public void ConvertToBoundaryConditionDirectionalSpreadingType_ReturnsCorrectValues(DirectionalSpreadingViewType input,
+                                                                                            BoundaryConditionDirectionalSpreadingType expectedOutput)
+        {
+            // Call
+            BoundaryConditionDirectionalSpreadingType result = input.ConvertToBoundaryConditionDirectionalSpreadingType();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+    }
+}
