de791ed780d3bbf242bbeefb184d6d3a3d3ab096
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-07 16:16:36 +0000

    D3DFMIQ-1479 - Add ViewShapeFactory - Add corresponding tests
    Former-commit-id: e3815a0b671cbfa180322cd3b04446481f615537
    Former-commit-id: 42f51d01889090cc65aeed67744b196e1d04d018

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/Factories/IViewShapeFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.Shapes;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Factories
+{
+    /// <summary>
+    /// <see cref="IViewShapeFactory"/> defines the interface to construct the different
+    /// <see cref="IViewShape"/>.
+    /// </summary>
+    public interface IViewShapeFactory
+    {
+        /// <summary>
+        /// Constructs the <see cref="IViewShape"/> corresponding with the
+        /// provided <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type"> The <see cref="IViewShape"/> to construct. </param>
+        /// <returns>
+        /// An <see cref="IViewShape"/> corresponding with the provided
+        /// <paramref name="type"/>. If <paramref name="type"/> is not a child
+        /// of <see cref="IViewShape"/> then <c>null</c> will be returned.
+        /// </returns>
+        IViewShape ConstructFromType(Type type);
+
+        /// <summary>
+        /// Constructs the <see cref="IViewShape"/> corresponding with the
+        /// provided <paramref name="shape"/>.
+        /// </summary>
+        /// <param name="shape"> The shape for which to construct a <see cref="IViewShape"/>. </param>
+        /// <returns>
+        /// The <see cref="IViewShape"/> corresponding with <paramref name="shape"/>, if no
+        /// corresponding <see cref="IViewShape"/> exists, null is returned.
+        /// </returns>
+        IViewShape ConstructFromShape(IBoundaryConditionShape shape);
+
+    }
+}
---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/Factories/ViewShapeFactory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.Shapes;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Factories
+{
+    /// <summary>
+    /// <see cref="IViewShapeFactory"/> implements the interface to construct the different
+    /// <see cref="IViewShape"/>.
+    /// </summary>
+    public class ViewShapeFactory
+    {
+        private readonly Dictionary<Type, Func<IViewShape>> mappingDefault =
+         new Dictionary<Type, Func<IViewShape>>();
+
+        private readonly IBoundaryConditionShapeFactory factory;
+
+        /// <summary>
+        /// Creates a new <see cref="ViewShapeFactory"/>.
+        /// </summary>
+        /// <param name="factory">
+        /// The factory with which to create default <see cref="IBoundaryConditionShape"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="factory"/> is <c>null</c>.
+        /// </exception>
+        public ViewShapeFactory(IBoundaryConditionShapeFactory factory)
+        {
+            this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            InitialiseDefaultMapping();
+        }
+
+        private void InitialiseDefaultMapping()
+        {
+            mappingDefault.Add(typeof(GaussViewShape), ConstructDefaultGaussViewShape);
+            mappingDefault.Add(typeof(JonswapViewShape), ConstructDefaultJonswapViewShape);
+            mappingDefault.Add(typeof(PiersonMoskowitzViewShape), ConstructDefaultPiersonMoskowitzViewShape);
+        }
+
+        public IViewShape ConstructFromType(Type type) =>
+            mappingDefault.ContainsKey(type) ? mappingDefault[type]() : null;
+
+        public IViewShape ConstructFromShape(IBoundaryConditionShape shape)
+        {
+            switch (shape)
+            {
+                case GaussShape gauss:
+                    return ConstructGaussViewShape(gauss);
+                case JonswapShape jonswap:
+                    return ConstructJonswapViewShape(jonswap);
+                case PiersonMoskowitzShape piersonMoskowitz:
+                    return ConstructPiersonMoskowitzViewShape(piersonMoskowitz);
+                default:
+                    return null;
+            }
+        }
+
+        private GaussViewShape ConstructDefaultGaussViewShape() =>
+            ConstructGaussViewShape(factory.ConstructDefaultGaussShape());
+
+        private JonswapViewShape ConstructDefaultJonswapViewShape() =>
+            ConstructJonswapViewShape(factory.ConstructDefaultJonswapShape());
+
+        private PiersonMoskowitzViewShape ConstructDefaultPiersonMoskowitzViewShape() =>
+            ConstructPiersonMoskowitzViewShape(factory.ConstructDefaultPiersonMoskowitzShape());
+
+        private GaussViewShape ConstructGaussViewShape(GaussShape shape) =>
+            new GaussViewShape(shape);
+
+        private JonswapViewShape ConstructJonswapViewShape(JonswapShape shape) =>
+            new JonswapViewShape(shape);
+
+        private PiersonMoskowitzViewShape ConstructPiersonMoskowitzViewShape(PiersonMoskowitzShape shape) =>
+            new PiersonMoskowitzViewShape(shape);
+    }
+}
