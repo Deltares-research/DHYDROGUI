cdeb3958913efd6e8b21027129c74257393830d0
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-01-07 12:06:29 +0000

    D3DFMIQ-1590 - Added Boundaries/BoundarySupportPointMapFeatureProvider - Added tests
    Former-commit-id: 24cf33b129247a5e3efb011d102c5e3a0e3c4e57
    Former-commit-id: a57d64f60824e2abe1f0731a7b71a6b0c9f7b54a

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/BoundarySupportPointMapFeatureProvider.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using DelftTools.Utils.Collections;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using GeoAPI.Extensions.Feature;
+using GeoAPI.Geometries;
+using SharpMap.Data.Providers;
+using IGeometryFactory = DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Factories.IGeometryFactory;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries
+{
+    /// <summary>
+    /// <see cref="BoundarySupportPointMapFeatureProvider" /> is responsible for showing
+    /// the appropriate boundary support points given a boundary container.
+    /// It provides the appropriate methods such that these features and their
+    /// underlying data can be created through the Map.
+    /// </summary>
+    /// <seealso cref="SharpMap.Data.Providers.FeatureCollection" />
+    /// <remarks>
+    /// This class leverages the <see cref="MultiIEventedListAdapter{TObserved,TDisplayed}" />
+    /// to create the appropriate lists. These classes are necessary to play
+    /// nice with the framework, and ensure a good separation of concerns between
+    /// view and data. As such, this feature provider can be seen as a view model
+    /// for the line data for the Map.
+    /// </remarks>
+    public class BoundarySupportPointMapFeatureProvider : Feature2DCollection
+    {
+        private readonly MultiIEventedListAdapter<SupportPoint, SupportPointFeature> pointFeatures;
+        private readonly IBoundaryContainer boundaryContainer;
+        private readonly IGeometryFactory geometryFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoundarySupportPointMapFeatureProvider"/> class.
+        /// </summary>
+        /// <param name="boundaryContainer">The boundary container.</param>
+        /// <param name="geometryFactory">The geometry factory.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when any parameter is <c>null</c>.
+        /// </exception>
+        public BoundarySupportPointMapFeatureProvider(IBoundaryContainer boundaryContainer,
+                                                      IGeometryFactory geometryFactory)
+        {
+            this.boundaryContainer = boundaryContainer ?? throw new ArgumentNullException(nameof(boundaryContainer));
+            this.geometryFactory = geometryFactory ?? throw new ArgumentNullException(nameof(geometryFactory));
+
+            pointFeatures = new MultiIEventedListAdapter<SupportPoint, SupportPointFeature>(ObtainSupportPointFromFeature,
+                                                                                            CreateSupportPointFeature);
+
+            RegisterBoundaries(boundaryContainer.Boundaries);
+            SubscribeToEventing();
+        }
+
+        public override IList Features
+        {
+            get => pointFeatures;
+            set => throw new NotSupportedException("Setting the Features to another value is currently not supported.");
+        }
+
+        public override IFeature Add(IGeometry geometry)
+        {
+            throw new NotImplementedException("Should be implemented when support points can be added from the User Interface.");
+        }
+
+        public override bool Add(IFeature feature)
+        {
+            throw new NotSupportedException("This is currently not supported, implement when needed.");
+        }
+
+        private SupportPointFeature CreateSupportPointFeature(SupportPoint supportPoint)
+        {
+            return new SupportPointFeature
+            {
+                ObservedSupportPoint = supportPoint,
+                Geometry = geometryFactory.ConstructBoundarySupportPoint(supportPoint)
+            };
+        }
+
+        private static Tuple<SupportPoint, IEventedList<SupportPoint>> ObtainSupportPointFromFeature(SupportPointFeature feature)
+        {
+            return new Tuple<SupportPoint, IEventedList<SupportPoint>>(feature.ObservedSupportPoint,
+                                                                       feature.ObservedSupportPoint.GeometricDefinition.SupportPoints);
+        }
+
+        private void SubscribeToEventing()
+        {
+            pointFeatures.CollectionChanged += OnFeaturesCollectionChanged;
+            boundaryContainer.Boundaries.CollectionChanged += OnBoundariesCollectionChanged;
+        }
+
+        private void UnsubscribeFromEventing()
+        {
+            pointFeatures.CollectionChanged -= OnFeaturesCollectionChanged;
+            boundaryContainer.Boundaries.CollectionChanged -= OnBoundariesCollectionChanged;
+        }
+
+        private void OnFeaturesCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            FireFeaturesChanged();
+        }
+
+        private void OnBoundariesCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (!(sender is IEnumerable<IWaveBoundary>))
+            {
+                return;
+            }
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    RegisterBoundaries(e.NewItems.Cast<IWaveBoundary>());
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                case NotifyCollectionChangedAction.Reset:
+                    DeregisterBoundaries(e.OldItems.Cast<IWaveBoundary>());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(e.Action));
+            }
+        }
+
+        private void RegisterBoundaries(IEnumerable<IWaveBoundary> waveBoundaries)
+        {
+            waveBoundaries.ForEach(b => pointFeatures.RegisterList(b.GeometricDefinition.SupportPoints));
+        }
+
+        private void DeregisterBoundaries(IEnumerable<IWaveBoundary> waveBoundaries)
+        {
+            waveBoundaries.ForEach(b => pointFeatures.DeregisterList(b.GeometricDefinition.SupportPoints));
+        }
+
+        #region IDisposable
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            DeregisterBoundaries(boundaryContainer.Boundaries);
+            UnsubscribeFromEventing();
+        }
+
+        #endregion
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/FeatureProviders/Boundaries/BoundarySupportPointMapFeatureProviderTest.cs
@@ -0,0 +1,236 @@
+﻿using DelftTools.TestUtils;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries;
+using GeoAPI.Extensions.Feature;
+using GeoAPI.Geometries;
+using NSubstitute;
+using NUnit.Framework;
+using SharpMap.Data.Providers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SharpMap.Api;
+using IGeometryFactory = DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Factories.IGeometryFactory;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.FeatureProviders.Boundaries
+{
+    [TestFixture]
+    public class BoundarySupportPointMapFeatureProviderTest
+    {
+        private readonly Random random = new Random();
+        private IBoundaryContainer boundaryContainer;
+        private IGeometryFactory geometryFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            boundaryContainer = Substitute.For<IBoundaryContainer>();
+            geometryFactory = Substitute.For<IGeometryFactory>();
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            using (var featureProvider = new BoundarySupportPointMapFeatureProvider(boundaryContainer,
+                                                                                    geometryFactory))
+            {
+                // Assert
+                Assert.That(featureProvider, Is.InstanceOf<Feature2DCollection>());
+                Assert.That(featureProvider.Features, Is.Not.Null);
+                Assert.That(featureProvider.Features, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void Constructor_BoundaryContainerNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new BoundarySupportPointMapFeatureProvider(null, geometryFactory);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception, Has.Property("ParamName").EqualTo("boundaryContainer"));
+        }
+
+        [Test]
+        public void Constructor_GeometryFactoryNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new BoundarySupportPointMapFeatureProvider(boundaryContainer, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception, Has.Property("ParamName").EqualTo("geometryFactory"));
+        }
+
+        [Test]
+        public void AddGeometry_Add_IFeature_ThrowsNotSupportedException()
+        {
+            using (var featureProvider = new BoundarySupportPointMapFeatureProvider(boundaryContainer,
+                                                                                    geometryFactory))
+            {
+                // Call
+                void Call() => featureProvider.Add(Substitute.For<IGeometry>());
+
+                // Assert
+                Assert.That(Call, Throws.TypeOf<NotImplementedException>()
+                                        .With.Message.EqualTo("Should be implemented when support points can be added from the User Interface."));
+            }
+        }
+
+        [Test]
+        [Category(TestCategory.Integration)]
+        public void GivenABoundarySupportPointMapFeatureProvider_WhenTheUnderlyingEventedListIsChanged_ThenFeaturesChangedIsCalled()
+        {
+            // Setup
+            IWaveBoundary boundary = CreateBoundary();
+            boundaryContainer.Boundaries.Returns(new EventedList<IWaveBoundary> {boundary});
+
+            var supportPoint = new SupportPoint(random.NextDouble(), boundary.GeometricDefinition);
+
+            var geometry = Substitute.For<IPoint>();
+            geometryFactory.ConstructBoundarySupportPoint(supportPoint).Returns(geometry);
+
+            using (var featureProvider = new BoundarySupportPointMapFeatureProvider(boundaryContainer,
+                                                                                    geometryFactory))
+            {
+                // Call
+                void Call() => boundary.GeometricDefinition.SupportPoints.Add(supportPoint);
+
+                // Assert
+                Assert.That(CountFeaturesChangedFired(featureProvider, Call), Is.EqualTo(1));
+                AssertCorrectFeature(featureProvider, supportPoint, geometry);
+            }
+        }
+
+        [Test]
+        [Category(TestCategory.Integration)]
+        [TestCaseSource(nameof(RemoveBoundaryCalls))]
+        public void GivenABoundarySupportPointMapFeatureProvider_WhenABoundaryIsRemovedFromTheBoundaryContainerAndNewSupportPointIsAdded_ThenFeaturesChangedIsNotCalled(Action<IBoundaryContainer> removeBoundary)
+        {
+            // Setup
+            IWaveBoundary boundary = CreateBoundary();
+            boundaryContainer.Boundaries.Returns(new EventedList<IWaveBoundary> {boundary});
+
+            var supportPoint = new SupportPoint(random.NextDouble(), boundary.GeometricDefinition);
+
+            using (var featureProvider = new BoundarySupportPointMapFeatureProvider(boundaryContainer,
+                                                                                    geometryFactory))
+            {
+                removeBoundary.Invoke(boundaryContainer);
+
+                // Call
+                void Call() => boundary.GeometricDefinition.SupportPoints.Add(supportPoint);
+
+                // Assert
+                Assert.That(CountFeaturesChangedFired(featureProvider, Call), Is.EqualTo(0));
+                Assert.That(featureProvider.Features, Has.Count.EqualTo(0));
+            }
+        }
+
+        private IEnumerable<Action<IBoundaryContainer>> RemoveBoundaryCalls()
+        {
+            yield return bc => bc.Boundaries.Clear();
+            yield return bc => bc.Boundaries.RemoveAt(0);
+        }
+
+        [Test]
+        [Category(TestCategory.Integration)]
+        public void GivenABoundarySupportPointMapFeatureProvider_WhenABoundaryIsAddedToTheBoundaryContainerAndNewSupportPointIsAdded_ThenFeaturesChangedIsCalled()
+        {
+            // Setup
+            IWaveBoundary boundary = CreateBoundary();
+
+            boundaryContainer.Boundaries.Returns(new EventedList<IWaveBoundary>());
+
+            var supportPoint = new SupportPoint(random.NextDouble(), boundary.GeometricDefinition);
+
+            var geometry = Substitute.For<IPoint>();
+            geometryFactory.ConstructBoundarySupportPoint(supportPoint).Returns(geometry);
+
+            using (var featureProvider = new BoundarySupportPointMapFeatureProvider(boundaryContainer,
+                                                                                    geometryFactory))
+            {
+                boundaryContainer.Boundaries.Add(boundary);
+
+                // Precondition
+                Assert.That(featureProvider.Features, Is.Empty);
+
+                // Call
+                void Call() => boundary.GeometricDefinition.SupportPoints.Add(supportPoint);
+
+                // Assert
+                Assert.That(CountFeaturesChangedFired(featureProvider, Call), Is.EqualTo(1));
+                AssertCorrectFeature(featureProvider, supportPoint, geometry);
+            }
+        }
+
+        [Test]
+        public void Add_IFeature_ThrowsNotSupportedException()
+        {
+            // Setup
+            var feature = Substitute.For<IFeature>();
+
+            using (var featureProvider = new BoundarySupportPointMapFeatureProvider(boundaryContainer,
+                                                                                    geometryFactory))
+            {
+                void Call() => featureProvider.Add(feature);
+                Assert.Throws<NotSupportedException>(Call);
+            }
+        }
+
+        [Test]
+        public void Features_Set_ThrowsNotSupportedException()
+        {
+            // Setup
+            var features = Substitute.For<IList>();
+
+            using (var featureProvider = new BoundarySupportPointMapFeatureProvider(boundaryContainer,
+                                                                                    geometryFactory))
+            {
+                void Call() => featureProvider.Features = features;
+                Assert.Throws<NotSupportedException>(Call);
+            }
+        }
+
+        private static IWaveBoundary CreateBoundary()
+        {
+            var boundary = Substitute.For<IWaveBoundary>();
+            var geometricDefinition = Substitute.For<IWaveBoundaryGeometricDefinition>();
+            var supportPoints = new EventedList<SupportPoint>();
+
+            boundary.GeometricDefinition.Returns(geometricDefinition);
+            geometricDefinition.SupportPoints.Returns(supportPoints);
+
+            return boundary;
+        }
+
+        private static int CountFeaturesChangedFired(IFeatureProvider fc, Action action)
+        {
+            var count = 0;
+
+            fc.FeaturesChanged += CountFeaturesChanged;
+
+            action.Invoke();
+
+            void CountFeaturesChanged(object sender, EventArgs e)
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        private static void AssertCorrectFeature(IFeatureProvider featureProvider, SupportPoint supportPoint, IPoint geometry)
+        {
+            Assert.That(featureProvider.Features, Has.Count.EqualTo(1));
+            var feature = featureProvider.Features[0] as SupportPointFeature;
+            Assert.That(feature, Is.Not.Null);
+            Assert.That(feature.ObservedSupportPoint, Is.SameAs(supportPoint));
+            Assert.That(feature.Geometry, Is.SameAs(geometry));
+        }
+    }
+}
