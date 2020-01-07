ddcb2bc06b5e1237b8e61043278cee45bc7b507a
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-07 11:53:15 +0000

    D3DFMIQ-1479 - Add IViewShape
    Former-commit-id: 4029cfef9a09bb3df124d9a7703da5237c8499b2
    Former-commit-id: 1d5cb06583d0e647cfb1a4fda5a5e6a2f97aca00

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/Shapes/IViewShape.cs
@@ -0,0 +1,16 @@
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Shapes;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.Shapes
+{
+    /// <summary>
+    /// <see cref="IViewShape"/> wraps the <see cref="IBoundaryConditionShape"/>
+    /// and presents it to the View layer.
+    /// </summary>
+    public interface IViewShape
+    {
+        /// <summary>
+        /// Gets the underlying <see cref="IBoundaryConditionShape"/>.
+        /// </summary>
+        IBoundaryConditionShape ObservedShape { get; }
+    }
+}
