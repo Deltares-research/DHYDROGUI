f8df19d1a3d0d5679d6d788fa682c73b900890ec
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-07 11:50:16 +0000

    D3DFMIQ-1479 - Fix namespace PeriodViewType
    Former-commit-id: ea5135dc472921e96d72af65c58b2b2dba9abb71
    Former-commit-id: 20f20512f7ade1e525b3a72a04f7af60bbd43718

--- src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/Enums/PeriodViewType.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/Enums/PeriodViewType.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel;
 
-namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.Enums
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Enums
 {
     /// <summary>
     /// <see cref="PeriodViewType"/> defines the possible options of
