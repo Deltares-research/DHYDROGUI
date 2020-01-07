cef4fb4dd73718c586db880699d1f6bdf4103726
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-07 11:36:20 +0000

    D3DFMIQ-1479 - Add DirectionalSpreadingViewType - Add corresponding test
    Former-commit-id: ac62076a28b1089d7c43dac70f1e1ae17b49730f
    Former-commit-id: 7f0eb2a7a81709eb36a0f6f0213f3ebfa83522f7

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/Enums/DirectionalSpreadingViewType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.Enums
+{
+    /// <summary>
+    /// <see cref="DirectionalSpreadingViewType"/> defines the possible
+    /// options of directional spreading types within the View layer.
+    /// </summary>
+    public enum DirectionalSpreadingViewType
+    {
+        [Description("Power")]
+        Power = 1,
+
+        [Description("Degrees")]
+        Degrees = 2,
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/Enums/DirectionalSpreadingViewTypeTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using DeltaShell.NGHS.TestUtils;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.Enums
+{
+    [TestFixture]
+    public class DirectionalSpreadingViewTypeTest :
+        EnumValuesTestFixture<DirectionalSpreadingViewType>
+    {
+        protected override IDictionary<DirectionalSpreadingViewType, int> ExpectedValueForEnumValues =>
+            new Dictionary<DirectionalSpreadingViewType, int>
+            {
+                {DirectionalSpreadingViewType.Power, 1},
+                {DirectionalSpreadingViewType.Degrees, 2}
+            };
+    }
+}
