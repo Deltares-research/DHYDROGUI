9a9e1d6ee3f9ca14e977edc5266eb7f6d2af5530
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-09 15:03:26 +0000

    D3DFMIQ-1479 - Add LabeledValueBox
    Former-commit-id: 3395581a1c904a66cb52fe46a9b3be13e26b09ba
    Former-commit-id: 2a709c08af6a40c50d20bbc203b9f1d9609e1d38

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/CommonElements/LabeledValueBox.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.CommonElements
+{
+    /// <summary>
+    /// Interaction logic for LabeledValueBox.xaml
+    /// </summary>
+    public partial class LabeledValueBox : UserControl
+    {
+        public static readonly DependencyProperty LabelProperty = 
+            DependencyProperty.Register(nameof(Label), 
+                                        typeof(string), 
+                                        typeof(LabeledValueBox),
+                                        new PropertyMetadata(default(string)));
+
+        /// <summary>
+        /// Gets or sets the label of this <see cref="LabeledValueBoxControl"/>.
+        /// </summary>
+        public string Label
+        {
+            get => (string) GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        /// <summary>
+        /// The unit property
+        /// </summary>
+        public static readonly DependencyProperty UnitProperty = 
+            DependencyProperty.Register(nameof(Unit),
+                                        typeof(string),
+                                        typeof(LabeledValueBox), 
+                                        new PropertyMetadata(default(string)));
+
+        /// <summary>
+        /// The value content property
+        /// </summary>
+        public static readonly DependencyProperty ValueContentProperty = 
+            DependencyProperty.Register(nameof(ValueContent),
+                                        typeof(string), 
+                                        typeof(LabeledValueBox), 
+                                        new FrameworkPropertyMetadata(default(string), 
+                                                                      FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// Gets or sets the Value of this <see cref="LabeledValueBoxControl"/>.
+        /// </summary>
+        public string ValueContent
+        {
+            get => (string) GetValue(ValueContentProperty);
+            set => SetValue(ValueContentProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the unit of this <see cref="LabeledValueBoxControl"/>.
+        /// </summary>
+        public string Unit
+        {
+            get => (string) GetValue(UnitProperty);
+            set => SetValue(UnitProperty, value);
+        }
+
+        public LabeledValueBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
