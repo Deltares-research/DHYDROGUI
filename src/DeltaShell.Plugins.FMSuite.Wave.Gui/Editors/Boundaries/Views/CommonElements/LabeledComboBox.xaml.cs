1a7aa558918cad5ed3b4736a42168fb48591f9b4
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-09 12:43:30 +0000

    D3DFMIQ-1479 - Update documentation LabeledComboBox
    Former-commit-id: 701454988b788e033e6ec7a206c00bc2a6439b9b
    Former-commit-id: d614baddb0e70dea9978272869802febef1b07ad

--- src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/CommonElements/LabeledComboBox.xaml.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/CommonElements/LabeledComboBox.xaml.cs
@@ -15,12 +15,18 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.CommonEle
                                         typeof(LabeledComboBox), 
                                         new PropertyMetadata(default(string)));
 
+        /// <summary>
+        /// Gets or sets the label of this <see cref="LabeledComboBoxControl"/>.
+        /// </summary>
         public string Label
         {
             get => (string) GetValue(LabelProperty);
             set => SetValue(LabelProperty, value);
         }
 
+        /// <summary>
+        /// The selected item property
+        /// </summary>
         public static readonly DependencyProperty SelectedItemProperty =
             DependencyProperty.Register(nameof(SelectedItem), 
                                         typeof(object), 
@@ -28,24 +34,36 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.CommonEle
                                         new FrameworkPropertyMetadata(default(object),
                                                                       FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        /// <summary>
+        /// Gets or sets the selected item of this <see cref="LabeledComboBoxControl"/>.
+        /// </summary>
         public object SelectedItem
         {
             get => (object) GetValue(SelectedItemProperty);
             set => SetValue(SelectedItemProperty, value);
         }
 
+        /// <summary>
+        /// The items source property
+        /// </summary>
         public static readonly DependencyProperty ItemsSourceProperty = 
             DependencyProperty.Register(nameof(ItemsSource), 
                                         typeof(IEnumerable), 
                                         typeof(LabeledComboBox), 
                                         new PropertyMetadata(default(IEnumerable)));
 
+        /// <summary>
+        /// Gets or sets the items source of this <see cref="LabeledComboBoxControl"/>.
+        /// </summary>
         public IEnumerable ItemsSource
         {
             get => (IEnumerable) GetValue(ItemsSourceProperty);
             set => SetValue(ItemsSourceProperty, value);
         }
 
+        /// <summary>
+        /// The item template property
+        /// </summary>
         public static readonly DependencyProperty ItemTemplateProperty = 
             DependencyProperty.Register(nameof(ItemTemplate), 
                                         typeof(DataTemplate), 
@@ -53,12 +71,18 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.CommonEle
                                         new FrameworkPropertyMetadata(default(DataTemplate), 
                                                                       FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        /// <summary>
+        /// Gets or sets the item template of this <see cref="LabeledComboBoxControl"/>.
+        /// </summary>
         public DataTemplate ItemTemplate
         {
             get => (DataTemplate) GetValue(ItemTemplateProperty);
             set => SetValue(ItemTemplateProperty, value);
         }
 
+        /// <summary>
+        /// Creates a new <see cref="LabeledComboBox"/>.
+        /// </summary>
         public LabeledComboBox()
         {
             InitializeComponent();
