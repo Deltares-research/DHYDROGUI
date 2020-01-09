583a735aa1e8a264062f706cd94277f6f911c686
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-09 12:11:35 +0000

    D3DFMIQ-1479 - Add LabeledComboBox
    Former-commit-id: 34564edb36fc3efeb560c7df641025cdb15e80f7
    Former-commit-id: edd70dde142721a9b2390053fc8791980b5d026d

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/CommonElements/LabeledComboBox.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.CommonElements
+{
+    /// <summary>
+    /// Interaction logic for LabeledComboBox.xaml
+    /// </summary>
+    public partial class LabeledComboBox : UserControl
+    {
+        public static readonly DependencyProperty LabelProperty = 
+            DependencyProperty.Register(nameof(Label), 
+                                        typeof(string), 
+                                        typeof(LabeledComboBox), 
+                                        new PropertyMetadata(default(string)));
+
+        public string Label
+        {
+            get => (string) GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), 
+                                        typeof(object), 
+                                        typeof(LabeledComboBox), 
+                                        new FrameworkPropertyMetadata(default(object),
+                                                                      FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public object SelectedItem
+        {
+            get => (object) GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty = 
+            DependencyProperty.Register(nameof(ItemsSource), 
+                                        typeof(IEnumerable), 
+                                        typeof(LabeledComboBox), 
+                                        new PropertyMetadata(default(IEnumerable)));
+
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable) GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemTemplateProperty = 
+            DependencyProperty.Register(nameof(ItemTemplate), 
+                                        typeof(DataTemplate), 
+                                        typeof(LabeledComboBox), 
+                                        new FrameworkPropertyMetadata(default(DataTemplate), 
+                                                                      FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public DataTemplate ItemTemplate
+        {
+            get => (DataTemplate) GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+
+        public LabeledComboBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
