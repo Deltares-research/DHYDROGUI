569ce7b31985f02ba0000bc5894cf55842d64614
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-10 05:07:01 +0000

    D3DFMIQ-1482 - Add Ensure.NotNull as a guard against null
    Former-commit-id: d3cb5836e71d21704438e505f941cef33689ab9c
    Former-commit-id: 9c0575d4dcef54db48ec56f0b6fbe0f78a18af0b

---
+++ src/DeltaShell.NGHS.Common/Ensure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace DeltaShell.NGHS.Common
+{
+    /// <summary>
+    /// <see cref="Ensure"/> provides a collection of methods to verify different
+    /// conditions on parameters.
+    /// </summary>
+    public static class Ensure
+    {
+        /// <summary>
+        /// Verifies that the specified <paramref name="obj"/> is not
+        /// <c>null</c>. If it is not, then an <see cref="ArgumentNullException"/>
+        /// is thrown.
+        /// </summary>
+        /// <typeparam name="T"> The type of the object. </typeparam>
+        /// <param name="obj"> The object to check. </param>
+        /// <param name="paramName"> The name of <paramref name="obj"/>. </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="obj"/> is <c>null</c>.
+        /// </exception>
+        public static void NotNull<T>(T obj, [CallerMemberName] string paramName = null) where T : class
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
