33621c89c4fac693320cdd6f7c9440b383263798
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-10 05:48:24 +0000

    D3DFMIQ-1482 - Remove default parameter value from paramName - Add corresponding tests for Ensure.NotNull
    Former-commit-id: 30ca074dc891131b45b1aee07d9c8a2fbd300235
    Former-commit-id: e43918d81e627d041fc104191db9a3797bda7737

---
+++ test/DeltaShell.NGHS.Common.Tests/EnsureTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DeltaShell.NGHS.Common.Tests
+{
+    [TestFixture]
+    public class EnsureTest
+    {
+        [Test]
+        public void NotNull_NullObj_ThrowsArgumentNullException()
+        {
+            // Setup
+            object obj = null;
+
+            // Call
+            void Call() => Ensure.NotNull(obj, nameof(obj));
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+
+            // Assert
+            Assert.That(exception.ParamName, Is.EqualTo(nameof(obj)));
+        }
+
+        [Test]
+        public void NotNull_NonNullObj_DoesNotThrowAnException()
+        {
+            // Setup
+            var obj = new object();
+
+            // Call | Assert
+            void Call() => Ensure.NotNull(obj, nameof(obj));
+            Assert.DoesNotThrow(Call);
+        }
+    }
+}
