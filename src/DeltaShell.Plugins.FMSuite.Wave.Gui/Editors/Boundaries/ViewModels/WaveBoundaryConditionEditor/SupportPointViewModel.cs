963451b16e45d36f8aece1975202bcb591d31325
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-01-13 12:10:25 +0000

    D3DFMIQ-1615 - Added SupportPointViewModel - Added tests
    Former-commit-id: 5a46e7fbac000bb44c3d2a3e507abae69cca8878
    Former-commit-id: 3d3207653f22f167ee9017aed676f3c8cbc0ae44

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/SupportPointViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DeltaShell.NGHS.Common;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Annotations;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    /// <summary>
+    /// <see cref="SupportPointViewModel" /> defines the view model for the support point view
+    /// </summary>
+    /// <seealso cref="INotifyPropertyChanged" />
+    public class SupportPointViewModel : INotifyPropertyChanged
+    {
+        private readonly SupportPoint supportPoint;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SupportPointViewModel" /> class.
+        /// </summary>
+        /// <param name="supportPoint"> The support point. </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="supportPoint" /> is <c> null </c>.
+        /// </exception>
+        public SupportPointViewModel(SupportPoint supportPoint)
+        {
+            Ensure.NotNull(supportPoint, nameof(supportPoint));
+            this.supportPoint = supportPoint;
+        }
+
+        private bool isEnabled;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is enabled.
+        /// </summary>
+        /// <value>
+        /// <c> true </c> if this instance is enabled; otherwise, <c> false </c>.
+        /// </value>
+        public bool IsEnabled
+        {
+            get => isEnabled;
+            set
+            {
+                if (isEnabled == value)
+                {
+                    return;
+                }
+
+                isEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the distance.
+        /// </summary>
+        /// <value>
+        /// The distance.
+        /// </value>
+        public double Distance
+        {
+            get => supportPoint.Distance;
+            set
+            {
+                if (!(Math.Abs(supportPoint.Distance - value) > 1E-15))
+                {
+                    return;
+                }
+
+                supportPoint.Distance = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/SupportPointViewModelTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using DeltaShell.NGHS.IO.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    [TestFixture]
+    public class SupportPointViewModelTest
+    {
+        private readonly Random random = new Random();
+        private SupportPointViewModel viewModel;
+        private SupportPoint supportPoint;
+
+        [SetUp]
+        public void SetUp()
+        {
+            supportPoint = new SupportPoint(0, Substitute.For<IWaveBoundaryGeometricDefinition>());
+            viewModel = new SupportPointViewModel(supportPoint);
+        }
+
+        [Test]
+        public void Constructor_SupportPointNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new SupportPointViewModel(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("supportPoint"));
+        }
+
+        [TestCase(false, false, 0)]
+        [TestCase(false, true, 1)]
+        [TestCase(true, false, 1)]
+        [TestCase(true, true, 0)]
+        public void SetEnabled_PropertyChangedFiredOnce(bool originalValue, bool setValue, int expectedPropChangedCount)
+        {
+            // Setup
+            viewModel.IsEnabled = originalValue;
+
+            // Call
+            void Call() => viewModel.IsEnabled = setValue;
+
+            // Assert
+            viewModel.AssertPropertyChangedFired(Call, expectedPropChangedCount, nameof(viewModel.IsEnabled));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1E-15, 0)]
+        [TestCase(1E-14, 1)]
+        [TestCase(1, 1)]
+        public void SetDistance_CorrectDistanceIsSetOnModelAndPropertyChangedFiredOnce(double setValueDifference,
+                                                                                       int expectedPropChangedCount)
+        {
+            // Setup
+            double originalValue = random.NextDouble();
+            double setValue = originalValue + setValueDifference;
+            viewModel.Distance = originalValue;
+
+            // Call
+            void Call() => viewModel.Distance = setValue;
+
+            // Assert
+            viewModel.AssertPropertyChangedFired(Call, expectedPropChangedCount, nameof(viewModel.Distance));
+            Assert.That(supportPoint.Distance, Is.EqualTo(setValue).Within(1E-15));
+        }
+    }
+}
