b0cad7fcc6444359f47c76b3570c44f23f0a060e
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-01-13 10:29:28 +0000

    D3DFMIQ-1615 - Add PositiveDoubleValidationRule to check if input value holds a positive double value - Added tests
    Former-commit-id: ee8d38e176c974d6b6ae1b731aedc24a2d27c72f
    Former-commit-id: 9163d92e04c64ca01ea87cb8f0683f6ed9f44e32

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Validation/PositiveDoubleValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Validation
+{
+    /// <summary>
+    /// Validation rule for validating whether the input value is a positive double.
+    /// </summary>
+    /// <seealso cref="System.Windows.Controls.ValidationRule" />
+    public class PositiveDoubleValidationRule : ValidationRule
+    {
+        /// <summary>
+        /// Validates the specified <paramref name="value" /> to be a positive double value.
+        /// </summary>
+        /// <param name="value"> The value. </param>
+        /// <param name="cultureInfo"> The culture info. </param>
+        /// <remarks> <paramref name="value" /> is expected to be a <see cref="string" />. </remarks>
+        /// <returns>
+        /// A valid <see cref="ValidationResult" /> when the <paramref name="value" /> is a positive double value;
+        /// otherwise an invalid <see cref="ValidationResult" />.
+        /// </returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return FieldValidator.IsPositiveDouble((string) value, cultureInfo)
+                       ? new ValidationResult(true, null)
+                       : new ValidationResult(false, null);
+        }
+    }
+}
