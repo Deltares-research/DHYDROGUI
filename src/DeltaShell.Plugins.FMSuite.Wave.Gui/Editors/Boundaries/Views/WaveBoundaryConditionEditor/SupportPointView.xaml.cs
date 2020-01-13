d95c5744d2dc63c2b7b9d925015b98ba12b093f7
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-01-13 12:14:18 +0000

    D3DFMIQ-1615 - Added SupportPointView
    Former-commit-id: 663a2d1a3986c1f5cc0b85ec571091b0effac2dd
    Former-commit-id: 83892a5c381ed68c8b8e481899d0e2ba48fe7ce2

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/WaveBoundaryConditionEditor/SupportPointView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.WaveBoundaryConditionEditor
+{
+    /// <summary>
+    /// Interaction logic for SupportPointView.xaml
+    /// </summary>
+    public partial class SupportPointView : UserControl
+    {
+        public SupportPointView()
+        {
+            InitializeComponent();
+        }
+    }
+}
