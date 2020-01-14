37b7d7a664d9f06678dd97d17a79a7ef7a50acd9
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-01-14 09:35:56 +0000

    D3DFMIQ-1615 - Added BoundaryGeometryViewModel - Added tests
    Former-commit-id: dfd578a57f329ab9dad208756b3f0dbec7276581
    Former-commit-id: e0475c48c49f95d38762abc3a897ec5c37d95693

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryGeometryViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DeltaShell.NGHS.Common;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    /// <summary>
+    /// <see cref="BoundaryGeometryViewModel" /> defines the view model for the boundary geometry view.
+    /// </summary>
+    public class BoundaryGeometryViewModel
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="BoundaryGeometryViewModel" /> class.
+        /// </summary>
+        /// <param name="observedGeometricDefinition"> The observed geometric definition. </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="observedGeometricDefinition" /> is <c> null </c>.
+        /// </exception>
+        public BoundaryGeometryViewModel(IWaveBoundaryGeometricDefinition observedGeometricDefinition)
+        {
+            Ensure.NotNull(observedGeometricDefinition, nameof(observedGeometricDefinition));
+            SupportPointEditorViewModel = new SupportPointEditorViewModel(observedGeometricDefinition);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="SupportPointEditorViewModel" />.
+        /// </summary>
+        public SupportPointEditorViewModel SupportPointEditorViewModel { get; }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryGeometryViewModelTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    [TestFixture]
+    public class BoundaryGeometryViewModelTest
+    {
+        [Test]
+        public void Constructor_SetsCorrectValues()
+        {
+            // Setup
+            var geometricDefinition = Substitute.For<IWaveBoundaryGeometricDefinition>();
+
+            // Call
+            var viewModel = new BoundaryGeometryViewModel(geometricDefinition);
+
+            // Assert
+            Assert.That(viewModel.SupportPointEditorViewModel, Is.Not.Null,
+                        "Expected SupportPointEditorViewModel to be set.");
+        }
+
+        [Test]
+        public void Constructor_GeometricDefinitionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new BoundaryGeometryViewModel(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("observedGeometricDefinition"));
+        }
+    }
+}
