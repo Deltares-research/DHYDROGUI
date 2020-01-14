130862e4bc21a95ba606afef0e6ef5a534ee06b6
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-01-14 08:12:01 +0000

    D3DFMIQ-1615 - Added SupportPointEditorViewModel - Added tests
    Former-commit-id: 645bbaee4e7b962a3fb7294e7601741baf017a42
    Former-commit-id: c3fbd2cf326fe873fbb5acdb56364e590f6fade9

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/SupportPointEditorViewModel.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using DelftTools.Controls.Wpf.Commands;
+using DeltaShell.NGHS.Common;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Validation;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    /// <summary>
+    /// <see cref="SupportPointEditorViewModel" /> defines the view model for the support point editor view
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
+    public class SupportPointEditorViewModel : INotifyPropertyChanged
+    {
+        private readonly IWaveBoundaryGeometricDefinition geometricDefinition;
+        private ObservableCollection<SupportPointViewModel> viewModels;
+        private SupportPointViewModel selectedViewModel;
+        private double newDistance;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SupportPointListViewModel" /> class.
+        /// </summary>
+        public SupportPointEditorViewModel(IWaveBoundaryGeometricDefinition geometricDefinition)
+        {
+            Ensure.NotNull(geometricDefinition, nameof(geometricDefinition));
+            this.geometricDefinition = geometricDefinition;
+
+            RemoveSupportPointCommand = new RelayCommand(RemoveSupportPointAction);
+            AddSupportPointCommand = new RelayCommand(AddSupportPointAction);
+
+            InitializeViewModels();
+            SelectedViewModel = ViewModels.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets or sets the view models.
+        /// </summary>
+        /// <value>
+        /// The view models.
+        /// </value>
+        public ObservableCollection<SupportPointViewModel> ViewModels
+        {
+            get => viewModels;
+            private set
+            {
+                if (viewModels == value)
+                {
+                    return;
+                }
+
+                if (viewModels != null)
+                {
+                    viewModels.CollectionChanged -= OnViewModelCollectionChanged;
+                }
+
+                viewModels = value;
+
+                if (viewModels != null)
+                {
+                    viewModels.CollectionChanged += OnViewModelCollectionChanged;
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the selected view model.
+        /// </summary>
+        /// <value>
+        /// The selected view model.
+        /// </value>
+        public SupportPointViewModel SelectedViewModel
+        {
+            get => selectedViewModel;
+            set
+            {
+                if (selectedViewModel == value)
+                {
+                    return;
+                }
+
+                selectedViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets the add support point command.
+        /// </summary>
+        /// <value>
+        /// The add support point command.
+        /// </value>
+        public ICommand AddSupportPointCommand { get; }
+
+        /// <summary>
+        /// Gets the remove support point command.
+        /// </summary>
+        /// <value>
+        /// The remove support point command.
+        /// </value>
+        public ICommand RemoveSupportPointCommand { get; }
+
+        private void InitializeViewModels()
+        {
+            IOrderedEnumerable<SupportPoint> sortedSupportPoints = geometricDefinition.SupportPoints
+                                                                                      .OrderBy(sp => sp.Distance);
+            IEnumerable<SupportPointViewModel> sortedViewModels = sortedSupportPoints
+                .Select(sp => new SupportPointViewModel(sp));
+
+            ViewModels = new ObservableCollection<SupportPointViewModel>(sortedViewModels);
+        }
+
+        private void AddSupportPointAction(object value)
+        {
+            var distanceString = (string) value;
+            if (!FieldValidator.IsPositiveDouble(distanceString, CultureInfo.CurrentCulture))
+            {
+                return;
+            }
+
+            double distance = double.Parse(distanceString, NumberStyles.Any, CultureInfo.CurrentCulture);
+            if (ViewModels.Any(vm => Math.Abs(vm.Distance - distance) < 1E-15))
+            {
+                return;
+            }
+
+            var newSupportPoint = new SupportPoint(distance, geometricDefinition);
+            ViewModels.Add(new SupportPointViewModel(newSupportPoint));
+        }
+
+        private void RemoveSupportPointAction(object viewModel)
+        {
+            ViewModels.Remove((SupportPointViewModel) viewModel);
+        }
+
+        private void OnViewModelCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Remove:
+                    OnViewModelRemoved((SupportPointViewModel) e.OldItems[0]);
+                    break;
+                case NotifyCollectionChangedAction.Add:
+                    OnViewModelAdded((SupportPointViewModel) e.NewItems[0]);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                case NotifyCollectionChangedAction.Move:
+                case NotifyCollectionChangedAction.Reset:
+                    throw new NotSupportedException(nameof(e.Action));
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(e.Action));
+            }
+        }
+
+        private void OnViewModelAdded(SupportPointViewModel addedViewModel)
+        {
+            geometricDefinition.SupportPoints.Add(addedViewModel.SupportPoint);
+
+            if (ViewModels.Count == 1)
+            {
+                SelectedViewModel = ViewModels[0];
+                return;
+            }
+
+            ViewModels = new ObservableCollection<SupportPointViewModel>(ViewModels.OrderBy(vm => vm.Distance));
+        }
+
+        private void OnViewModelRemoved(SupportPointViewModel viewModel)
+        {
+            geometricDefinition.SupportPoints.Remove(viewModel.SupportPoint);
+
+            if (SelectedViewModel == viewModel)
+            {
+                SelectedViewModel = ViewModels.FirstOrDefault();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/SupportPointEditorViewModelTest.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using DelftTools.Utils.Collections;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.NGHS.IO.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor
+{
+    [TestFixture]
+    public class SupportPointEditorViewModelTest
+    {
+        private readonly Random random = new Random();
+        private SupportPointEditorViewModel viewModel;
+        private IWaveBoundaryGeometricDefinition geometricDefinition;
+
+        private IEventedList<SupportPoint> SupportPoints => geometricDefinition.SupportPoints;
+        private ObservableCollection<SupportPointViewModel> SubViewModels => viewModel.ViewModels;
+
+        [SetUp]
+        public void SetUp()
+        {
+            geometricDefinition = Substitute.For<IWaveBoundaryGeometricDefinition>();
+            geometricDefinition.SupportPoints.Returns(new EventedList<SupportPoint>());
+            viewModel = new SupportPointEditorViewModel(geometricDefinition);
+        }
+
+        [Test]
+        public void Constructor_GeometricDefinitionNull_ThrownArgumentNullException()
+        {
+            // Call
+            void Call() => new SupportPointEditorViewModel(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("geometricDefinition"));
+        }
+
+        [Test]
+        public void Constructor_SetsCorrectValues()
+        {
+            // Assert
+            Assert.That(SubViewModels, Is.Not.Null);
+            Assert.That(SubViewModels, Is.Empty);
+            Assert.That(viewModel.SelectedViewModel, Is.Null);
+
+            Assert.That(viewModel.AddSupportPointCommand, Is.Not.Null);
+            Assert.That(viewModel.RemoveSupportPointCommand, Is.Not.Null);
+        }
+
+        [Test]
+        public void Constructor_WithGeometricDefinitionWithSupportPoints_SetsCorrectValues()
+        {
+            // Setup
+            double[] distances =
+            {
+                random.NextDouble(),
+                random.NextDouble(),
+                random.NextDouble(),
+                random.NextDouble(),
+                random.NextDouble()
+            };
+            distances.ForEach(d => geometricDefinition.SupportPoints.Add(GetSupportPoint(d)));
+
+            // Call
+            viewModel = new SupportPointEditorViewModel(geometricDefinition);
+
+            // Assert
+            Assert.That(SubViewModels, Is.Not.Null);
+            Assert.That(SubViewModels, Has.Count.EqualTo(5));
+
+            double[] orderedDistances = distances.OrderBy(d => d).ToArray();
+            orderedDistances.ForEach((d, i) => Assert.That(SubViewModels[i].Distance, Is.EqualTo(d)));
+
+            Assert.That(viewModel.SelectedViewModel, Is.SameAs(SubViewModels[0]));
+
+            Assert.That(viewModel.AddSupportPointCommand, Is.Not.Null);
+            Assert.That(viewModel.RemoveSupportPointCommand, Is.Not.Null);
+        }
+
+        [TestCaseSource(nameof(SelectedViewModelCases))]
+        public void SetSelectedViewModel_WithOtherValue_PropertyChangedFiredOnce(SupportPointViewModel originalValue,
+                                                                                 SupportPointViewModel setValue,
+                                                                                 int expectedPropertyChangedCount)
+        {
+            // Setup
+            viewModel.SelectedViewModel = originalValue;
+
+            // Call
+            void Call() => viewModel.SelectedViewModel = setValue;
+
+            // Assert
+            viewModel.AssertPropertyChangedFired(Call, expectedPropertyChangedCount,
+                                                 nameof(viewModel.SelectedViewModel));
+        }
+
+        [Test]
+        public void ExecuteAddSupportPointCommand_CorrectViewModelIsAdded()
+        {
+            // Setup
+            double value = random.NextDouble();
+
+            // Call
+            viewModel.AddSupportPointCommand.Execute(value.ToString(CultureInfo.CurrentCulture));
+
+            // Assert
+            Assert.That(SubViewModels.Count, Is.EqualTo(1));
+            Assert.That(SubViewModels[0].Distance, Is.EqualTo(value).Within(1E-15));
+        }
+
+        [TestCase("-1.0")]
+        [TestCase("NaN")]
+        [TestCase("One")]
+        public void ExecuteAddSupportPointCommand_WithInvalidValue_NoViewModelIsAdded(object invalidValue)
+        {
+            // Call
+            viewModel.AddSupportPointCommand.Execute(invalidValue);
+
+            // Assert
+            Assert.That(SubViewModels.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ExecuteAddSupportPointCommand_WhenViewModelWithSameDistanceAlreadyExists_NoViewModelIsAdded()
+        {
+            // Setup
+            double value = random.NextDouble();
+            viewModel.ViewModels.Add(GetSupportPointViewModel(value));
+
+            // Call
+            viewModel.AddSupportPointCommand.Execute(value.ToString(CultureInfo.CurrentCulture));
+
+            // Assert
+            Assert.That(SubViewModels.Count, Is.EqualTo(1));
+            Assert.That(SubViewModels[0].Distance, Is.EqualTo(value).Within(1E-15));
+        }
+
+        [Test]
+        public void ExecuteRemoveSupportPointCommand_RemovesViewModel()
+        {
+            // Setup
+            SupportPointViewModel subViewModel = GetSupportPointViewModel();
+            viewModel.ViewModels.Add(subViewModel);
+
+            // Call
+            viewModel.RemoveSupportPointCommand.Execute(subViewModel);
+
+            // Assert
+            Assert.That(SubViewModels.Contains(subViewModel), Is.False);
+        }
+
+        [Test]
+        public void AddFirstViewModel_SupportPointIsAddedToGeometricDefinitionAndSelectionIsCorrect()
+        {
+            SupportPointViewModel newSubViewModel = GetSupportPointViewModel();
+
+            // Call
+            viewModel.ViewModels.Add(newSubViewModel);
+
+            // Assert
+
+            Assert.That(SupportPoints, Has.Count.EqualTo(1));
+            Assert.That(SupportPoints, Contains.Item(newSubViewModel.SupportPoint));
+            Assert.That(viewModel.SelectedViewModel, Is.SameAs(newSubViewModel));
+        }
+
+        [Test]
+        public void AddSecondViewModel_SelectedViewModelShouldNotChange()
+        {
+            // Setup
+            SupportPointViewModel firstSubViewModel = GetSupportPointViewModel();
+            viewModel.ViewModels.Add(firstSubViewModel);
+
+            // Precondition
+            Assert.That(viewModel.SelectedViewModel, Is.EqualTo(firstSubViewModel));
+
+            SupportPointViewModel newSubViewModel = GetSupportPointViewModel();
+
+            // Call
+            viewModel.ViewModels.Add(newSubViewModel);
+
+            // Assert
+            Assert.That(viewModel.SelectedViewModel, Is.SameAs(firstSubViewModel));
+
+            Assert.That(SupportPoints, Has.Count.EqualTo(2));
+            Assert.That(SupportPoints, Contains.Item(firstSubViewModel.SupportPoint));
+            Assert.That(SupportPoints, Contains.Item(newSubViewModel.SupportPoint));
+
+            Assert.That(SubViewModels[1].Distance, Is.GreaterThan(SubViewModels[0].Distance));
+        }
+
+        [Test]
+        public void AddViewModels_AllViewModelsAreSortedOnDistance()
+        {
+            // Setup
+            double[] distances =
+            {
+                random.NextDouble(),
+                random.NextDouble(),
+                random.NextDouble(),
+                random.NextDouble(),
+                random.NextDouble(),
+            };
+
+            // Calls
+            distances.ForEach(d => viewModel.ViewModels.Add(GetSupportPointViewModel(d)));
+
+            // Assert
+            double[] orderedDistances = distances.OrderBy(d => d).ToArray();
+            orderedDistances.ForEach((d, i) => Assert.That(SubViewModels[i].Distance, Is.EqualTo(d)));
+
+            // Assert
+            Assert.That(SupportPoints, Has.Count.EqualTo(5));
+            SubViewModels.ForEach(vm => Assert.That(SupportPoints, Contains.Item(vm.SupportPoint)));
+            Assert.That(viewModel.SelectedViewModel.Distance, Is.EqualTo(distances[0]));
+        }
+
+        [Test]
+        public void RemoveOnlyViewModel_SupportPointIsRemovedFromGeometricDefinitionAndSelectionIsCorrect()
+        {
+            // Setup
+            SupportPointViewModel subViewModel = GetSupportPointViewModel();
+            viewModel.ViewModels.Add(subViewModel);
+
+            // Call
+            viewModel.ViewModels.Remove(subViewModel);
+
+            // Assert
+            Assert.That(SupportPoints, Has.Count.EqualTo(0));
+            Assert.That(viewModel.SelectedViewModel, Is.Null);
+        }
+
+        [Test]
+        public void RemoveSelectedViewModel_FirstViewModelShouldBeSelected()
+        {
+            // Setup
+            SupportPointViewModel firstSubViewModel = GetSupportPointViewModel();
+            viewModel.ViewModels.Add(firstSubViewModel);
+            SupportPointViewModel subViewModel = GetSupportPointViewModel();
+            viewModel.ViewModels.Add(subViewModel);
+
+            viewModel.SelectedViewModel = subViewModel;
+
+            // Call
+            viewModel.ViewModels.Remove(subViewModel);
+
+            // Assert
+            Assert.That(SupportPoints, Has.Count.EqualTo(1));
+            Assert.That(SupportPoints.Contains(firstSubViewModel.SupportPoint));
+            Assert.That(viewModel.SelectedViewModel, Is.SameAs(firstSubViewModel));
+        }
+
+        private SupportPointViewModel GetSupportPointViewModel(double distance)
+        {
+            return new SupportPointViewModel(GetSupportPoint(distance));
+        }
+
+        private SupportPointViewModel GetSupportPointViewModel()
+        {
+            return new SupportPointViewModel(GetSupportPoint(random.NextDouble()));
+        }
+
+        private SupportPoint GetSupportPoint(double distance)
+        {
+            return new SupportPoint(distance, geometricDefinition);
+        }
+
+        private IEnumerable<TestCaseData> SelectedViewModelCases()
+        {
+            SupportPointViewModel subViewModel = GetSupportPointViewModel();
+
+            yield return new TestCaseData(subViewModel, subViewModel, 0);
+            yield return new TestCaseData(subViewModel, GetSupportPointViewModel(), 1);
+        }
+    }
+}
