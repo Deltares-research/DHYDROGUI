b9d631c22cb8dfea54aca2233d6fef1ab9c4276a
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-14 06:40:45 +0000

    D3DFMIQ-1482 - Remove unused using
    Former-commit-id: 427f96e8268de52b8ae2b48e9c11499fdf889f83
    Former-commit-id: a551deb2752b766e63be380a3df007eb0d4a5d55

--- src/DeltaShell.Plugins.FMSuite.Wave.Gui/Layers/WaveLayerFactory.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Layers/WaveLayerFactory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using DelftTools.Utils.Collections.Generic;
 using DeltaShell.NGHS.Common;
