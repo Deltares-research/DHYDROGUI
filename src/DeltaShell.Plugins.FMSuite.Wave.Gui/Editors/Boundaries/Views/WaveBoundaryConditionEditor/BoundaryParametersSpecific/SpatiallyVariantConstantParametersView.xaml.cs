fba9bea24b36e719a710588ad04185fcaa56fece
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-15 10:03:48 +0000

    D3DFMIQ-1479 - Add skeleton SpatiallyVariantConstantParametersView
    Former-commit-id: 0777eff11c6d074d29479f07090637e1a0cc89fe
    Former-commit-id: f6c808f6c93a67c3d746721bac20a5d3aa62b758

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/WaveBoundaryConditionEditor/BoundaryParametersSpecific/SpatiallyVariantConstantParametersView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    /// <summary>
+    /// Interaction logic for SpatiallyVariantConstantParametersView.xaml
+    /// </summary>
+    public partial class SpatiallyVariantConstantParametersView : UserControl
+    {
+        public SpatiallyVariantConstantParametersView()
+        {
+            InitializeComponent();
+        }
+    }
+}
