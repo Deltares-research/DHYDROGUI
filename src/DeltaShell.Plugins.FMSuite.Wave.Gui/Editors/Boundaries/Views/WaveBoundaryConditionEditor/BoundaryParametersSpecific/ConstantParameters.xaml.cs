ec7b293986de8bedb5ab9fb700ccfa30fe7e7e54
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-15 06:24:19 +0000

    D3DFMIQ-1479 - Add Placeholder uniform parameter values
    Former-commit-id: 025b4a542161838a635b9d5e8460ecd7a400c94c
    Former-commit-id: 1b0ca537d6a961b7349b86146ea17e7e1bd0acb8

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/WaveBoundaryConditionEditor/BoundaryParametersSpecific/ConstantParameters.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    /// <summary>
+    /// Interaction logic for ConstantParameters.xaml
+    /// </summary>
+    public partial class ConstantParameters : UserControl
+    {
+        public ConstantParameters()
+        {
+            InitializeComponent();
+        }
+    }
+}
