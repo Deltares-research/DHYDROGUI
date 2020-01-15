754c9882cf99cd32d983d6f4d10ffdbb1c999b4d
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-15 09:44:09 +0000

    D3DFMIQ-1479 - Add UniformConstantParametersViewModel tests
    Former-commit-id: da4080f951703c27d5948d87a765975f1f147114
    Former-commit-id: d518b7dd1dd89fdd4c627f08904dab80ea4e173b

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParametersSpecific/UniformConstantParametersViewModelTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    [TestFixture]
+    public class UniformConstantParametersViewModelTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var parameters = new ConstantParameters(0, 0, 0, 0);
+            var parametersViewModel = new ConstantParametersViewModel(parameters);
+
+            // Call
+            var viewModel = new UniformConstantParametersViewModel(parametersViewModel);
+
+            // Assert
+            Assert.That(viewModel.ActiveParametersViewModel, Is.SameAs(parametersViewModel));
+        }
+
+        [Test]
+        public void Constructor_ActiveParametersViewModelNull_ThrowsArgumentNullException()
+        {
+            void Call() => new UniformConstantParametersViewModel(null);
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("activeParametersViewModel"));
+        }
+    }
+}
