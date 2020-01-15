f8066bd99563304f9434911faea0d6a3e878f197
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-15 10:20:18 +0000

    D3DFMIQ-1479 - Add LabeledCheckBox
    Former-commit-id: 453660f98e12d689c82d95ea9c2d2cf3a2b78a17
    Former-commit-id: b1f3266a6c9b5474119b185fae6f463e79aedcec

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/CommonElements/LabeledCheckBox.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.CommonElements
+{
+    /// <summary>
+    /// Interaction logic for LabeledCheckBox.xaml
+    /// </summary>
+    public partial class LabeledCheckBox : UserControl
+    {
+        /// <summary>
+        /// The label property
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty = 
+            DependencyProperty.Register(nameof(Label), 
+                                        typeof(string), 
+                                        typeof(LabeledCheckBox), 
+                                        new FrameworkPropertyMetadata(default(string),
+                                                                      FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// Gets or sets the label of this <see cref="LabeledComboBoxControl"/>.
+        /// </summary>
+        public string Label
+        {
+            get => (string) GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        /// <summary>
+        /// The is checked property
+        /// </summary>
+        public static readonly DependencyProperty IsCheckedProperty = 
+            DependencyProperty.Register(nameof(IsChecked), 
+                                        typeof(bool), 
+                                        typeof(LabeledCheckBox), 
+                                        new FrameworkPropertyMetadata(default(bool),
+                                                                      FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is checked.
+        /// </summary>
+        public bool IsChecked
+        {
+            get => (bool) GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="LabeledCheckBox"/>.
+        /// </summary>
+        public LabeledCheckBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
