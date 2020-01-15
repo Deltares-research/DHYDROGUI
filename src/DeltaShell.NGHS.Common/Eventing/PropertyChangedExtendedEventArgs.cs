541bfe2c81632f41f0fd06651640409940b499b6
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-01-15 15:14:50 +0000

    D3DFMIQ-1615 - Added INotifyPropertyChangedExtended (holds extra OriginalValue property) - Added PropertyChangedExtendedEventArgs - Added PropertyChangedExtendedEventHandler
    Former-commit-id: 37d8de59251e9b41551dd8691b4954de608cfbad
    Former-commit-id: b65f30647c046f07761afab9e8e51c2a2f905f93

---
+++ src/DeltaShell.NGHS.Common/Eventing/PropertyChangedExtendedEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace DeltaShell.NGHS.Common.Eventing
+{
+    /// <summary>
+    /// Provides data for the <see cref="INotifyPropertyChangedExtended.PropertyChanged" /> event.
+    /// </summary>
+    /// <seealso cref="PropertyChangedEventArgs" />
+    public class PropertyChangedExtendedEventArgs : PropertyChangedEventArgs
+    {
+        /// <summary>
+        /// Gets the original property value.
+        /// </summary>
+        /// <value>
+        /// The original property value.
+        /// </value>
+        public virtual object OriginalValue { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyChangedExtendedEventArgs" /> class.
+        /// </summary>
+        /// <param name="propertyName"> Name of the property. </param>
+        /// <param name="originalValue"> The original value of the property. </param>
+        public PropertyChangedExtendedEventArgs(string propertyName, object originalValue)
+            : base(propertyName)
+        {
+            OriginalValue = originalValue;
+        }
+    }
+}
---
+++ src/DeltaShell.NGHS.Common/Eventing/PropertyChangedExtendedEventHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace DeltaShell.NGHS.Common.Eventing
+{
+    /// <summary>
+    /// Represents the method that will handle the <see cref="INotifyPropertyChangedExtended.PropertyChanged" />
+    /// event raised when a property is changed on a component.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">A <see cref="PropertyChangedExtendedEventArgs"/> that contains the event data. </param>
+    public delegate void PropertyChangedExtendedEventHandler(object sender, PropertyChangedExtendedEventArgs e);
+}
