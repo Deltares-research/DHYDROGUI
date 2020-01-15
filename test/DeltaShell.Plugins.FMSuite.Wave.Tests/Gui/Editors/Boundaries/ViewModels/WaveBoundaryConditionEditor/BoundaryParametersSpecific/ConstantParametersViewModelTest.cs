e5f1c614a6b5766b1a82985b644145dd190ee0ac
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-15 09:38:17 +0000

    D3DFMIQ-1479 - Add ConstantParametersViewModel tests
    Former-commit-id: 3c19e51e01029e4c108ee2b5608e1ebeac31abb7
    Former-commit-id: 6c00741da536103c00ef3e005111bf4c5e99cc28

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParametersSpecific/ConstantParametersViewModelTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    [TestFixture]
+    public class ConstantParametersViewModelTest
+    {
+        private readonly Random random = new Random();
+
+        private ConstantParameters GetRandomConstantParameters()
+        {
+            return new ConstantParameters(random.NextDouble() * 100.0,
+                                          random.NextDouble() * 100.0,
+                                          random.NextDouble() * 100.0,
+                                          random.NextDouble() * 100.0);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            ConstantParameters parameters = GetRandomConstantParameters();
+
+            // Call
+            var viewModel = new ConstantParametersViewModel(parameters);
+
+            // Assert
+            Assert.That(viewModel.Height,    Is.EqualTo(parameters.Height));
+            Assert.That(viewModel.Period,    Is.EqualTo(parameters.Period));
+            Assert.That(viewModel.Direction, Is.EqualTo(parameters.Direction));
+            Assert.That(viewModel.Spreading, Is.EqualTo(parameters.Spreading));
+        }
+
+        [Test]
+        public void Constructor_ParametersNull_ThrowsArgumentNullException()
+        {
+            void Call() => new ConstantParametersViewModel(null);
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("parameters"));
+        }
+
+        [Test]
+        public void SetHeight_ExpectedValues()
+        {
+            // Setup
+            ConstantParameters parameters = GetRandomConstantParameters();
+            var viewModel = new ConstantParametersViewModel(parameters);
+            const double expectedHeight = 50.0;
+
+            // Call
+            viewModel.Height = expectedHeight;
+
+            // Assert
+            Assert.That(viewModel.Height,    Is.EqualTo(expectedHeight));
+        }
+
+        [Test]
+        public void SetPeriod_ExpectedValues()
+        {
+            // Setup
+            ConstantParameters parameters = GetRandomConstantParameters();
+            var viewModel = new ConstantParametersViewModel(parameters);
+            const double expectedPeriod = 50.0;
+
+            // Call
+            viewModel.Period = expectedPeriod;
+
+            // Assert
+            Assert.That(viewModel.Period,    Is.EqualTo(expectedPeriod));
+        }
+
+        [Test]
+        public void SetDirection_ExpectedValues()
+        {
+            // Setup
+            ConstantParameters parameters = GetRandomConstantParameters();
+            var viewModel = new ConstantParametersViewModel(parameters);
+            const double expectedDirection = 50.0;
+
+            // Call
+            viewModel.Direction = expectedDirection;
+
+            // Assert
+            Assert.That(viewModel.Direction,    Is.EqualTo(expectedDirection));
+        }
+
+        [Test]
+        public void SetSpreading_ExpectedValues()
+        {
+            // Setup
+            ConstantParameters parameters = GetRandomConstantParameters();
+            var viewModel = new ConstantParametersViewModel(parameters);
+            const double expectedSpreading = 50.0;
+
+            // Call
+            viewModel.Spreading = expectedSpreading;
+
+            // Assert
+            Assert.That(viewModel.Spreading,    Is.EqualTo(expectedSpreading));
+        }
+    }
+}
