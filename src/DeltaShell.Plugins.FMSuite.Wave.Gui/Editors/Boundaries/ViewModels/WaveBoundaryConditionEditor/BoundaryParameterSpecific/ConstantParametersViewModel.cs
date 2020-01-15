be14b626a9f9c111fb024951c50ef891b4d7f195
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-15 09:05:44 +0000

    D3DFMIQ-1479 - Add ConstantParametersViewModel - Add UniformConstantParametersViewModel - Connect with corresponding views
    Former-commit-id: 0d4490f907a1bc8023a7f7002357eecf593aa38e
    Former-commit-id: 2b47268748a4e9da8fe5b1b16c8bc94bbcc72997

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParameterSpecific/ConstantParametersViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using DeltaShell.NGHS.Common;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific
+{
+    /// <summary>
+    /// <see cref="ConstantParametersViewModel"/> defines the view model for the ConstantParametersView.
+    /// </summary>
+    public class ConstantParametersViewModel
+    {
+        private readonly ConstantParameters observedConstantParameters;
+
+        /// <summary>
+        /// Creates a new <see cref="ConstantParametersViewModel"/>.
+        /// </summary>
+        /// <param name="parameters"> The observed constant parameters. </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="parameters"/> is <c>null</c>;
+        /// </exception>
+        public ConstantParametersViewModel(ConstantParameters parameters)
+        {
+            Ensure.NotNull(parameters, nameof(parameters));
+            observedConstantParameters = parameters;
+        }
+
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
+        public double Height
+        {
+            get => observedConstantParameters.Height;
+            set => observedConstantParameters.Height = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the period.
+        /// </summary>
+        public double Period
+        {
+            get => observedConstantParameters.Period;
+            set => observedConstantParameters.Period = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the direction.
+        /// </summary>
+        public double Direction
+        {
+            get => observedConstantParameters.Direction;
+            set => observedConstantParameters.Direction = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the spreading.
+        /// </summary>
+        public double Spreading
+        {
+            get => observedConstantParameters.Spreading;
+            set => observedConstantParameters.Spreading = value;
+        }
+    }
+}
---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParameterSpecific/UniformConstantParametersViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DeltaShell.NGHS.Common;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific
+{
+    /// <summary>
+    /// <see cref="UniformConstantParametersViewModel"/> defines the view model for the UniformConstantParametersView.
+    /// </summary>
+    public class UniformConstantParametersViewModel
+    {
+        /// <summary>
+        /// Creates a new <see cref="UniformConstantParametersViewModel"/>.
+        /// </summary>
+        /// <param name="activeParametersViewModel"> The vie data to be displayed.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="activeParametersViewModel"/> is <c>null</c>.
+        /// </exception>
+        public UniformConstantParametersViewModel(ConstantParametersViewModel activeParametersViewModel)
+        {
+            Ensure.NotNull(activeParametersViewModel, nameof(activeParametersViewModel));
+            ActiveParametersViewModel = activeParametersViewModel;
+        }
+
+        /// <summary>
+        /// Get the currently displayed <see cref="ConstantParametersViewModel"/>.
+        /// </summary>
+        public ConstantParametersViewModel ActiveParametersViewModel { get; }
+    }
+}
