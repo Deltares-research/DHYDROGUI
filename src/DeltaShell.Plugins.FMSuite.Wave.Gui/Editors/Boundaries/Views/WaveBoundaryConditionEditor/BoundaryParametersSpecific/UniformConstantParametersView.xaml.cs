a61d54d61ec7db65e077efe21de5f418f8de25b1
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-15 06:36:15 +0000

    D3DFMIQ-1479 - Add skeleton UniformConstantParametersView
    Former-commit-id: 97aeb85169bf7fff17b61e6c0596bb929cb4d776
    Former-commit-id: 99cecc767a98408b6ebd2e3b74b3f27b582c75f8

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/WaveBoundaryConditionEditor/BoundaryParametersSpecific/UniformConstantParametersView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    /// <summary>
+    /// Interaction logic for UniformConstantParametersView.xaml
+    /// </summary>
+    public partial class UniformConstantParameters : UserControl
+    {
+        public UniformConstantParameters()
+        {
+            InitializeComponent();
+        }
+    }
+}
