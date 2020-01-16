ee9eaa57b8dffb1b4d70e6067e630bda4d981d34
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-16 10:35:55 +0000

    D3DFMIQ-1479 - Add IViewDataComponentFactory
    Former-commit-id: f7c005376c32919ba8cf65d72be2e4a8f0ec0bb5
    Former-commit-id: 53efb4e78acd9cd3250b5c6d3c06a020e2a45d1a

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Factories/IViewDataComponentFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.DataComponents;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Enums;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Factories
+{
+    /// <summary>
+    /// <see cref="IViewDataComponentFactory"/> defines the interface with
+    /// which to create the ViewModels necessary for the
+    /// BoundaryParameterSpecific view.
+    /// </summary>
+    public interface IViewDataComponentFactory
+    {
+        /// <summary>
+        /// Gets the <see cref="ForcingViewType"/> corresponding with the
+        /// provided <paramref name="dataComponent"/>.
+        /// </summary>
+        /// <param name="dataComponent">The data component.</param>
+        /// <returns>
+        /// The <see cref="ForcingViewType"/> corresponding with the provided
+        /// <paramref name="dataComponent"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="dataComponent"/> is <c>null</c>.
+        /// </exception>
+        ForcingViewType GetForcingType(IBoundaryConditionDataComponent dataComponent);
+
+        /// <summary>
+        /// Gets the <see cref="SpatialDefinitionViewType"/> corresponding with
+        /// the provided <paramref name="dataComponent"/>.
+        /// </summary>
+        /// <param name="dataComponent">The data component.</param>
+        /// <returns>
+        /// The <see cref="SpatialDefinitionViewType"/> corresponding with the provided
+        /// <paramref name="dataComponent"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="dataComponent"/> is <c>null</c>.
+        /// </exception>
+        SpatialDefinitionViewType GetSpatialDefinition(IBoundaryConditionDataComponent dataComponent);
+    }
+}
