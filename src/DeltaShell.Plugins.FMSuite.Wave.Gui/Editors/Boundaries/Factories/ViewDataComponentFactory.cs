7dfac026f428acd956e2dcd0309880f3772301aa
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-16 11:02:20 +0000

    D3DFMIQ-1479 - Add ViewDataComponentFactory - Add corresponding tests
    Former-commit-id: 1be78b3bd1ef34c6ad1c29890f2af59c12d1a2c9
    Former-commit-id: e5ac37058525dde92ac90b58d3b5f084cebfeeb1

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Factories/ViewDataComponentFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DeltaShell.NGHS.Common;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.DataComponents;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Enums;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Factories
+{
+    /// <summary>
+    /// <see cref="ViewDataComponentFactory"/> implements the interface with
+    /// which to create the ViewModels necessary for the
+    /// BoundaryParameterSpecific view.
+    /// </summary>
+    /// <seealso cref="IViewDataComponentFactory" />
+    public class ViewDataComponentFactory : IViewDataComponentFactory
+    {
+        public ForcingViewType GetForcingType(IBoundaryConditionDataComponent dataComponent)
+        {
+            Ensure.NotNull(dataComponent, nameof(dataComponent));
+
+            return ForcingViewType.Constant;
+        }
+
+        public SpatialDefinitionViewType GetSpatialDefinition(IBoundaryConditionDataComponent dataComponent)
+        {
+            Ensure.NotNull(dataComponent, nameof(dataComponent));
+
+            switch (dataComponent)
+            {
+                case UniformDataComponent _:
+                    return SpatialDefinitionViewType.Uniform;
+                case SpatiallyVaryingDataComponent _:
+                    return SpatialDefinitionViewType.SpatiallyVarying;
+                default:
+                    throw new NotSupportedException("The type of the specified dataComponent does not correspond with a supported ");
+            }
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/Factories/ViewDataComponentFactoryTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.DataComponents;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Enums;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Factories;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.Factories
+{
+    [TestFixture]
+    public class ViewDataComponentFactoryTest
+    {
+        [Test]
+        public void Constructor_ExpectedResults()
+        {
+            // Call
+            var factory = new ViewDataComponentFactory();
+
+            // Assert
+            Assert.That(factory, Is.InstanceOf<IViewDataComponentFactory>());
+        }
+
+        [Test]
+        public void GetForcingType_ReturnsConstant()
+        {
+            // Setup
+            var dataComponent = Substitute.For<IBoundaryConditionDataComponent>();
+            var factory = new ViewDataComponentFactory();
+
+            // Call
+            ForcingViewType result = factory.GetForcingType(dataComponent);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(ForcingViewType.Constant));
+        }
+
+        [Test]
+        public void GetForcingType_DataComponentNull_ThrowsArgumentNullException()
+        {
+            var factory = new ViewDataComponentFactory();
+
+            void Call() => factory.GetForcingType(null);
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("dataComponent"));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetSpatialDefinitionData))]
+        public void GetSpatialDefinition_ValidData_ReturnsCorrectResults(IBoundaryConditionDataComponent dataComponent,
+                                                                         SpatialDefinitionViewType expecteDefinitionViewType)
+        {
+            // Setup
+            var factory = new ViewDataComponentFactory();
+
+            // Call
+            SpatialDefinitionViewType result = factory.GetSpatialDefinition(dataComponent);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expecteDefinitionViewType));
+        }
+
+        private static IEnumerable<TestCaseData> GetSpatialDefinitionData()
+        {
+            yield return  new TestCaseData(new UniformDataComponent(Substitute.For<IBoundaryConditionParameters>()), SpatialDefinitionViewType.Uniform);
+            yield return  new TestCaseData(new SpatiallyVaryingDataComponent(), SpatialDefinitionViewType.SpatiallyVarying);
+        }
+
+        [Test]
+        public void GetSpatialDefinition_InvalidDataComponent_ThrowsNotSupportedException()
+        { 
+            var factory = new ViewDataComponentFactory();
+            var dataComponent = Substitute.For<IBoundaryConditionDataComponent>();
+
+            void Call() => factory.GetSpatialDefinition(dataComponent);
+
+            Assert.Throws<NotSupportedException>(Call);
+        }
+
+        [Test]
+        public void GetSpatialDefinition_DataComponentNull_ThrowsArgumentNullException()
+        {
+            var factory = new ViewDataComponentFactory();
+
+            void Call() => factory.GetSpatialDefinition(null);
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("dataComponent"));
+        }
+
+        
+    }
+}
