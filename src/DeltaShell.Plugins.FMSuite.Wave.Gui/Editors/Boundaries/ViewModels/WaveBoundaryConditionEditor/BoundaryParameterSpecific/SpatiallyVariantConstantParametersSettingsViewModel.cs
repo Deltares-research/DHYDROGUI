635d844c131ea10e0868316e5d683a180078bd45
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-17 11:02:22 +0000

    D3DFMIQ-1479 - Add SpatiallyVariantConstantParametersSettingsViewModel - Add corresponding tests
    Former-commit-id: 7fc61a288c2bd274abac23a39fff18d3d99c1ebb
    Former-commit-id: 5a4a85bc6825109d6a3a329c292e87ffb766bdd7

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParameterSpecific/SpatiallyVariantConstantParametersSettingsViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DeltaShell.NGHS.Common;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific
+{
+    /// <summary>
+    /// <see cref="SpatiallyVariantConstantParametersSettingsViewModel"/> defines the view model for the
+    /// ConstantParametersSettingsView given spatially varying data.
+    /// </summary>
+    /// <seealso cref="IConstantParametersSettingsViewModel" />
+    /// <seealso cref="INotifyPropertyChanged" />
+    public sealed class SpatiallyVariantConstantParametersSettingsViewModel : IConstantParametersSettingsViewModel, INotifyPropertyChanged
+    {
+        private readonly IReadOnlyDictionary<SupportPoint, ConstantParameters> supportPointToParametersMapping;
+
+        /// <summary>
+        /// Creates a new <see cref="SpatiallyVariantConstantParametersSettingsViewModel"/>.
+        /// </summary>
+        /// <param name="supportPointToParametersMapping">
+        /// The mapping of support points to their corresponding <see cref="ConstantParameters"/>.
+        /// </param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <paramref name="supportPointToParametersMapping"/> is <c>null</c>.
+        /// </exception>
+         public SpatiallyVariantConstantParametersSettingsViewModel(IReadOnlyDictionary<SupportPoint, ConstantParameters> supportPointToParametersMapping)
+        {
+            Ensure.NotNull(supportPointToParametersMapping, nameof(supportPointToParametersMapping));
+            this.supportPointToParametersMapping = supportPointToParametersMapping;
+        }
+
+        public ConstantParametersViewModel ActiveParametersViewModel
+        {
+            get => activeParametersViewModel;
+            private set
+            {
+                if (value == ActiveParametersViewModel)
+                {
+                    return;
+                }
+
+                activeParametersViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ConstantParametersViewModel activeParametersViewModel;
+
+        /// <summary>
+        /// Updates the currently selected <see cref="ConstantParameters"/>
+        /// with the newly selected <paramref name="supportPoint"/>.
+        /// </summary>
+        /// <param name="supportPoint">The support point.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when <paramref name="supportPoint"/> is null.
+        /// </exception>
+        public void UpdateActiveSupportPoint(SupportPoint supportPoint)
+        {
+            Ensure.NotNull(supportPoint, nameof(supportPoint));
+
+            ConstantParameters correspondingParameters =
+                supportPointToParametersMapping.TryGetValue(supportPoint, out ConstantParameters value) 
+                    ? value 
+                    : null;
+
+            if (correspondingParameters == ActiveParametersViewModel?.ObservedParameters)
+            {
+                return;
+            }
+
+            ActiveParametersViewModel = correspondingParameters != null
+                                            ? new ConstantParametersViewModel(correspondingParameters)
+                                            : null;
+        }
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParametersSpecific/SpatiallyVariantConstantParametersSettingsViewModelTest.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using DeltaShell.NGHS.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    [TestFixture]
+    public class SpatiallyVariantConstantParametersSettingsViewModelTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var dictionary = new Dictionary<SupportPoint, ConstantParameters>();
+
+            // Call
+            var viewModel = new SpatiallyVariantConstantParametersSettingsViewModel(dictionary);
+
+            // Assert
+            Assert.That(viewModel, Is.InstanceOf<IConstantParametersSettingsViewModel>());
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Null, 
+                        "Expected ActiveParametersViewModel to be null upon construction.");
+        }
+
+        [Test]
+        public void Constructor_SupportPointToParametersMappingNull_ThrowsArgumentNullException()
+        {
+            void Call() => new SpatiallyVariantConstantParametersSettingsViewModel(null);
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("supportPointToParametersMapping"));
+        }
+
+
+        [Test]
+        public void UpdateActiveSupportPoint_ObservedParameters_ValueInDictionary()
+        {
+            // Setup
+            var supportPoint = new SupportPoint(20.0, Substitute.For<IWaveBoundaryGeometricDefinition>());
+            var dictionary = new Dictionary<SupportPoint, ConstantParameters>()
+            {
+                {supportPoint, new ConstantParameters(0.0, 0.0, 0.0, 0.0)}
+            };
+
+            var viewModel = new SpatiallyVariantConstantParametersSettingsViewModel(dictionary);
+
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Null, "Precondition violated.");
+
+            var propertyChangedObserver = new NotifyPropertyChangedTestObserver();
+            viewModel.PropertyChanged += propertyChangedObserver.OnPropertyChanged;
+
+            // Call
+            viewModel.UpdateActiveSupportPoint(supportPoint);
+
+            // Assert
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Not.Null);
+            Assert.That(viewModel.ActiveParametersViewModel.ObservedParameters, 
+                        Is.SameAs(dictionary[supportPoint]));
+
+            Assert.That(propertyChangedObserver.NCalls, Is.EqualTo(1));
+            Assert.That(propertyChangedObserver.Senders.First(), Is.SameAs(viewModel));
+
+            PropertyChangedEventArgs relevantEventArgs = propertyChangedObserver.EventArgses.First();
+            Assert.That(relevantEventArgs.PropertyName, 
+                        Is.EqualTo(nameof(SpatiallyVariantConstantParametersSettingsViewModel.ActiveParametersViewModel)));
+        }
+
+        [Test]
+        public void UpdateActiveSupportPoint_ObservedParameters_ValueNotInDictionary()
+        {
+            // Setup
+            var supportPoint = new SupportPoint(20.0, Substitute.For<IWaveBoundaryGeometricDefinition>());
+            var dictionary = new Dictionary<SupportPoint, ConstantParameters>()
+            {
+                {supportPoint, new ConstantParameters(0.0, 0.0, 0.0, 0.0)}
+            };
+
+            var viewModel = new SpatiallyVariantConstantParametersSettingsViewModel(dictionary);
+
+            viewModel.UpdateActiveSupportPoint(supportPoint);
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Not.Null, "Precondition violated.");
+
+            var propertyChangedObserver = new NotifyPropertyChangedTestObserver();
+            viewModel.PropertyChanged += propertyChangedObserver.OnPropertyChanged;
+
+            var otherSupportPoint = new SupportPoint(20.0, Substitute.For<IWaveBoundaryGeometricDefinition>());
+
+            // Call
+            viewModel.UpdateActiveSupportPoint(otherSupportPoint);
+
+            // Assert
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Null);
+
+            Assert.That(propertyChangedObserver.NCalls, Is.EqualTo(1));
+            Assert.That(propertyChangedObserver.Senders.First(), Is.SameAs(viewModel));
+
+            PropertyChangedEventArgs relevantEventArgs = propertyChangedObserver.EventArgses.First();
+            Assert.That(relevantEventArgs.PropertyName, 
+                        Is.EqualTo(nameof(SpatiallyVariantConstantParametersSettingsViewModel.ActiveParametersViewModel)));
+        }
+
+        [Test]
+        public void UpdateActiveSupportPoint_ObservedParameters_ValueAlreadySet()
+        {
+            // Setup
+            var supportPoint = new SupportPoint(20.0, Substitute.For<IWaveBoundaryGeometricDefinition>());
+            var dictionary = new Dictionary<SupportPoint, ConstantParameters>()
+            {
+                {supportPoint, new ConstantParameters(0.0, 0.0, 0.0, 0.0)}
+            };
+
+            var viewModel = new SpatiallyVariantConstantParametersSettingsViewModel(dictionary);
+
+            viewModel.UpdateActiveSupportPoint(supportPoint);
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Not.Null, "Precondition violated.");
+            Assert.That(viewModel.ActiveParametersViewModel.ObservedParameters, 
+                        Is.SameAs(dictionary[supportPoint]), "Precondition violated.");
+
+            var propertyChangedObserver = new NotifyPropertyChangedTestObserver();
+            viewModel.PropertyChanged += propertyChangedObserver.OnPropertyChanged;
+
+            // Call
+            viewModel.UpdateActiveSupportPoint(supportPoint);
+
+            // Assert
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Not.Null);
+            Assert.That(viewModel.ActiveParametersViewModel.ObservedParameters, 
+                        Is.SameAs(dictionary[supportPoint]));
+
+            Assert.That(propertyChangedObserver.NCalls, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void UpdateActiveSupportPoint_ObservedParameters_ActiveParametersAlreadySet()
+        {
+            // Setup
+            var dictionary = new Dictionary<SupportPoint, ConstantParameters>();
+            var viewModel = new SpatiallyVariantConstantParametersSettingsViewModel(dictionary);
+
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Null, "Precondition violated.");
+
+            var propertyChangedObserver = new NotifyPropertyChangedTestObserver();
+            viewModel.PropertyChanged += propertyChangedObserver.OnPropertyChanged;
+
+            var supportPoint = new SupportPoint(20.0, Substitute.For<IWaveBoundaryGeometricDefinition>());
+
+            // Call
+            viewModel.UpdateActiveSupportPoint(supportPoint);
+
+            // Assert
+            Assert.That(viewModel.ActiveParametersViewModel, Is.Null);
+            Assert.That(propertyChangedObserver.NCalls, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void UpdateActiveSupportPoint_SupportPointNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var dictionary = new Dictionary<SupportPoint, ConstantParameters>();
+            var viewModel = new SpatiallyVariantConstantParametersSettingsViewModel(dictionary);
+
+            // Call | Assert
+            void Call() => viewModel.UpdateActiveSupportPoint(null);
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("supportPoint"));
+        }
+    }
+}
