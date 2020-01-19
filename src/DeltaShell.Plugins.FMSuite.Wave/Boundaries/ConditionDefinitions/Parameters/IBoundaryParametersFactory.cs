eec0897abc4dc69ba02ddcd6615251a8f3743320
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-01-19 14:06:52 +0000

    D3DFMIQ-1479 - Add IBoundaryParametersFactory
    Former-commit-id: ed2dfa73bab2e8e99f6639d51c199231c42c0f30
    Former-commit-id: a484a44f036146c67e9e08f2677a126899897e9d

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Parameters/IBoundaryParametersFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters
+{
+    /// <summary>
+    /// <see cref="IBoundaryParametersFactory"/> defines the interface with which to construct
+    /// <see cref="IBoundaryConditionParameters"/>.
+    /// </summary>
+    public interface IBoundaryParametersFactory
+    {
+        /// <summary>
+        /// Construct a new <see cref="ConstantParameters"/> instance with default values.
+        /// </summary>
+        ConstantParameters ConstructDefaultConstantParameters();
+
+        /// <summary>
+        /// Construct a new <see cref="ConstantParameters"/> instance.
+        /// </summary>
+        /// <param name="height">The height.</param>
+        /// <param name="period">The period.</param>
+        /// <param name="direction">The direction.</param>
+        /// <param name="spreading">The spreading.</param>
+        ConstantParameters ConstructConstantParameters(double height,
+                                                       double period,
+                                                       double direction,
+                                                       double spreading);
+    }
+}
