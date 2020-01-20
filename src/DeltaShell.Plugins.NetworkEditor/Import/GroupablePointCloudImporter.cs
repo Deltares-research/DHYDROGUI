1e654c28ea648a6e63b93e93533ec50ff71e7e8d
kahmann <kahmann@deltares.nl>
2020-01-20 12:50:13 +0000

    D3DFMIQ-1580: Refactored code in GroupablePointCloudImporter and created a test for checking the progress messages during import. Former-commit-id: 354753e165133d71bc7be68c46eb2f42f001ee43 Former-commit-id: c6e43c9bb42a1718eb4602b20d94bf0b1c5bf2f1

--- src/DeltaShell.Plugins.NetworkEditor/Import/GroupablePointCloudImporter.cs
+++ src/DeltaShell.Plugins.NetworkEditor/Import/GroupablePointCloudImporter.cs
@@ -8,20 +8,16 @@ using DelftTools.Utils.Editing;
 using DelftTools.Utils.IO;
 using DeltaShell.Plugins.SharpMapGis.ImportExport;
 using DeltaShell.Plugins.SharpMapGis.Properties;
-using GeoAPI.Extensions.Feature;
 using log4net;
 
 namespace DeltaShell.Plugins.NetworkEditor.Import
 {
     public class GroupablePointCloudImporter : PointCloudImporter<GroupablePointFeature>
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof(GroupablePointCloudImporter));
-        private const int GroupNameProgressTextInterval = 1000;
+        private static readonly ILog log = LogManager.GetLogger(typeof(GroupablePointCloudImporter));
+        private const int groupNameProgressTextInterval = 1000;
         
-        public override bool CanImportOnRootLevel
-        {
-            get { return false; }
-        }
+        public override bool CanImportOnRootLevel => false;
 
         public Func<IList<GroupablePointFeature>, string> GetBaseFolder { get; set; }
 
@@ -29,10 +25,9 @@ namespace DeltaShell.Plugins.NetworkEditor.Import
         {
             try
             {
-                var dataItem = target as IDataItem;
-                var pointFeatureList = dataItem != null
-                    ? dataItem.Value as IList<GroupablePointFeature>
-                    : target as IList<GroupablePointFeature>;
+                IList<GroupablePointFeature> pointFeatureList = target is IDataItem dataItem
+                                                                    ? dataItem.Value as IList<GroupablePointFeature>
+                                                                    : target as IList<GroupablePointFeature>;
 
                 if (pointFeatureList == null) return null;
 
@@ -49,18 +44,17 @@ namespace DeltaShell.Plugins.NetworkEditor.Import
                 var importedFeatures = new List<GroupablePointFeature>();
                 onImportItem = base.OnImportItem(path, importedFeatures);
 
-                var region = GetRegion?.Invoke(pointFeatureList);
-                var baseFolder = GetBaseFolder(pointFeatureList);
-                var relativePathToFile = FileUtils.GetRelativePath(baseFolder, path);
-                var groupName = GroupableFeatureExtensions.GetNewGroupName(relativePathToFile, baseFolder, path);
+                string baseFolder = GetBaseFolder(pointFeatureList);
+                string relativePathToFile = FileUtils.GetRelativePath(baseFolder, path);
+                string groupName = GroupableFeatureExtensions.GetNewGroupName(relativePathToFile, baseFolder, path);
 
-                AddNewFeaturesToListWithGroupName(region, pointFeatureList, importedFeatures, groupName);
+                AddNewFeaturesToListWithGroupName(pointFeatureList, importedFeatures, groupName);
 
                 return onImportItem;
             }
             catch (Exception e)
             {
-                Log.Error(e.Message);
+                log.Error(e.Message);
             }
 
             return null;
@@ -68,44 +62,45 @@ namespace DeltaShell.Plugins.NetworkEditor.Import
 
         /// <summary>
         /// We rename the group names of all GroupablePointFeatures and add them to the original list of features.
-        /// This is done in batches of the number that is defined by 'groupNameProgressTextInterval', as we execute this code on
+        /// This is done in batches of the number that is defined by <see cref="groupNameProgressTextInterval"/>, as we execute this code on
         /// a different thread in method AddFeatures and we can only change the progress text (shown to the user) on the current thread.
         /// </summary>
-        /// <param name="region">The region where oiginalfeatures come from. We disable event bubbling while adding features to the originalFeatures list.</param>
-        /// <param name="originalFeatures">The feature list on which the newly imported features will be added.</param>
-        /// <param name="newFeatures">The new features that will be added to originalFeatures list after renaming their group names.</param>
-        /// <param name="groupName">The new group name.</param>
-        private void AddNewFeaturesToListWithGroupName(IRegion region, IList<GroupablePointFeature> originalFeatures, List<GroupablePointFeature> newFeatures, string groupName)
+        /// <param name="originalFeatures"> The feature list on which the newly imported features will be added. </param>
+        /// <param name="newFeatures"> The new features that will be added to originalFeatures list after renaming their group names. </param>
+        /// <param name="groupName"> The new group name. </param>
+        private void AddNewFeaturesToListWithGroupName(ICollection<GroupablePointFeature> originalFeatures, List<GroupablePointFeature> newFeatures, string groupName)
         {
-            region?.BeginEdit("Setting group names");
-            var numOfFeaturesToAdd = newFeatures.Count;
-            for (var startIndex = 0; startIndex < numOfFeaturesToAdd; startIndex += GroupNameProgressTextInterval)
+            GetRegion(originalFeatures)?.BeginEdit("Setting group names");
+
+            int numOfFeaturesToAdd = newFeatures.Count;
+            for (var startIndex = 0; startIndex < numOfFeaturesToAdd; startIndex += groupNameProgressTextInterval)
             {
-                var numOfFeaturesLeft = numOfFeaturesToAdd - startIndex;
-                var listToAdd = numOfFeaturesLeft > GroupNameProgressTextInterval
-                                    ? newFeatures.GetRange(startIndex, GroupNameProgressTextInterval)
-                                    : newFeatures.GetRange(startIndex, numOfFeaturesLeft);
+                int numOfFeaturesLeft = numOfFeaturesToAdd - startIndex;
+                List<GroupablePointFeature> listToAdd = numOfFeaturesLeft > groupNameProgressTextInterval
+                                                            ? newFeatures.GetRange(startIndex, groupNameProgressTextInterval)
+                                                            : newFeatures.GetRange(startIndex, numOfFeaturesLeft);
                 AddFeatures(listToAdd, originalFeatures, groupName);
                 UpdateProgress(string.Format(Properties.Resources.GroupablePointCloudImporter_Setting_group_names__0_____1_, startIndex, newFeatures.Count), 2);
             }
 
             UpdateProgress(string.Format(Resources.PointCloudImporter_OnImportItem_Finished_importing__0__point_features, numOfFeaturesToAdd), 3);
-            region?.EndEdit();
+
+            GetRegion(originalFeatures)?.EndEdit();
         }
 
         /// <summary>
-        /// Adds the features in newList to originalList, after changing the group name of the feature.
+        /// Adds the features in <paramref name="newFeatures"/> to <paramref name="originalFeatures"/>, after changing the group name of the feature.
         /// </summary>
-        /// <param name="newList">New list of features.</param>
-        /// <param name="originalList">original list of features.</param>
-        /// <param name="groupName">The new group name of the new features.</param>
+        /// <param name="newFeatures"> A collection of new features. </param>
+        /// <param name="originalFeatures"> The list of features to add the new features to. </param>
+        /// <param name="groupName"> The new group name of the new features. </param>
         [InvokeRequired]
-        private static void AddFeatures(IList<GroupablePointFeature> newList, IList<GroupablePointFeature> originalList, string groupName)
+        private static void AddFeatures(IEnumerable<GroupablePointFeature> newFeatures, ICollection<GroupablePointFeature> originalFeatures, string groupName)
         {
-            foreach (var groupablePointFeature in newList)
+            foreach (GroupablePointFeature groupablePointFeature in newFeatures)
             {
                 groupablePointFeature.GroupName = groupName;
-                originalList.Add(groupablePointFeature);
+                originalFeatures.Add(groupablePointFeature);
             }
         }
     }
