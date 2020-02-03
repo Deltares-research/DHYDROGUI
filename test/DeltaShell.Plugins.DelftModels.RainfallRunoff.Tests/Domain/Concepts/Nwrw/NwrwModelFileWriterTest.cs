adb53c9eb78dacad3f0cfaabdad649e16baf25f7
Tim van den Aardweg <Tim.vandenAardweg@deltares.nl>
2020-02-03 07:45:47 +0000

    FM1D2D-446 Added missing test.
    Former-commit-id: 19bdf9bdd018afa36bae43c24f134b2f8e64828a
    Former-commit-id: 66ea543c596fc13762b31c2eaa821b3f66504c44

---
+++ test/DeltaShell.Plugins.DelftModels.RainfallRunoff.Tests/Domain/Concepts/Nwrw/NwrwModelFileWriterTest.cs
@@ -0,0 +1,38 @@
+﻿using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain.Concepts.Nwrw;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Tests.Domain.Concepts.Nwrw
+{
+    [TestFixture]
+    public class NwrwModelFileWriterTest
+    {
+        [Test]
+        public void Write_WithWriters_CallWritersWithCorrectArguments()
+        {
+            // Setup
+            const string path = "just path";
+
+            var mocks = new MockRepository();
+            var writerOne = mocks.StrictMock<NwrwComponentFileWriterBase>();
+            writerOne.Expect(writer => writer.Write(path));
+
+            var writerTwo = mocks.StrictMock<NwrwComponentFileWriterBase>();
+            writerTwo.Expect(writer => writer.Write(path));
+            mocks.ReplayAll();
+
+            var modelWriter = new NwrwModelFileWriter(new []
+            {
+                writerOne,
+                writerTwo
+            });
+
+            // Call
+            modelWriter.WriteNwrwFiles(path);
+
+            // Assert
+            mocks.VerifyAll(); // Verifies whether the write methods are called accordingly
+            
+        }
+    }
+}
