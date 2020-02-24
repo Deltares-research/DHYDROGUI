3bc132abebde7892e6acef094140957719b724bc
Tim van den Aardweg <Tim.vandenAardweg@deltares.nl>
2020-02-24 15:13:41 +0000

    FM1D2D-643: - Added tests for DefinitionsProvider class. Former-commit-id: 7e0bb9d3cd775bf6c86a86a49775a64c674a281f Former-commit-id: 1cde14120efa0a279f5b32977032aa5262367c83

---
+++ test/DeltaShell.Plugins.ImportExport.GWSW.Tests/IO/Importers/DefinitionsVersionProviderTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using DelftTools.Hydro;
+using DelftTools.TestUtils;
+using DelftTools.Utils.IO;
+using DeltaShell.Plugins.ImportExport.Gwsw;
+using DeltaShell.Plugins.ImportExport.GWSW.Properties;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.ImportExport.GWSW.Tests.IO.Importers
+{
+    [TestFixture]
+    public class DefinitionsVersionProviderTest
+    {
+        /*
+         *  Currently, we determine the Gwsw version by reading the header of knooppunt.csv.
+         *  If the header contains a column 'AAN_PRO', we deal with the new Gwsw format.
+         */
+
+        [Test]
+        public void GivenInvalidFolderToImport_WhenDeterminingGwswVersion_ThenOldDefinitionIsReturnedWithWarning()
+        {
+            var testDir = FileUtils.CreateTempDirectory();
+
+            try
+            {
+                string gwswVersion = DefinitionsVersionProvider.GetDefinitionVersionName(testDir);
+                Assert.That(gwswVersion, Is.EqualTo("GWSWDefinition"));
+
+                var msg =
+                    "Can't determine the Gwsw file format. Please select a folder with a valid Verbinding.csv file.";
+                TestHelper.AssertAtLeastOneLogMessagesContains(
+                    () => DefinitionsVersionProvider.GetDefinitionVersionName(testDir), msg);
+            }
+            finally
+            {
+                FileUtils.DeleteIfExists(testDir);
+            }
+        }
+
+        [Test]
+        public void GivenValidGwswFolderToImportWithOldVersion_WhenDeterminingGwswVersion_ThenOldDefinitionIsReturned()
+        {
+            var originalDir = TestHelper.GetTestFilePath(@"gwswFiles\2Connection3Manholes");
+            var testDir = FileUtils.CreateTempDirectory();
+            FileUtils.CopyDirectory(originalDir, testDir);
+
+            try
+            {
+                string gwswVersion = DefinitionsVersionProvider.GetDefinitionVersionName(testDir);
+                Assert.That(gwswVersion, Is.EqualTo("GWSWDefinition"));
+            }
+            finally
+            {
+                FileUtils.DeleteIfExists(testDir);
+            }
+        }
+
+        [Test]
+        public void GivenValidGwswFolderToImportWithNewVersion_WhenDeterminingGwswVersion_ThenOldDefinitionIsReturned()
+        {
+            var originalDir = TestHelper.GetTestFilePath(@"gwswFiles\GWSW_Juinen_New");
+            var testDir = FileUtils.CreateTempDirectory();
+            FileUtils.CopyDirectory(originalDir, testDir);
+
+            try
+            {
+                string gwswVersion = DefinitionsVersionProvider.GetDefinitionVersionName(testDir);
+                Assert.That(gwswVersion, Is.EqualTo("GWSWDefinition1_5"));
+            }
+            finally
+            {
+                FileUtils.DeleteIfExists(testDir);
+            }
+        }
+    }
+}
