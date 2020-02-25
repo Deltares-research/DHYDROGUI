ec06eb3e56ec6cdb9bf653a94302393c9c0f5255
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-02-25 14:57:04 +0000

    D3DFMIQ-1628 - Introduce new WaveEnvironmentHelper
    Former-commit-id: 91f6b35d522f110821bd652eec17ac643e55dfa0
    Former-commit-id: b382351e1b4862550e2fc3621289741b7a5d62b3

--- src/DeltaShell.Plugins.FMSuite.Wave/WaveModel.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/WaveModel.cs
@@ -1155,13 +1155,6 @@ namespace DeltaShell.Plugins.FMSuite.Wave
 
         private static void RestoreEnvironment()
         {
-            string oldD3DHome = Environment.GetEnvironmentVariable("OLD_D3D_HOME");
-            if (!string.IsNullOrEmpty(oldD3DHome))
-            {
-                Environment.SetEnvironmentVariable("D3D_HOME", oldD3DHome);
-                Environment.SetEnvironmentVariable("OLD_D3D_HOME", null);
-            }
-
             string oldArch = Environment.GetEnvironmentVariable("OLD_ARCH");
             if (!string.IsNullOrEmpty(oldArch))
             {
@@ -1169,7 +1162,7 @@ namespace DeltaShell.Plugins.FMSuite.Wave
                 Environment.SetEnvironmentVariable("OLD_ARCH", null);
             }
 
-            WaveModelApi.WaveDllHelper.DimrRun = false;
+            WaveEnvironmentHelper.DimrRun = false;
         }
 
         protected override void OnCancel()
@@ -1592,12 +1585,16 @@ namespace DeltaShell.Plugins.FMSuite.Wave
         {
             get
             {
-                using (var waveDllHelper = new WaveModelApi.WaveDllHelper(string.Empty))
+                using (var waveDllHelper = new WaveEnvironmentHelper(string.Empty))
                 {
-                    WaveModelApi.WaveDllHelper.DimrRun = true;
-                    return waveDllHelper.WaveExeDir + ";" + waveDllHelper.SwanExeDir + ";" +
-                           waveDllHelper.SwanScriptDir + ";" + waveDllHelper.EsmfPath + ";" +
-                           waveDllHelper.EsmfScriptPath;
+                    WaveEnvironmentHelper.DimrRun = true;
+
+                    return string.Join(";",
+                                       DimrApiDataSet.WaveExePath,
+                                       DimrApiDataSet.SwanExePath,
+                                       DimrApiDataSet.SwanScriptPath,
+                                       DimrApiDataSet.EsmfExePath,
+                                       DimrApiDataSet.EsmfScriptPath);
                 }
             }
         }
