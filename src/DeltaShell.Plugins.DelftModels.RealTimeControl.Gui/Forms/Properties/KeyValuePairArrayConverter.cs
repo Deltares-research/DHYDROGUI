1e3beab323036cf5b75ea64d080b151cb3c087b2
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-03-06 15:04:09 +0000

    D3DFMIQ-1633 - Add tests and documentation KeyValuePairArrayConverter and KeyValuePairPropertyDescriptor
    Former-commit-id: c9308909e3f620ffc4792eb9610b225c7f0afd20
    Former-commit-id: 5739a8c606becd7c5bca4543c87f52c8ee504ce4

--- src/DeltaShell.Plugins.DelftModels.RealTimeControl.Gui/Forms/Properties/KeyValuePairArrayConverter.cs
+++ src/DeltaShell.Plugins.DelftModels.RealTimeControl.Gui/Forms/Properties/KeyValuePairArrayConverter.cs
@@ -6,26 +6,55 @@ using System.Linq;
 
 namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Gui.Forms.Properties
 {
+    /// <summary>
+    /// Provides a type converter to convert a <see cref="KeyValuePair{TKey,TValue}" /> array to another representation.
+    /// </summary>
+    /// <typeparam name="T"> </typeparam>
+    /// <seealso cref="ArrayConverter" />
     public class KeyValuePairArrayConverter<T> : ArrayConverter
     {
+        /// <summary>
+        /// Converts to.
+        /// </summary>
+        /// <param name="context"> The context. </param>
+        /// <param name="culture"> The culture. </param>
+        /// <param name="value"> The value. </param>
+        /// <param name="destinationType"> Type of the destination. </param>
+        /// <returns> </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="value" /> is not an array of type <see cref="KeyValuePair{String, T}" />
+        /// </exception>
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value,
                                          Type destinationType)
         {
-            if (destinationType == typeof(string) &&
-                value is KeyValuePair<string, T>[] keyValuePairs)
+            if (!(value is KeyValuePair<string, T>[] keyValuePairs))
             {
-                return $"({keyValuePairs.Length})";
+                throw new ArgumentException($@"Must be of type {typeof(KeyValuePair<string, T>[])}.",
+                                            nameof(value));
             }
 
-            return base.ConvertTo(context, culture, value, destinationType);
+            return destinationType == typeof(string)
+                       ? $"({keyValuePairs.Length})"
+                       : base.ConvertTo(context, culture, value, destinationType);
         }
 
+        /// <summary>
+        /// Gets the property descriptor collection for the specified <paramref name="value" />.
+        /// </summary>
+        /// <param name="context"> This parameter is not used. </param>
+        /// <param name="value"> The key value pairs. </param>
+        /// <param name="attributes"> The attributes. </param>
+        /// <returns> </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="value" /> is not an array of type <see cref="KeyValuePair{String, T}" />
+        /// </exception>
         public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value,
                                                                    Attribute[] attributes)
         {
             if (!(value is KeyValuePair<string, T>[] keyValuePairs))
             {
-                return PropertyDescriptorCollection.Empty;
+                throw new ArgumentException($@"Must be of type {typeof(KeyValuePair<string, T>[])}.",
+                                            nameof(value));
             }
 
             PropertyDescriptor[] descriptors = keyValuePairs
--- src/DeltaShell.Plugins.DelftModels.RealTimeControl.Gui/Forms/Properties/KeyValuePairPropertyDescriptor.cs
+++ src/DeltaShell.Plugins.DelftModels.RealTimeControl.Gui/Forms/Properties/KeyValuePairPropertyDescriptor.cs
@@ -5,23 +5,56 @@ using System.Linq;
 
 namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Gui.Forms.Properties
 {
+    /// <summary>
+    /// Property descriptor for a <see cref="KeyValuePair{TKey,TValue}" />
+    /// </summary>
+    /// <typeparam name="T"> </typeparam>
+    /// <seealso cref="PropertyDescriptor" />
     public class KeyValuePairPropertyDescriptor<T> : PropertyDescriptor
     {
+        private readonly string key;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyValuePairPropertyDescriptor{T}" /> class.
+        /// </summary>
+        /// <param name="name"> The key. </param>
+        /// <param name="attrs"> The attributes. </param>
+        /// <param name="isReadOnly"> Whether or not this property should be read-only. </param>
         public KeyValuePairPropertyDescriptor(string name, Attribute[] attrs, bool isReadOnly) : base(name, attrs)
         {
+            key = name;
             IsReadOnly = isReadOnly;
         }
 
+        /// <summary>
+        /// Gets the value of the key value pair.
+        /// </summary>
+        /// <param name="component"> The key value pair array. </param>
+        /// <returns> The value of corresponding key value pair of this descriptor. </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="component" /> is not an array of type <see cref="KeyValuePair{String, T}" />
+        /// </exception>
         public override object GetValue(object component)
         {
             if (component is KeyValuePair<string, T>[] keyValuePairs)
             {
-                return keyValuePairs.FirstOrDefault(p => p.Key == Name).Value;
+                return keyValuePairs.FirstOrDefault(p => p.Key == key).Value;
             }
 
-            return null;
+            throw new ArgumentException($@"Must be of type {ComponentType}.", nameof(component));
         }
 
+        /// <summary>
+        /// Sets the value of the key value pair.
+        /// </summary>
+        /// <param name="component"> The key value pair array. </param>
+        /// <param name="value"> The new value. </param>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the property is read-only.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="component" /> is not an array of type <see cref="KeyValuePair{String, T}" />
+        /// </exception>
         public override void SetValue(object component, object value)
         {
             if (IsReadOnly)
@@ -29,14 +62,16 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Gui.Forms.Properties
                 throw new InvalidOperationException("Property is read-only.");
             }
 
-            if (component is KeyValuePair<string, T>[] keyValuePairs)
+            if (!(component is KeyValuePair<string, T>[] keyValuePairs))
             {
-                KeyValuePair<string, T> kvp = keyValuePairs
-                    .FirstOrDefault(p => p.Key == Name);
-
-                int index = Array.IndexOf(keyValuePairs, kvp);
-                keyValuePairs[index] = new KeyValuePair<string, T>(kvp.Key, (T)value);
+                throw new ArgumentException($@"Must be of type {ComponentType}.", nameof(component));
             }
+
+            KeyValuePair<string, T> kvp = keyValuePairs
+                .FirstOrDefault(p => p.Key == key);
+
+            int index = Array.IndexOf(keyValuePairs, kvp);
+            keyValuePairs[index] = new KeyValuePair<string, T>(kvp.Key, (T) value);
         }
 
         public override bool CanResetValue(object component)
@@ -44,9 +79,15 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Gui.Forms.Properties
             return false;
         }
 
+        /// <summary>
+        /// Throws an <exception cref="NotSupportedException" />.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        /// Thrown when this method is called.
+        /// </exception>
         public override void ResetValue(object component)
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Resetting property value is not supported.");
         }
 
         public override bool ShouldSerializeValue(object component)
