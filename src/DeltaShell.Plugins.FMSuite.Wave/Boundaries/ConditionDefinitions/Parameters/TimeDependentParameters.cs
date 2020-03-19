f0a9aa1311dc2c9233e2540e78b059e697143d87
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-19 06:40:41 +0000

    D3DFMIQ-1658 - Add initial TimeDependentParameters - Add corresponding tests
    Former-commit-id: 7fe6daa56bf51c018bf2985e7861d811c5dbc875
    Former-commit-id: f6fac359a3d242ac9ad4b6ddc84050037fdf1d22

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Parameters/TimeDependentParameters.cs
@@ -0,0 +1,33 @@
+﻿using DelftTools.Functions;
+using DelftTools.Utils.Guards;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters
+{
+    /// <summary>
+    /// <see cref="TimeDependentParameters"/> provides the time-dependent parameters associated with
+    /// a <see cref="IWaveBoundaryConditionDefinition"/> in the case of uniform data, or the
+    /// parameters associated with a <see cref="GeometricDefinitions.SupportPoint"/> in the case
+    /// of a spatially variant <see cref="IWaveBoundaryConditionDefinition"/>.
+    /// </summary>
+    /// <seealso cref="IBoundaryConditionParameters"/>
+    public class TimeDependentParameters : IBoundaryConditionParameters
+    {
+        /// <summary>
+        /// Creates a new <see cref="TimeDependentParameters"/>.
+        /// </summary>
+        /// <param name="waveEnergyFunction">The wave energy function.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <paramref name="waveEnergyFunction"/> is <c>null</c>.
+        /// </exception>
+        public TimeDependentParameters(IFunction waveEnergyFunction)
+        {
+            Ensure.NotNull(waveEnergyFunction, nameof(waveEnergyFunction));
+            WaveEnergyFunction = waveEnergyFunction;
+        }
+
+        /// <summary>
+        /// Gets the wave energy function.
+        /// </summary>
+        public IFunction WaveEnergyFunction { get; }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/Parameters/TimeDependentParametersTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DelftTools.Functions;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions.Parameters
+{
+    [TestFixture]
+    public class TimeDependentParametersTest
+    {
+        [Test]
+        public void Constructor_ExpectedResults()
+        {
+            // Setup
+            var waveEnergyFunction = Substitute.For<IFunction>();
+
+            // Call
+            var parameters = new TimeDependentParameters(waveEnergyFunction);
+
+            // Assert
+            Assert.That(parameters, Is.InstanceOf<IBoundaryConditionParameters>());
+            Assert.That(parameters, Has.Property(nameof(TimeDependentParameters.WaveEnergyFunction))
+                                       .SameAs(waveEnergyFunction));
+        }
+
+        [Test]
+        public void Constructor_WaveEnergyFunctionNull_ThrowsArgumentNullException()
+        {
+            // Call | Assert
+            void Call() => new TimeDependentParameters(null);
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("waveEnergyFunction"));
+        }
+    }
+}
