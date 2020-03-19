2a485aa4339c58be053b46ec73bc5aea90acf241
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-19 07:49:03 +0000

    D3DFMIQ-1658 - Add WaveParametersConstants
    Former-commit-id: 6198f1cf1e72b7f42a7f6130c62bc140f2ed0a11
    Former-commit-id: ba6918480785539191c527c545ef375db25590d3

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Parameters/WaveParametersConstants.cs
@@ -0,0 +1,94 @@
+﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters
+{
+    /// <summary>
+    /// <see cref="WaveParametersConstants"/> provides the constants used within the
+    /// boundary condition parameters.
+    /// </summary>
+    public static class WaveParametersConstants
+    {
+        /// <summary>
+        /// The time variable name.
+        /// </summary>
+        public const string TimeVariableName = "Time";
+
+        /// <summary>
+        /// The height variable name.
+        /// </summary>
+        public const string HeightVariableName = "Hs";
+
+        /// <summary>
+        /// The period variable name.
+        /// </summary>
+        public const string PeriodVariableName = "Tp";
+
+        /// <summary>
+        /// The direction variable name.
+        /// </summary>
+        public const string DirectionVariableName = "Dir";
+
+        /// <summary>
+        /// The spreading variable name.
+        /// </summary>
+        public const string SpreadingVariableName = "Spreading";
+
+        /// <summary>
+        /// The minute unit name.
+        /// </summary>
+        public const string MinuteUnitName = "minutes";
+
+        /// <summary>
+        /// The meter unit name.
+        /// </summary>
+        public const string MeterUnitName = "meter";
+
+        /// <summary>
+        /// The meter unit symbol.
+        /// </summary>
+        public const string MeterUnitSymbol = "m";
+
+        /// <summary>
+        /// The second unit name.
+        /// </summary>
+        public const string SecondUnitName = "second";
+
+        /// <summary>
+        /// The second unit symbol.
+        /// </summary>
+        public const string SecondUnitSymbol = "s";
+
+        /// <summary>
+        /// The empty unit name.
+        /// </summary>
+        public const string EmptyUnitName = "";
+
+        /// <summary>
+        /// The empty unit symbol.
+        /// </summary>
+        public const string EmptyUnitSymbol = "-";
+
+        /// <summary>
+        /// The degrees unit name.
+        /// </summary>
+        public const string DegreesUnitName = "degrees";
+
+        /// <summary>
+        /// The degrees unit symbol.
+        /// </summary>
+        public const string DegreesUnitSymbol = "deg";
+
+        /// <summary>
+        /// The power unit name.
+        /// </summary>
+        public const string PowerUnitName = "power";
+
+        /// <summary>
+        /// The power unit symbol.
+        /// </summary>
+        public const string PowerUnitSymbol = "-";
+
+        /// <summary>
+        /// The non equidistant time function attribute name.
+        /// </summary>
+        public const string NonEquidistantTimeFunctionAttributeName = "non-equidistant";
+    }
+}
