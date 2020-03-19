2ffafd41fa22e19abd97a1d15cd4aa69e937c407
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-19 11:24:36 +0000

    D3DFMIQ-1658 - Add SpreadingConversionTest.cs
    Former-commit-id: 473801d2d87dbd6ce9de65d1eb667c334170ea44
    Former-commit-id: 0d45ea34b750221947df0ef2b5e9d764918c0f7d

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/Spreading/SpreadingConversionTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using DelftTools.Units;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Spreading;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions.Spreading
+{
+    [TestFixture]
+    public class SpreadingConversionTest
+    {
+        private class DummyConditionSpreading : IBoundaryConditionSpreading { }
+
+        [Test]
+        public void GetSpreadingUnit_DegreesDefinedSpreading_ExpectedResults()
+        {
+            // Call
+            Unit unit = SpreadingConversion.GetSpreadingUnit<DegreesDefinedSpreading>();
+
+            // Assert
+            Assert.That(unit.Name, Is.EqualTo(WaveParametersConstants.DegreesUnitName));
+            Assert.That(unit.Symbol, Is.EqualTo(WaveParametersConstants.DegreesUnitSymbol));
+        }
+
+        [Test]
+        public void GetSpreadingUnit_PowerDefinedSpreading_ExpectedResults()
+        {
+            // Call
+            Unit unit = SpreadingConversion.GetSpreadingUnit<PowerDefinedSpreading>();
+
+            // Assert
+            Assert.That(unit.Name, Is.EqualTo(WaveParametersConstants.PowerUnitName));
+            Assert.That(unit.Symbol, Is.EqualTo(WaveParametersConstants.PowerUnitSymbol));
+        }
+
+        [Test]
+        public void GetSpreadingUnit_UnsupportedSpreading_ThrowsNotSupportedException()
+        {
+            // Call | Assert
+            void Call() => SpreadingConversion.GetSpreadingUnit<DummyConditionSpreading>();
+
+            Assert.Throws<NotSupportedException>(Call);
+        }
+
+        [Test]
+        public void GetSpreadingDefaultValue_DegreesDefinedSpreading_ExpectedResults()
+        {
+            // Call
+            double defaultValue = SpreadingConversion.GetSpreadingDefaultValue<DegreesDefinedSpreading>();
+
+            // Assert
+            Assert.That(defaultValue, Is.EqualTo(WaveSpreadingConstants.DegreesDefaultSpreading));
+        }
+
+        [Test]
+        public void GetSpreadingDefaultValue_PowerDefinedSpreading_ExpectedResults()
+        {
+            // Call
+            double defaultValue = SpreadingConversion.GetSpreadingDefaultValue<PowerDefinedSpreading>();
+
+            // Assert
+            Assert.That(defaultValue, Is.EqualTo(WaveSpreadingConstants.PowerDefaultSpreading));
+        }
+
+        [Test]
+        public void GetSpreadingDefaultValue_UnsupportedSpreading_ThrowsNotSupportedException()
+        {
+            // Call | Assert
+            void Call() => SpreadingConversion.GetSpreadingDefaultValue<DummyConditionSpreading>();
+
+            Assert.Throws<NotSupportedException>(Call);
+        }
+    }
+}
