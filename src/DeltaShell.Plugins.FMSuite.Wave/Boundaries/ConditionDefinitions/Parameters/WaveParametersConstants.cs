904feda0d69d3b5861a55b96980e9f494882d097
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-19 09:13:48 +0000

    D3DFMIQ-1658 - Add Unit construction methods to WaveParametersConstants - Add corresponding tests
    Former-commit-id: e5896908c64f550903851da499b217172a4238b3
    Former-commit-id: 9796388d27a528b6d73ef635858ba29fe6ce7b63

--- src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Parameters/WaveParametersConstants.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/ConditionDefinitions/Parameters/WaveParametersConstants.cs
@@ -1,4 +1,6 @@
-﻿namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters
+﻿using DelftTools.Units;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters
 {
     /// <summary>
     /// <see cref="WaveParametersConstants"/> provides the constants used within the
@@ -42,6 +44,15 @@
         public const string MinuteUnitName = "minutes";
 
         /// <summary>
+        /// Constructs a new meter <see cref="Unit"/>.
+        /// </summary>
+        /// <returns>
+        /// A new meter <see cref="Unit"/>.
+        /// </returns>
+        public static Unit ConstructMeterUnit() => new Unit(MeterUnitName, 
+                                                            MeterUnitSymbol);
+
+        /// <summary>
         /// The meter unit name.
         /// </summary>
         public const string MeterUnitName = "meter";
@@ -50,6 +61,15 @@
         /// The meter unit symbol.
         /// </summary>
         public const string MeterUnitSymbol = "m";
+
+        /// <summary>
+        /// Construct a new second <see cref="Unit"/>.
+        /// </summary>
+        /// <returns>
+        /// A new second <see cref="Unit"/>.
+        /// </returns>
+        public static Unit ConstructSecondUnit() => new Unit(SecondUnitName, 
+                                                             SecondUnitSymbol);
 
         /// <summary>
         /// The second unit name.
@@ -62,6 +82,15 @@
         public const string SecondUnitSymbol = "s";
 
         /// <summary>
+        /// Gets a new empty <see cref="Unit"/>.
+        /// </summary>
+        /// <returns>
+        /// A new empty <see cref="Unit"/>.
+        /// </returns>
+        public static Unit ConstructEmptyUnit() => new Unit(EmptyUnitName, 
+                                                            EmptyUnitSymbol);
+
+        /// <summary>
         /// The empty unit name.
         /// </summary>
         public const string EmptyUnitName = "";
@@ -72,6 +101,15 @@
         public const string EmptyUnitSymbol = "-";
 
         /// <summary>
+        /// Constructs a new degrees <see cref="Unit"/>.
+        /// </summary>
+        /// <returns>
+        /// A new degrees <see cref="Unit"/>.
+        /// </returns>
+        public static Unit ConstructDegreesUnit() => new Unit(DegreesUnitName, 
+                                                              DegreesUnitSymbol);
+
+        /// <summary>
         /// The degrees unit name.
         /// </summary>
         public const string DegreesUnitName = "degrees";
@@ -80,6 +118,15 @@
         /// The degrees unit symbol.
         /// </summary>
         public const string DegreesUnitSymbol = "deg";
+
+        /// <summary>
+        /// Constructs a new Power <see cref="Unit"/>.
+        /// </summary>
+        /// <returns>
+        /// A new power <see cref="Unit"/>.
+        /// </returns>
+        public static Unit ConstructPowerUnit() => new Unit(PowerUnitName, 
+                                                            PowerUnitSymbol);
 
         /// <summary>
         /// The power unit name.
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/ConditionDefinitions/Parameters/WaveParametersConstantsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using DelftTools.Units;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.ConditionDefinitions.Parameters
+{
+    [TestFixture]
+    public class WaveParametersConstantsTests
+    {
+        private static IEnumerable<TestCaseData> GetConstructUnitTestData()
+        {
+            yield return new TestCaseData((Func<Unit>) WaveParametersConstants.ConstructMeterUnit, 
+                                           WaveParametersConstants.MeterUnitName, 
+                                           WaveParametersConstants.MeterUnitSymbol);
+            yield return new TestCaseData((Func<Unit>) WaveParametersConstants.ConstructSecondUnit, 
+                                           WaveParametersConstants.SecondUnitName, 
+                                           WaveParametersConstants.SecondUnitSymbol);
+            yield return new TestCaseData((Func<Unit>) WaveParametersConstants.ConstructEmptyUnit, 
+                                           WaveParametersConstants.EmptyUnitName, 
+                                           WaveParametersConstants.EmptyUnitSymbol);
+            yield return new TestCaseData((Func<Unit>) WaveParametersConstants.ConstructDegreesUnit, 
+                                           WaveParametersConstants.DegreesUnitName, 
+                                           WaveParametersConstants.DegreesUnitSymbol);
+            yield return new TestCaseData((Func<Unit>) WaveParametersConstants.ConstructPowerUnit, 
+                                           WaveParametersConstants.PowerUnitName, 
+                                           WaveParametersConstants.PowerUnitSymbol);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetConstructUnitTestData))]
+        public void ConstructUnit_ExpectedResults(Func<Unit> constructFunc, string expectedName, string expectedSymbol)
+        {
+            // Call
+            Unit unit = constructFunc.Invoke();
+
+            // Assert
+            Assert.That(unit.Name, Is.EqualTo(expectedName), $"Expected a different {nameof(Unit.Name)}:");
+            Assert.That(unit.Symbol, Is.EqualTo(expectedSymbol), $"Expected a different {nameof(Unit.Symbol)}:");
+        }
+    }
+}
