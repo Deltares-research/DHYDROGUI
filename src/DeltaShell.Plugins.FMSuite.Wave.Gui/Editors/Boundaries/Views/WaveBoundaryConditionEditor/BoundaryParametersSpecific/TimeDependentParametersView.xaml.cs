4813f68092dd89a03e769eca3e89d1a44a50440a
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-20 10:13:50 +0000

    D3DFMIQ-1658 - Add skeleton TimeDependentParametersView
    Former-commit-id: 9154fdf650182ef15479f42d5c1ade7a4968e1f1
    Former-commit-id: f9a1c04a4e0439da02ff7470865a54c40f18a072

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/Views/WaveBoundaryConditionEditor/BoundaryParametersSpecific/TimeDependentParametersView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.Views.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    /// <summary>
+    /// Interaction logic for TimeDependentParametersView.xaml
+    /// </summary>
+    public partial class TimeDependentParametersView : UserControl
+    {
+        public TimeDependentParametersView()
+        {
+            InitializeComponent();
+        }
+    }
+}
