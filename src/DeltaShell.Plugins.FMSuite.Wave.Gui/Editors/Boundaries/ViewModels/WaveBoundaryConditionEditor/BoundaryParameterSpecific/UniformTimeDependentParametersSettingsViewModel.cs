ea3056da33611ca1b47a40e322ea6fc53e7271cd
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-20 11:36:34 +0000

    D3DFMIQ-1658 - Add UniformTimeDependentParametersSettingsViewModel - Add corresponding tests
    Former-commit-id: 6c392431e494874e34cf4c4446e301e764d38c4f
    Former-commit-id: aaba4b1679725ceb0f6fb64dff16ef6ebd653a6f

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParameterSpecific/UniformTimeDependentParametersSettingsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DelftTools.Utils.Guards;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Spreading;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific
+{
+    /// <summary>
+    /// <see cref="UniformConstantParametersSettingsViewModel{TSpreading}"/> defines the view model for the
+    /// ParametersSettingsView given uniform time dependent data.
+    /// </summary>
+    /// <typeparam name="TSpreading">The type of the spreading.</typeparam>
+    /// <seealso cref="TimeDependentParametersSettingsViewModel" />
+    public sealed class UniformTimeDependentParametersSettingsViewModel<TSpreading> : TimeDependentParametersSettingsViewModel
+        where TSpreading : class, IBoundaryConditionSpreading, new()
+    {
+        public UniformTimeDependentParametersSettingsViewModel(TimeDependentParameters<TSpreading> parameters)
+        {
+            Ensure.NotNull(parameters, nameof(parameters));
+            ActiveParametersViewModel = new TimeDependentParametersViewModel<TSpreading>(parameters);
+        }
+
+        public override TimeDependentParametersViewModel ActiveParametersViewModel { get; protected set; }
+
+        public override string GroupBoxTitle { get; protected set; } = "Uniform Time Dependent Parameters";
+    }
+}
