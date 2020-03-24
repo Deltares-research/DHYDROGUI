2e3cb1b1fa7326aebd113a2a79f7a8a254e7bb23
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-03-24 10:18:56 +0000

    D3DFMIQ-1577 - Put requests calls into wrapper methods.
    Former-commit-id: 494979cce30180a46aa39ba0a85268c2a0f479c2
    Former-commit-id: be32b5de5582d010a902eea091ab025ae551a76c

--- tools/pin_nuget_packages.py
+++ tools/pin_nuget_packages.py
@@ -15,14 +15,50 @@ JSON_RESPONSE_HEADER = {'Accept': 'application/json'}
 # 0: nuget package id, 1: nuget package build configuration id
 NUGET_PACKAGES = [
     ("Dimr.Libs", "DeltaShell_3rdPartyNuGetPackages_Dimr"),
-    ("RGFGRID", "DeltaShell_3rdPartyNuGetPackages_Rgfgrid"),
-    ("DIDO", "DeltaShell_3rdPartyNuGetPackages_Dido"),
-    ("PLCT.Libs", "DeltaShell_3rdPartyNuGetPackages_PlctWaq"),
-    ("Substances.Libs", "DeltaShell_3rdPartyNuGetPackages_SubstancesWaq")
+    # ("RGFGRID", "DeltaShell_3rdPartyNuGetPackages_Rgfgrid"),
+    # ("DIDO", "DeltaShell_3rdPartyNuGetPackages_Dido"),
+    # ("PLCT.Libs", "DeltaShell_3rdPartyNuGetPackages_PlctWaq"),
+    # ("Substances.Libs", "DeltaShell_3rdPartyNuGetPackages_SubstancesWaq")
 ]
 
 
-def get_previous_build(build_config: str, tag: str, user: str, password: str) -> dict:
+class RequestWrapper:
+
+    def __init__(self, user: str, password: str):
+        """
+        user : str
+            The user to authenticate with.
+        password : str
+            The password to authenticate with.
+        """
+
+        self.user = user
+        self.password = password
+        self.header = JSON_RESPONSE_HEADER
+
+    def get(self, url: str):
+        return requests.get(url,
+                            auth=(self.user, self.password),
+                            headers=JSON_RESPONSE_HEADER)
+
+    def delete(self, url: str):
+        requests.delete(url,
+                        auth=(self.user, self.password),
+                        headers=JSON_RESPONSE_HEADER)
+
+    def put_json(self, url: str, new_json: dict):
+        requests.put(url,
+                     auth=(self.user, self.password),
+                     headers=JSON_RESPONSE_HEADER,
+                     json=new_json)
+
+    def put(self, url: str):
+        requests.put(url,
+                     auth=(self.user, self.password),
+                     headers=JSON_RESPONSE_HEADER)
+
+
+def get_previous_build(build_config: str, tag: str) -> dict:
     """
     Get the previous build tagged with the specified tag
 
@@ -32,15 +68,9 @@ def get_previous_build(build_config: str, tag: str, user: str, password: str) ->
         The build configuration id of the build to be retrieved.
     tag : str
         The tag with which the build should be retrieved.
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     build_url = f"{BUILDS_ROOT}buildType:{build_config},tag:{tag},pinned:true,count:1"
-    previous_build_response = requests.get(build_url,
-                                           auth=(user, password),
-                                           headers=JSON_RESPONSE_HEADER)
+    previous_build_response = wrapper.get(build_url)
 
     if previous_build_response.status_code != 200:
         return None
@@ -48,7 +78,7 @@ def get_previous_build(build_config: str, tag: str, user: str, password: str) ->
     return json.loads(previous_build_response.text)
 
 
-def unpin_build(build_id: str, user: str, password: str) -> None:
+def unpin_build(build_id: str) -> None:
     """
     Unpin the build with build_id.
 
@@ -56,16 +86,12 @@ def unpin_build(build_id: str, user: str, password: str) -> None:
     ----------
     build_id : str
         The id of the build to be unpinned.
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     pin_url = f"{BUILDS_ROOT}id:{build_id}/pin/"
-    requests.delete(pin_url, auth=(user, password), headers=JSON_RESPONSE_HEADER)
+    wrapper.delete(pin_url)
 
 
-def clean_up_build(build_info: dict, tag: str, user: str, password: str) -> None:
+def clean_up_build(build_info: dict, tag: str) -> None:
     """
     Remove the specified tag from the specified build and unpin if necessary.
 
@@ -75,19 +101,13 @@ def clean_up_build(build_info: dict, tag: str, user: str, password: str) -> None
         A dictionary describing the build to be modified.
     tag : str
         The tag to be removed from the build
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     build_id = build_info["id"]
-    untag_build(build_info, tag,
-                user, password)
+    untag_build(build_info, tag)
 
     tag_info = build_info["tags"]
     if tag in get_tag_values(tag_info) and tag_info["count"] == 1:
-        unpin_build(build_id,
-                    user, password)
+        unpin_build(build_id)
 
 
 def get_tag_values(tags):
@@ -102,7 +122,7 @@ def get_tag_values(tags):
     return list(x["name"] for x in tags["tag"])
 
 
-def untag_build(build_info: dict, tag: str, user: str, password: str) -> None:
+def untag_build(build_info: dict, tag: str) -> None:
     """
     Remove the specified tag from the build specified with build_info.
 
@@ -112,10 +132,6 @@ def untag_build(build_info: dict, tag: str, user: str, password: str) -> None:
         A dictionary describing the build to be modified.
     tag : str
         The tag to be removed from the build
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     build_tags = get_tag_values(build_info["tags"])
     if tag not in build_tags:
@@ -128,7 +144,7 @@ def untag_build(build_info: dict, tag: str, user: str, password: str) -> None:
     }
 
     tag_url = f"{BUILDS_ROOT}id:{build_info['id']}/tags/"
-    requests.put(tag_url, auth=(user, password), headers=JSON_RESPONSE_HEADER, json=new_tags)
+    wrapper.put_json(tag_url, new_tags)
 
 
 def get_packages(files) -> dict:
@@ -173,7 +189,7 @@ def get_packages_files(dir_path: Path):
     return dir_path.glob("**/packages.config")
 
 
-def get_new_build(build_config_id: str, nuget_package_file_name: str, user: str, password: str, ) -> dict:
+def get_new_build(build_config_id: str, nuget_package_file_name: str) -> dict:
     """
     Get the build from build_config with the specified revision number.
 
@@ -183,16 +199,11 @@ def get_new_build(build_config_id: str, nuget_package_file_name: str, user: str,
         The id of the build configuration of which the build is part.
     nuget_package_file_name : str
         The expected nuget package file name within the build to be retrieved.
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     builds_url = f"{BUILDS_ROOT}?locator=buildType:{build_config_id}"
 
-    new_builds_response = requests.get(builds_url,
-                                       auth=(user, password),
-                                       headers=JSON_RESPONSE_HEADER)
+    new_builds_response = wrapper.get(builds_url)
+
     if new_builds_response.status_code != 200:
         return None
 
@@ -202,9 +213,7 @@ def get_new_build(build_config_id: str, nuget_package_file_name: str, user: str,
         new_build_url = f"{BUILDS_ROOT}id:{build['id']}/"
         build_artifacts_url = f"{new_build_url}artifacts/"
 
-        artifacts_response = requests.get(build_artifacts_url,
-                                          auth=(user, password),
-                                          headers=JSON_RESPONSE_HEADER)
+        artifacts_response = wrapper.get(build_artifacts_url)
 
         if artifacts_response.status_code != 200:
             continue
@@ -213,9 +222,7 @@ def get_new_build(build_config_id: str, nuget_package_file_name: str, user: str,
         if not artifact_info['file'][0]['name'] == nuget_package_file_name:
             continue
 
-        new_build_info = requests.get(new_build_url,
-                                      auth=(user, password),
-                                      headers=JSON_RESPONSE_HEADER)
+        new_build_info = wrapper.get(new_build_url)
 
         if new_build_info.status_code != 200:
             return None
@@ -225,7 +232,7 @@ def get_new_build(build_config_id: str, nuget_package_file_name: str, user: str,
     return None
 
 
-def pin_build(build_id: str, user: str, password: str) -> None:
+def pin_build(build_id: str) -> None:
     """
     Pin the build with build_id.
 
@@ -233,16 +240,12 @@ def pin_build(build_id: str, user: str, password: str) -> None:
     ----------
     build_id : str
         The id of the build to be pinned.
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     pin_url = f"{BUILDS_ROOT}id:{build_id}/pin/"
-    requests.put(pin_url, auth=(user, password), headers=JSON_RESPONSE_HEADER)
+    wrapper.put(pin_url)
 
 
-def tag_build(build_info, tag: str, user: str, password: str) -> None:
+def tag_build(build_info, tag: str) -> None:
     """
     Add a tag with value tag to the build specified with build_info.
 
@@ -252,10 +255,6 @@ def tag_build(build_info, tag: str, user: str, password: str) -> None:
         A dictionary describing the build to be modified.
     tag : str
         The new tag to be added to the build
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     if 'tags' in build_info:
         new_tag_values = list({"name": x} for x in get_tag_values(build_info['tags']) if x != tag)
@@ -270,10 +269,10 @@ def tag_build(build_info, tag: str, user: str, password: str) -> None:
     }
 
     tag_url = f"{BUILDS_ROOT}id:{build_info['id']}/tags/"
-    requests.put(tag_url, auth=(user, password), headers=JSON_RESPONSE_HEADER, json=new_tags)
+    wrapper.put_json(tag_url, new_tags)
 
 
-def bag_new_build(build_info: dict, tag: str, user: str, password: str) -> None:
+def bag_new_build(build_info: dict, tag: str) -> None:
     """
     Pin and tag the build specified with build_info.
 
@@ -283,18 +282,12 @@ def bag_new_build(build_info: dict, tag: str, user: str, password: str) -> None:
         A dictionary describing the build to be modified.
     tag : str
         The new tag to be added to the build
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
-    pin_build(build_info['id'],
-              user, password)
-    tag_build(build_info, tag,
-              user, password, )
+    pin_build(build_info['id'])
+    tag_build(build_info, tag)
 
 
-def set_pins_and_tags(packages_with_versions: dict, tag: str, user: str, password: str):
+def set_pins_and_tags(packages_with_versions: dict, tag: str):
     """
     Pin and tag the build specified with build_info.
 
@@ -304,10 +297,6 @@ def set_pins_and_tags(packages_with_versions: dict, tag: str, user: str, passwor
         A dictionary containing the nuget package ids and their current versions in the solution.
     tag : str
         The new tag to be added to the build
-    user : str
-        The user to authenticate with.
-    password : str
-        The password to authenticate with.
     """
     for p in NUGET_PACKAGES:
         p_id = p[0]
@@ -316,18 +305,14 @@ def set_pins_and_tags(packages_with_versions: dict, tag: str, user: str, passwor
         if not len(packages_with_versions[p_id]) == 1:
             continue
 
-        old_build_info = get_previous_build(build_config_id, tag,
-                                            user, password)
+        old_build_info = get_previous_build(build_config_id, tag)
         if old_build_info:
-            clean_up_build(old_build_info, tag,
-                           user, password)
+            clean_up_build(old_build_info, tag)
 
         nuget_package_file_name = f"{p_id}.{packages_with_versions[p_id][0]}.nupkg"
-        new_build_info = get_new_build(build_config_id, nuget_package_file_name,
-                                       user, password)
+        new_build_info = get_new_build(build_config_id, nuget_package_file_name)
         if new_build_info:
-            bag_new_build(new_build_info, tag,
-                          user, password)
+            bag_new_build(new_build_info, tag)
 
 
 def parse_arguments():
@@ -343,9 +328,9 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
+    wrapper = RequestWrapper(args.user, args.password)
 
     package_files = get_packages_files(Path(args.checkout_dir))
     all_packages = get_packages(package_files)
 
-    set_pins_and_tags(all_packages, args.tag_string,
-                      args.user, args.password, )
+    set_pins_and_tags(all_packages, args.tag_string)
