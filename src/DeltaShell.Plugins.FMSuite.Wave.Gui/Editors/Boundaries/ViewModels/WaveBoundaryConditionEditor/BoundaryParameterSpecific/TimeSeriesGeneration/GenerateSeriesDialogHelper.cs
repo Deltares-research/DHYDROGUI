a6df3ffeec01b545c2deb9f8fa70e3ba6bb1a3bb
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-25 08:26:06 +0000

    D3DFMIQ-1658 - Add initial GenerateSeriesDialogHelper without GetSupportPointSelectionResponse
    Former-commit-id: 92b4c880a1f0f061c00cd6999c5667eec9db0ced
    Former-commit-id: 5643ab55e2e82ae2a3abba064e2abc401e131144

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParameterSpecific/TimeSeriesGeneration/GenerateSeriesDialogHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using DeltaShell.Plugins.CommonTools.Gui.Forms.Functions;
+using DeltaShell.Plugins.FMSuite.Common.Gui.Forms;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific.TimeSeriesGeneration
+{
+    /// <summary>
+    /// <see cref="GenerateSeriesDialogHelper"/> implements the interface with
+    /// which the dialogs of the generate series component can be created and shown.
+    /// </summary>
+    /// <seealso cref="IGenerateSeriesDialogHelper" />
+    public class GenerateSeriesDialogHelper : IGenerateSeriesDialogHelper
+    {
+        public TimeSeriesGeneratorDialog GetTimeSeriesGeneratorResponse(IWin32Window owner, 
+                                                                        DateTime startTime, 
+                                                                        DateTime stopTime,
+                                                                        TimeSpan timeStep)
+        {
+            var generateDialog = new TimeSeriesGeneratorDialog { ApplyOnAccept = false};
+            generateDialog.SetData(null, startTime, stopTime, timeStep);
+            generateDialog.ShowDialog(owner);
+
+            return generateDialog;
+        }
+
+        public SupportPointSelectionForm GetSupportPointSelectionResponse(IWin32Window owner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
