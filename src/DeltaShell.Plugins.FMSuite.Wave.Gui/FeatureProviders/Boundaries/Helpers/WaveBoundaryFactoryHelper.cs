3dd2da4684b031d028aff3e755e9113fa8f04b00
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-03-25 10:31:52 +0000

    D3DFMIQ-1636 - Move logic GetSnappedEndPoints and GetGeometricDefinition from WaveBoundaryFactoryHelper (in GUI project) to new WaveBoundaryGeometricDefinitionFactoryHelper - 'Copý' original tests from WaveBoundaryFactoryHelperTest to WaveBoundaryGeometricDefinitionFactoryHelperTest
    Former-commit-id: 8eefb139829f0687dbb81ea986b7a1dfadc9058b
    Former-commit-id: 077c0fe64780a214e8e3c791aa4dda6ebe00b16c

--- src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/Helpers/WaveBoundaryFactoryHelper.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/Helpers/WaveBoundaryFactoryHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using DelftTools.Utils.Guards;
 using DeltaShell.Plugins.FMSuite.Wave.Boundaries.Calculators;
 using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions;
@@ -36,63 +35,16 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Helper
             this.componentFactory = componentFactory;
         }
 
-        public IEnumerable<GridBoundaryCoordinate> GetSnappedEndPoints(IBoundarySnappingCalculator boundarySnappingCalculator, 
+        public IEnumerable<GridBoundaryCoordinate> GetSnappedEndPoints(IBoundarySnappingCalculator boundarySnappingCalculator,
                                                                        IEnumerable<Coordinate> coordinates)
         {
-            List<Coordinate> distinctCoordinates = coordinates.Distinct(new Coordinate2DEqualityComparer()).ToList();
-
-            if (distinctCoordinates.Count < 2)
-            {
-                throw new ArgumentException("There should be two or more distinct coordinates in coordinates.");
-            }
-
-            Coordinate firstCoordinate = distinctCoordinates.First();
-            Coordinate lastCoordinate = distinctCoordinates.Last();
-
-            IEnumerable<GridBoundaryCoordinate> firstSnappedCoordinates =
-                boundarySnappingCalculator.SnapCoordinateToGridBoundaryCoordinate(firstCoordinate);
-            IEnumerable<GridBoundaryCoordinate> lastSnappedCoordinates =
-                boundarySnappingCalculator.SnapCoordinateToGridBoundaryCoordinate(lastCoordinate);
-
-            return firstSnappedCoordinates.Concat(lastSnappedCoordinates);
+            return WaveBoundaryGeometricDefinitionFactoryHelper.GetSnappedEndPoints(boundarySnappingCalculator,
+                                                                                    coordinates);
         }
 
         public IWaveBoundaryGeometricDefinition GetGeometricDefinition(IEnumerable<GridBoundaryCoordinate> snappedCoordinates, IBoundarySnappingCalculator calculator)
         {
-            IEnumerable<IGrouping<GridSide, GridBoundaryCoordinate>> groupedCoordinates =
-                snappedCoordinates.GroupBy(x => x.GridSide)
-                                  .Where(group => group.Count() >= 2);
-
-            // TODO: improve this method. Discuss with Maarten.
-            var candidateFound = false;
-
-            var startIndexCandidate = 0;
-            var endIndexCandidate = 0;
-            var gridSideCandidate = GridSide.North;
-            double lengthCandidate = 0;
-
-            foreach (IGrouping<GridSide, GridBoundaryCoordinate> coordinateGroup in groupedCoordinates)
-            {
-                int first = coordinateGroup.Min(x => x.Index);
-                int last = coordinateGroup.Max(x => x.Index);
-
-                if (first == last ||
-                    (candidateFound && last - first < endIndexCandidate - startIndexCandidate))
-                {
-                    continue;
-                }
-
-                candidateFound = true;
-
-                startIndexCandidate = first;
-                endIndexCandidate = last;
-                gridSideCandidate = coordinateGroup.Key;
-                lengthCandidate = calculator.CalculateDistanceBetweenBoundaryIndices(first, last, coordinateGroup.Key);
-            }
-
-            return candidateFound
-                       ? new WaveBoundaryGeometricDefinition(startIndexCandidate, endIndexCandidate, gridSideCandidate, lengthCandidate)
-                       : null;
+            return WaveBoundaryGeometricDefinitionFactoryHelper.GetGeometricDefinition(snappedCoordinates, calculator);
         }
 
         public IWaveBoundaryConditionDefinition GetConditionDefinition()
