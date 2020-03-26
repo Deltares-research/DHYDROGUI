697f57fd46ab106247c901b1e1ee5d13e1a9fbbd
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-03-26 09:21:26 +0000

    D3DFMIQ-1636 - Add EnumUtils - Add EnumUtilsTest
    Former-commit-id: 9f569dc6d58864bc02ced19fc88ce8f19a497761
    Former-commit-id: 3919262458c0565da67f35576fa22905c5161a50

---
+++ src/DeltaShell.NGHS.Common/Utils/EnumUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using DelftTools.Utils.Reflection;
+
+namespace DeltaShell.NGHS.Common.Utils
+{
+    /// <summary>
+    /// Provides helper methods for enums.
+    /// </summary>
+    public static class EnumUtils
+    {
+        /// <summary>
+        /// Gets the enum value with the specified <paramref name="description" />.
+        /// </summary>
+        /// <typeparam name="T"> The enum type. </typeparam>
+        /// <param name="description"> The description. </param>
+        /// <returns>
+        /// If found, the enum value with the specified <paramref name="description" />;
+        /// otherwise, the <c> default </c> of <typeparam name="T" />
+        /// </returns>
+        public static T GetEnumValueByDescription<T>(string description)
+        {
+            return Enum.GetValues(typeof(T)).OfType<T>()
+                       .FirstOrDefault(v => (v as Enum).GetDescription() == description);
+        }
+    }
+}
---
+++ test/DeltaShell.NGHS.Common.Tests/Utils/EnumUtilsTest.cs
@@ -0,0 +1,37 @@
+﻿using DeltaShell.NGHS.Common.Utils;
+using NUnit.Framework;
+
+namespace DeltaShell.NGHS.Common.Tests.Utils
+{
+    [TestFixture]
+    public class EnumUtilsTest
+    {
+        [TestCase("first_value", TestEnum.A)]
+        [TestCase("second_value", TestEnum.B)]
+        [TestCase("third_value", TestEnum.C)]
+        [TestCase("fourth_value", default(TestEnum))]
+        [TestCase(null, default(TestEnum))]
+        [TestCase("", default(TestEnum))]
+        public void GetEnumValueByDescription_ReturnsCorrectResult(string description,
+                                                                   object expectedResult)
+        {
+            // Call
+            var result = EnumUtils.GetEnumValueByDescription<TestEnum>(description);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        private enum TestEnum
+        {
+            [System.ComponentModel.Description("first_value")]
+            A,
+
+            [System.ComponentModel.Description("second_value")]
+            B,
+
+            [System.ComponentModel.Description("third_value")]
+            C,
+        }
+    }
+}
