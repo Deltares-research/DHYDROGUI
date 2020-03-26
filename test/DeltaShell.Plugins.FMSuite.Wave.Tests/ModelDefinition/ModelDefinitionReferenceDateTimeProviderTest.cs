ce3813ce4a8253f5f736f7a7fe64ae30bd114241
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-26 12:55:45 +0000

    D3DFMIQ-1658 - Add IReferenceDateTimeProvider - Add ModelDefinition/IReferenceDateTimeProvider - Add corresponding tests
    Former-commit-id: 347682eaac08165b53b3225633eead796c8c3908
    Former-commit-id: fe28abcfe725cb5ef3cfaca2b01f5161160bca57

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/ModelDefinition/ModelDefinitionReferenceDateTimeProviderTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.Wave.ModelDefinition;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.ModelDefinition
+{
+    [TestFixture]
+    public class ModelDefinitionReferenceDateTimeProviderTest
+    {
+        [Test]
+        public void Constructor_ExpectedResults()
+        {
+            // Setup
+            DateTime expectedReferenceTime = DateTime.Today - TimeSpan.FromDays(2);
+
+            var waveModelDefinition = new WaveModelDefinition();
+            waveModelDefinition.ModelReferenceDateTime = expectedReferenceTime;
+
+            // Call
+            var referenceTimeProvider = new ModelDefinitionReferenceDateTimeProvider(waveModelDefinition);
+
+            // Assert
+            Assert.That(referenceTimeProvider, Is.InstanceOf<IReferenceDateTimeProvider>());
+            Assert.That(referenceTimeProvider.ModelReferenceDateTime, Is.EqualTo(expectedReferenceTime));
+        }
+
+        [Test]
+        public void Constructor_WaveModelDefinitionNull_ThrowsArgumentNullException()
+        {
+            void Call() => new ModelDefinitionReferenceDateTimeProvider(null);
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("modelDefinition"));
+        }
+    }
+}
