7c4f903b5f806af073aa3af089b4e9d947d0e3a8
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-26 10:10:13 +0000

    D3DFMIQ-1658 - Add TimeDependentSpatiallyVaryingParametersViewModel tests
    Former-commit-id: 93410e98247abf689df934d52506b37d2d676847
    Former-commit-id: 97e535e96e0c075f1a153ea1759823e42628910f

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParametersSpecific/TimeDependentSpatiallyVaryingParametersViewModelTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using DelftTools.Functions;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Spreading;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.WaveEnergyFunctions;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParametersSpecific
+{
+    [TestFixture(typeof(PowerDefinedSpreading))]
+    [TestFixture(typeof(DegreesDefinedSpreading))]
+    public class TimeDependentSpatiallyVaryingParametersViewModelTest<TSpreading> where TSpreading : class, IBoundaryConditionSpreading, new()
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var underlyingFunction = Substitute.For<IFunction>();
+            var waveEnergyFunction = Substitute.For<IWaveEnergyFunction<TSpreading>>();
+            waveEnergyFunction.UnderlyingFunction.Returns(underlyingFunction);
+
+            var supportPoint = new SupportPoint(10.0, Substitute.For<IWaveBoundaryGeometricDefinition>());
+            var parameters = new TimeDependentParameters<TSpreading>(waveEnergyFunction);
+
+            var allParameters = new Dictionary<SupportPoint, TimeDependentParameters<TSpreading>> {
+                { supportPoint, parameters }
+            };
+
+            // Call
+            var viewModel = new TimeDependentSpatiallyVaryingParametersViewModel<TSpreading>(parameters, allParameters);
+
+            // Assert
+            Assert.That(viewModel.ObservedParameters, Is.SameAs(parameters));
+            Assert.That(viewModel.TimeDependentParametersFunctions, Is.EquivalentTo(new[] {underlyingFunction}));
+        }
+
+        [Test]
+        public void Constructor_ParametersNull_ThrowsArgumentNullException()
+        {
+            void Call() => new TimeDependentSpatiallyVaryingParametersViewModel<TSpreading>(
+                null,
+                new Dictionary<SupportPoint, TimeDependentParameters<TSpreading>>());
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("parameters"));
+        }
+
+        [Test]
+        public void Constructor_Null_ThrowsArgumentNullException()
+        {
+            void Call() => new TimeDependentSpatiallyVaryingParametersViewModel<TSpreading>(
+                null,
+                new Dictionary<SupportPoint, TimeDependentParameters<TSpreading>>());
+
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("parameters"));
+        }
+    }
+}
