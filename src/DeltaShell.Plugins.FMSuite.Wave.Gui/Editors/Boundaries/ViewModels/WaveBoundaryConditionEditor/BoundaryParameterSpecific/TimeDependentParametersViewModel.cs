0043dae669f33c1ab7a63692efeee48cd8e7d380
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-03-26 08:28:22 +0000

    D3DFMIQ-1658 - Add initial support GenerateSeries to TimeDependentParametersView
    Former-commit-id: 4694e199f375c13244f0bfa9ca6f9f55ec7be765
    Former-commit-id: d1c9985444933eac7044aca9ef0a54ce7cf61ed9

--- src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParameterSpecific/TimeDependentParametersViewModel.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/Editors/Boundaries/ViewModels/WaveBoundaryConditionEditor/BoundaryParameterSpecific/TimeDependentParametersViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Input;
+using DelftTools.Controls.Wpf.Commands;
 using DelftTools.Functions;
 using DelftTools.Utils.Guards;
 using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Parameters;
 using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Spreading;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific.TimeSeriesGeneration;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.WaveBoundaryConditionEditor.BoundaryParameterSpecific
 {
@@ -25,10 +29,14 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Wave
     public class TimeDependentParametersViewModel<TSpreading> : TimeDependentParametersViewModel
         where TSpreading : IBoundaryConditionSpreading, new()
     {
+        private readonly GenerateSeries generateSeries = new GenerateSeries(new GenerateSeriesDialogHelper());
+
         public TimeDependentParametersViewModel(TimeDependentParameters<TSpreading> parameters)
         {
             Ensure.NotNull(parameters, nameof(parameters));
             ObservedParameters = parameters;
+
+            GenerateTimeSeriesCommand = new RelayCommand(view => GenerateSeries((IWin32Window) view));
         }
 
         /// <summary>
@@ -38,5 +46,9 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Gui.Editors.Boundaries.ViewModels.Wave
 
         public override IEnumerable<IFunction> TimeDependentParametersFunctions =>
             new[] {ObservedParameters.WaveEnergyFunction.UnderlyingFunction};
+
+        public ICommand GenerateTimeSeriesCommand { get; }
+
+        private void GenerateSeries(IWin32Window owner) => generateSeries.Execute(owner, ObservedParameters.WaveEnergyFunction);
     }
 }
