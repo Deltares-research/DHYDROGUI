60ba64b1e2d34ac67115c581805c413718dfb370
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-04-08 08:15:21 +0000

    D3DFMIQ-1603 - Add Skeleton MapView.xaml
    Former-commit-id: 2606a09ba66b3acd535e93de356873528a5b0f4a
    Former-commit-id: f0d2e283cc2f0a41ab9316ed9d827bdba6414681

---
+++ src/DeltaShell.Plugins.FMSuite.Common.Gui/MapView/MapView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.FMSuite.Common.Gui.MapView
+{
+    /// <summary>
+    /// Interaction logic for MapView.xaml
+    /// </summary>
+    public partial class MapView : UserControl
+    {
+        public MapView()
+        {
+            InitializeComponent();
+        }
+    }
+}
