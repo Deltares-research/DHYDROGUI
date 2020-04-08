43e1a1695364999e2235c770dab4f565c1837a94
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-04-08 10:27:56 +0000

    D3DFMIQ-1636 - Add documentation KnownWaveBoundariesFileConstants - Add extra constant to KnownWaveBoundariesFileConstants - Add tests KnownWaveBoundariesFileConstants
    Former-commit-id: 638041acbc0986f8e8b97b56ee2dbbd9e25f018c
    Former-commit-id: b30be0de00cf0e023a7d7b91f2db2d3dd4383d16

--- src/DeltaShell.Plugins.FMSuite.Wave/IO/Helpers/Boundaries/KnownWaveBoundariesFileConstants.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/IO/Helpers/Boundaries/KnownWaveBoundariesFileConstants.cs
@@ -1,19 +1,63 @@
 ﻿namespace DeltaShell.Plugins.FMSuite.Wave.IO.Helpers.Boundaries
 {
+    /// <summary>
+    /// Contains known boundary values for the mdw file.
+    /// </summary>
     public static class KnownWaveBoundariesFileConstants
     {
+        /// <summary>
+        /// The degrees defined spreading value description.
+        /// </summary>
         public const string DegreesDefinedSpreading = "Degrees";
-        
+
+        /// <summary>
+        /// The power defined spreading value description.
+        /// </summary>
         public const string PowerDefinedSpreading = "Power";
-        
+
+        /// <summary>
+        /// The gauss shape value description.
+        /// </summary>
         public const string GaussShape = "Gauss";
-        
+
+        /// <summary>
+        /// The jonswap shape value description.
+        /// </summary>
         public const string JonswapShape = "Jonswap";
-        
+
+        /// <summary>
+        /// The pierson moskowitz shape value description.
+        /// </summary>
         public const string PiersonMoskowitzShape = "Pierson-Moskowitz";
-        
+
+        /// <summary>
+        /// The peak period type value description.
+        /// </summary>
         public const string PeakPeriodType = "peak";
-        
+
+        /// <summary>
+        /// The mean period type value description.
+        /// </summary>
         public const string MeanPeriodType = "mean";
+
+        /// <summary>
+        /// The coordinates definition type value description.
+        /// </summary>
+        public const string CoordinatesDefinitionType = "xy-coordinates";
+
+        /// <summary>
+        /// The orientation definition type value description.
+        /// </summary>
+        public const string OrientationDefinitionType = "orientation";
+
+        /// <summary>
+        /// From file spectrum type value description.
+        /// </summary>
+        public const string FromFileSpectrumType = "from file";
+
+        /// <summary>
+        /// The parametrized spectrum type value description.
+        /// </summary>
+        public const string ParametrizedSpectrumType = "parametric";
     }
 }
