555a009c36c940ada9c9a61ec626ed7599cf2d30
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-04-08 13:31:23 +0000

    D3DFMIQ-1603 - Move Boundaries property to separate IBoundaryProvider interface
    Former-commit-id: 7e9a8906334286a7f472c3ef78424e1a1d725725
    Former-commit-id: 410941a7feceefdbdc748685d740cdcd49da9747

--- src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/IBoundaryContainer.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Boundaries/IBoundaryContainer.cs
@@ -1,5 +1,4 @@
-﻿using DelftTools.Utils.Collections.Generic;
-using DeltaShell.Plugins.FMSuite.Wave.Boundaries.Calculators;
+﻿using DeltaShell.Plugins.FMSuite.Wave.Boundaries.Calculators;
 using DeltaShell.Plugins.FMSuite.Wave.Boundaries.GeometricDefinitions;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries
@@ -8,16 +7,10 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Boundaries
     /// <see cref="IBoundaryContainer"/> is responsible for managing a set of
     /// boundaries linked to the current outer grid.
     /// </summary>
-    public interface IBoundaryContainer : IBoundarySnappingCalculatorProvider, IGridBoundaryProvider
+    public interface IBoundaryContainer : IBoundarySnappingCalculatorProvider,
+                                          IGridBoundaryProvider,
+                                          IBoundaryProvider
     {
-        /// <summary>
-        /// Gets the boundaries defined on the current outer grid.
-        /// </summary>
-        /// <value>
-        /// The boundaries defined on the current outer grid.
-        /// </value>
-        IEventedList<IWaveBoundary> Boundaries { get; }
-
         /// <summary>
         /// Updates the current <see cref="IGridBoundary"/> to
         /// <paramref name="gridBoundary"/>.
