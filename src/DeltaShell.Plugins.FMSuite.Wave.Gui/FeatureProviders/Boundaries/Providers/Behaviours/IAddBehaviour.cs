0d2362557b29a7159af56004261ae6d7b55f8df6
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-04-09 06:40:59 +0000

    D3DFMIQ-1603 - Add IAddBehaviour
    Former-commit-id: 713e66bd603e365f246047d5d1e83babea61be63
    Former-commit-id: 1dc6e00a54e57a3572ec72e2bd1b53ec4a75be0f

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/FeatureProviders/Boundaries/Providers/Behaviours/IAddBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using GeoAPI.Geometries;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.FeatureProviders.Boundaries.Providers.Behaviours
+{
+    /// <summary>
+    /// <see cref="IAddBehaviour"/> defines the add behaviour to which
+    /// <see cref="SharpMap.Api.IFeatureProvider"/> of <see cref="Providers"/>
+    /// can delegate their add methods. 
+    /// </summary>
+    public interface IAddBehaviour
+    {
+        /// <summary>
+        /// Execute the this <see cref="IAddBehaviour"/> for the given
+        /// </summary>
+        /// <param name="geometry">The geometry of the Feature to add.</param>
+        /// <remarks>
+        /// The actual behaviour and potential exceptions are left up to the
+        /// implementing classes.
+        /// </remarks>
+        void Execute(IGeometry geometry);
+    }
+}
