39bbe8de97c755cac6af173063eb06f8ad4dc8cf
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-04-18 14:22:25 +0000

    D3DFMIQ-1820 - Add SpectrumParametersVisitor - Add corresponding tests
    Former-commit-id: fdbd44a0d236f5232df23c4aeda89bcbd67c83ab
    Former-commit-id: e60154f65925d38f6f2e2c857ed3d43a6bda0622

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/IO/SpectrumParametersVisitor.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using DelftTools.Utils.Guards;
+using DelftTools.Utils.Reflection;
+using DeltaShell.NGHS.Common.IO;
+using DeltaShell.NGHS.IO.DelftIniObjects;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.ForcingTypeDefinedParameters;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Spreading;
+using DeltaShell.Plugins.FMSuite.Wave.IO.Helpers.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.ModelDefinition;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.IO
+{
+    /// <summary>
+    /// Visits <see cref="IForcingTypeDefinedParameters"/> to build a <see cref="DelftIniCategory"/>
+    /// with spectrum data.
+    /// </summary>
+    /// <seealso cref="IForcingTypeDefinedParametersVisitor"/>
+    public class SpectrumParametersVisitor : IForcingTypeDefinedParametersVisitor
+    {
+        private readonly DelftIniCategory category;
+        private readonly IFilesManager filesManager;
+
+        /// <summary>
+        /// Gets the spectrum file name.
+        /// </summary>
+        public string SpectrumFile { get; private set; }
+
+        /// <summary>
+        /// Gets the spectrum type of the boundary.
+        /// </summary>
+        public SpectrumImportType SpectrumType { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpectrumParametersVisitor"/> class.
+        /// </summary>
+        /// <param name="category">The category.</param>
+        /// <param name="filesManager">The files manager.</param>
+        public SpectrumParametersVisitor(DelftIniCategory category, IFilesManager filesManager)
+        {
+            Ensure.NotNull(category, nameof(category));
+            Ensure.NotNull(filesManager, nameof(filesManager));
+
+            this.category = category;
+            this.filesManager = filesManager;
+        }
+
+        public void Visit<T>(ConstantParameters<T> constantParameters) where T : IBoundaryConditionSpreading, new()
+        {
+            Ensure.NotNull(constantParameters, nameof(constantParameters));
+
+            SpectrumType = SpectrumImportType.Parametrized;
+            SetSpectrumProperty();
+        }
+
+        public void Visit<T>(TimeDependentParameters<T> timeDependentParameters) where T : IBoundaryConditionSpreading, new()
+        {
+            Ensure.NotNull(timeDependentParameters, nameof(timeDependentParameters));
+
+            SpectrumType = SpectrumImportType.Parametrized;
+            SetSpectrumProperty();
+        }
+
+        public void Visit(FileBasedParameters fileBasedParameters)
+        {
+            Ensure.NotNull(fileBasedParameters, nameof(fileBasedParameters));
+
+            SpectrumType = SpectrumImportType.FromFile;
+            SetSpectrumProperty();
+
+            filesManager.Add(fileBasedParameters.FilePath);
+            SpectrumFile = Path.GetFileName(fileBasedParameters.FilePath);
+        }
+
+        private void SetSpectrumProperty()
+        {
+            category.SetProperty(KnownWaveProperties.SpectrumSpec, SpectrumType.GetDescription());
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/IO/SpectrumParametersVisitorTest.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DeltaShell.NGHS.Common.IO;
+using DeltaShell.NGHS.IO.DelftIniObjects;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.ForcingTypeDefinedParameters;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.Spreading;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.ConditionDefinitions.WaveEnergyFunctions;
+using DeltaShell.Plugins.FMSuite.Wave.IO;
+using DeltaShell.Plugins.FMSuite.Wave.IO.Helpers.Boundaries;
+using DeltaShell.Plugins.FMSuite.Wave.ModelDefinition;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
+{
+    [TestFixture]
+    public class SpectrumParametersVisitorTest
+    {
+        private static readonly Random random = new Random();
+        private static int RandomValue => random.Next();
+
+        private IEnumerable<TestCaseData> ConstructorArgumentNullCases()
+        {
+            yield return new TestCaseData(null, Substitute.For<IFilesManager>(), "category");
+            yield return new TestCaseData(new DelftIniCategory(""), null, "filesManager");
+        }
+
+        [TestCaseSource(nameof(ConstructorArgumentNullCases))]
+        public void Constructor_ArgumentNull_ThrowsArgumentNullException(DelftIniCategory category, IFilesManager filesManager, string expectedParamName)
+        {
+            // Call
+            void Call() => new SpectrumParametersVisitor(category, filesManager);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo(expectedParamName));
+        }
+
+        [Test]
+        public void Constructor_InitializesInstanceCorrectly()
+        {
+            // Call
+            var visitor = new SpectrumParametersVisitor(new DelftIniCategory(""),
+                                                        Substitute.For<IFilesManager>());
+
+            // Assert
+            Assert.That(visitor, Is.InstanceOf<IForcingTypeDefinedParametersVisitor>());
+        }
+
+        [Test]
+        public void Visit_FileBasedParameters_FileBasedParametersNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var visitor = new SpectrumParametersVisitor(new DelftIniCategory(""),
+                                                        Substitute.For<IFilesManager>());
+
+            // Call
+            void Call() => visitor.Visit((FileBasedParameters) null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("fileBasedParameters"));
+        }
+
+        [Test]
+        public void Visit_FileBasedParameters_SetsCorrectSpectrumTypeAndFileNameAndProperties()
+        {
+            // Setup
+            var category = new DelftIniCategory("");
+            var visitor = new SpectrumParametersVisitor(category,
+                                                        Substitute.For<IFilesManager>());
+
+            const string fileName = "file.txt";
+            var parameters = new FileBasedParameters($"D:\\some_directory\\{fileName}");
+
+            // Call
+            visitor.Visit(parameters);
+
+            // Assert
+            DelftIniProperty property = category.Properties.Single();
+            Assert.That(property.Name, Is.EqualTo(KnownWaveProperties.SpectrumSpec));
+            Assert.That(property.Value, Is.EqualTo("from file"));
+            Assert.That(visitor.SpectrumType, Is.EqualTo(SpectrumImportType.FromFile));
+            Assert.That(visitor.SpectrumFile, Is.EqualTo(fileName));
+        }
+
+        [TestFixture]
+        [TestFixture(typeof(DegreesDefinedSpreading))]
+        [TestFixture(typeof(PowerDefinedSpreading))]
+        public class WithSpreadingTypes<T> where T : class, IBoundaryConditionSpreading, new()
+        {
+            [Test]
+            public void Visit_ConstantParameters_ConstantParametersNull_ThrowsArgumentNullException()
+            {
+                // Setup
+                var visitor = new SpectrumParametersVisitor(new DelftIniCategory(""),
+                                                            Substitute.For<IFilesManager>());
+
+                // Call
+                void Call() => visitor.Visit((ConstantParameters<T>) null);
+
+                // Assert
+                var exception = Assert.Throws<ArgumentNullException>(Call);
+                Assert.That(exception.ParamName, Is.EqualTo("constantParameters"));
+            }
+
+            [Test]
+            public void Visit_ConstantParameters_SetCorrectSpectrumTypeAndProperty()
+            {
+                // Setup
+                var category = new DelftIniCategory("");
+                var visitor = new SpectrumParametersVisitor(category,
+                                                            Substitute.For<IFilesManager>());
+
+                var parameters = new ConstantParameters<T>(RandomValue, RandomValue, RandomValue, new T());
+
+                // Call
+                visitor.Visit(parameters);
+
+                // Assert
+                DelftIniProperty property = category.Properties.Single();
+                Assert.That(property.Name, Is.EqualTo(KnownWaveProperties.SpectrumSpec));
+                Assert.That(property.Value, Is.EqualTo("parametric"));
+                Assert.That(visitor.SpectrumType, Is.EqualTo(SpectrumImportType.Parametrized));
+                Assert.That(visitor.SpectrumFile, Is.Null);
+            }
+
+            [Test]
+            public void Visit_TimeDependentParameters_TimeDependentParametersNull_ThrowsArgumentNullException()
+            {
+                // Setup
+                var visitor = new SpectrumParametersVisitor(new DelftIniCategory(""),
+                                                            Substitute.For<IFilesManager>());
+
+                // Call
+                void Call() => visitor.Visit((TimeDependentParameters<T>) null);
+
+                // Assert
+                var exception = Assert.Throws<ArgumentNullException>(Call);
+                Assert.That(exception.ParamName, Is.EqualTo("timeDependentParameters"));
+            }
+
+            [Test]
+            public void Visit_TimeDependentParameters_SetCorrectSpectrumTypeAndProperty()
+            {
+                // Setup
+                var category = new DelftIniCategory("");
+                var visitor = new SpectrumParametersVisitor(category,
+                                                            Substitute.For<IFilesManager>());
+
+                var parameters = new TimeDependentParameters<T>(Substitute.For<IWaveEnergyFunction<T>>());
+
+                // Call
+                visitor.Visit(parameters);
+
+                // Assert
+                DelftIniProperty property = category.Properties.Single();
+                Assert.That(property.Name, Is.EqualTo(KnownWaveProperties.SpectrumSpec));
+                Assert.That(property.Value, Is.EqualTo("parametric"));
+                Assert.That(visitor.SpectrumType, Is.EqualTo(SpectrumImportType.Parametrized));
+                Assert.That(visitor.SpectrumFile, Is.Null);
+            }
+        }
+    }
+}
