1c27d5552338ebdc7f0050c96eb3235b469562da
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-04-18 13:09:38 +0000

    D3DFMIQ-1820 - Resharper code-cleanup MdwBoundaryCategoriesCreator
    Former-commit-id: de3ddb5d48b361e450c859d1989762c6111358a9
    Former-commit-id: baf64ca0b70fcf39a1d851f1ad72a84ce6c6fcb8

--- src/DeltaShell.Plugins.FMSuite.Wave/IO/MdwBoundaryCategoriesCreator.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/IO/MdwBoundaryCategoriesCreator.cs
@@ -12,7 +12,8 @@ namespace DeltaShell.Plugins.FMSuite.Wave.IO
     public static class MdwBoundaryCategoriesCreator
     {
         /// <summary>
-        /// Creates and returns a collection of <see cref="DelftIniCategory"/> from the wave boundaries in the specified <paramref name="boundaryContainer"/>
+        /// Creates and returns a collection of <see cref="DelftIniCategory"/> from the wave boundaries in the specified
+        /// <paramref name="boundaryContainer"/>
         /// </summary>
         /// <param name="boundaryContainer"> Boundary container of the model definition </param>
         /// <returns>A collection of <see cref="DelftIniCategory"/>.</returns>
@@ -23,11 +24,11 @@ namespace DeltaShell.Plugins.FMSuite.Wave.IO
         public static IEnumerable<DelftIniCategory> CreateCategories(IBoundaryContainer boundaryContainer)
         {
             Ensure.NotNull(boundaryContainer, nameof(boundaryContainer));
-            
+
             foreach (IWaveBoundary boundary in boundaryContainer.Boundaries)
             {
                 var boundaryCategory = new DelftIniCategory(KnownWaveCategories.BoundaryCategory);
-                
+
                 boundaryCategory.AddProperty(KnownWaveProperties.Name, boundary.Name);
                 MdwBoundaryCategoryGeometryExtender.AddNewProperties(boundaryCategory, boundaryContainer, boundary.GeometricDefinition.SupportPoints);
                 boundaryCategory.AddProperty(KnownWaveProperties.SpectrumSpec, "parametric");
