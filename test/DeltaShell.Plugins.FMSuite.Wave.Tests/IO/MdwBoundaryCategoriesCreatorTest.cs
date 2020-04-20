457d729dadb5519ba3a62ea1cc05c7264ee813f6
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-04-20 08:16:41 +0000

    D3DFMIQ-1820 - Clean-up file. - Rename variable.
    Former-commit-id: e3f14754855cd81f6dcda4ac62bc5a79d415810b
    Former-commit-id: d6fd4a76b3eb188aa131141662b0a1fdc7f1ed7c

--- test/DeltaShell.Plugins.FMSuite.Wave.Tests/IO/MdwBoundaryCategoriesCreatorTest.cs
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/IO/MdwBoundaryCategoriesCreatorTest.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using DelftTools.Utils.Collections.Generic;
-using DelftTools.Utils.Reflection;
 using DeltaShell.NGHS.IO.DelftIniObjects;
 using DeltaShell.Plugins.FMSuite.Wave.Boundaries;
 using DeltaShell.Plugins.FMSuite.Wave.Boundaries.Calculators;
@@ -30,7 +28,7 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
         private static readonly Random random = new Random();
 
         private const double factor = 3.3;
-        private readonly JonswapShape jonswapShape = new JonswapShape { PeakEnhancementFactor = factor };
+        private readonly JonswapShape jonswapShape = new JonswapShape {PeakEnhancementFactor = factor};
 
         private const BoundaryConditionPeriodType periodType = BoundaryConditionPeriodType.Peak;
 
@@ -38,14 +36,14 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
         public void CreateCategories_ShouldCreateACompleteCategoryForOneBoundary()
         {
             var boundaryContainer = Substitute.For<IBoundaryContainer>();
-            
+
             // Boundary
             IWaveBoundary boundary1 = CreateWaveBoundary(out SupportPoint supportPoint1, out SupportPoint supportPoint2);
             const string boundaryName = "boundary1";
             boundary1.Name = boundaryName;
 
             // Add boundary to container
-            var boundaries = new EventedList<IWaveBoundary> { boundary1 };
+            var boundaries = new EventedList<IWaveBoundary> {boundary1};
             boundaryContainer.Boundaries.Returns(boundaries);
 
             // Setup boundary container For boundary
@@ -55,14 +53,14 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
 
             // Act
             IEnumerable<DelftIniCategory> categories = MdwBoundaryCategoriesCreator.CreateCategories(boundaryContainer);
-            
+
             // Assert
             DelftIniCategory createdCategory = categories.Single();
             List<DelftIniProperty> properties = createdCategory.Properties.ToList();
-            
+
             CheckCreatedCategory(properties, boundaryName, coordinate1, coordinate2);
         }
-        
+
         [Test]
         public void CreateCategories_ShouldCreateCategoriesForAllBoundaries()
         {
@@ -77,16 +75,20 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
             boundary2.Name = boundary2Name;
 
             // Add Boundaries to container
-            var boundaries = new EventedList<IWaveBoundary> { boundary1 , boundary2};
+            var boundaries = new EventedList<IWaveBoundary>
+            {
+                boundary1,
+                boundary2
+            };
             boundaryContainer.Boundaries.Returns(boundaries);
 
             // Setup boundary container for boundaries
             var boundarySnappingCalculator = Substitute.For<IBoundarySnappingCalculator>();
             boundaryContainer.GetBoundarySnappingCalculator().Returns(boundarySnappingCalculator);
             SetupBoundaryContainerForBoundary(boundarySnappingCalculator, boundary1SupportPoint1, boundary1SupportPoint2,
-                                               out Coordinate boundary1Coordinate1, out Coordinate boundary1Coordinate2);
+                                              out Coordinate boundary1Coordinate1, out Coordinate boundary1Coordinate2);
             SetupBoundaryContainerForBoundary(boundarySnappingCalculator, boundary2SupportPoint1, boundary2SupportPoint2,
-                                               out Coordinate boundary2Coordinate1, out Coordinate boundary2Coordinate2);
+                                              out Coordinate boundary2Coordinate1, out Coordinate boundary2Coordinate2);
 
             // Act
             List<DelftIniCategory> categories = MdwBoundaryCategoriesCreator.CreateCategories(boundaryContainer).ToList();
@@ -95,7 +97,7 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
             Assert.AreEqual(2, categories.Count);
             DelftIniCategory category1 = categories.First();
             DelftIniCategory category2 = categories.Last();
-            
+
             CheckCreatedCategory(category1.Properties.ToList(), boundary1Name, boundary1Coordinate1, boundary1Coordinate2);
             CheckCreatedCategory(category2.Properties.ToList(), boundary2Name, boundary2Coordinate1, boundary2Coordinate2);
         }
@@ -113,7 +115,7 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
 
         private IWaveBoundary CreateWaveBoundary(out SupportPoint supportPoint1, out SupportPoint supportPoint2)
         {
-            var boundary1 = Substitute.For<IWaveBoundary>();
+            var boundary = Substitute.For<IWaveBoundary>();
 
             // Geometry definition for boundary
             var geometryDefinition = Substitute.For<IWaveBoundaryGeometricDefinition>();
@@ -124,20 +126,20 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
                 supportPoint2,
                 supportPoint1
             };
-            boundary1.GeometricDefinition.SupportPoints.Returns(supportPoints);
+            boundary.GeometricDefinition.SupportPoints.Returns(supportPoints);
 
             // Condition definition for boundary
             var dataComponent = new UniformDataComponent<TimeDependentParameters<PowerDefinedSpreading>>(
                 new TimeDependentParameters<PowerDefinedSpreading>(
                     Substitute.For<IWaveEnergyFunction<PowerDefinedSpreading>>()));
             var conditionDefinition = new WaveBoundaryConditionDefinition(jonswapShape, periodType, dataComponent);
-            boundary1.ConditionDefinition.Returns(conditionDefinition);
-            return boundary1;
+            boundary.ConditionDefinition.Returns(conditionDefinition);
+            return boundary;
         }
 
         private static void SetupBoundaryContainerForBoundary(IBoundarySnappingCalculator boundarySnappingCalculator, SupportPoint supportPoint1,
-                                                               SupportPoint supportPoint2, out Coordinate coordinate1,
-                                                               out Coordinate coordinate2)
+                                                              SupportPoint supportPoint2, out Coordinate coordinate1,
+                                                              out Coordinate coordinate2)
         {
             coordinate1 = Substitute.For<Coordinate>();
             coordinate1.X = random.NextDouble();
@@ -148,7 +150,7 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
             boundarySnappingCalculator.CalculateCoordinateFromSupportPoint(supportPoint1).Returns(coordinate1);
             boundarySnappingCalculator.CalculateCoordinateFromSupportPoint(supportPoint2).Returns(coordinate2);
         }
-        
+
         private static void CheckCreatedCategory(List<DelftIniProperty> properties, string boundary1Name, Coordinate coordinate1,
                                                  Coordinate coordinate2)
         {
@@ -177,9 +179,6 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO
             Assert.AreEqual(GetStringValue(factor), properties[10].Value);
         }
 
-        private static string GetStringValue(double value)
-        {
-            return value.ToString("e7", CultureInfo.InvariantCulture);
-        }
+        private static string GetStringValue(double value) => value.ToString("e7", CultureInfo.InvariantCulture);
     }
 }
