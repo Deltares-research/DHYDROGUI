60289230320e1f9ffaa228a1c18f59bd95840ad6
Hidde Elzinga <hidde.elzinga@deltares.nl>
2020-04-23 13:39:38 +0000

    FM1D2D-531 Added extra logic for deleting a SewerConnection (remove manholes without pipe connections (and there inner-links))
    Former-commit-id: afeb85d795653c3a5fbc3e188e5a9fd54d00f648
    Former-commit-id: e73a3afa465acc0877d270cc1d9602616ac9d60e

--- src/DeltaShell.Plugins.NetworkEditor/MapLayers/Editors/Interactors/SewerConnectionInteractor.cs
+++ src/DeltaShell.Plugins.NetworkEditor/MapLayers/Editors/Interactors/SewerConnectionInteractor.cs
@@ -1,4 +1,5 @@
-﻿using DelftTools.Hydro.SewerFeatures;
+﻿using System.Linq;
+using DelftTools.Hydro.SewerFeatures;
 using DelftTools.Hydro.Structures;
 using DelftTools.Utils.Editing;
 using GeoAPI.Extensions.Feature;
@@ -17,6 +18,33 @@ namespace DeltaShell.Plugins.NetworkEditor.MapLayers.Editors.Interactors
         public override void Add(IFeature feature)
         {
             SewerFactory.AddDefaultPipeToNetwork((IPipe)feature, Network);
+        }
+
+        public override void Delete()
+        {
+            base.Delete();
+
+            if (!(SourceFeature is ISewerConnection connection))
+            {
+                return;
+            }
+            
+            var nodes = new[] {connection.Source, connection.Target};
+
+            // check for pipe connections if non then remove nodes
+            var nodesToRemove = nodes
+                .Where(n => !n.IncomingBranches.OfType<IPipe>().Concat(n.OutgoingBranches.OfType<IPipe>()).Any())
+                .ToList();
+
+            // check for internal branches to remove
+            var internalBranches = nodesToRemove
+                .SelectMany(n => n.IncomingBranches.Concat(n.OutgoingBranches))
+                .OfType<ISewerConnection>()
+                .Distinct()
+                .ToList();
+            
+            nodesToRemove.ForEach(n => Network.Nodes.Remove(n));
+            internalBranches.ForEach(b => Network.Branches.Remove(b));
         }
     }
 }
