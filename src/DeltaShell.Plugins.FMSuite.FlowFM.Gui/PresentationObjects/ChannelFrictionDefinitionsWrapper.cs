ec38632e0c4e6140c2a60d1549798a40e7736b59
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-04-30 17:35:39 +0000

    FM1D2D-753 Introduced presentation objects.
    Former-commit-id: 2c8ffa0b686207a4a3d3ca9fefbe9cb0abfba466
    Former-commit-id: 1dcb9d7a7972050cde45356c58b0d644af2cb106

---
+++ src/DeltaShell.Plugins.FMSuite.FlowFM.Gui/PresentationObjects/ChannelFrictionDefinitionsWrapper.cs
@@ -0,0 +1,23 @@
+﻿using DelftTools.Utils.Collections.Generic;
+using DeltaShell.NGHS.IO.DataObjects.Friction;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.Gui.PresentationObjects
+{
+    /// <summary>
+    /// Presentation object for visualizing <see cref="IEventedList{ChannelFrictionDefinition}"/>
+    /// in project explorer and map tree.
+    /// </summary>
+    /// <remarks>
+    /// Directly using the <see cref="IEventedList{ChannelFrictionDefinition}"/> for presentation results
+    /// in unwanted selection synchronization after clicking the related node in the project explorer.
+    /// </remarks>
+    public class ChannelFrictionDefinitionsWrapper
+    {
+        public ChannelFrictionDefinitionsWrapper(IEventedList<ChannelFrictionDefinition> wrappedData)
+        {
+            WrappedData = wrappedData;
+        }
+
+        public IEventedList<ChannelFrictionDefinition> WrappedData { get; private set; }
+    }
+}
