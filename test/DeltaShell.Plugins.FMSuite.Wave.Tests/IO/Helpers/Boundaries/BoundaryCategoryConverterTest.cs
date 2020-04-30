24bb7e24285a7d0c794cfc193af909d47b97914f
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-04-30 08:15:44 +0000

    D3DFMIQ-1487 - Refactor BoundaryCategoryConverterTest
    Former-commit-id: 6609a1385966cbc32fe86d7a5e22eb0b43a6b338
    Former-commit-id: 3879d3d89a38ac1a7370728c381b1c1f7a8e865d

--- test/DeltaShell.Plugins.FMSuite.Wave.Tests/IO/Helpers/Boundaries/BoundaryCategoryConverterTest.cs
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/IO/Helpers/Boundaries/BoundaryCategoryConverterTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using DelftTools.Utils.Reflection;
 using DeltaShell.NGHS.IO.DelftIniObjects;
 using DeltaShell.NGHS.TestUtils;
@@ -58,10 +59,10 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Boundaries
 
         [Test]
         [TestCaseSource(nameof(GetParameterizedTestCases))]
-        public void Convert_ParameterizedImportType_ReturnsCorrectResult(string shapeTypeStr, ShapeImportType expectedShapeType,
-                                                                         string periodTypeStr, PeriodImportExportType expectedPeriodType,
-                                                                         string spreadingTypeStr, SpreadingImportType expectedSpreadingType,
-                                                                         string definitionTypeStr, DefinitionImportType expectedDefinitionType)
+        public void Convert_ParameterizedImportType_ReturnsCorrectResult(CategoryTestKeyValue<ShapeImportType> shapeImportData,
+                                                                         CategoryTestKeyValue<PeriodImportExportType> periodImportExportData,
+                                                                         CategoryTestKeyValue<SpreadingImportType> spreadingImportData,
+                                                                         CategoryTestKeyValue<DefinitionImportType> definitionImportData)
         {
             // Setup
             var category = new DelftIniCategory(KnownWaveCategories.BoundaryCategory);
@@ -85,15 +86,15 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Boundaries
             double gaussianSpread = RandomDouble;
 
             category.AddProperty(KnownWaveProperties.Name, name);
-            category.AddProperty(KnownWaveProperties.Definition, definitionTypeStr);
+            AddToCategory(category, definitionImportData); 
             category.AddProperty(KnownWaveProperties.StartCoordinateX, ToString(startX));
             category.AddProperty(KnownWaveProperties.StartCoordinateY, ToString(startY));
             category.AddProperty(KnownWaveProperties.EndCoordinateX, ToString(endX));
             category.AddProperty(KnownWaveProperties.EndCoordinateY, ToString(endY));
             category.AddProperty(KnownWaveProperties.SpectrumSpec, SpectrumImportExportType.Parametrized.GetDescription());
-            category.AddProperty(KnownWaveProperties.ShapeType, shapeTypeStr);
-            category.AddProperty(KnownWaveProperties.PeriodType, periodTypeStr);
-            category.AddProperty(KnownWaveProperties.DirectionalSpreadingType, spreadingTypeStr);
+            AddToCategory(category, shapeImportData); 
+            AddToCategory(category, periodImportExportData); 
+            AddToCategory(category, spreadingImportData); 
             category.AddProperty(KnownWaveProperties.PeakEnhancementFactor, ToString(peakEnhancementFactor));
             category.AddProperty(KnownWaveProperties.GaussianSpreading, ToString(gaussianSpread));
             category.AddProperty(KnownWaveProperties.CondSpecAtDist, ToString(distance1));
@@ -112,15 +113,15 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Boundaries
 
             // Assert
             Assert.That(result.Name, Is.EqualTo(name));
-            Assert.That(result.DefinitionType, Is.EqualTo(expectedDefinitionType));
+            Assert.That(result.DefinitionType, Is.EqualTo(definitionImportData.ExpectedValue));
             Assert.That(result.XStartCoordinate, Is.EqualTo(startX));
             Assert.That(result.YStartCoordinate, Is.EqualTo(startY));
             Assert.That(result.XEndCoordinate, Is.EqualTo(endX));
             Assert.That(result.YEndCoordinate, Is.EqualTo(endY));
             Assert.That(result.SpectrumType, Is.EqualTo(SpectrumImportExportType.Parametrized));
-            Assert.That(result.ShapeType, Is.EqualTo(expectedShapeType));
-            Assert.That(result.PeriodType, Is.EqualTo(expectedPeriodType));
-            Assert.That(result.SpreadingType, Is.EqualTo(expectedSpreadingType));
+            Assert.That(result.ShapeType, Is.EqualTo(shapeImportData.ExpectedValue));
+            Assert.That(result.PeriodType, Is.EqualTo(periodImportExportData.ExpectedValue));
+            Assert.That(result.SpreadingType, Is.EqualTo(spreadingImportData.ExpectedValue));
             Assert.That(result.Distances, Is.EqualTo(Doubles(distance1, distance2)));
             Assert.That(result.WaveHeights, Is.EqualTo(Doubles(waveHeight1, waveHeight2)));
             Assert.That(result.Periods, Is.EqualTo(Doubles(period1, period2)));
@@ -130,7 +131,10 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Boundaries
 
         [Test]
         [TestCaseSource(nameof(GetFromFileTestCases))]
-        public void Convert_FromFileImportType_ReturnsCorrectResult(string shapeTypeStr, string periodTypeStr, string spreadingTypeStr, string definitionTypeStr, DefinitionImportType expectedDefinitionType)
+        public void Convert_FromFileImportType_ReturnsCorrectResult(string shapeTypeStr, 
+                                                                    string periodTypeStr, 
+                                                                    string spreadingTypeStr, 
+                                                                    CategoryTestKeyValue<DefinitionImportType> definitionImportData)
         {
             // Setup
             var category = new DelftIniCategory(KnownWaveCategories.BoundaryCategory);
@@ -148,7 +152,7 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Boundaries
             double gaussianSpread = RandomDouble;
 
             category.AddProperty(KnownWaveProperties.Name, name);
-            category.AddProperty(KnownWaveProperties.Definition, definitionTypeStr);
+            AddToCategory(category, definitionImportData);
             category.AddProperty(KnownWaveProperties.StartCoordinateX, ToString(startX));
             category.AddProperty(KnownWaveProperties.StartCoordinateY, ToString(startY));
             category.AddProperty(KnownWaveProperties.EndCoordinateX, ToString(endX));
@@ -169,7 +173,7 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Boundaries
 
             // Assert
             Assert.That(result.Name, Is.EqualTo(name));
-            Assert.That(result.DefinitionType, Is.EqualTo(expectedDefinitionType));
+            Assert.That(result.DefinitionType, Is.EqualTo(definitionImportData.ExpectedValue));
             Assert.That(result.XStartCoordinate, Is.EqualTo(startX));
             Assert.That(result.YStartCoordinate, Is.EqualTo(startY));
             Assert.That(result.XEndCoordinate, Is.EqualTo(endX));
@@ -232,68 +236,85 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Boundaries
             Assert.That(result.PeakEnhancementFactor, Is.NaN);
         }
 
-        private static IEnumerable<TestCaseData> GetParameterizedTestCases()
+        private static IEnumerable<TestCaseData> GetParameterizedTestCases() =>
+            from shapeTypeCase in ShapeTypeTestCases() 
+            from periodTypeCase in PeriodTypeTestCases() 
+            from spreadingTypeCase in SpreadingTypeTestCases() 
+            from definitionTypeCase in DefinitionTypeTestCases() 
+            select new TestCaseData(shapeTypeCase,
+                                    periodTypeCase,
+                                    spreadingTypeCase,
+                                    definitionTypeCase);
+
+        private static IEnumerable<TestCaseData> GetFromFileTestCases() =>
+            from shapeTypeCase in ShapeTypeTestCases() 
+            from periodTypeCase in PeriodTypeTestCases() 
+            from spreadingTypeCase in SpreadingTypeTestCases() 
+            from definitionTypeCase in DefinitionTypeTestCases() 
+            select new TestCaseData(shapeTypeCase.StringValue,
+                                    periodTypeCase.StringValue,
+                                    spreadingTypeCase.StringValue,
+                                    definitionTypeCase);
+
+        private static IEnumerable<CategoryTestKeyValue<ShapeImportType>> ShapeTypeTestCases()
         {
-            foreach (object[] shapeTypeCase in ShapeTypeTestCases())
-            foreach (object[] periodTypeCase in PeriodTypeTestCases())
-            foreach (object[] spreadingTypeCase in SpreadingTypeTestCases())
-            foreach (object[] definitionTypeCase in DefinitionTypeTestCases())
+            yield return new CategoryTestKeyValue<ShapeImportType>(KnownWaveProperties.ShapeType,
+                                                                   "gauss", 
+                                                                   ShapeImportType.Gauss);
+            yield return new CategoryTestKeyValue<ShapeImportType>(KnownWaveProperties.ShapeType,
+                                                                   "jonswap", 
+                                                                   ShapeImportType.Jonswap);
+            yield return new CategoryTestKeyValue<ShapeImportType>(KnownWaveProperties.ShapeType,
+                                                                   "pierson-moskowitz", 
+                                                                   ShapeImportType.PiersonMoskowitz);
+        }
+
+        private static IEnumerable<CategoryTestKeyValue<PeriodImportExportType>> PeriodTypeTestCases()
+        {
+            yield return new CategoryTestKeyValue<PeriodImportExportType>(KnownWaveProperties.PeriodType, 
+                                                                          "mean", 
+                                                                          PeriodImportExportType.Mean);
+            yield return new CategoryTestKeyValue<PeriodImportExportType>(KnownWaveProperties.PeriodType, 
+                                                                          "peak", 
+                                                                          PeriodImportExportType.Peak);
+        }
+
+        private static IEnumerable<CategoryTestKeyValue<SpreadingImportType>> SpreadingTypeTestCases()
+        {
+            yield return new CategoryTestKeyValue<SpreadingImportType>(KnownWaveProperties.DirectionalSpreadingType, 
+                                                                       "degrees", 
+                                                                       SpreadingImportType.Degrees);
+            yield return new CategoryTestKeyValue<SpreadingImportType>(KnownWaveProperties.DirectionalSpreadingType, 
+                                                                       "power", 
+                                                                       SpreadingImportType.Power);
+        }
+
+        private static IEnumerable<CategoryTestKeyValue<DefinitionImportType>> DefinitionTypeTestCases()
+        {
+            yield return new CategoryTestKeyValue<DefinitionImportType>(KnownWaveProperties.Definition, 
+                                                                        "xy-coordinates", 
+                                                                        DefinitionImportType.Coordinates);
+            yield return new CategoryTestKeyValue<DefinitionImportType>(KnownWaveProperties.Definition, 
+                                                                        "orientation", 
+                                                                        DefinitionImportType.Oriented);
+        }
+
+        public class CategoryTestKeyValue<T>
+        {
+            public CategoryTestKeyValue(string key, string stringValue, T expectedValue)
             {
-                yield return new TestCaseData(shapeTypeCase[0], shapeTypeCase[1],
-                                              periodTypeCase[0], periodTypeCase[1],
-                                              spreadingTypeCase[0], spreadingTypeCase[1],
-                                              definitionTypeCase[0], definitionTypeCase[1]);
+                Key = key;
+                StringValue = stringValue;
+                ExpectedValue = expectedValue;
             }
+
+            public string Key { get; }
+            public string StringValue { get; }
+            public T ExpectedValue { get; }
         }
 
-        private static IEnumerable<TestCaseData> GetFromFileTestCases()
-        {
-            foreach (object[] shapeTypeCase in ShapeTypeTestCases())
-            foreach (object[] periodTypeCase in PeriodTypeTestCases())
-            foreach (object[] spreadingTypeCase in SpreadingTypeTestCases())
-            foreach (object[] definitionTypeCase in DefinitionTypeTestCases())
-            {
-                yield return new TestCaseData(shapeTypeCase[0],
-                                              periodTypeCase[0],
-                                              spreadingTypeCase[0],
-                                              definitionTypeCase[0],
-                                              definitionTypeCase[1]);
-            }
-        }
-
-        private static IEnumerable<object[]> ShapeTypeTestCases()
-        {
-            yield return SubTestCase("gauss", ShapeImportType.Gauss);
-            yield return SubTestCase("jonswap", ShapeImportType.Jonswap);
-            yield return SubTestCase("pierson-moskowitz", ShapeImportType.PiersonMoskowitz);
-        }
-
-        private static IEnumerable<object[]> PeriodTypeTestCases()
-        {
-            yield return SubTestCase("mean", PeriodImportExportType.Mean);
-            yield return SubTestCase("peak", PeriodImportExportType.Peak);
-        }
-
-        private static IEnumerable<object[]> SpreadingTypeTestCases()
-        {
-            yield return SubTestCase("degrees", SpreadingImportType.Degrees);
-            yield return SubTestCase("power", SpreadingImportType.Power);
-        }
-
-        private static IEnumerable<object[]> DefinitionTypeTestCases()
-        {
-            yield return SubTestCase("xy-coordinates", DefinitionImportType.Coordinates);
-            yield return SubTestCase("orientation", DefinitionImportType.Oriented);
-        }
-
-        private static object[] SubTestCase(string value, object expected)
-        {
-            return new[]
-            {
-                value,
-                expected
-            };
-        }
+        private static void AddToCategory<T>(DelftIniCategory category, CategoryTestKeyValue<T> keyValuePair) =>
+            category.AddProperty(keyValuePair.Key, keyValuePair.StringValue);
 
         private static IEnumerable<double> Doubles(double a, double b)
         {
