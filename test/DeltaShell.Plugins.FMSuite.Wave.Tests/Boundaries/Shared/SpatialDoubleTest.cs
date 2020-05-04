11c8baff70bbfaa3d2fb989a8a13c77691b1a80b
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-05-04 13:41:40 +0000

    D3DFMIQ-1885 - Add SpatialDouble. - Add SpatialDoubleTest.
    Former-commit-id: db4c9612b47b7c5cff8fd67c6eca1c3b7b5d2964
    Former-commit-id: 9e101ed15fbff50e04f9b2b232b0abeb9e8e07ef

---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Boundaries/Shared/SpatialDoubleTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.Wave.Boundaries.Shared;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Boundaries.Shared
+{
+    [TestFixture]
+    public class SpatialDoubleTest
+    {
+        private readonly Random random = new Random();
+
+        [TestCaseSource(nameof(GetRoundTestCases))]
+        public void Round_ReturnsCorrectResult(double value, double expected)
+        {
+            // Call
+            double result = SpatialDouble.Round(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        private IEnumerable<TestCaseData> GetRoundTestCases()
+        {
+            double value = random.NextDouble();
+            double expected = Math.Round(value, 7, MidpointRounding.AwayFromZero);
+            yield return new TestCaseData(value, expected);
+
+            value = 0.00000005;
+            expected = 0.0000001;
+            yield return new TestCaseData(value, expected);
+        }
+
+        [TestCaseSource(nameof(GetAreEqualTestCases))]
+        public void AreEqual_ReturnsCorrectResult(double valueA, double valueB, bool expectedResult)
+        {
+            // Call
+            bool result = SpatialDouble.AreEqual(valueA, valueB);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        private IEnumerable<TestCaseData> GetAreEqualTestCases()
+        {
+            const double maxDif = 1E-7;
+            double valueA = 0;
+            double valueB = maxDif;
+            yield return new TestCaseData(valueA, valueB, false);
+
+            valueA = random.NextDouble();
+            valueB = valueA + maxDif + 1E-10;
+            yield return new TestCaseData(valueA, valueB, false);
+
+            valueA = random.NextDouble();
+            valueB = valueA + (maxDif - 1E-10);
+            yield return new TestCaseData(valueA, valueB, true);
+        }
+    }
+}
