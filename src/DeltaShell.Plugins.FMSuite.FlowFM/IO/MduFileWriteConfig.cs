de8404d77c22c5764933b2f400b9138aa2354ede
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-05-12 07:27:20 +0000

    D3DFMIQ-1895 Merged revision(s) 47823 from branches/FeatureBranches/NGHS_BO_DataAccessLayerFM: Moved interface to separate file. ........
    Former-commit-id: 86b4c1f0dbf931e1da038b3c0978c624b522eb2e
    Former-commit-id: c3b34dc7ea3d7bbc856dd8752bdc33fde51f994e

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/MduFileWriteConfig.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/MduFileWriteConfig.cs
@@ -1,55 +1,9 @@
-﻿using DeltaShell.Plugins.FMSuite.FlowFM.IO.Files;
-
-namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
+﻿namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
 {
-    /// <summary>
-    /// IMduFileWriteConfig collects a set of boolean values used to configure
-    /// which parts should be written, when <see cref="MduFile.Write"/> and
-    /// <see cref="MduFile.WriteProperties"/> are called.
-    /// </summary>
-    public interface IMduFileWriteConfig
-    {
-        /// <summary>
-        /// Get whether [the external forcings should be written].
-        /// </summary>
-        /// <value>
-        /// <c> true </c> if [the external forcings should be written];
-        /// otherwise, <c> false </c>.
-        /// </value>
-        bool WriteExtForcings { get; }
-
-        /// <summary>
-        /// Get whether [the features should be written].
-        /// </summary>
-        /// <value>
-        /// <c> true </c> if [the features should be written];
-        /// otherwise, <c> false </c>.
-        /// </value>
-        bool WriteFeatures { get; }
-
-        /// <summary>
-        /// Get whether [the Sediment and Morphology should be written].
-        /// </summary>
-        /// <value>
-        /// <c> true </c> if [the Sediment and Morphology should be written];
-        /// otherwise, <c> false </c>.
-        /// </value>
-        bool WriteMorphologySediment { get; }
-
-        /// <summary>
-        /// Get whether [the flow renumbering should be disabled].
-        /// </summary>
-        /// <value>
-        /// <c> true </c> if [the flow renumbering should be disabled];
-        /// otherwise, <c> false </c>.
-        /// </value>
-        bool DisableFlowNodeRenumbering { get; }
-    }
-
     /// <summary>
     /// Concrete implementation of the <see cref="IMduFileWriteConfig"/> interface.
     /// </summary>
-    /// <seealso cref="DeltaShell.Plugins.FMSuite.FlowFM.IO.IMduFileWriteConfig"/>
+    /// <seealso cref="IMduFileWriteConfig" />
     public class MduFileWriteConfig : IMduFileWriteConfig
     {
         /// <summary>
