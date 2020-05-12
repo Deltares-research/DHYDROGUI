0c6b0095cda5161ceb86a16de09c2f9930c208e4
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-05-12 10:03:43 +0000

    D3DFMIQ-1895 Merged revision(s) 48025 from branches/FeatureBranches/NGHS_BO_DataAccessLayerFM: Refactored ExtForceFile. ........
    Former-commit-id: dd79f725e49450fd6abcd327ce6b45b7f9f57504
    Former-commit-id: 87a0f6e13ba112548855204ec89e30524881a49d

---
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Files/ExtForceFileConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.Files {
+    public static class ExtForceFileConstants
+    {
+        public static readonly string[] UnsupportedQuantityKeys = 
+        {
+            "WUANTITY",
+            "_UANTITY"
+        };
+
+        public const string FricTypeKey = "IFRCTYP";
+        public const string AreaKey = "AREA";
+        public const string AveragingTypeKey = "AVERAGINGTYPE";
+        public const string RelSearchCellSizeKey = "RELATIVESEARCHCELLSIZE";
+        public const string ExtForcesFileQuantBlockStarter = "QUANTITY=";
+        public const string DisabledQuantityKey = "DISABLED_QUANTITY";
+        public const string QuantityKey = "QUANTITY";
+        public const string FileNameKey = "FILENAME";
+        public const string FileTypeKey = "FILETYPE";
+        public const string MethodKey = "METHOD";
+        public const string OperandKey = "OPERAND";
+        public const string ValueKey = "VALUE";
+        public const string FactorKey = "FACTOR";
+        public const string OffsetKey = "OFFSET";
+        public const string SedConcPostfix = "_SedConc";
+    }
+}
