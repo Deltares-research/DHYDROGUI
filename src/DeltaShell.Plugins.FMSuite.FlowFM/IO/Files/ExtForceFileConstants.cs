30a9b65e5bc342d1404f84ba6f43416cdb5ba1d9
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-05-14 16:19:28 +0000

    D3DFMIQ-1895 - Add documentation to ExtForceFileConstants. - Add ExtForceFileConstantsTest.
    Former-commit-id: cca6c38c53246056685e4f8d404b0933ac439689
    Former-commit-id: 70959ab9b84dfb6212868a9939c24ebdd29e4379

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Files/ExtForceFileConstants.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Files/ExtForceFileConstants.cs
@@ -1,10 +1,28 @@
 ﻿namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.Files
 {
+    /// <summary>
+    /// <see cref="ExtForceFileConstants"/> contains constant values for the external forcing file.
+    /// </summary>
     public static class ExtForceFileConstants
     {
+        /// <summary>
+        /// The area key.
+        /// </summary>
         public const string AreaKey = "AREA";
+
+        /// <summary>
+        /// The averaging type key.
+        /// </summary>
         public const string AveragingTypeKey = "AVERAGINGTYPE";
+
+        /// <summary>
+        /// The relative search cell size key.
+        /// </summary>
         public const string RelSearchCellSizeKey = "RELATIVESEARCHCELLSIZE";
+
+        /// <summary>
+        /// The sediment concentration postfix.
+        /// </summary>
         public const string SedimentConcentrationPostfix = "_SedConc";
     }
 }
