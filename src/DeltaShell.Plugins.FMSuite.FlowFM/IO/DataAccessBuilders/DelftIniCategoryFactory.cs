1fa8fc9cf5bcee1a26debcf517026ea5c5ae9d3d
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-05-14 16:48:54 +0000

    D3DFMIQ-1895 - Add documentation to DelftIniCategoryFactory. - Add DelftIniCategoryFactoryTest.
    Former-commit-id: 76193112dcd65199859fe281f2ff1cb489845336
    Former-commit-id: f2bcd2772aa2df0ada07a4ccb958e787f3e324ac

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/DataAccessBuilders/DelftIniCategoryFactory.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/DataAccessBuilders/DelftIniCategoryFactory.cs
@@ -4,13 +4,26 @@ using DeltaShell.Plugins.FMSuite.FlowFM.IO.Files;
 
 namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.DataAccessBuilders
 {
+    /// <summary>
+    /// This <see cref="DelftIniCategoryFactory"/> provides a method to
+    /// create a <see cref="DelftIniCategory"/>.
+    /// </summary>
     public static class DelftIniCategoryFactory
     {
         private const double openBoundaryTolerance = 0.5;
 
+        /// <summary>
+        /// Creates the boundary <see cref="DelftIniCategory"/> with the specified property values.
+        /// </summary>
+        /// <param name="quantity">The quantity.</param>
+        /// <param name="locationFilePath">The location file path.</param>
+        /// <param name="forcingFilePath">The forcing file path.</param>
+        /// <param name="thatcherHarlemanTimeLag">The Thatcher-Harleman time lag.</param>
+        /// <param name="isEmbankment">Whether or not the feature is an embankment. </param>
+        /// <returns></returns>
         public static DelftIniCategory CreateBoundaryBlock(string quantity, string locationFilePath,
-                                                            string forcingFilePath, TimeSpan thatcherHarlemanTimeLag,
-                                                            bool isEmbankment = false)
+                                                           string forcingFilePath, TimeSpan thatcherHarlemanTimeLag,
+                                                           bool isEmbankment = false)
         {
             var block = new DelftIniCategory(BndExtForceFileConstants.BoundaryBlockKey);
             if (quantity != null)
---
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/DataAccessBuilders/DelftIniCategoryFactoryTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using DeltaShell.NGHS.IO.DelftIniObjects;
+using DeltaShell.Plugins.FMSuite.FlowFM.IO.DataAccessBuilders;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.IO.DataAccessBuilders
+{
+    [TestFixture]
+    public class DelftIniCategoryFactoryTest
+    {
+        [Test]
+        public void CreateBoundaryBlock_CreatesCorrectDelftIniCategory()
+        {
+            const string quantity = "some_quantity";
+            const string locationFilePath = "some_location_file_path";
+            const string forcingFilePath = "some_forcing_file_path";
+            var thatcherHarlemanTimeLag = new TimeSpan(1, 2, 3);
+            const bool isEmbankment = true;
+
+            // Call
+            DelftIniCategory result = DelftIniCategoryFactory.CreateBoundaryBlock(quantity, locationFilePath, forcingFilePath, thatcherHarlemanTimeLag, isEmbankment);
+
+            // Assert
+            Assert.That(result.Name, Is.EqualTo("[boundary]"));
+            Assert.That(result.Properties, Has.Count.EqualTo(5));
+            CategoryContains(result, "quantity", quantity);
+            CategoryContains(result, "locationfile", locationFilePath);
+            CategoryContains(result, "forcingfile", forcingFilePath);
+            CategoryContains(result, "return_time", "3.7230000e+003");
+            CategoryContains(result, "OpenBoundaryTolerance", "5.0000000e-001");
+        }
+
+        [Test]
+        public void CreateBoundaryBlock_InvalidValues_CreatesCorrectDelftIniCategory()
+        {
+            // Call
+            DelftIniCategory result = DelftIniCategoryFactory.CreateBoundaryBlock(null, null, null, TimeSpan.Zero);
+
+            // Assert
+            Assert.That(result.Name, Is.EqualTo("[boundary]"));
+            Assert.That(result.Properties, Has.Count.EqualTo(0));
+            CategoryDoesNotContain(result, "quantity");
+            CategoryDoesNotContain(result, "locationfile");
+            CategoryDoesNotContain(result, "forcingfile");
+            CategoryDoesNotContain(result, "return_time");
+            CategoryDoesNotContain(result, "OpenBoundaryTolerance");
+        }
+
+        public static void CategoryContains(DelftIniCategory category, string propertyName, object propertyValue)
+        {
+            DelftIniProperty property = category.Properties.FirstOrDefault(p => p.Name == propertyName);
+            Assert.That(property, Is.Not.Null,
+                        $"Category should contain property <{propertyName}>.");
+            Assert.That(property.Value, Is.EqualTo(propertyValue),
+                        $"Property '{propertyName}' has an incorrect value.");
+        }
+
+        public static void CategoryDoesNotContain(DelftIniCategory category, string propertyName)
+        {
+            DelftIniProperty property = category.Properties.FirstOrDefault(p => p.Name == propertyName);
+            Assert.That(property, Is.Null,
+                        $"Category should not contain property <{propertyName}>.");
+        }
+    }
+}
