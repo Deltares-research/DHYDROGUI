88436b0f45fdaf83fe68c34bd0a433f5ef1e4d6b
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-05-14 16:13:37 +0000

    D3DFMIQ-1895 - Add documentation to ImportSamplesSpatialOperation. - Add ImportSamplesSpatialOperationTest.
    Former-commit-id: c39fd1fee4f9c8b71b23a26fe2bcb8487befe3f6
    Former-commit-id: b9a997ffdd04a3c76c45a1b6430ae6dc26c55d37

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Files/Helpers/ImportSamplesSpatialOperation.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/Files/Helpers/ImportSamplesSpatialOperation.cs
@@ -6,12 +6,39 @@ using SharpMap.SpatialOperations;
 
 namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.Files.Helpers
 {
+    /// <summary>
+    /// Spatial operation that imports the samples.
+    /// </summary>
+    /// <seealso cref="ImportSamplesOperation" />
     public class ImportSamplesSpatialOperation : ImportSamplesOperation
     {
+        /// <summary>
+        /// Gets or sets the relative search cell size.
+        /// </summary>
+        /// <value>
+        /// The relative search cell size.
+        /// </value>
         public double RelativeSearchCellSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the averaging method.
+        /// </summary>
+        /// <value>
+        /// The averaging method.
+        /// </value>
         public GridCellAveragingMethod AveragingMethod { get; set; }
+
+        /// <summary>
+        /// Gets or sets the interpolation method.
+        /// </summary>
+        /// <value>
+        /// The interpolation method.
+        /// </value>
         public SpatialInterpolationMethod InterpolationMethod { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImportSamplesSpatialOperation"/> class.
+        /// </summary>
         public ImportSamplesSpatialOperation() : base(false)
         {
             RelativeSearchCellSize = 1;
@@ -19,6 +46,12 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO.Files.Helpers
             InterpolationMethod = SpatialInterpolationMethod.Averaging;
         }
 
+        /// <summary>
+        /// Creates an <see cref="ImportSamplesOperation"/> with an <see cref="InterpolateOperation"/>.
+        /// </summary>
+        /// <returns>
+        /// A tuple containing an <see cref="ImportSamplesOperation"/> with an <see cref="InterpolateOperation"/>.
+        /// </returns>
         public Tuple<ImportSamplesOperation, InterpolateOperation> CreateOperations()
         {
             var importSamplesOperation = new ImportSamplesOperation(false)
