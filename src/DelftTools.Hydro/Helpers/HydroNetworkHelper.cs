12cb228cc10bbc90432049ae95e4f8db165be71c
Pauline Belt <pauline.belt@deltares.nl>
2020-05-26 17:31:06 +0200

    D3DFMIQ-1629 Removed unused using
    Former-commit-id: b140288f1ab003aabdd5e25dc86c47492e1ae45e

--- src/DelftTools.Hydro/Helpers/HydroNetworkHelper.cs
+++ src/DelftTools.Hydro/Helpers/HydroNetworkHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using DelftTools.Hydro.CrossSections;
-using DelftTools.Hydro.Structures;
 using GeoAPI.Extensions.Feature;
 using GeoAPI.Extensions.Networks;
 using GeoAPI.Geometries;
