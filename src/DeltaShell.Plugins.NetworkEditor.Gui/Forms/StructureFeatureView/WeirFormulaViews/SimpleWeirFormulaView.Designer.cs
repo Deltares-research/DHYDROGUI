089eee2f413675cad95657d75f75920eef3ccf73
Tim van den Aardweg <Tim.vandenAardweg@deltares.nl>
2020-06-04 08:52:30 +0000

    FM1D2D-433: fixed a typo Former-commit-id: dc6031c97a900d0cea07a55a492e899969875e5a Former-commit-id: 168c43398391d61be9d52db910176abd9cb12abd

--- src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/StructureFeatureView/WeirFormulaViews/SimpleWeirFormulaView.Designer.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/StructureFeatureView/WeirFormulaViews/SimpleWeirFormulaView.Designer.cs
@@ -42,7 +42,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Correction coefficent";
+            this.label1.Text = "Correction coefficient";
             // 
             // textBoxDischargeCoefficient
             // 
