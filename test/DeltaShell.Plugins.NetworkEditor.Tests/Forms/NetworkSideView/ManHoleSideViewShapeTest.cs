ceb45178060249ad5cdbd3eb1cb218aecae79836
Hidde Elzinga <hidde.elzinga@deltares.nl>
2020-06-09 08:12:33 +0000

    Added test for ManHoleSideViewShape
    Former-commit-id: 4fbac712ae57883ba3659abac42edb6f14e028a9
    Former-commit-id: c3b146fec29390ef8a1285af3cbbff5cd839e599

---
+++ test/DeltaShell.Plugins.NetworkEditor.Tests/Forms/NetworkSideView/ManHoleSideViewShapeTest.cs
@@ -0,0 +1,32 @@
+﻿using DelftTools.Hydro.SewerFeatures;
+using DelftTools.Hydro.Structures;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Plugins.NetworkEditor.Gui.Forms.NetworkSideView;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.NetworkEditor.Tests.Forms.NetworkSideView
+{
+    [TestFixture]
+    public class ManHoleSideViewShapeTest
+    {
+        [Test]
+        public void GivenManHoleSideViewShape_GettingDimensions_ShouldReturnCorrectValues()
+        {
+            //Arrange
+            var manhole = new Manhole{Compartments = new EventedList<ICompartment>
+            {
+                new Compartment{SurfaceLevel = 5, BottomLevel =  0, ManholeWidth = 1},
+                new Compartment{SurfaceLevel = 3, BottomLevel = -2, ManholeWidth = 2}
+            }};
+
+            // Act
+            var shape = new ManHoleSideViewShape(null, 10, manhole);
+
+            // Assert
+            Assert.AreEqual(10,shape.X);
+            Assert.AreEqual(5, shape.Y);
+            Assert.AreEqual(7, shape.Height);
+            Assert.AreEqual(3, shape.Width);
+        }
+    }
+}
