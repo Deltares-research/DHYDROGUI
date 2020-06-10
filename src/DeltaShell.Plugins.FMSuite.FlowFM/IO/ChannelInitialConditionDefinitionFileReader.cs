5621d452ae63942b4c1497c359a7a45c0265cba8
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-06-10 18:19:19 +0000

    FM1D2D-963 Performed small cleanup.
    Former-commit-id: ea665abddef1fd29d19b54356af6464ed9d65f62
    Former-commit-id: 746aab5b933c28340653cfc549d35db8a2dfd355

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/ChannelInitialConditionDefinitionFileReader.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/ChannelInitialConditionDefinitionFileReader.cs
@@ -28,48 +28,46 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
         /// <param name="filePath">Path to the file to read.</param>
         /// <param name="modelDefinition">A <see cref="WaterFlowFMModelDefinition"/>.</param>
         /// <param name="branchDictionary">A dictionary of branch names and <see cref="IBranch"/>.</param>
-        /// <param name="channelInitialConditionDefintions">A collection of <see cref="ChannelInitialConditionDefinition"/> to write.</param>
+        /// <param name="channelInitialConditionDefinitions">A collection of <see cref="ChannelInitialConditionDefinition"/> to write.</param>
         /// <exception cref="FileReadingException">When an error occcurs during reading of the file.</exception>
         public static void ReadFile(
             string filePath,
             WaterFlowFMModelDefinition modelDefinition,
             Dictionary<string, IBranch> branchDictionary,
-            IList<ChannelInitialConditionDefinition> channelInitialConditionDefintions)
+            IEnumerable<ChannelInitialConditionDefinition> channelInitialConditionDefinitions)
         {
             if (!File.Exists(filePath)) throw new FileReadingException(string.Format(Resources.ReadFile_Could_not_read_file__0__properly__it_doesn_t_exist, filePath));
+
             var categories = new DelftIniReader().ReadDelftIniFile(filePath);
             if (categories.Count == 0) throw new FileReadingException(string.Format(Resources.ReadFile_Could_not_read_file__0__properly__it_seems_empty, filePath));
 
             // [Global]
             var globalCategory = categories.FirstOrDefault(category => category.Name.Equals(InitialConditionRegion.GlobalDefinitionIniHeader));
             if (globalCategory == null) throw new FileReadingException(string.Format(Resources.ReadFile_Could_not_read_file__0__properly_no_global_property_was_found, filePath));
-            SetGlobalDefinition(globalCategory, modelDefinition, filePath);
+            SetGlobalDefinition(globalCategory, modelDefinition);
 
             // [Branch]
-            IEnumerable<DelftIniCategory> channelInitialConditionDefinitionsCategories =
-                categories.Where(category => category.Name.Equals(InitialConditionRegion.ChannelInitialConditionDefinitionIniHeader));
-
-            var channelInitialConditionDefinitionDictionary = channelInitialConditionDefintions.ToDictionary(
-                def => def.Channel.Name, def => def, StringComparer.InvariantCultureIgnoreCase);
-            ReadChannelInitialConditionDefinitions(filePath, branchDictionary, channelInitialConditionDefinitionDictionary, channelInitialConditionDefinitionsCategories);
-
+            var channelInitialConditionDefinitionsCategories = categories.Where(category => category.Name.Equals(InitialConditionRegion.ChannelInitialConditionDefinitionIniHeader));
+            var channelInitialConditionDefinitionDictionary = channelInitialConditionDefinitions.ToDictionary(def => def.Channel.Name, def => def, StringComparer.InvariantCultureIgnoreCase);
+            ReadChannelInitialConditionDefinitions(branchDictionary, channelInitialConditionDefinitionDictionary, channelInitialConditionDefinitionsCategories);
         }
 
         private static void ReadChannelInitialConditionDefinitions(
-            string filePath,
-            Dictionary<string, IBranch> branchDictionary,
-            Dictionary<string, ChannelInitialConditionDefinition> channelInitialConditionDefinitionDictionary, 
+            IReadOnlyDictionary<string, IBranch> branchDictionary,
+            IReadOnlyDictionary<string, ChannelInitialConditionDefinition> channelInitialConditionDefinitionDictionary, 
             IEnumerable<DelftIniCategory> channelInitialConditionDefinitionsCategories)
         {
             foreach (var channelInitialConditionDefinitionsCategory in channelInitialConditionDefinitionsCategories)
             {
                 var branchId = channelInitialConditionDefinitionsCategory.ReadProperty<string>(InitialConditionRegion.BranchId.Key);
                 if (!branchDictionary.ContainsKey(branchId)) throw new FileReadingException(string.Format(Resources.ChannelInitialConditionDefinitionFileReader_ReadFile_Branch___0___where_the_initial_condition_should_be_put_on_is_not_available_in_the_model, branchId));
+
                 var branch = branchDictionary[branchId];
                 if (!(branch is IChannel)) throw new FileReadingException(string.Format(Resources.ChannelInitialConditionDefinitionFileReader_ReadFile_Branch___0___where_the_initial_condition_should_be_put_on_is_not_available_in_the_model, branchId));
-                var branchName = branch.Name;
 
+                var branchName = branch.Name;
                 if (!channelInitialConditionDefinitionDictionary.ContainsKey(branchName)) throw new FileReadingException(string.Format(Resources.ChannelInitialConditionDefinitionFileReader_ReadFile_Branch___0___where_the_initial_condition_should_be_put_on_is_not_available_in_the_model, branchId));
+
                 var channelInitialConditionDefinition = channelInitialConditionDefinitionDictionary[branchName];
 
                 if (IsSpatialDefinition(channelInitialConditionDefinitionsCategory))
@@ -84,52 +82,44 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
         }
 
         private static void ReadConstantDefinition(
-            DelftIniCategory channelInitialConditionDefinitionsCategory, 
+            IDelftIniCategory channelInitialConditionDefinitionsCategory, 
             ChannelInitialConditionDefinition channelInitialConditionDefinition)
         {
             var value = channelInitialConditionDefinitionsCategory.ReadProperty<double>(InitialConditionRegion.Values.Key);
-            channelInitialConditionDefinition.SpecificationType =
-                ChannelInitialConditionSpecificationType.ConstantChannelInitialConditionDefinition;
 
+            channelInitialConditionDefinition.SpecificationType = ChannelInitialConditionSpecificationType.ConstantChannelInitialConditionDefinition;
             channelInitialConditionDefinition.ConstantChannelInitialConditionDefinition.Value = value;
             channelInitialConditionDefinition.ConstantChannelInitialConditionDefinition.Quantity = readQuantity;
         }
 
         private static void ReadSpatialDefinition(
-            DelftIniCategory channelInitialConditionDefinitionsCategory, 
+            IDelftIniCategory channelInitialConditionDefinitionsCategory, 
             ChannelInitialConditionDefinition channelInitialConditionDefinition)
         {
             var numLocations = channelInitialConditionDefinitionsCategory.ReadProperty<int>(InitialConditionRegion.NumLocations.Key);
             var chainages = channelInitialConditionDefinitionsCategory.ReadPropertiesToListOfType<double>(InitialConditionRegion.Chainage.Key);
             var values = channelInitialConditionDefinitionsCategory.ReadPropertiesToListOfType<double>(InitialConditionRegion.Values.Key);
 
-            channelInitialConditionDefinition.SpecificationType =
-                ChannelInitialConditionSpecificationType.SpatialChannelInitialConditionDefinition;
+            channelInitialConditionDefinition.SpecificationType = ChannelInitialConditionSpecificationType.SpatialChannelInitialConditionDefinition;
 
-            for (int i = 0; i < numLocations; i++)
+            for (var i = 0; i < numLocations; i++)
             {
                 var constantDefinition = new ConstantSpatialChannelInitialConditionDefinition
                 {
                     Chainage = chainages[i],
                     Value = values[i]
                 };
-                channelInitialConditionDefinition.SpatialChannelInitialConditionDefinition
-                    .ConstantSpatialChannelInitialConditionDefinitions.Add(constantDefinition);
+
+                channelInitialConditionDefinition.SpatialChannelInitialConditionDefinition.ConstantSpatialChannelInitialConditionDefinitions.Add(constantDefinition);
             }
         }
 
-        /// <summary>
-        /// Checks if a DelftIniCategory contains a 'chainage' property indicating
-        /// a spatial definition.
-        /// </summary>
-        /// <param name="channelInitialConditionDefinitionsCategory"></param>
-        /// <returns>True if the category contains a 'chainage' property.</returns>
-        private static bool IsSpatialDefinition(DelftIniCategory channelInitialConditionDefinitionsCategory)
+        private static bool IsSpatialDefinition(IDelftIniCategory channelInitialConditionDefinitionsCategory)
         {
             return channelInitialConditionDefinitionsCategory.Properties.Any(p => p.Name.Equals(InitialConditionRegion.Chainage.Key));
         }
 
-        private static void SetGlobalDefinition(DelftIniCategory globalCategory, WaterFlowFMModelDefinition modelDefinition, string filePath)
+        private static void SetGlobalDefinition(IDelftIniCategory globalCategory, WaterFlowFMModelDefinition modelDefinition)
         {
             var globalValue = globalCategory.ReadProperty<string>(InitialConditionRegion.Value.Key);
             if (string.IsNullOrWhiteSpace(globalValue))
@@ -148,6 +138,5 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
             
             readQuantity = globalQuantity;
         }
-
     }
 }
