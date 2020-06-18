476fb5b66fbffe030292dd4066625989da0fcb90
Dennis Tang <dennis.tang@deltares.nl>
2020-06-18 13:07:20 +0200

    D3DFMIQ-1452 Added unit tests for the ViewInfo (and in particular the AfterCreate call on ProjectItemMapView) of HydroModel to verify behaviour
    Former-commit-id: 3298be68643ef6da8335d09186c0b2a7280ed18f

--- test/DeltaShell.Plugins.DelftModels.HydroModel.Tests/HydroModelGuiTest.cs
+++ test/DeltaShell.Plugins.DelftModels.HydroModel.Tests/HydroModelGuiTest.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Forms;
+using DelftTools.Controls;
 using DelftTools.Controls.Swf;
 using DelftTools.Shell.Core;
 using DelftTools.Shell.Core.Workflow;
 using DelftTools.Shell.Gui;
 using DelftTools.Shell.Gui.Forms;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Gui.Forms.ViewManager;
 using DeltaShell.Plugins.DelftModels.HydroModel.Gui;
+using DeltaShell.Plugins.DelftModels.HydroModel.Gui.Forms;
 using DeltaShell.Plugins.DelftModels.HydroModel.Gui.Properties;
+using DeltaShell.Plugins.SharpMapGis.Gui.Forms;
+using NSubstitute;
 using NUnit.Framework;
 using Rhino.Mocks;
+using SharpMap.Api;
+using SharpMap.Api.Layers;
 
 namespace DeltaShell.Plugins.DelftModels.HydroModel.Tests
 {
@@ -23,6 +33,179 @@ namespace DeltaShell.Plugins.DelftModels.HydroModel.Tests
                 yield return new TestCaseData(null, model);
                 yield return new TestCaseData(new HydroModel() {Name = "Squirtle"}, model);
                 yield return new TestCaseData(model, model);
+            }
+        }
+
+        [Test]
+        public void GetViewInfoForHydroModel_ReturnsExpectedViewInfoConfiguration()
+        {
+            // Setup
+            using (var plugin = new HydroModelGuiPlugin())
+            {
+                // Call
+                ViewInfo viewInfo = plugin.GetViewInfoObjects()
+                                          .First(vi => vi.DataType == typeof(HydroModel));
+
+                // Assert
+                Assert.That(viewInfo.ViewType, Is.EqualTo(typeof(HydroModelSettings)));
+                Assert.That(viewInfo.Description, Is.EqualTo("Hydro Model Settings"));
+                Assert.That(viewInfo.GetViewName, Is.Not.Null);
+                Assert.That(viewInfo.CompositeViewType, Is.EqualTo(typeof(ProjectItemMapView)));
+                Assert.That(viewInfo.GetCompositeViewData, Is.Not.Null);
+                Assert.That(viewInfo.AfterCreate, Is.Not.Null);
+            }
+        }
+
+        [Test]
+        public void ViewInfoForHydroModelGetViewName_WithHydroModel_ReturnsExpectedValue()
+        {
+            // Setup
+            using (var model = new HydroModel {Name = "Name of HydroModel"})
+            using (var plugin = new HydroModelGuiPlugin())
+            {
+                ViewInfo viewInfo = plugin.GetViewInfoObjects()
+                                          .First(vi => vi.DataType == typeof(HydroModel));
+
+                // Call
+                string viewName = viewInfo.GetViewName(null, model);
+
+                // Assert
+                Assert.That(viewName, Is.EqualTo($"{model.Name} Settings"));
+            }
+        }
+
+        [Test]
+        public void ViewInfoForHydroModelGetCompositeViewData_WithHydroModel_ReturnsExpectedResult()
+        {
+            // Setup
+            using (var model = new HydroModel())
+            using (var plugin = new HydroModelGuiPlugin())
+            {
+                ViewInfo viewInfo = plugin.GetViewInfoObjects()
+                                          .First(vi => vi.DataType == typeof(HydroModel));
+
+                // Call
+                object compositeViewData = viewInfo.GetCompositeViewData(model);
+
+                // Assert
+                Assert.That(compositeViewData, Is.SameAs(model));
+            }
+        }
+
+        /// <summary>
+        /// This test indirectly verifies that the map is forced to re-render after the view is created. This causes a double
+        /// Render()
+        /// call to the map.
+        /// The test verifies the AfterCreate call by mimicking the situation. There is no need to fully configure the plugin,
+        /// because:
+        /// - MapView.GetLayerForData is set. If it returns any arbitrary MapLayer --> This ProjectItemMapView belongs to the
+        /// argument
+        /// (in this case a HydroModel for which the view is created)
+        /// - The GUI and its corresponding ViewList can be substituted as desired. For the AfterCreate method to work, the view is
+        /// retrieved from the ViewList and further operations are done based on the HydroModel argument that was used to retrieve
+        /// (In this case, it only forces the corresponding ProjectItemMapView.MapView.Map to render twice to force the grid to
+        /// become visible)
+        /// This test does NOT verify additional properties being set on a HydroModelSettings.
+        /// </summary>
+        [Test]
+        public void GivenPluginWithProjectItemMapViewForHydroModel_WhenViewInfoForHydroModelAfterCreateCalled_ThenMapViewDoubleRefreshed()
+        {
+            // Setup
+            object modelArgument = null;
+
+            IMap map = Substitute.For<IMap, INotifyPropertyChanged>();
+            map.Layers.Returns(new EventedList<ILayer>());
+
+            using (var mapView = new ProjectItemMapView
+            {
+                MapView =
+                {
+                    GetLayerForData = o =>
+                    {
+                        modelArgument = o;
+                        return Substitute.For<ILayer>();
+                    },
+                    Map = map
+                }
+            })
+            using (var model = new HydroModel())
+            {
+                var dockingManager = Substitute.For<IDockingManager>();
+                var viewList = new ViewList(dockingManager, ViewLocation.Bottom) {mapView};
+
+                var gui = Substitute.For<IGui>();
+                gui.DocumentViews.Returns(viewList);
+
+                using (var view = new HydroModelSettings())
+                using (var plugin = new HydroModelGuiPlugin {Gui = gui})
+                {
+                    ViewInfo viewInfo = plugin.GetViewInfoObjects()
+                                              .First(vi => vi.DataType == typeof(HydroModel));
+
+                    // Call
+                    viewInfo.AfterCreate(view, model);
+
+                    // Assert
+                    Assert.That(modelArgument, Is.SameAs(model));
+                    map.Received(2).Render();
+                }
+            }
+        }
+
+        /// <summary>
+        /// This test indirectly verifies that the map is not forced to re-render after the view is created. This causes a single
+        /// Render()
+        /// call to the map. (probably when the view is added to the view list)
+        /// The test verifies the AfterCreate call by mimicking the situation. There is no need to fully configure the plugin,
+        /// because:
+        /// - MapView.GetLayerForData is set. If it returns NULL --> No ProjectItemMapView belongs to the argument
+        /// - The GUI and its corresponding ViewList can be substituted as desired. For the verification of the AfterCreate method
+        /// in this situation, it is only verified that no NullReferenceException is generated when there's no associated
+        /// ProjectItemMapView found
+        /// This test does NOT verify additional properties being set on a HydroModelSettings.
+        /// </summary>
+        [Test]
+        public void GivenPluginWithNoProjectItemMapViewForHydroModel_WhenViewInfoForHydroModelAfterCreateCalled_ThenMapViewRefreshed()
+        {
+            // Setup
+            object modelArgument = null;
+
+            IMap map = Substitute.For<IMap, INotifyPropertyChanged>();
+            map.Layers.Returns(new EventedList<ILayer>());
+
+            using (var mapView = new ProjectItemMapView
+            {
+                MapView =
+                {
+                    GetLayerForData = o =>
+                    {
+                        modelArgument = o;
+                        return null;
+                    },
+                    Map = map
+                }
+            })
+            using (var model = new HydroModel())
+            {
+                var dockingManager = Substitute.For<IDockingManager>();
+                var viewList = new ViewList(dockingManager, ViewLocation.Bottom) {mapView};
+
+                var gui = Substitute.For<IGui>();
+                gui.DocumentViews.Returns(viewList);
+
+                using (var view = new HydroModelSettings())
+                using (var plugin = new HydroModelGuiPlugin {Gui = gui})
+                {
+                    ViewInfo viewInfo = plugin.GetViewInfoObjects()
+                                              .First(vi => vi.DataType == typeof(HydroModel));
+
+                    // Call
+                    viewInfo.AfterCreate(view, model);
+
+                    // Assert
+                    Assert.That(modelArgument, Is.SameAs(model));
+                    map.Received(1).Render();
+                }
             }
         }
 
