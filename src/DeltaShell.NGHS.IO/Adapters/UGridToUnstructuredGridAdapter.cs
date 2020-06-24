f06bc8a6afa1223a6e2d502418b9fc9eb083ce13
Carlos Lubbers <carlos.lubbers@deltares.nl>
2020-06-24 14:53:36 +0200

    Call create/find cells routine after importing grid files in new UGRID format D3DFMIQ-1937
    Former-commit-id: b6b3d7f2295d603ebc4229aaa29387e086a56842

--- src/DeltaShell.NGHS.IO/Adapters/UGridToUnstructuredGridAdapter.cs
+++ src/DeltaShell.NGHS.IO/Adapters/UGridToUnstructuredGridAdapter.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using DeltaShell.NGHS.IO.Grid;
 using NetTopologySuite.Extensions.Grids;
+using SharpMap.Api;
+using SharpMap.Api.GridGeom;
 
 namespace DeltaShell.NGHS.IO.Adapters
 {
@@ -31,6 +33,8 @@ namespace DeltaShell.NGHS.IO.Adapters
                 uGrid.FaceNodesByMeshId[meshId - 1],
                 oneBased: oneBased);
 
+            CreateCells(grid);
+
             if (grid != null)
             {
                 grid.CoordinateSystem = uGrid.CoordinateSystem;
@@ -39,12 +43,19 @@ namespace DeltaShell.NGHS.IO.Adapters
             return grid;
         }
 
+        private static void CreateCells(UnstructuredGrid grid)
+        {
+            using (var api = new RemoteGridGeomApi())
+            using (var mesh = new DisposableMeshGeometry(grid))
+            {
+                DisposableMeshGeometry resultMesh = api.CreateCells(mesh);
+                grid.Cells = resultMesh.CreateCells();
+            }
+        }
+
         public void Dispose()
         {
-            if (uGrid != null)
-            {
-                uGrid.Dispose();
-            }
+            uGrid?.Dispose();
         }
     }
 }
