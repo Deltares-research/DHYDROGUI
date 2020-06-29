33d38d7983cf03d13fd3fb26e45a5581b9239457
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-06-29 14:18:58 +0000

    FM1D2D-898 Extended WaterFlowFMRoughnessValidator.
    Former-commit-id: 48c1e43a41dba87491edf335e8eb3c53aa00f923
    Former-commit-id: c2ab2cd0f55c94eae5bb86963e6da1b3af1b48ee

--- src/DeltaShell.Plugins.FMSuite.FlowFM/Validation/WaterFlowFMRoughnessValidator.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/Validation/WaterFlowFMRoughnessValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using DelftTools.Hydro;
+using DelftTools.Hydro.Roughness;
 using DelftTools.Utils.Validation;
 using DeltaShell.NGHS.IO.DataObjects.Friction;
 
@@ -12,42 +13,78 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Validation
         {
             IList<ValidationIssue> issues = new List<ValidationIssue>();
 
+            ValidateForMissingChannelFrictionData(model, issues);
+            ValidateForConflictingChannelFrictionSpecifications(model, issues);
+
+            return new ValidationReport("Roughness", issues);
+        }
+
+        private static void ValidateForMissingChannelFrictionData(WaterFlowFMModel model, ICollection<ValidationIssue> issues)
+        {
+            foreach (var channelFrictionDefinition in model.ChannelFrictionDefinitions.Where(cfd => cfd.SpecificationType == ChannelFrictionSpecificationType.SpatialChannelFrictionDefinition))
+            {
+                var spatialChannelFrictionDefinition = channelFrictionDefinition.SpatialChannelFrictionDefinition;
+                switch (spatialChannelFrictionDefinition.FunctionType)
+                {
+                    case RoughnessFunction.Constant:
+                        var constantSpatialChannelFrictionDefinitions = spatialChannelFrictionDefinition.ConstantSpatialChannelFrictionDefinitions;
+                        if (constantSpatialChannelFrictionDefinitions.Count == 0)
+                        {
+                            issues.Add(new ValidationIssue(channelFrictionDefinition.Channel,
+                                ValidationSeverity.Error,
+                                "No constant values defined",
+                                model.ChannelFrictionDefinitions));
+
+                        }
+                        break;
+                    case RoughnessFunction.FunctionOfQ:
+                    case RoughnessFunction.FunctionOfH:
+                        var function = spatialChannelFrictionDefinition.Function;
+                        if (function.GetValues().Count == 0)
+                        {
+                            issues.Add(new ValidationIssue(channelFrictionDefinition.Channel,
+                                ValidationSeverity.Error,
+                                "No function values defined",
+                                model.ChannelFrictionDefinitions));
+                        }
+                        break;
+                }
+            }
+        }
+
+        private static void ValidateForConflictingChannelFrictionSpecifications(WaterFlowFMModel model, ICollection<ValidationIssue> issues)
+        {
             var channelFrictionDefinitionPerChannelLookup = model.ChannelFrictionDefinitions.ToDictionary(cfd => cfd.Channel, cfd => cfd);
             var channelsPerCrossSectionDefinitionLookup = model.Network.GetChannelsPerCrossSectionDefinitionLookup();
 
             foreach (var sharedCrossSectionDefinition in model.Network.SharedCrossSectionDefinitions)
             {
-                if (!channelsPerCrossSectionDefinitionLookup.TryGetValue(sharedCrossSectionDefinition,
-                    out var channelsUsingSharedCrossSectionDefinition))
+                if (!channelsPerCrossSectionDefinitionLookup.TryGetValue(sharedCrossSectionDefinition, out var channelsUsingSharedCrossSectionDefinition))
                 {
                     continue; // Skip shared cross section definitions that are unused
                 }
 
                 var relatedChannelFrictionDefinitions = channelsUsingSharedCrossSectionDefinition.Select(c => channelFrictionDefinitionPerChannelLookup[c]).ToArray();
 
-                if (relatedChannelFrictionDefinitions.All(cfd =>
-                    cfd.SpecificationType == ChannelFrictionSpecificationType.RoughnessSections
-                    || cfd.SpecificationType == ChannelFrictionSpecificationType.CrossSectionFrictionDefinitions))
+                if (relatedChannelFrictionDefinitions.All(cfd => cfd.SpecificationType == ChannelFrictionSpecificationType.RoughnessSections
+                                                                 || cfd.SpecificationType == ChannelFrictionSpecificationType.CrossSectionFrictionDefinitions))
                 {
                     continue;
                 }
 
-                if (relatedChannelFrictionDefinitions.All(cfd =>
-                    cfd.SpecificationType != ChannelFrictionSpecificationType.RoughnessSections
-                    && cfd.SpecificationType != ChannelFrictionSpecificationType.CrossSectionFrictionDefinitions))
+                if (relatedChannelFrictionDefinitions.All(cfd => cfd.SpecificationType != ChannelFrictionSpecificationType.RoughnessSections
+                                                                 && cfd.SpecificationType != ChannelFrictionSpecificationType.CrossSectionFrictionDefinitions))
                 {
                     continue;
                 }
 
-                issues.Add(new ValidationIssue("Conflicting roughness specification types",
+                issues.Add(new ValidationIssue(sharedCrossSectionDefinition,
                     ValidationSeverity.Error,
-                    $"Shared cross section definition '{sharedCrossSectionDefinition.Name}' is used on branches " +
-                    "that have a conflicting roughness Specification type. The branches involved are: " +
+                    "This shared cross section definition is used on branches that have a conflicting " +
+                    "roughness Specification type. The branches involved are: " +
                     $"{string.Join(", ", channelsUsingSharedCrossSectionDefinition.Select(c => c.Name))}.",
                     model.ChannelFrictionDefinitions));
             }
-
-            return new ValidationReport("Roughness", issues);
         }
     }
 }
