d818345e8b73eed8c7c29495a7ad44749d59190b
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-07-03 10:22:59 +0000

    FM1D2D-767 Added validation in CompartmentView.
    Former-commit-id: 48fd22e255a245c9d2dac48d9a3c2ed5f7647b59
    Former-commit-id: e0bd7b7c216dd7b871c433739fecfdfbc1468f1c

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/SewerFeatureViews/CompartmentValueValidationRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.SewerFeatureViews
+{
+    public class CompartmentValueValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null)
+            {
+                return new ValidationResult(false, "Null value is invalid");
+            }
+
+            double doubleValue;
+            try
+            {
+                doubleValue = Convert.ToDouble(value, cultureInfo);
+            }
+            catch
+            {
+                return new ValidationResult(false, "The value of this parameter must be a double precision number.");
+            }
+
+            if (doubleValue <= 0)
+            {
+                return new ValidationResult(false, "The value of this parameter must be larger than 0.");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.NetworkEditor.Tests/Forms/SewerFeatureViews/CompartmentValueValidationRuleTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using DeltaShell.Plugins.NetworkEditor.Gui.Forms.SewerFeatureViews;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.NetworkEditor.Tests.Forms.SewerFeatureViews
+{
+    [TestFixture]
+    public class CompartmentValueValidationRuleTest
+    {
+        [Test]
+        public void GivenValidValue_WhenCallingValidate_ThenResultIsValid()
+        {
+            // Given
+            var rule = new CompartmentValueValidationRule();
+
+            // When
+            var result = rule.Validate(1.1, CultureInfo.InvariantCulture);
+
+            // Then
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        [TestCase(null, "Null value is invalid")]
+        [TestCase("Test", "The value of this parameter must be a double precision number.")]
+        [TestCase(-1, "The value of this parameter must be larger than 0.")]
+        [TestCase(0, "The value of this parameter must be larger than 0.")]
+        public void GivenInvalidValue_WhenCallingValidate_ThenResultIsInvalidWithExpectedMessage(object invalidValue, string expectedMessage)
+        {
+            // Given
+            var rule = new CompartmentValueValidationRule();
+
+            // When
+            var result = rule.Validate(invalidValue, CultureInfo.InvariantCulture);
+
+            // Then
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(expectedMessage, result.ErrorContent.ToString());
+        }
+    }
+}
