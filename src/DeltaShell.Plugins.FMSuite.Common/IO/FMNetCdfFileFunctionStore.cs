169268c85a519712e2b03d2cbab9fff25807bfa3
Ralph Peelen <ralph.peelen@deltares.nl>
2020-07-14 06:40:30 +0000

    Fixed broken tests on build server
    Former-commit-id: fb80e5a7debf63fc80c908b6175bed997b5914d8
    Former-commit-id: 617fba4d950c2657e56449d561a2e366c4232601

--- src/DeltaShell.Plugins.FMSuite.Common/IO/FMNetCdfFileFunctionStore.cs
+++ src/DeltaShell.Plugins.FMSuite.Common/IO/FMNetCdfFileFunctionStore.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using DelftTools.Functions;
 using DelftTools.Utils;
+using DelftTools.Utils.NetCdf;
 
 namespace DeltaShell.Plugins.FMSuite.Common.IO
 {
@@ -32,6 +35,25 @@ namespace DeltaShell.Plugins.FMSuite.Common.IO
         protected override string GetTimeVariableName(string dimName)
         {
             return "time";
+        }
+
+        protected override string ReadReferenceDateFromFile(string timeVariableName)
+        {
+            NetCdfVariable timeVariable = netCdfFile.GetVariableByName(timeVariableName);
+            string timeReference = netCdfFile.GetAttributeValue(timeVariable, "units");
+
+            const string secondsSinceStr = "seconds since ";
+
+            var dateTime = new DateTime(1970, 1, 1); // assume epoch otherwise
+            if (timeReference.StartsWith(secondsSinceStr))
+            {
+                string timeStr = timeReference.Substring(secondsSinceStr.Length);
+
+                if (!DateTime.TryParseExact(timeStr, $"{dateTimeFormat} zzz",
+                    CultureInfo.InvariantCulture,DateTimeStyles.None, out dateTime))
+                    return base.ReadReferenceDateFromFile(timeVariableName); // for backward compatibility, i have no time for a better solution
+            }
+            return dateTime.ToString(DateTimeFormatInfo.InvariantInfo.FullDateTimePattern, CultureInfo.InvariantCulture);
         }
 
         protected override void UpdateFunctionsAfterPathSet()
