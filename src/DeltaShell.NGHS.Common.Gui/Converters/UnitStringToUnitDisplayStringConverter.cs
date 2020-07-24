2290861b184373975b7d536df38480bf4e20f646
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-24 09:23:27 +0200

    Move UnitStringToUnitDisplayStringConverter
    - Moved to DeltaShell.NGHS.Common.GUI
    - Moved corresponding test
    - Made the converter sealed
    
    D3DFMIQ-2058
    
    
    Former-commit-id: 0c6f7ac9f0b4bb03f4de3c37c0013f97017b161c

---
+++ src/DeltaShell.NGHS.Common.Gui/Converters/UnitStringToUnitDisplayStringConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DeltaShell.NGHS.Common.Gui.Converters
+{
+    /// <summary>
+    /// <see cref="UnitStringToUnitDisplayStringConverter"/> converts a Unit
+    /// string, e.g. 'm' or 'Kg', to its display equivalent, respectively
+    /// '[m]' and '[Kg]'.
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(string))]
+    public sealed class UnitStringToUnitDisplayStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string unit && targetType == typeof(string))
+            {
+                return $"[{unit}]";
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string unit && targetType == typeof(string))
+            {
+                return unit.Substring(1, unit.Length - 2);
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
