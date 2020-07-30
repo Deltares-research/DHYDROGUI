ea36474f0d6bfe95f760254835f12d6482ab6b0a
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-30 09:18:39 +0200

    Add initial StructureViewModel D3DFMIQ-2058
    Former-commit-id: dc31e72a66f31e45c3061bd63261a524920ade85

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/ViewModels/StructureViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels
+{
+    /// <summary>
+    /// <see cref="StructureViewModel"/> defines the view model for the
+    /// structure view.
+    /// </summary>
+    public sealed class StructureViewModel
+    {
+    }
+}
