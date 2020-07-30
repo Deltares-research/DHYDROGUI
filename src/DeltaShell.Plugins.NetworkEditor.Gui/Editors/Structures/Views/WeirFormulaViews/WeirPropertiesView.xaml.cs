76579be2e99657fc0cadabb99f09bfdb217ae21c
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-30 07:14:15 +0200

    Add WeirPropertiesView D3DFMIQ-2058
    Former-commit-id: 2814df229d2a74e18ed06bafde270f1c20f09c95

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/Views/WeirFormulaViews/WeirPropertiesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Views.WeirFormulaViews
+{
+    /// <summary>
+    /// Interaction logic for WeirPropertiesView.xaml
+    /// </summary>
+    public partial class WeirPropertiesView : UserControl
+    {
+        public WeirPropertiesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
