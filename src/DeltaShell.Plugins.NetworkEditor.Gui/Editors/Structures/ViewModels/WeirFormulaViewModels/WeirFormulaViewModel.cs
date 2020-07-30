337ff1ec510719ebb0288b7f32c16a78d17a483b
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-30 09:22:05 +0200

    Add initial WeirFormulaViewModel D3DFMIQ-2058
    Former-commit-id: e385f184a96225bcb8e69de6cbb92f5eb31b6e8d

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/ViewModels/WeirFormulaViewModels/WeirFormulaViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels.WeirFormulaViewModels
+{
+    /// <summary>
+    /// <see cref="WeirFormulaViewModel"/> acts as the base class for all weir
+    /// formula view models.
+    /// </summary>
+    public abstract class WeirFormulaViewModel { }
+}
