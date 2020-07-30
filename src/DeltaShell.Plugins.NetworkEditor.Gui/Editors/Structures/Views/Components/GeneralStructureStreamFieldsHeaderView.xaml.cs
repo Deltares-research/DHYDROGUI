c73402fa07e8a6f00607c4f43e79c9e76a64d615
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-30 07:12:08 +0200

    Add general structure stream fields D3DFMIQ-2058
    - Add header
    - Add actual fields
    
    
    Former-commit-id: 65e5ed5ac19a6a64fc7aa6f5e3277263b4873ec1

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/Views/Components/GeneralStructureStreamFieldsHeaderView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for GeneralStructureStreamFieldsHeaderView.xaml
+    /// </summary>
+    public partial class GeneralStructureStreamFieldsHeaderView : UserControl
+    {
+        public GeneralStructureStreamFieldsHeaderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
