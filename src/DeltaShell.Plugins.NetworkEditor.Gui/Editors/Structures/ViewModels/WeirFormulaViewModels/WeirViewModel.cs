12c34e7d79a8c45911f406a54edfe0c5a1dfe479
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-30 09:52:03 +0200

    Rename WeirFormulaViewModel to WeirViewModel D3DFMIQ-2058
    Former-commit-id: cda552e31064bcc0db745854059c235807dce4b9

--- src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/ViewModels/WeirFormulaViewModels/WeirViewModel.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/ViewModels/WeirFormulaViewModels/WeirViewModel.cs
@@ -1,8 +1,8 @@
 ﻿namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels.WeirFormulaViewModels
 {
     /// <summary>
-    /// <see cref="WeirFormulaViewModel"/> acts as the base class for all weir
+    /// <see cref="WeirViewModel"/> acts as the base class for all weir
     /// formula view models.
     /// </summary>
-    public abstract class WeirFormulaViewModel { }
+    public abstract class WeirViewModel { }
 }
