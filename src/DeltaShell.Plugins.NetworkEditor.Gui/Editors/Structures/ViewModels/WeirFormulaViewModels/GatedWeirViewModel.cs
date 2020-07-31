4bd8966bd241f50c040074dd1906cf51f094351b
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-31 07:41:47 +0200

    Add GatedWeirViewModel D3DFMIQ-2058
    Former-commit-id: 10be76305f45f29e4365c8501cb6f9a64651b96a

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/ViewModels/WeirFormulaViewModels/GatedWeirViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DelftTools.Hydro.Structures.WeirFormula;
+using DelftTools.Utils.Guards;
+using DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Enums;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels.WeirFormulaViewModels
+{
+    /// <summary>
+    /// <see cref="GatedWeirViewModel"/> provides the view model for the
+    /// <see cref="Views.WeirFormulaViews.GatedWeirView"/>.
+    /// </summary>
+    /// <seealso cref="WeirViewModel" />
+    public sealed class GatedWeirViewModel : WeirViewModel
+    {
+        /// <summary>
+        /// Creates a new <see cref="GatedWeirViewModel"/>.
+        /// </summary>
+        /// <param name="formula">The formula.</param>
+        /// <param name="weirPropertiesViewModel">The weir properties view model.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when any parameter is <c>null</c>.
+        /// </exception>
+        public GatedWeirViewModel(GatedWeirFormula formula,
+                                  WeirPropertiesViewModel weirPropertiesViewModel) :
+            base(weirPropertiesViewModel)
+        {
+            Ensure.NotNull(formula, nameof(formula));
+            GatePropertiesViewModel = new GatePropertiesViewModel(formula, true);
+        }
+
+        /// <summary>
+        /// Gets the gate properties view model.
+        /// </summary>
+        public GatePropertiesViewModel GatePropertiesViewModel { get; }
+
+        public override FormulaViewType FormulaViewType => FormulaViewType.SimpleGate;
+    }
+}
