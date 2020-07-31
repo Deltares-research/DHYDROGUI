c3935d148fa609f39483d5e450dae4f39bef75a5
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-31 07:27:09 +0200

    Add FormulaViewType D3DFMIQ-2058
    Former-commit-id: bc856430381b68f9bc7ecb6374e8f412fb6a45b7

---
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/Enums/FormulaViewType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Enums
+{
+    /// <summary>
+    /// <see cref="FormulaViewType"/> defines the possible weir structure views
+    /// currently supported within the D-HYDRO application.
+    /// </summary>
+    public enum FormulaViewType
+    {
+        [Description("Simple Weir")]
+        SimpleWeir = 1,
+        [Description("Simple Gate")]
+        SimpleGate = 2,
+        [Description("General Structure")]
+        GeneralStructure = 3,
+    }
+}
