9db0240399cbf45b99278ccdedba5f27bcbe6ae0
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-31 09:20:54 +0200

    Add documentation GeneralStructureStreamFieldsView D3DFMIQ-2058
    Former-commit-id: 7b3f0e99535289e314b973a4a06df3a780503f96

--- src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/Views/Components/GeneralStructureStreamFieldsView.xaml.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/Views/Components/GeneralStructureStreamFieldsView.xaml.cs
@@ -8,6 +8,9 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Views.Componen
     /// </summary>
     public partial class GeneralStructureStreamFieldsView : UserControl
     {
+        /// <summary>
+        /// The upstream1 property
+        /// </summary>
         public static readonly DependencyProperty Upstream1Property = 
             DependencyProperty.Register(nameof(Upstream1), 
                                         typeof(string), 
@@ -15,6 +18,9 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Views.Componen
                                         new FrameworkPropertyMetadata(default(string),
                                                                       FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        /// <summary>
+        /// The upstream2 property
+        /// </summary>
         public static readonly DependencyProperty Upstream2Property = 
             DependencyProperty.Register(nameof(Upstream2), 
                                         typeof(string), 
@@ -22,6 +28,9 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Views.Componen
                                         new FrameworkPropertyMetadata(default(string),
                                                                       FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        /// <summary>
+        /// The downstream1 property
+        /// </summary>
         public static readonly DependencyProperty Downstream1Property = 
             DependencyProperty.Register(nameof(Downstream1), 
                                         typeof(string), 
@@ -29,6 +38,9 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Views.Componen
                                         new FrameworkPropertyMetadata(default(string),
                                                                       FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        /// <summary>
+        /// The downstream2 property
+        /// </summary>
         public static readonly DependencyProperty Downstream2Property = 
             DependencyProperty.Register(nameof(Downstream2), 
                                         typeof(string), 
@@ -36,29 +48,44 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Views.Componen
                                         new FrameworkPropertyMetadata(default(string),
                                                                       FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        /// <summary>
+        /// Creates a new <see cref="GeneralStructureStreamFieldsView"/>.
+        /// </summary>
         public GeneralStructureStreamFieldsView()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Gets or sets the upstream1.
+        /// </summary>
         public string Upstream1
         {
             get => (string) GetValue(Upstream1Property);
             set => SetValue(Upstream1Property, value);
         }
 
+        /// <summary>
+        /// Gets or sets the upstream2.
+        /// </summary>
         public string Upstream2
         {
             get => (string) GetValue(Upstream2Property);
             set => SetValue(Upstream2Property, value);
         }
 
+        /// <summary>
+        /// Gets or sets the downstream1.
+        /// </summary>
         public string Downstream1
         {
             get => (string) GetValue(Downstream1Property);
             set => SetValue(Downstream1Property, value);
         }
 
+        /// <summary>
+        /// Gets or sets the downstream2.
+        /// </summary>
         public string Downstream2
         {
             get => (string) GetValue(Downstream2Property);
