b239b7479e58df6b1828e47bf4256e77213e7b68
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-07-31 07:35:45 +0200

    Extend WeirViewModel with WeirPropertiesViewModel D3DFMIQ-2058
    Former-commit-id: 418920293fd293b9f38bf4543d2801f0df37cbf9

--- src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/ViewModels/WeirFormulaViewModels/WeirViewModel.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Editors/Structures/ViewModels/WeirFormulaViewModels/WeirViewModel.cs
@@ -1,8 +1,35 @@
-﻿namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels.WeirFormulaViewModels
+﻿using DelftTools.Utils.Guards;
+using DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.Enums;
+
+namespace DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels.WeirFormulaViewModels
 {
     /// <summary>
     /// <see cref="WeirViewModel"/> acts as the base class for all weir
     /// formula view models.
     /// </summary>
-    public abstract class WeirViewModel { }
+    public abstract class WeirViewModel
+    {
+        /// <summary>
+        /// Creates a new <see cref="WeirViewModel"/>.
+        /// </summary>
+        /// <param name="weirPropertiesViewModel">The weir properties view model.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <paramref name="weirPropertiesViewModel"/> is <c>null</c>.
+        /// </exception>
+        protected WeirViewModel(WeirPropertiesViewModel weirPropertiesViewModel)
+        {
+            Ensure.NotNull(weirPropertiesViewModel, nameof(weirPropertiesViewModel));
+            WeirPropertiesViewModel = weirPropertiesViewModel;
+        }
+
+        /// <summary>
+        /// Gets the weir properties view model.
+        /// </summary>
+        public WeirPropertiesViewModel WeirPropertiesViewModel { get; }
+
+        /// <summary>
+        /// Gets the type of the formula view.
+        /// </summary>
+        public abstract FormulaViewType FormulaViewType { get; }
+    }
 }
