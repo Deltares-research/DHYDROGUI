20f6f2cc6d3ea6a3dde37523c4f12933bffd6788
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-08-02 09:10:58 +0200

    Add GatedWeirViewModelTest D3DFMIQ-2058
    Former-commit-id: ef9650b172e86c09e29639aa194ad2c22bfa1655

---
+++ test/DeltaShell.Plugins.NetworkEditor.Tests/Gui/Editors/Structures/ViewModels/GatedWeirViewModelTest.cs
@@ -0,0 +1,58 @@
+﻿using DelftTools.Hydro.Structures;
+using DelftTools.Hydro.Structures.WeirFormula;
+using DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels.WeirFormulaViewModels;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.NetworkEditor.Tests.Gui.Editors.Structures.ViewModels
+{
+    [TestFixture]
+    public class GatedWeirViewModelTest
+    {
+        [Test]
+        public void Constructor_SetsPropertiesCorrectly()
+        {
+            // Setup
+            var formula = new GatedWeirFormula(true);
+            var weir = new Weir2D { WeirFormula = formula };
+
+            using (var weirPropertiesViewModel = new WeirPropertiesViewModel(weir))
+            {
+                // Call
+                var viewModel = 
+                    new GatedWeirViewModel(formula, 
+                                           weirPropertiesViewModel);
+
+                // Assert
+                Assert.That(viewModel.GatePropertiesViewModel, Is.Not.Null);
+                Assert.That(viewModel.WeirPropertiesViewModel, Is.SameAs(weirPropertiesViewModel));
+            }
+        }
+
+        [Test]
+        public void Constructor_FormulaNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var weir = new Weir2D();
+
+            using (var weirPropertiesViewModel = new WeirPropertiesViewModel(weir))
+            {
+                // Call | Assert
+                void Call() => new GatedWeirViewModel(null, weirPropertiesViewModel);
+
+                var exception = Assert.Throws<System.ArgumentNullException>(Call);
+                Assert.That(exception.ParamName, Is.EqualTo("formula"));
+            }
+        }
+
+        [Test]
+        public void Constructor_WeirPropertiesViewModelNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var formula = new GatedWeirFormula(true);
+            void Call() => new GatedWeirViewModel(formula, null);
+
+            var exception = Assert.Throws<System.ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("weirPropertiesViewModel"));
+        }
+    }
+}
