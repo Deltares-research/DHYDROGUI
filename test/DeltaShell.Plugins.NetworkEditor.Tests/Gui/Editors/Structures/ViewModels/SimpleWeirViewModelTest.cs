ee26d6e7ddca437cc7e4f679d8d779bcb3d6d037
Maarten Tegelaers <maarten.tegelaers@deltares.nl>
2020-08-02 09:18:30 +0200

    Add SimpleWeirViewModelTest D3DFMIQ-2058
    Former-commit-id: daa9edd026902fbace9649991c6eaf4867ea3880

---
+++ test/DeltaShell.Plugins.NetworkEditor.Tests/Gui/Editors/Structures/ViewModels/SimpleWeirViewModelTest.cs
@@ -0,0 +1,83 @@
+﻿using DelftTools.Hydro.Structures;
+using DelftTools.Hydro.Structures.WeirFormula;
+using DeltaShell.Plugins.NetworkEditor.Gui.Editors.Structures.ViewModels.WeirFormulaViewModels;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.NetworkEditor.Tests.Gui.Editors.Structures.ViewModels
+{
+    [TestFixture]
+    public class SimpleWeirViewModelTest
+    {
+        [Test]
+        public void Constructor_SetsPropertiesCorrectly()
+        {
+            var formula = new SimpleWeirFormula { LateralContraction = 20.0 };
+            
+            var weir = new Weir2D { WeirFormula = formula };
+
+            using (var weirPropertiesViewModel = new WeirPropertiesViewModel(weir))
+            {
+                // Call
+                var viewModel =
+                    new SimpleWeirViewModel(formula, weirPropertiesViewModel);
+
+                Assert.That(viewModel.WeirPropertiesViewModel, 
+                            Is.SameAs(weirPropertiesViewModel));
+                Assert.That(viewModel.ContractionCoefficient, 
+                            Is.EqualTo(formula.LateralContraction));
+            }
+        }
+
+        [Test]
+        public void Constructor_FormulaNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var weir = new Weir2D();
+
+            using (var weirPropertiesViewModel = new WeirPropertiesViewModel(weir))
+            {
+                // Call | Assert
+                void Call() => new SimpleWeirViewModel(null, weirPropertiesViewModel);
+
+                var exception = Assert.Throws<System.ArgumentNullException>(Call);
+                Assert.That(exception.ParamName, Is.EqualTo("formula"));
+            }
+        }
+
+        [Test]
+        public void Constructor_WeirPropertiesViewModelNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var formula = new SimpleWeirFormula();
+            void Call() => new SimpleWeirViewModel(formula, null);
+
+            var exception = Assert.Throws<System.ArgumentNullException>(Call);
+            Assert.That(exception.ParamName, Is.EqualTo("weirPropertiesViewModel"));
+        }
+
+        [Test]
+        public void ContractionCoefficient_PropagatesSetCorrectly()
+        {
+            // Setup
+            var formula = new SimpleWeirFormula() { LateralContraction=  6.0 };
+            var weir2D = new Weir2D { WeirFormula = formula };
+
+            const double contractionCoefficient = 20.0;
+
+            using (var weirPropertiesViewModel = new WeirPropertiesViewModel(weir2D))
+            {
+                var viewModel = new SimpleWeirViewModel(formula,
+                                                        weirPropertiesViewModel);
+
+                // Call
+                viewModel.ContractionCoefficient = contractionCoefficient;
+
+                // Assert
+                Assert.That(viewModel.ContractionCoefficient, 
+                            Is.EqualTo(contractionCoefficient));
+                Assert.That(formula.LateralContraction, 
+                            Is.EqualTo(contractionCoefficient));
+            }
+        }
+    }
+}
