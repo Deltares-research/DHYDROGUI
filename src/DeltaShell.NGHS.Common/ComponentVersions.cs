60206af95fe6c519a924b44650d3a62151fc65a1
Dennis Tang <dennis.tang@deltares.nl>
2020-08-03 09:37:02 +0200

    D3DFMIQ-2087 Added generic component to hold the information about the versions
    Former-commit-id: e8046977e33f7d83b6636715d43ce70dcf25ebd7

---
+++ src/DeltaShell.NGHS.Common/ComponentVersions.cs
@@ -0,0 +1,13 @@
+﻿namespace DeltaShell.NGHS.Common
+{
+    /// <summary>
+    /// Class containing values for the versions of the components that are used.
+    /// </summary>
+    public static class ComponentVersions
+    {
+        /// <summary>
+        /// String representing the version of the FM Suite.
+        /// </summary>
+        public const string FMSuiteVersion = "2020.05";
+    }
+}
---
+++ test/DeltaShell.NGHS.Common.Tests/ComponentVersionsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DeltaShell.NGHS.Common.Tests
+{
+    [TestFixture]
+    public class ComponentVersionsTest
+    {
+        [Test]
+        public void Versions_Always_ReturnExpectedValues()
+        {
+            // Call
+            const string fmSuiteVersion = ComponentVersions.FMSuiteVersion;
+
+            // Assert
+            Assert.That(fmSuiteVersion, Is.EqualTo("2020.05"));
+        }
+    }
+}
