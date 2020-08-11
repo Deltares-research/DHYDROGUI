cad96839172bbe9972940cc55aec2fb7957b9a62
tegelaer <maarten.tegelaers@deltares.nl>
2020-08-11 08:47:10 +0200

    Move EnumToDescriptionConverter to NGHS.Common.Gui D3DFMIQ-2102
    Former-commit-id: 1393eb227ee87728cd26cb10783f9f096ba8c44e

---
+++ src/DeltaShell.NGHS.Common.Gui/Converters/EnumToDescriptionConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using DelftTools.Utils.Reflection;
+
+namespace DeltaShell.NGHS.Common.Gui.Converters
+{
+    /// <summary>
+    /// Value converter for converting an <see cref="Enum"/> to its description.
+    /// </summary>
+    [ValueConversion(typeof(Enum), typeof(string))]
+    public class EnumToDescriptionConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the specified <paramref name="value"/> as an <see cref="Enum"/> to its description.
+        /// </summary>
+        /// <param name="value">The enum value.</param>
+        /// <param name="targetType">Type of the target, which should be <see cref="string"/>.</param>
+        /// <param name="parameter">This parameter is not used.</param>
+        /// <param name="culture">This parameter is not used.</param>
+        /// <returns> The enum description. </returns>
+        /// <remarks>
+        /// <see cref="DependencyProperty.UnsetValue"/> is returned if
+        /// the <paramref name="value"/> is not an <see cref="Enum"/> or
+        /// the <paramref name="targetType"/> is not a <see cref="string"/>.
+        /// </remarks>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum enumValue && targetType == typeof(string))
+            {
+                return enumValue.GetDescription();
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        /// <summary>Method is not implemented.</summary>
+        /// <exception cref="NotSupportedException">Thrown when this method is called.</exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
