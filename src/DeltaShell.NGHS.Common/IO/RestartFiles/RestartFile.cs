dd676cd2d62c50b0f5546e90a54fb6593eca84a9
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-08-18 15:47:31 +0200

    Add RestartFile D3DFMIQ-2075
    Former-commit-id: 3c976d01a5ce0a5261edcd5446da952a19a0e906

---
+++ src/DeltaShell.NGHS.Common/IO/RestartFiles/RestartFile.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using DelftTools.Utils.IO;
+
+namespace DeltaShell.NGHS.Common.IO.RestartFiles
+{
+    /// <summary>
+    /// Represents a restart file.
+    /// </summary>
+    public sealed class RestartFile : ICloneable
+    {
+        private FileInfo pathInfo;
+        private string path;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestartFile"/> class.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="path"/> contains invalid characters such as ", &, >, or |, or if <paramref name="path"/> is empty.
+        /// </exception>
+        /// <exception cref="PathTooLongException">
+        /// The specified <paramref name="path"/>, file name, or both exceed the system-defined maximum length.
+        /// </exception>
+        public RestartFile(string path) => Path = path;
+
+        /// <summary>
+        /// Gets or sets the path.
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="value"/> contains invalid characters such as ", &, >, or |, or if <paramref name="value"/> is empty.
+        /// </exception>
+        /// <exception cref="PathTooLongException">
+        /// The specified path, file name, or both exceed the system-defined maximum length.
+        /// </exception>
+        public string Path
+        {
+            get => path;
+            set
+            {
+                FileInfo newPathInfo = value != null
+                                           ? new FileInfo(value)
+                                           : null;
+
+                if (IsSamePath(newPathInfo))
+                {
+                    return;
+                }
+
+                pathInfo = newPathInfo;
+                path = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the file.
+        /// </summary>
+        public string Name => pathInfo?.Name ?? string.Empty;
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is empty.
+        /// </summary>
+        public bool IsEmpty => Path == null;
+
+        /// <summary>
+        /// Copies the file in to the specified <paramref name="directoryPath"/>.
+        /// </summary>
+        /// <param name="directoryPath"> The destination directory. </param>
+        /// <param name="switchTo">Whether this instance should be switched to the new path./></param>
+        /// <exception cref="ArgumentException">
+        /// Throws when <paramref name="directoryPath"/> contains invalid characters such as ", &, or |.
+        /// </exception>
+        /// <exception cref="PathTooLongException">
+        /// Throws when the specified <paramref name="directoryPath"/> exceeds the system-defined maximum length.
+        /// </exception>
+        /// <remarks>
+        /// If <paramref name="directoryPath"/> is <c>null</c> or empty, the destination file path equals the current file path
+        /// or this <see cref="RestartFile"/> does not exist, the method returns.
+        /// </remarks>
+        /// <remarks>The <paramref name="directoryPath"/> will be created without overwriting the existing one.</remarks>
+        public void CopyInto(string directoryPath, bool switchTo = false)
+        {
+            if (string.IsNullOrEmpty(directoryPath) || !Exists)
+            {
+                return;
+            }
+
+            var dirInfo = new DirectoryInfo(directoryPath);
+            FileUtils.CreateDirectoryIfNotExists(dirInfo.FullName);
+            string targetFilePath = System.IO.Path.Combine(dirInfo.FullName, Name);
+
+            CopyTo(targetFilePath);
+
+            if (switchTo)
+            {
+                SwitchTo(targetFilePath);
+            }
+        }
+
+        /// <summary>
+        /// Copies the file to the specified <paramref name="destinationPath"/>.
+        /// </summary>
+        /// <param name="destinationPath"> The destination directory. </param>
+        /// <param name="switchTo">Whether this instance should be switched to the new path./></param>
+        /// <exception cref="ArgumentException">
+        /// Throws when <paramref name="destinationPath"/> contains invalid characters such as ", &, or |.
+        /// </exception>
+        /// <exception cref="PathTooLongException">
+        /// Throws when the specified <paramref name="destinationPath"/> exceeds the system-defined maximum length.
+        /// </exception>
+        /// <remarks>
+        /// If <paramref name="destinationPath"/> is <c>null</c> or empty, equals the current file path
+        /// or this <see cref="RestartFile"/> does not exist, the method returns.
+        /// </remarks>
+        /// <remarks>
+        /// The target directory of <paramref name="destinationPath"/> will be created without
+        /// overwriting the existing one.
+        /// </remarks>
+        public void CopyTo(string destinationPath, bool switchTo = false)
+        {
+            if (string.IsNullOrEmpty(destinationPath) || !Exists)
+            {
+                return;
+            }
+
+            var destinationFileInfo = new FileInfo(destinationPath);
+
+            CreateParentDirectory(destinationFileInfo);
+
+            if (IsSamePath(destinationFileInfo))
+            {
+                return;
+            }
+
+            pathInfo.CopyTo(destinationFileInfo.FullName, switchTo);
+        }
+
+        /// <summary>
+        /// Switches to the specified <paramref name="newPath"/>
+        /// </summary>
+        /// <param name="newPath"> The new path. </param>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="newPath"/> contains invalid characters such as ", &, >, or |, or if <paramref name="newPath"/> is
+        /// empty.
+        /// </exception>
+        /// <exception cref="PathTooLongException">
+        /// The specified path, file name, or both exceed the system-defined maximum length.
+        /// </exception>
+        public void SwitchTo(string newPath)
+        {
+            Path = newPath;
+        }
+
+        public override string ToString() => Name;
+
+        public object Clone() => new RestartFile(Path);
+
+        private bool Exists => pathInfo?.Exists ?? false;
+
+        private static void CreateParentDirectory(FileInfo destinationFileInfo)
+        {
+            DirectoryInfo parentDirInfo = destinationFileInfo.Directory;
+            if (parentDirInfo == null)
+            {
+                return;
+            }
+
+            if (!parentDirInfo.Exists)
+            {
+                parentDirInfo.Create();
+            }
+        }
+
+        private bool IsSamePath(FileInfo fileInfo) => pathInfo?.FullName == fileInfo?.FullName;
+    }
+}
