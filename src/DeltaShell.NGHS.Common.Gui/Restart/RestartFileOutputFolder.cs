8fd23fac820384fdfd2657e34084a7df8b9d3240
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-08-19 16:06:23 +0200

    Add RestartFileOutputTreeFolder D3DFMIQ-2075
    Former-commit-id: 80b6ffc4c947650d45b13a2651ba1065c5b5a654

---
+++ src/DeltaShell.NGHS.Common.Gui/Restart/RestartFileOutputFolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DelftTools.Shell.Core.Workflow.DataItems;
+using DelftTools.Shell.Gui.Swf;
+using DelftTools.Utils.Guards;
+using DeltaShell.NGHS.Common.IO.RestartFiles;
+
+namespace DeltaShell.NGHS.Common.Gui.Restart
+{
+    /// <summary>
+    /// Represent the output restart file tree folder..
+    /// </summary>
+    /// <seealso cref="TreeFolder"/>
+    public class RestartFileOutputTreeFolder : TreeFolder
+    {
+        private const string folderName = "Restart";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestartFileOutputTreeFolder"/> class.
+        /// </summary>
+        /// <param name="model">The restart model.</param>
+        public RestartFileOutputTreeFolder(IRestartModel model) : base(model,
+                                                                       GetDataItems(model).ToList(),
+                                                                       folderName,
+                                                                       FolderImageType.None) {}
+
+        private static IEnumerable<IDataItem> GetDataItems(IRestartModel model)
+        {
+            Ensure.NotNull(model, nameof(model));
+
+            foreach (RestartFile rstFile in model.RestartOutput)
+            {
+                yield return new DataItem(rstFile, DataItemRole.Output)
+                {
+                    Tag = rstFile.Path,
+                };
+            }
+        }
+    }
+}
---
+++ test/DeltaShell.NGHS.Common.Tests/Gui/Restart/RestartFileOutputTreeFolderTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DelftTools.Shell.Core.Workflow.DataItems;
+using DeltaShell.NGHS.Common.Gui.Restart;
+using DeltaShell.NGHS.Common.IO.RestartFiles;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.NGHS.Common.Tests.Gui.Restart
+{
+    [TestFixture]
+    public class RestartFileOutputTreeFolderTest
+    {
+        [Test]
+        public void Constructor_ModelNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new RestartFileOutputTreeFolder(null);
+
+            // Assert
+            var e = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("model"));
+        }
+
+        [Test]
+        public void Constructor_InitializesInstanceCorrectly()
+        {
+            // Setup
+            var model = Substitute.For<IRestartModel>();
+            RestartFile[] restartFiles = GetRestartFiles().ToArray();
+            model.RestartOutput.Returns(restartFiles);
+
+            // Call
+            var folder = new RestartFileOutputTreeFolder(model);
+
+            // Assert
+            Assert.That(folder.Text, Is.EqualTo("Restart"));
+
+            IDataItem[] items = folder.ChildItems.Cast<IDataItem>().ToArray();
+
+            for (var i = 0; i < 3; i++)
+            {
+                IDataItem item = items[i];
+                RestartFile restartFile = restartFiles[i];
+
+                Assert.That(item.Value, Is.SameAs(restartFile));
+                Assert.That(item.Tag, Is.SameAs(restartFile.Name));
+            }
+        }
+
+        private IEnumerable<RestartFile> GetRestartFiles()
+        {
+            yield return new RestartFile("first.file");
+            yield return new RestartFile("second.file");
+            yield return new RestartFile("third.file");
+        }
+    }
+}
