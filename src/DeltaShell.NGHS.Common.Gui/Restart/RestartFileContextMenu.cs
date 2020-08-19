fa4b68fd80fe1221ee7f3cc49d4f0bcdfbe1da8d
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-08-19 15:44:53 +0200

    Add RestartFileContextMenu D3DFMIQ-2075
    Former-commit-id: 868b8a5e49ce2eadb892396ecf957263c6cff58f

---
+++ src/DeltaShell.NGHS.Common.Gui/Restart/RestartFileContextMenu.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using DelftTools.Controls;
+using DelftTools.Controls.Swf;
+using DelftTools.Utils.Guards;
+using DeltaShell.NGHS.Common.IO.RestartFiles;
+
+namespace DeltaShell.NGHS.Common.Gui.Restart
+{
+    /// <summary>
+    /// Context menu for a <seealso cref="RestartFile"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of the <seealso cref="IRestartModel"/></typeparam>
+    /// <seealso cref="MenuItemContextMenuStripAdapter"/>
+    public class RestartFileContextMenu<T> : MenuItemContextMenuStripAdapter where T : IRestartModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestartFileContextMenu{T}"/> class.
+        /// </summary>
+        /// <param name="restartFile">The restart file.</param>
+        /// <param name="node">The corresponding node.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="restartFile"/> or <paramref name="node"/> is <c>null</c>.
+        /// </exception>
+        public RestartFileContextMenu(RestartFile restartFile, ITreeNode node) : base(new ContextMenuStrip())
+        {
+            Ensure.NotNull(restartFile, nameof(restartFile));
+            Ensure.NotNull(node, nameof(node));
+
+            if (!TryGetModel(node, out T model))
+            {
+                return;
+            }
+
+            if (model.RestartInput == restartFile)
+            {
+                AddItemsForInputRestartFile(model);
+            }
+            else if (model.RestartOutput.Contains(restartFile))
+            {
+                AddItemsForOutputRestartFile(model, restartFile);
+            }
+        }
+
+        private void AddItemsForInputRestartFile(T model)
+        {
+            ContextMenuStrip.Items.Add(GetRemoveRestartMenuItem(model));
+            ContextMenuStrip.Items.Add(GetUseLastValidRestartMenuItem(model));
+        }
+
+        private void AddItemsForOutputRestartFile(T model, RestartFile restartFile)
+        {
+            ContextMenuStrip.Items.Add(GetUseAsRestartMenuItem(model, restartFile));
+        }
+
+        private static ClonableToolStripMenuItem GetUseAsRestartMenuItem(T model, RestartFile restartFile)
+        {
+            var menuItem = new ClonableToolStripMenuItem {Text = "Use as restart"};
+            menuItem.Click += (s, e) =>
+            {
+                model.RestartInput.Path = restartFile.Path;
+                model.UseRestart = true;
+            };
+
+            return menuItem;
+        }
+
+        private static ClonableToolStripMenuItem GetUseLastValidRestartMenuItem(T model)
+        {
+            var menuItem = new ClonableToolStripMenuItem
+            {
+                Text = "Use last restart",
+                Enabled = false
+            };
+
+            RestartFile outputRestartFile = model.RestartOutput.LastOrDefault();
+            if (outputRestartFile == null)
+            {
+                return menuItem;
+            }
+
+            menuItem.Enabled = true;
+            menuItem.Click += (s, e) =>
+            {
+                model.RestartInput.Path = outputRestartFile.Path;
+                model.UseRestart = true;
+            };
+
+            return menuItem;
+        }
+
+        private static ClonableToolStripMenuItem GetRemoveRestartMenuItem(T model)
+        {
+            var menuItem = new ClonableToolStripMenuItem
+            {
+                Text = "Remove restart",
+                Enabled = model.UseRestart,
+            };
+            menuItem.Click += (s, e) =>
+            {
+                model.RestartInput.Path = null;
+                model.UseRestart = false;
+            };
+
+            return menuItem;
+        }
+
+        private static bool TryGetModel(ITreeNode node, out T result)
+        {
+            ITreeNode parent = node.Parent;
+            while (parent != null)
+            {
+                if (parent.Tag is T model)
+                {
+                    result = model;
+                    return true;
+                }
+
+                parent = parent.Parent;
+            }
+
+            result = default(T);
+            return false;
+        }
+    }
+}
---
+++ test/DeltaShell.NGHS.Common.Tests/Gui/Restart/RestartFileContextMenuTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DelftTools.Controls;
+using DeltaShell.NGHS.Common.Gui.Restart;
+using DeltaShell.NGHS.Common.IO.RestartFiles;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.NGHS.Common.Tests.Gui.Restart
+{
+    [TestFixture]
+    public class RestartFileContextMenuTest
+    {
+        [Test]
+        public void Constructor_RestartFileNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var node = Substitute.For<ITreeNode>();
+
+            // Call
+            void Call() => new RestartFileContextMenu<IRestartModel>(null, node);
+
+            // Assert
+            var e = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("restartFile"));
+        }
+
+        [Test]
+        public void Constructor_NodeNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var restartFile = new RestartFile();
+
+            // Call
+            void Call() => new RestartFileContextMenu<IRestartModel>(restartFile, null);
+
+            // Assert
+            var e = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("node"));
+        }
+
+        [Test]
+        public void Constructor_NodeDoesNotBelongToModel_InitializesInstanceCorrectly()
+        {
+            // Setup
+            var restartFile = new RestartFile();
+            var node = Substitute.For<ITreeNode>();
+            node.Parent.Parent.Returns((ITreeNode) null);
+
+            // Call
+            var menu = new RestartFileContextMenu<IRestartModel>(restartFile, node);
+
+            // Assert
+            Assert.That(menu.ContextMenuStrip.Items, Is.Empty);
+        }
+
+        [Test]
+        public void Constructor_InputRestartFile_InitializesInstanceCorrectly()
+        {
+            // Setup
+            var restartFile = new RestartFile();
+            var node = Substitute.For<ITreeNode>();
+            var model = Substitute.For<IRestartModel>();
+            node.Parent.Parent.Tag.Returns(model);
+            model.RestartInput.Returns(restartFile);
+
+            // Call
+            var menu = new RestartFileContextMenu<IRestartModel>(restartFile, node);
+
+            // Assert
+            ToolStripItemCollection toolStripItems = menu.ContextMenuStrip.Items;
+            Assert.That(toolStripItems, Has.Count.EqualTo(2));
+            Assert.That(toolStripItems[0].Text, Is.EqualTo("Remove restart"));
+            Assert.That(toolStripItems[1].Text, Is.EqualTo("Use last restart"));
+        }
+
+        [Test]
+        public void Constructor_OutputRestartFile_InitializesInstanceCorrectly()
+        {
+            // Setup
+            var restartFile = new RestartFile();
+            var node = Substitute.For<ITreeNode>();
+            var model = Substitute.For<IRestartModel>();
+            node.Parent.Parent.Tag.Returns(model);
+            model.RestartOutput.Returns(new List<RestartFile> {restartFile});
+
+            // Call
+            var menu = new RestartFileContextMenu<IRestartModel>(restartFile, node);
+
+            // Assert
+            ToolStripItemCollection toolStripItems = menu.ContextMenuStrip.Items;
+            Assert.That(toolStripItems, Has.Count.EqualTo(1));
+            Assert.That(toolStripItems[0].Text, Is.EqualTo("Use as restart"));
+        }
+    }
+}
