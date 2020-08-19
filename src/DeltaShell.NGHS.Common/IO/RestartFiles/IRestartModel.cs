a457d9e29befe1e35273c6a45ba262c5e5c0a42b
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-08-19 11:45:32 +0200

    Add IRestartModel interface D3DFMIQ-2075
    Former-commit-id: 1b0ad5b7aaeaea6af85292d8f2cc0a47eb94995b

---
+++ src/DeltaShell.NGHS.Common/IO/RestartFiles/IRestartModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaShell.NGHS.Common.IO.RestartFiles
+{
+    /// <summary>
+    /// <see cref="IRestartModel"/> provides the interface for models supporting restart files.
+    /// </summary>
+    public interface IRestartModel
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether or not this model uses a restart file.
+        /// </summary>
+        bool UseRestart { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not this model should write restart files.
+        /// </summary>
+        bool WriteRestart { get; set; }
+
+        /// <summary>
+        /// Gets the restart input file.
+        /// </summary>
+        RestartFile RestartInput { get; }
+
+        /// <summary>
+        /// Gets the restart output files.
+        /// </summary>
+        IEnumerable<RestartFile> RestartOutput { get; }
+    }
+}
