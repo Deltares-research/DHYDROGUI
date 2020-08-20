73e6b93599886c126cbbfcf53742159198f54f7c
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-08-20 15:32:40 +0200

    Rename FMRstFileImporterTest to FMRestartFileImporterTest D3DFMIQ-2075
    Former-commit-id: a07e833bfb863bfb0f18ef9538a6ea4ef12a657b

---
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/IO/Importers/FMRestartFileImporterTest.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.IO.Importers
+{
+    [TestFixture]
+    public class FMRestartFileImporterTest
+    {
+        // TODO D3DFMIQ-2075
+    }
+}
