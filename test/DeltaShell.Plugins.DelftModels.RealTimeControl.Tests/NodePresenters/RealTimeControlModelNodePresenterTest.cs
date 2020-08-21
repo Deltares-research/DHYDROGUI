8f0a14fc83cffb4d7ed152560a8572a991a7772c
Carlos Lubbers <carlos.lubbers@deltares.nl>
2020-08-21 11:55:10 +0200

    Add unit test for node presenter D3DFMIQ-2077
    Former-commit-id: fe8bf609ee95b0120454ccc6e428b821fe2ba9ea

--- test/DeltaShell.Plugins.DelftModels.RealTimeControl.Tests/NodePresenters/RealTimeControlModelNodePresenterTest.cs
+++ test/DeltaShell.Plugins.DelftModels.RealTimeControl.Tests/NodePresenters/RealTimeControlModelNodePresenterTest.cs
@@ -1,4 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using DelftTools.Shell.Gui;
+using DelftTools.Shell.Gui.Swf;
+using DeltaShell.NGHS.Common.Gui.Restart;
 using DeltaShell.Plugins.DelftModels.RealTimeControl.Gui.NodePresenters;
 using NUnit.Framework;
 using Rhino.Mocks;
@@ -23,6 +28,22 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.NodePresenters
             pluginGuiMock.Expect(pg => pg.Gui).Return(gui);
 
             return new RealTimeControlModelNodePresenter(pluginGuiMock);
+        }
+
+        [Test]
+        public void GetChildNodeObjects_ContainsRestartOutputTreeFolder()
+        {
+            // Setup
+            RealTimeControlModelNodePresenter nodePresenter = GetRealTimeControlModelNodePresenter();
+            var model = new RealTimeControlModel();
+
+            // Call
+            IEnumerable childObjects = nodePresenter.GetChildNodeObjects(model, null);
+
+            // Assert
+            TreeFolder outputTreeFolder = childObjects.OfType<TreeFolder>().Single(f => f.Text == "Output");
+            List<RestartFileOutputTreeFolder> restartFileOutputTreeFolders = outputTreeFolder.ChildItems.OfType<RestartFileOutputTreeFolder>().ToList();
+            Assert.That(restartFileOutputTreeFolders, Has.Count.EqualTo(1));
         }
     }
 }
