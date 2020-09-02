7af220a6795e45ba1f998386b397baba0a782d58
tegelaer <maarten.tegelaers@deltares.nl>
2020-09-02 17:37:25 +0200

    Add skeleton for Waves Directory Structure integration tests D3DFMIQ-2203
    Former-commit-id: 01e2b558efe671d523232c6a304a056301732371

---
+++ test/DeltaShell.Plugins.DelftModels.HydroModel.Tests/WavesDirectoryStructureTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using DelftTools.TestUtils;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.DelftModels.HydroModel.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [Category(TestCategory.DataAccess)]
+    [Category(TestCategory.Slow)]
+    [TestFixture]
+    public class WavesDirectoryStructureTest
+    {
+        
+    }
+}
