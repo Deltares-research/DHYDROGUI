317a972ac1573a4cedf32fa15c38c9bbd018aad1
tegelaer <maarten.tegelaers@deltares.nl>
2020-09-03 09:27:27 +0200

    Add GetTemporaryDirectoryName method D3DFMIQ-2203
    Former-commit-id: 20939e18eea3237d6cb9ef80f03db8abb185c0ff

--- src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/WaveDirectoryStructureMigrationHelper.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/WaveDirectoryStructureMigrationHelper.cs
@@ -1,4 +1,9 @@
-﻿using DelftTools.Utils.Guards;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DelftTools.Utils;
+using DelftTools.Utils.Guards;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0
 {
@@ -14,12 +19,38 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0
         /// associated with file format version 1.2.0.0.
         /// </summary>
         /// <param name="waveModel">The wave model.</param>
-        /// <exception cref="System.ArgumentNullException">
+        /// <exception cref="ArgumentNullException">
         /// Thrown when <paramref name="waveModel"/> is <c>null</c>.
         /// </exception>
         public static void Migrate(IWaveModel waveModel)
         {
             Ensure.NotNull(waveModel, nameof(waveModel));
+        }
+
+        /// <summary>
+        /// Gets an unique temporary migration directory name for the provided
+        /// <paramref name="srcDirectory"/>.
+        /// </summary>
+        /// <param name="srcDirectory">The source directory.</param>
+        /// <returns>
+        /// An unique temporary directory name formatted as <c>srcDirectory.Name_tmp.{0}</c>
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="srcDirectory"/> parent is <c>null</c>.
+        /// </exception>
+        public static string GetTemporaryMigrationDirectoryName(DirectoryInfo srcDirectory)
+        {
+            if (srcDirectory.Parent == null)
+            {
+                throw new ArgumentException("Cannot create a temporary directory name if the parent folder is null.");
+            }
+
+            IEnumerable<string> folderNames = 
+                srcDirectory.Parent.GetDirectories()
+                                   .Select(x => x.Name);
+
+            string nameFormat = srcDirectory.Name + "_tmp.{0}";
+            return NamingHelper.GenerateUniqueNameFromList(nameFormat, true, folderNames);
         }
     }
 }
--- test/DeltaShell.Plugins.FMSuite.Wave.Tests/Migrations/1.1.0.0/WaveDirectoryStructureMigrationHelperTest.cs
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Migrations/1.1.0.0/WaveDirectoryStructureMigrationHelperTest.cs
@@ -1,4 +1,7 @@
-﻿using DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0;
+﻿using System.IO;
+using DelftTools.TestUtils;
+using DeltaShell.NGHS.IO.TestUtils;
+using DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0;
 using NUnit.Framework;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Migrations._1._1._0._0
@@ -13,6 +16,60 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Migrations._1._1._0._0
 
             var exception = Assert.Throws<System.ArgumentNullException>(Call);
             Assert.That(exception.ParamName, Is.EqualTo("waveModel"));
+        }
+
+        [Test]
+        public void GetTemporaryMigrationDirectoryName_SrcDirectoryParentNull_ThrowsArgumentException()
+        {
+            var dirInfo = new DirectoryInfo("C:\\");
+            void Call() => WaveDirectoryStructureMigrationHelper.GetTemporaryMigrationDirectoryName(dirInfo);
+
+            Assert.Throws<System.ArgumentException>(Call);
+        }
+
+        [Test]
+        [Category(TestCategory.DataAccess)]
+        public void GetTemporaryMigrationDirectoryName_NoNameClashes_ExpectedNameIsReturned()
+        {
+            // Setup
+            const string srcDirName = "srcDirName";
+
+            using (var tempDir = new TemporaryDirectory())
+            {
+                var parentDirInfo = new DirectoryInfo(tempDir.Path);
+                DirectoryInfo srcDirInfo = parentDirInfo.CreateSubdirectory(srcDirName);
+
+                // Call
+                string migrationDirectoryName =
+                    WaveDirectoryStructureMigrationHelper.GetTemporaryMigrationDirectoryName(srcDirInfo);
+
+                // Assert
+                Assert.That(migrationDirectoryName, Is.EqualTo(srcDirName + "_tmp.1"));
+            }
+        }
+
+        [Test]
+        [Category(TestCategory.DataAccess)]
+        public void GetTemporaryMigrationDirectoryName_NameClashes_ExpectedNameIsReturned()
+        {
+            // Setup
+            const string srcDirName = "srcDirName";
+
+            using (var tempDir = new TemporaryDirectory())
+            {
+                var parentDirInfo = new DirectoryInfo(tempDir.Path);
+                DirectoryInfo srcDirInfo = parentDirInfo.CreateSubdirectory(srcDirName);
+                parentDirInfo.CreateSubdirectory(srcDirName + "_tmp.1");
+                parentDirInfo.CreateSubdirectory(srcDirName + "_tmp.2");
+                parentDirInfo.CreateSubdirectory(srcDirName + "_tmp.3");
+
+                // Call
+                string migrationDirectoryName =
+                    WaveDirectoryStructureMigrationHelper.GetTemporaryMigrationDirectoryName(srcDirInfo);
+
+                // Assert
+                Assert.That(migrationDirectoryName, Is.EqualTo(srcDirName + "_tmp.4"));
+            }
         }
     }
 }
