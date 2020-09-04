c444dd903c29e12ba646d0d13e4d3f41b6c0132d
tegelaer <maarten.tegelaers@deltares.nl>
2020-09-04 08:03:00 +0200

    Add initial documentation IDelftIniMigrator D3DFMIQ-2203
    Former-commit-id: 5f1797bf892f065eeb2d387060a99f3f8a1feef0

--- src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/IDelftIniMigrator.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/IDelftIniMigrator.cs
@@ -2,8 +2,23 @@
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0
 {
+    /// <summary>
+    /// The <see cref="IDelftIniMigrator"/> is responsible for migrating a delft ini
+    /// file from one directory to another.
+    /// </summary>
     public interface IDelftIniMigrator
     {
-        void MigrateFile(string srcFile, string targetDirectory, ILogHandler logHandler);
+        /// <summary>
+        /// Migrates the delft ini file at <paramref name="srcFilePath"/> to the
+        /// specified <paramref name="targetDirectory"/>.
+        /// </summary>
+        /// <param name="srcFilePath">The source file.</param>
+        /// <param name="targetDirectory">The target directory.</param>
+        /// <param name="logHandler">The log handler.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown if any parameter is <c>null</c>.
+        /// </exception>
+        // TODO: add exceptions from writing here
+        void MigrateFile(string srcFilePath, string targetDirectory, ILogHandler logHandler);
     }
 }
