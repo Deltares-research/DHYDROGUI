7eba244b58d0b2128bf99b2bc608bdb02fcdc8e5
tegelaer <maarten.tegelaers@deltares.nl>
2020-09-04 08:07:28 +0200

    Introduce a IDelftIniReader interface D3DFMIQ-2203
    Former-commit-id: 84566d54af411aa53fa4d9c1a3a3d3d526a66ec8

---
+++ src/DeltaShell.NGHS.IO/IDelftIniReader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using DeltaShell.NGHS.IO.DelftIniObjects;
+
+namespace DeltaShell.NGHS.IO
+{
+    /// <summary>
+    /// <see cref="IDelftIniReader"/> defines the interface to read delft ini
+    /// files.
+    /// </summary>
+    public interface IDelftIniReader
+    {
+        /// <summary>
+        /// Reads a Delft .ini format file.
+        /// </summary>
+        /// <param name="stream"> The <see cref="Stream"/> to read the ini file from. </param>
+        /// <param name="filePath"> The path to the file location. </param>
+        /// <returns> A collection of <see cref="DelftIniCategory"/> instances. </returns>
+        /// <remarks> The stream is implicitly disposed. </remarks>
+        IList<DelftIniCategory> ReadDelftIniFile(Stream stream, string filePath);
+    }
+}
