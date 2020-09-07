bcff3cf4799ce15321a8f4bbff623eff50580676
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-07 16:04:56 +0200

    Add WaveMeteoDataFactory D3DFMIQ-2054
    Former-commit-id: b4a92ca9aca80da35abf409910cac07321e07f15

---
+++ src/DeltaShell.Plugins.FMSuite.Wave/IO/Helpers/Domain/WaveMeteoDataFactory.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DelftTools.Utils.Guards;
+using DeltaShell.Plugins.FMSuite.Common.Wind;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.IO.Helpers.Domain
+{
+    /// <summary>
+    /// Helper factory for creating <see cref="WaveDomainData"/>.
+    /// </summary>
+    public static class WaveMeteoDataFactory
+    {
+        private const string quantityKey = "quantity1";
+        private const string xWind = "x_wind";
+        private const string yWind = "y_wind";
+
+        /// <summary>
+        /// Creates the spider web meteo data.
+        /// </summary>
+        /// <param name="spwFile">The spiderweb file.</param>
+        /// <returns>The created <see cref="WaveMeteoData"/></returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="spwFile"/> is <c>null</c> or empty.
+        /// </exception>
+        public static WaveMeteoData CreateSpiderWebMeteoData(string spwFile)
+        {
+            Ensure.NotNullOrEmpty(spwFile, nameof(spwFile));
+
+            return new WaveMeteoData
+            {
+                FileType = WindDefinitionType.SpiderWebGrid,
+                HasSpiderWeb = true,
+                SpiderWebFilePath = spwFile
+            };
+        }
+
+        /// <summary>
+        /// Creates the xy component meteo data.
+        /// </summary>
+        /// <param name="xFile">The file for the x component.</param>
+        /// <param name="yFile">The file for the y component.</param>
+        /// <param name="spwFile">The optional spider web file.</param>
+        /// <returns>The created <see cref="WaveMeteoData"/></returns>
+        /// ///
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="xFile"/> or <paramref name="yFile"/> is <c>null</c> or empty.
+        /// </exception>
+        public static WaveMeteoData CreateXYComponentMeteoData(string xFile, string yFile, string spwFile = null)
+        {
+            Ensure.NotNullOrEmpty(xFile, nameof(xFile));
+            Ensure.NotNullOrEmpty(yFile, nameof(yFile));
+
+            return new WaveMeteoData
+            {
+                FileType = WindDefinitionType.WindXWindY,
+                HasSpiderWeb = spwFile != null,
+                SpiderWebFilePath = spwFile,
+                XComponentFilePath = xFile,
+                YComponentFilePath = yFile
+            };
+        }
+
+        /// <summary>
+        /// Creates the xy component meteo data for wnd files. Determines the wind quantity (x/y) by reading the property
+        /// 'quantity1' from the file.
+        /// </summary>
+        /// <param name="file1">The first wnd file.</param>
+        /// <param name="file2">The second wnd file.</param>
+        /// <param name="spwFile">The optional spider web file.</param>
+        /// <returns>
+        /// The created <see cref="WaveMeteoData"/> if the data is valid and supported; otherwise, <c>null</c>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="file1"/> or <paramref name="file2"/> is <c>null</c> or empty.
+        /// </exception>
+        /// <exception cref="FileNotFoundException">
+        /// Thrown when <paramref name="file1"/> or <paramref name="file2"/> does not exist.
+        /// </exception>
+        /// <remarks>
+        /// The supported quantities are "x_wind" and "y_wind", and both need to be present in either one of the specified files.
+        /// </remarks>
+        public static WaveMeteoData CreateWndXYComponentMeteoData(string file1, string file2, string spwFile = null)
+        {
+            Ensure.NotNullOrEmpty(file1, nameof(file1));
+            Ensure.NotNullOrEmpty(file2, nameof(file2));
+
+            if (!File.Exists(file1))
+            {
+                throw new FileNotFoundException("Meteo file does not exist.", file1);
+            }
+
+            if (!File.Exists(file2))
+            {
+                throw new FileNotFoundException("Meteo file does not exist.", file2);
+            }
+
+            string quantity1 = GetQuantity(file1);
+            string quantity2 = GetQuantity(file2);
+
+            if (quantity1 == quantity2)
+            {
+                return null;
+            }
+
+            if (!IsSupported(quantity1))
+            {
+                return null;
+            }
+
+            if (!IsSupported(quantity2))
+            {
+                return null;
+            }
+
+            var mapping = new Dictionary<string, string>
+            {
+                [quantity1] = file1,
+                [quantity2] = file2
+            };
+
+            return CreateXYComponentMeteoData(mapping[xWind], mapping[yWind], spwFile);
+        }
+
+        /// <summary>
+        /// Creates the vector meteo data.
+        /// </summary>
+        /// <param name="xyFile">The xy file.</param>
+        /// <param name="spwFile">The optional spw file.</param>
+        /// <returns>The created <see cref="WaveMeteoData"/></returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="spwFile"/> is <c>null</c> or empty.
+        /// </exception>
+        public static WaveMeteoData CreateVectorMeteoData(string xyFile, string spwFile = null)
+        {
+            Ensure.NotNullOrEmpty(xyFile, nameof(xyFile));
+
+            return new WaveMeteoData
+            {
+                FileType = WindDefinitionType.WindXY,
+                HasSpiderWeb = spwFile != null,
+                SpiderWebFilePath = spwFile,
+                XYVectorFilePath = xyFile,
+            };
+        }
+
+        private static bool IsSupported(string quantity) => quantity == xWind || quantity == yWind;
+
+        private static string GetQuantity(string file)
+        {
+            using (var sr = new StreamReader(new FileStream(file, FileMode.Open)))
+            {
+                string line;
+
+                while ((line = sr.ReadLine())?.Trim() != null)
+                {
+                    if (line.StartsWith(quantityKey, StringComparison.Ordinal))
+                    {
+                        return line.Split('=').ElementAt(1).Trim();
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/IO/Helpers/Domain/WaveMeteoDataFactoryTest.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.IO;
+using DeltaShell.NGHS.IO.TestUtils;
+using DeltaShell.Plugins.FMSuite.Common.Wind;
+using DeltaShell.Plugins.FMSuite.Wave.IO.Helpers.Domain;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.IO.Helpers.Domain
+{
+    [TestFixture]
+    public class WaveMeteoDataFactoryTest
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        public void CreateSpiderWebMeteoData_SpwFileNullOrEmpty_ThrowsArgumentException(string spwFile)
+        {
+            // Call
+            void Call() => WaveMeteoDataFactory.CreateSpiderWebMeteoData(spwFile);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("spwFile"));
+        }
+
+        [Test]
+        public void CreateSpiderWebMeteoData_CreatesCorrectMeteoData()
+        {
+            // Call
+            WaveMeteoData meteoData = WaveMeteoDataFactory.CreateSpiderWebMeteoData("path/to/file.spw");
+
+            // Assert
+            Assert.That(meteoData, Is.Not.Null);
+            Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.SpiderWebGrid));
+            Assert.That(meteoData.HasSpiderWeb, Is.True);
+            Assert.That(meteoData.SpiderWebFilePath, Is.EqualTo("path/to/file.spw"));
+        }
+
+        [TestCase(null, "path/to/file", "xFile")]
+        [TestCase("", "path/to/file", "xFile")]
+        [TestCase("path/to/file", null, "yFile")]
+        [TestCase("path/to/file", "", "yFile")]
+        public void CreateXYComponentMeteoData_XFileOrYFileNullOrEmpty_ThrowsArgumentException(string xFile, string yFile, string expParamName)
+        {
+            // Call
+            void Call() => WaveMeteoDataFactory.CreateXYComponentMeteoData(xFile, yFile);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo(expParamName));
+        }
+
+        [Test]
+        public void CreateXYComponentMeteoData_WithoutSpiderweb_CreatesCorrectMeteoData()
+        {
+            // Call
+            WaveMeteoData meteoData = WaveMeteoDataFactory.CreateXYComponentMeteoData("path/to/x_file", "path/to/y_file");
+
+            // Assert
+            Assert.That(meteoData, Is.Not.Null);
+            Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.WindXWindY));
+            Assert.That(meteoData.HasSpiderWeb, Is.False);
+            Assert.That(meteoData.SpiderWebFilePath, Is.Null);
+            Assert.That(meteoData.XComponentFilePath, Is.EqualTo("path/to/x_file"));
+            Assert.That(meteoData.YComponentFilePath, Is.EqualTo("path/to/y_file"));
+        }
+
+        [Test]
+        public void CreateXYComponentMeteoData_WithSpiderweb_CreatesCorrectMeteoData()
+        {
+            // Call
+            WaveMeteoData meteoData = WaveMeteoDataFactory.CreateXYComponentMeteoData("path/to/x_file", "path/to/y_file", "path/to/spiderweb");
+
+            // Assert
+            Assert.That(meteoData, Is.Not.Null);
+            Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.WindXWindY));
+            Assert.That(meteoData.HasSpiderWeb, Is.True);
+            Assert.That(meteoData.SpiderWebFilePath, Is.EqualTo("path/to/spiderweb"));
+            Assert.That(meteoData.XComponentFilePath, Is.EqualTo("path/to/x_file"));
+            Assert.That(meteoData.YComponentFilePath, Is.EqualTo("path/to/y_file"));
+        }
+
+        [TestCase(null, "path/to/file", "file1")]
+        [TestCase("", "path/to/file", "file1")]
+        [TestCase("path/to/file", null, "file2")]
+        [TestCase("path/to/file", "", "file2")]
+        public void CreateWndXYComponentMeteoData_File1OrFile2NullOrEmpty_ThrowsArgumentException(string file1, string file2, string expParamName)
+        {
+            // Call
+            void Call() => WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo(expParamName));
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_File1DoesNotExist_ThrowsFileNotFoundException()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file2 = temp.CreateFile("file2.wnd");
+
+                // Call
+                void Call() => WaveMeteoDataFactory.CreateWndXYComponentMeteoData("does/not/exist", file2);
+
+                // Assert
+                var e = Assert.Throws<FileNotFoundException>(Call);
+                Assert.That(e.FileName, Is.EqualTo("does/not/exist"));
+                Assert.That(e.Message, Is.EqualTo("Meteo file does not exist."));
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_File2DoesNotExist_ThrowsFileNotFoundException()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file2.wnd");
+
+                // Call
+                void Call() => WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, "does/not/exist");
+
+                // Assert
+                var e = Assert.Throws<FileNotFoundException>(Call);
+                Assert.That(e.FileName, Is.EqualTo("does/not/exist"));
+                Assert.That(e.Message, Is.EqualTo("Meteo file does not exist."));
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_QuantitiesEqual_ReturnsNull()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd", GetExampleContent("x_wind"));
+                string file2 = temp.CreateFile("file2.wnd", GetExampleContent("x_wind"));
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+                // Assert
+                Assert.That(meteoData, Is.Null);
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_UnsupportedQuantityFile1_ReturnsNull()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd", GetExampleContent("the_wind"));
+                string file2 = temp.CreateFile("file2.wnd", GetExampleContent("y_wind"));
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+                // Assert
+                Assert.That(meteoData, Is.Null);
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_UnsupportedQuantityFile2_ReturnsNull()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd", GetExampleContent("x_wind"));
+                string file2 = temp.CreateFile("file2.wnd", GetExampleContent("the_wind"));
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+                // Assert
+                Assert.That(meteoData, Is.Null);
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_QuantityNotSpecifiedInFile1_ReturnsCorrectMeteoData()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd");
+                string file2 = temp.CreateFile("file2.wnd", GetExampleContent("y_wind"));
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+                // Assert
+                Assert.That(meteoData, Is.Null);
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_QuantityNotSpecifiedInFile2_ReturnsCorrectMeteoData()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd", GetExampleContent("x_wind"));
+                string file2 = temp.CreateFile("file2.wnd");
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+                // Assert
+                Assert.That(meteoData, Is.Null);
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_SupportedQuantities_WithoutSpiderweb_ReturnsCorrectMeteoData()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd", GetExampleContent("x_wind"));
+                string file2 = temp.CreateFile("file2.wnd", GetExampleContent("y_wind"));
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+                // Assert
+                Assert.That(meteoData, Is.Not.Null);
+                Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.WindXWindY));
+                Assert.That(meteoData.HasSpiderWeb, Is.False);
+                Assert.That(meteoData.SpiderWebFilePath, Is.Null);
+                Assert.That(meteoData.XComponentFilePath, Is.EqualTo(file1));
+                Assert.That(meteoData.YComponentFilePath, Is.EqualTo(file2));
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_SupportedQuantities_Unordered_ReturnsCorrectMeteoData()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd", GetExampleContent("y_wind"));
+                string file2 = temp.CreateFile("file2.wnd", GetExampleContent("x_wind"));
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2);
+
+                // Assert
+                Assert.That(meteoData, Is.Not.Null);
+                Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.WindXWindY));
+                Assert.That(meteoData.HasSpiderWeb, Is.False);
+                Assert.That(meteoData.SpiderWebFilePath, Is.Null);
+                Assert.That(meteoData.XComponentFilePath, Is.EqualTo(file2));
+                Assert.That(meteoData.YComponentFilePath, Is.EqualTo(file1));
+            }
+        }
+
+        [Test]
+        public void CreateWndXYComponentMeteoData_SupportedQuantities_WithSpiderweb_ReturnsCorrectMeteoData()
+        {
+            // Setup
+            using (var temp = new TemporaryDirectory())
+            {
+                string file1 = temp.CreateFile("file1.wnd", GetExampleContent("x_wind"));
+                string file2 = temp.CreateFile("file2.wnd", GetExampleContent("y_wind"));
+
+                // Call
+                WaveMeteoData meteoData = WaveMeteoDataFactory.CreateWndXYComponentMeteoData(file1, file2, "path/to/spiderweb");
+
+                // Assert
+                Assert.That(meteoData, Is.Not.Null);
+                Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.WindXWindY));
+                Assert.That(meteoData.HasSpiderWeb, Is.True);
+                Assert.That(meteoData.SpiderWebFilePath, Is.EqualTo("path/to/spiderweb"));
+                Assert.That(meteoData.XComponentFilePath, Is.EqualTo(file1));
+                Assert.That(meteoData.YComponentFilePath, Is.EqualTo(file2));
+            }
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void CreateVectorMeteoData_XYFileNullOrEmpty_ThrowsArgumentException(string xyFile)
+        {
+            // Call
+            void Call() => WaveMeteoDataFactory.CreateVectorMeteoData(xyFile);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("xyFile"));
+        }
+
+        [Test]
+        public void CreateVectorMeteoData_WithoutSpiderweb_CreatesCorrectMeteoData()
+        {
+            // Call
+            WaveMeteoData meteoData = WaveMeteoDataFactory.CreateVectorMeteoData("path/to/file");
+
+            // Assert
+            Assert.That(meteoData, Is.Not.Null);
+            Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.WindXY));
+            Assert.That(meteoData.HasSpiderWeb, Is.False);
+            Assert.That(meteoData.SpiderWebFilePath, Is.Null);
+            Assert.That(meteoData.XYVectorFilePath, Is.EqualTo("path/to/file"));
+        }
+
+        [Test]
+        public void CreateVectorMeteoData_WithSpiderweb_CreatesCorrectMeteoData()
+        {
+            // Call
+            WaveMeteoData meteoData = WaveMeteoDataFactory.CreateVectorMeteoData("path/to/file", "path/to/spiderweb");
+
+            // Assert
+            Assert.That(meteoData, Is.Not.Null);
+            Assert.That(meteoData.FileType, Is.EqualTo(WindDefinitionType.WindXY));
+            Assert.That(meteoData.HasSpiderWeb, Is.True);
+            Assert.That(meteoData.SpiderWebFilePath, Is.EqualTo("path/to/spiderweb"));
+            Assert.That(meteoData.XYVectorFilePath, Is.EqualTo("path/to/file"));
+        }
+
+        private static string GetExampleContent(string quantity)
+        {
+            return "FileVersion      = 1.03\n" +
+                   "filetype = meteo_on_equidistant_grid\n" +
+                   "NODATA_value = -999\n" +
+                   "n_cols = 2\n" +
+                   "n_rows = 2\n" +
+                   "grid_unit = m\n" +
+                   "x_llcorner = 1.00E+03\n" +
+                   "y_llcorner = 1.00E+03\n" +
+                   "dx = 2.44E+04\n" +
+                   "dy = 3.00E+04\n" +
+                   "n_quantity = 1\n" +
+                   $"quantity1 = {quantity}\n" +
+                   "unit1 = m s - 1\n";
+        }
+    }
+}
