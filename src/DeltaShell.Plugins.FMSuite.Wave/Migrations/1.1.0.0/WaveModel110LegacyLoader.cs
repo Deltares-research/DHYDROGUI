b465079851c7be01ad055264acd35c91be2e0dc6
tegelaer <maarten.tegelaers@deltares.nl>
2020-09-10 12:50:29 +0200

    Implement WaveModel110LegacyLoader D3DFMIQ-2203
    - Extend WaveModel.1.1.0.hbm.xml to persist using the new legacy loader
    
    
    Former-commit-id: 5156c2db04076cec6c62f83c5e3f10d350a6c0ee

--- src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/WaveModel110LegacyLoader.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/WaveModel110LegacyLoader.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
+using System.IO;
 using DelftTools.Shell.Core.Dao;
+using log4net;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0
 {
@@ -11,9 +12,62 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0
     /// <seealso cref="LegacyLoader" />
     public class WaveModel110LegacyLoader : LegacyLoader
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(WaveModel110LegacyLoader));
+
         public override void OnAfterInitialize(object entity, IDbConnection dbConnection)
         {
+            base.OnAfterInitialize(entity, dbConnection);
 
+            if (!(entity is WaveModel model))
+            {
+                log.Error($"Provided entity is not a {nameof(WaveModel)}");
+                return;
+            }
+
+            if (!TryObtainDatabasePath(dbConnection.ConnectionString, out string dbPath))
+            {
+                log.Error($"Could not determine dsproj location from database connection: {dbConnection.ConnectionString}");
+            }
+
+            string mdwPath = Path.Combine(dbPath + "_data", model.Name, $"{model.Name}.mdw");
+
+            WaveDirectoryStructureMigrationHelper.Migrate(mdwPath);
+        }
+
+        private static bool TryObtainDatabasePath(string connectionString, 
+                                                  out string databasePath)
+        {
+            string[] keyValuePairs = connectionString.Split(';');
+
+            databasePath = null;
+
+            foreach (string keyValueString in keyValuePairs)
+            {
+                if (TryParseDataSource(keyValueString, out string parsedPath))
+                {
+                    databasePath = parsedPath;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TryParseDataSource(string keyValueString, out string parsedDataSourcePath)
+        {
+            parsedDataSourcePath = null;
+
+            string[] keyValuePair = keyValueString.Split('=');
+            string key = keyValuePair[0].Trim();
+            string value = keyValuePair[1].Trim();
+
+            if (key != "Data Source")
+            {
+                return false;
+            }
+
+            parsedDataSourcePath = value;
+            return true;
         }
     }
 }
