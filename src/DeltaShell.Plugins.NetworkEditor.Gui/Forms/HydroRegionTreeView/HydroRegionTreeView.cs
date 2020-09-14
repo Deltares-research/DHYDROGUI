2ce4da20e35e324984f565ae863248a0a82284c7
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-14 17:06:53 +0200

    Remove WasteWaterTreatmentPlantsTreeViewNodePresenter D3DFMIQ-2135
    Former-commit-id: 7c83fe11c035a9e39d55254b0174307d251abc80

--- src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/HydroRegionTreeView/HydroRegionTreeView.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/HydroRegionTreeView/HydroRegionTreeView.cs
@@ -117,7 +117,6 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.HydroRegionTreeView
             {
                 new HydroRegionTreeViewNodePresenter(guiPlugin),
                 new HydroAreaTreeViewNodePresenter(guiPlugin),
-                new WasteWaterTreatmentPlantsTreeViewNodePresenter(guiPlugin),
                 new WasteWaterTreatmentPlantTreeViewNodePresenter(guiPlugin),
             };
 
