6fad2aca03355b407c10969d506359d2cfd835c6
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-17 17:29:47 +0200

    Clear IRetention D3DFMIQ-2083
    Former-commit-id: 348219a8f8b45e41edf5d045bd767b5ac0f44a67

--- src/DelftTools.Hydro/IRetention.cs
+++ src/DelftTools.Hydro/IRetention.cs
@@ -8,22 +8,5 @@ namespace DelftTools.Hydro
     /// </summary>
     public interface IRetention : IHydroNetworkFeature, IBranchFeature
     {
-        RetentionType Type { get; set; }
-        double StorageArea { get; set; }
-        double BedLevel { get; set; }
-        double LevelBL { get; set; }
-
-        /// <summary>
-        /// Storage bed definition
-        /// </summary>
-        IFunction Data { get; set; }
-
-        double StreetLevel { get; set; }
-        double StreetStorageArea { get; set; }
-
-        /// <summary>
-        /// Use storage as function of level
-        /// </summary>
-        bool UseTable { get; set; }
     }
 }
