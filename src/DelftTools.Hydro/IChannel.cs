50b59a5794f7ae796f65cfa67af2064c9b3bcbc6
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-17 17:26:54 +0200

    Clear IChannel D3DFMIQ-2083
    Former-commit-id: 57d9d14d0dae3ebbe5c8a981ab4b745906768c96

--- src/DelftTools.Hydro/IChannel.cs
+++ src/DelftTools.Hydro/IChannel.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using DelftTools.Hydro.Structures;
 using DelftTools.Utils;
 using GeoAPI.Extensions.Networks;
 
@@ -8,22 +6,9 @@ namespace DelftTools.Hydro
     public interface IChannel : IBranch, IHydroNetworkFeature, IItemContainer
     {
         //reintroducing members for databinding.
-        string Name { get; set; }
+        string Name { get; }
 
-        /// <summary>
-        /// Name as defined in sobek under name. Since Name is taken by ID name is stored here
-        /// </summary>
-        string LongName { get; set; }
-
-        double Length { get; set; }
-
-        INode Source { get; set; }
+        INode Source { set; }
         INode Target { get; set; }
-        IEnumerable<IStructure1D> Structures { get; }
-        IEnumerable<IPump> Pumps { get; }
-        IEnumerable<IWeir> Weirs { get; }
-        IEnumerable<IGate> Gates { get; }
-        IEnumerable<LateralSource> BranchSources { get; }
-        IEnumerable<ObservationPoint> ObservationPoints { get; }
     }
 }
