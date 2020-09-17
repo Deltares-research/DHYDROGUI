5fd4ddf7da15ea63adac8eb3436e804739619dde
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-17 16:32:05 +0200

    Clear implementation of HydroNetwork D3DFMIQ-2083
    Former-commit-id: 2824ea15f336a793bc538c2f9ac90265ca7ef6d6

--- src/DelftTools.Hydro/HydroNetwork.cs
+++ src/DelftTools.Hydro/HydroNetwork.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using DelftTools.Hydro.Structures;
 using DelftTools.Utils.Aop;
 using DelftTools.Utils.Collections.Generic;
 using GeoAPI.Extensions.Feature;
 using GeoAPI.Extensions.Networks;
-using NetTopologySuite.Extensions.Coverages;
 using NetTopologySuite.Extensions.Networks;
 
 namespace DelftTools.Hydro
@@ -19,92 +16,11 @@ namespace DelftTools.Hydro
     [Entity]
     public partial class HydroNetwork : Network, IHydroNetwork
     {
-        private IEventedList<Route> routes;
+        public override IEventedList<IBranch> Branches { get; set; }
 
+        public override IEventedList<INode> Nodes { get; set; }
 
-        public HydroNetwork()
-        {
-            Name = "network1";
-           Routes = new EventedList<Route>();
-
-            Links = new EventedList<HydroLink>();
-            SubRegions = new EventedList<IRegion>();
-        }
-
-      public virtual IEventedList<Route> Routes
-        {
-            get => routes;
-            protected set
-            {
-                if (routes != null)
-                {
-                    routes.CollectionChanged -= RoutesCollectionChanged;
-                }
-
-                routes = value;
-
-                if (routes != null)
-                {
-                    routes.CollectionChanged += RoutesCollectionChanged;
-                }
-            }
-        }
-
-       public override IEventedList<IBranch> Branches
-        {
-            get => base.Branches;
-            set
-            {
-                base.Branches = value;
-
-                Pipes = Branches.OfType<IPipe>();
-                Channels = Branches.OfType<IChannel>();
-
-                // always use filter to Interface or multiple data editor will fail on Cast<T>; reason unclear; too much smart linqing
-
-                Structures = BranchFeatures.OfType<IStructure1D>(); // TODO: join with node features (manholes)
-                CompositeBranchStructures = BranchFeatures.OfType<ICompositeBranchStructure>();
-
-                Pumps = BranchFeatures.OfType<IPump>();
-                Weirs = BranchFeatures.OfType<IWeir>();
-                Gates = BranchFeatures.OfType<IGate>();
-                Gullies = BranchFeatures.OfType<IGully>();
-                ExtraResistances = BranchFeatures.OfType<IExtraResistance>();
-                LateralSources = BranchFeatures.OfType<ILateralSource>();
-                Retentions = BranchFeatures.OfType<IRetention>();
-                ObservationPoints = BranchFeatures.OfType<IObservationPoint>();
-            }
-        }
-
-        public override IEventedList<INode> Nodes
-        {
-            get => base.Nodes;
-            set
-            {
-                base.Nodes = value;
-
-                Manholes = NodeFeatures.OfType<IManhole>();
-                HydroNodes = Nodes.OfType<IHydroNode>();
-            }
-        }
-
-        public virtual IEnumerable<IHydroNode> HydroNodes { get; protected set; }
-
-        public virtual IEnumerable<IPipe> Pipes { get; protected set; }
         public virtual IEnumerable<IChannel> Channels { get; protected set; }
-
-        public virtual IEnumerable<IManhole> Manholes { get; protected set; }
-
-        public virtual IEnumerable<IStructure1D> Structures { get; protected set; }
-        public virtual IEnumerable<ICompositeBranchStructure> CompositeBranchStructures { get; protected set; } 
-        public virtual IEnumerable<IPump> Pumps { get; protected set; }
-        public virtual IEnumerable<IWeir> Weirs { get; protected set; }
-        public virtual IEnumerable<IGate> Gates { get; protected set; }
-        public virtual IEnumerable<IGully> Gullies { get; protected set; }
-        public virtual IEnumerable<IExtraResistance> ExtraResistances { get; protected set; }
-        public virtual IEnumerable<ILateralSource> LateralSources { get; protected set; }
-        public virtual IEnumerable<IRetention> Retentions { get; protected set; }
-        public virtual IEnumerable<IObservationPoint> ObservationPoints { get; protected set; }
 
         public virtual IEventedList<IRegion> SubRegions { get; set; }
 
@@ -113,19 +29,11 @@ namespace DelftTools.Hydro
         [Aggregation]
         public virtual IRegion Parent { get; set; }
 
-        public virtual IEnumerable<IHydroObject> AllHydroObjects =>
-            Nodes.Cast<IHydroObject>()
-                 .Concat(Branches.Cast<IHydroObject>())
-                 .Concat(BranchFeatures.Cast<IHydroObject>())
-                 .Concat(NodeFeatures.Cast<IHydroObject>());
+        public virtual IEnumerable<IHydroObject> AllHydroObjects { get; }
 
         public virtual IEventedList<HydroLink> Links { get; set; }
 
-        public new virtual bool EditWasCancelled
-        {
-            get => base.EditWasCancelled;
-            set => base.EditWasCancelled = value;
-        }
+        public new virtual bool EditWasCancelled { get; }
 
         public override string ToString()
         {
@@ -134,41 +42,12 @@ namespace DelftTools.Hydro
 
         public virtual IEnumerable<object> GetDirectChildren()
         {
-            foreach (Route route in Routes)
-            {
-                yield return route;
-            }
-
-            foreach (IChannel channel in Channels)
-            {
-                yield return channel;
-            }
-
-            foreach (IHydroNode node in HydroNodes)
-            {
-                yield return node;
-            }
+            yield break;
         }
 
         public override object Clone()
         {
-            var clone = (HydroNetwork) base.Clone();
-            
-            foreach (Route route in Routes)
-            {
-                var clonedRoute = (Route) route.Clone();
-                NetworkCoverage.ReplaceNetworkForClone(clone, clonedRoute);
-                clone.Routes.Add(clonedRoute);
-            }
-
-           foreach (IRegion subRegion in SubRegions)
-            {
-                clone.SubRegions.Add((IHydroRegion) subRegion.Clone());
-            }
-
-            clone.Links = new EventedList<HydroLink>(Links);
-
-            return clone;
+            return (HydroNetwork) base.Clone();
         }
 
         public virtual HydroLink AddNewLink(IHydroObject source, IHydroObject target)
@@ -183,19 +62,7 @@ namespace DelftTools.Hydro
 
         public virtual bool CanLinkTo(IHydroObject source, IHydroObject target)
         {
-            return HydroRegion.CanLinkTo(source, target);
-        }
-
-        public override INode NewNode()
-        {
-            return new HydroNode();
-        }
-
-        public virtual INode GetNodeByName(string nodeName)
-        {
-            return string.IsNullOrEmpty(nodeName)
-                       ? null
-                       : Nodes.FirstOrDefault(n => n.Name == nodeName);
+            return false;
         }
     }
 }
