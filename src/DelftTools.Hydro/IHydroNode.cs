7231d54a4d6a74c76e007019e3703265a8e47b29
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-17 17:28:33 +0200

    Clear IHydroNode D3DFMIQ-2083
    Former-commit-id: 02a4f11704796a9da4cbdb35ac7ae8b2c82129f3

--- src/DelftTools.Hydro/IHydroNode.cs
+++ src/DelftTools.Hydro/IHydroNode.cs
@@ -5,15 +5,5 @@ namespace DelftTools.Hydro
 {
     public interface IHydroNode : INode, IItemContainer, IHydroNetworkFeature
     {
-        /// <summary>
-        /// The name of the hydro node
-        /// </summary>
-        /// <remarks> Reintroduced just for data binding in a feature attribute table (HydroNetworkEditor) </remarks>
-        string Name { get; set; }
-
-        /// <summary>
-        /// The long name of the hydro node
-        /// </summary>
-        string LongName { get; set; }
     }
 }
