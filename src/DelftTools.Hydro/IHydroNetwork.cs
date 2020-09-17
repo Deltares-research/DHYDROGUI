a9063c5c6b4920d2b1f002a39ca70708adcb5d3c
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-17 16:24:35 +0200

    Remove most of body IHydroNetwork D3DFMIQ-2083
    Former-commit-id: 0c83ff182296dad1728323876c9356977f93145e

--- src/DelftTools.Hydro/IHydroNetwork.cs
+++ src/DelftTools.Hydro/IHydroNetwork.cs
@@ -8,23 +8,6 @@ namespace DelftTools.Hydro
 {
     public interface IHydroNetwork : INetwork, IHydroRegion
     {
-        IEventedList<Route> Routes { get; }
-
-        IEnumerable<IHydroNode> HydroNodes { get; }
-        IEnumerable<IPipe> Pipes { get; }
         IEnumerable<IChannel> Channels { get; }
-        IEnumerable<IStructure1D> Structures { get; }
-        IEnumerable<ICompositeBranchStructure> CompositeBranchStructures { get; }
-        IEnumerable<IPump> Pumps { get; }
-        IEnumerable<IWeir> Weirs { get; }
-        IEnumerable<IGate> Gates { get; }
-        IEnumerable<ILateralSource> LateralSources { get; }
-        IEnumerable<IRetention> Retentions { get; }
-        IEnumerable<IObservationPoint> ObservationPoints { get; }
-        IEnumerable<IExtraResistance> ExtraResistances { get; }
-
-        IEnumerable<IManhole> Manholes { get; }
-        IEnumerable<IGully> Gullies { get; }
-        INode GetNodeByName(string nodeName);
     }
 }
