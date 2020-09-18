68dfc4fd0adbddadb82a16fb3c0e7420337c9675
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-18 12:56:37 +0200

    Clear IHydroNetworkFeature D3DFMIQ-2083
    Former-commit-id: 0b341920bc50179229ae5f7a2f6db36ef0dca256

--- src/DelftTools.Hydro/IHydroNetworkFeature.cs
+++ src/DelftTools.Hydro/IHydroNetworkFeature.cs
@@ -2,12 +2,5 @@ using GeoAPI.Extensions.Networks;
 
 namespace DelftTools.Hydro
 {
-    public interface IHydroNetworkFeature : INetworkFeature, IHydroObject
-    {
-        IHydroNetwork HydroNetwork { get; }
-
-        string Description { get; set; }
-
-        string LongName { get; set; }
-    }
+    public interface IHydroNetworkFeature : INetworkFeature, IHydroObject {}
 }
