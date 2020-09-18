5d6bf4607736d1d306042235a63578683b7adab1
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-09-18 10:05:10 +0200

    Clear IStructure1D D3DFMIQ-2083
    Former-commit-id: ad90bf1b1582f0fc8ec0b42b88594cb15512944f

--- src/DelftTools.Hydro/IStructure1D.cs
+++ src/DelftTools.Hydro/IStructure1D.cs
@@ -8,17 +8,10 @@ namespace DelftTools.Hydro
     /// </summary>
     public interface IStructure1D : IStructure, IHydroNetworkFeature, IBranchFeature //TODO : get this inheritance out
     {
-        ICompositeBranchStructure ParentStructure { get; set; }
-
-        // TODO: why?
-        IChannel Channel { get; set; }
-
         /// <summary>
         /// Y offset relative in the profile. This value is used by the structure view to display
         /// the structure in the cross section. It is not used by the 1d model engine.
         /// </summary>
         double OffsetY { get; set; }
-
-        StructureType GetStructureType();
     }
 }
