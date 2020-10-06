4358c76fc339ee322283642b7609eb7ccb99b7c5
BeardedPlatypus <mwtegelaers@gmail.com>
2020-10-06 11:38:27 +0200

    Add WaveOutputDataNodePresenter D3DFMIQ-2282
    Former-commit-id: 7c1cd6dcfdd3d7f6568f7ee0857a44194de32d68

---
+++ src/DeltaShell.Plugins.FMSuite.Wave.Gui/NodePresenters/OutputData/WaveOutputDataNodePresenter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Drawing;
+using DelftTools.Controls;
+using DelftTools.Shell.Gui.Swf;
+using DeltaShell.Plugins.FMSuite.Common.Gui.Properties;
+using DeltaShell.Plugins.FMSuite.Wave.OutputData;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Gui.NodePresenters.OutputData
+{
+    /// <summary>
+    /// <see cref="WaveOutputDataNodePresenter"/> implements the node presenter
+    /// for the <see cref="IWaveOutputData"/>.
+    /// </summary>
+    public class WaveOutputDataNodePresenter : TreeViewNodePresenterBaseForPluginGui<IWaveOutputData>
+    {
+        private static readonly Bitmap outputFolderImage = Resources.folder_output;
+
+        public override void UpdateNode(ITreeNode parentNode, ITreeNode node, IWaveOutputData nodeData)
+        {
+            node.Text = "Output";
+            node.Image = outputFolderImage;
+        }
+
+        public override IEnumerable GetChildNodeObjects(IWaveOutputData parentNodeData, ITreeNode node)
+        {
+            // TODO: Child objects should go in here
+            // note that the creation of sub folders should be dependent on
+            // whether there exists actual data in these folders.
+            yield break;
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/Gui/NodePresenters/OutputData/WaveOutputDataNodePresenterTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using DelftTools.Controls;
+using DelftTools.Controls.Swf.TreeViewControls;
+using DeltaShell.Plugins.FMSuite.Wave.Gui.NodePresenters.OutputData;
+using DeltaShell.Plugins.FMSuite.Wave.OutputData;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.Wave.Tests.Gui.NodePresenters.OutputData
+{
+    [TestFixture]
+    public class WaveOutputDataNodePresenterTest
+    {
+        [Test]
+        public void Constructor_ExpectedResults()
+        {
+            // Call
+            var nodePresenter = new WaveOutputDataNodePresenter();
+
+            // Assert
+            Assert.That(nodePresenter, Is.InstanceOf<TreeViewNodePresenterBase<IWaveOutputData>>());
+        }
+
+        [Test]
+        public void UpdateNode_SetsCorrectNodeTextAndImage()
+        {
+            // Setup
+            var nodePresenter = new WaveOutputDataNodePresenter();
+
+            var parentNode = Substitute.For<ITreeNode>();
+            var node = Substitute.For<ITreeNode>();
+            var nodeData = Substitute.For<IWaveOutputData>();
+
+            // Call
+            nodePresenter.UpdateNode(parentNode, node, nodeData);
+
+            // Assert
+            Assert.That(node.Text, Is.EqualTo("Output"));
+            Assert.That(node.Image, Is.Not.Null);
+        }
+
+        [Test]
+        public void GetChildNodeObjects_ReturnsWaveOutputDataChildren()
+        {
+            // Setup
+            var nodePresenter = new WaveOutputDataNodePresenter();
+            var node = Substitute.For<ITreeNode>();
+            var nodeData = Substitute.For<IWaveOutputData>();
+
+            // Call
+            IEnumerable result = nodePresenter.GetChildNodeObjects(nodeData, node);
+
+            // Assert
+            // TODO: this should be updated once more child objects are added
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
