1cf4ca45c21a815cc47bfc24fa1f57cd8286f76b
Carlos Lubbers <carlos.lubbers@deltares.nl>
2020-10-06 15:54:24 +0200

    Cleanup D3DFMIQ-2236
    Former-commit-id: 7220fd33a89570256d14f1794fe08f62fa8e73c1

--- src/DeltaShell.NGHS.IO/FileReaders/DelftConfigXmlFileParser.cs
+++ src/DeltaShell.NGHS.IO/FileReaders/DelftConfigXmlFileParser.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using DeltaShell.Dimr.xsd;
 using DeltaShell.NGHS.Common.Logging;
 using DeltaShell.NGHS.IO.FileConverters;
 
