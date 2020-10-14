2b7c9609037acef23ad44ed55704429c9a7a254e
Pauline Belt <pauline.belt@deltares.nl>
2020-10-14 10:54:44 +0200

    Implemented review comment for documentation of ICoupledModel. Also updated filename of ICoupledModel, since it was still using the old class name. D3DFMIQ-2281
    Former-commit-id: a0a7221430df9aee803e8b81d935e4e70ab32bb8

--- src/DeltaShell.NGHS.Common/ICoupledModel.cs
+++ src/DeltaShell.NGHS.Common/ICoupledModel.cs
@@ -4,9 +4,12 @@ using DelftTools.Shell.Core.Workflow.DataItems;
 namespace DeltaShell.NGHS.Common
 {
     /// <summary>
-    /// Interface for models, which can be coupled
-    /// to other models.
+    /// Interface for coupling DataItems of different origins.
     /// </summary>
+    /// <remarks>
+    /// Currently, only used in addition to
+    /// <see cref="DelftTools.Shell.Core.Workflow.IModel"/>
+    /// </remarks>
     public interface ICoupledModel
     {
         /// <summary>
