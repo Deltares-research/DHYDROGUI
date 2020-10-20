0e94bf15a0c05bab047e35da58bd88fca734cd12
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2020-10-20 11:06:34 +0200

    Add RealTimeControlOutputRestartFileContextMenu shell D3DFMIQ-2294
    Former-commit-id: f97e7b3d12064a93974b9cafa9be0b36df1a55f2

---
+++ test/DeltaShell.Plugins.DelftModels.RealTimeControl.Tests/Gui/Restart/RealTimeControlOutputRestartFileContextMenuTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using DelftTools.Controls;
+using DeltaShell.NGHS.Common.IO.RestartFiles;
+using DeltaShell.Plugins.DelftModels.RealTimeControl.Gui.Restart;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Tests.Gui.Restart
+{
+    [TestFixture]
+    public class RealTimeControlOutputRestartFileContextMenuTest
+    {
+        [Test]
+        public void Constructor_RealTimeControlRestartFileNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var node = Substitute.For<ITreeNode>();
+
+            // Call
+            void Call() => new RealTimeControlOutputRestartFileContextMenu(null, node);
+
+            // Assert
+            var e = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("restartFile"));
+        }
+
+        [Test]
+        public void Constructor_NodeNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var restartFile = new RestartFile();
+
+            // Call
+            void Call() => new RealTimeControlOutputRestartFileContextMenu(restartFile, null);
+
+            // Assert
+            var e = Assert.Throws<ArgumentNullException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("node"));
+        }
+
+        [Test]
+        public void Constructor_NodeDoesNotBelongToModel_InitializesInstanceCorrectly()
+        {
+            // Setup
+            var restartFile = new RestartFile();
+            var node = Substitute.For<ITreeNode>();
+            node.Parent.Parent.Returns((ITreeNode) null);
+
+            // Call
+            var menu = new RealTimeControlOutputRestartFileContextMenu(restartFile, node);
+
+            // Assert
+            Assert.That(menu.ContextMenuStrip.Items, Is.Empty);
+        }
+    }
+}
