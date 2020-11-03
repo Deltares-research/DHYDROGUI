0cc65ffb8ee095c0cfe122b4707726b147168f6e
tegelaer <maarten.tegelaers@deltares.nl>
2020-11-03 07:28:35 +0100

    Add constructor test WawmFileFunctionStore D3DFMIQ-2283
    Former-commit-id: 47707410399335bdbbbd0738ed4fdf22c3fb2895

--- test/DeltaShell.Plugins.FMSuite.Wave.Tests/OutputData/WavmFileFunctionStoreTest.cs
+++ test/DeltaShell.Plugins.FMSuite.Wave.Tests/OutputData/WavmFileFunctionStoreTest.cs
@@ -1,4 +1,8 @@
-﻿using DelftTools.TestUtils;
+﻿using System.Linq;
+using System.Runtime.Remoting;
+using DelftTools.TestUtils;
+using DeltaShell.NGHS.IO.TestUtils;
+using DeltaShell.Plugins.FMSuite.Common.FunctionStores;
 using DeltaShell.Plugins.FMSuite.Wave.OutputData;
 using NUnit.Framework;
 
@@ -8,7 +12,68 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Tests.OutputData
     [Category(TestCategory.DataAccess)]
     public class WavmFileFunctionStoreTest
     {
+        private const string ncPath = "./WaveOutputDataHarvesterTest/wavm-Waves.nc";
+
         [Test]
+        [Category(TestCategory.DataAccess)]
+        public void Constructor_ExpectedResults()
+        {
+            // Setup
+            using (var tempDir = new TemporaryDirectory())
+            {
+                string localNcPath = tempDir.CopyTestDataFileToTempDirectory(ncPath);
+
+                // Call
+                var store = new WavmFileFunctionStore(localNcPath);
+
+                // Assert
+                Assert.That(store, Is.InstanceOf<FMNetCdfFileFunctionStore>());
+
+                Assert.That(store.DisableCaching, Is.True);
+
+                Assert.That(store.Grid, Is.Not.Null);
+                Assert.That(store.Grid.Size1, Is.EqualTo(3));
+                Assert.That(store.Grid.Size2, Is.EqualTo(3));
+
+                Assert.That(store.Functions.Count, Is.EqualTo(27));
+                string[] expectedFunctionNames =
+                {
+                    "hsign",
+                    "dir",
+                    "pdir",
+                    "period",
+                    "rtp",
+                    "depth",
+                    "veloc-x",
+                    "veloc-y",
+                    "transp-x",
+                    "transp-y",
+                    "dspr",
+                    "dissip",
+                    "leak",
+                    "qb",
+                    "ubot",
+                    "steepw",
+                    "wlength",
+                    "tps",
+                    "tm02",
+                    "tmm10",
+                    "dhsign",
+                    "drtm01",
+                    "setup",
+                    "fx",
+                    "fy",
+                    "windu",
+                    "windv",
+                };
+
+                string[] storeFunctionNames = store.Functions.Select(x => x.Name).ToArray();
+                Assert.That(storeFunctionNames, Is.EquivalentTo(expectedFunctionNames));
+            }
+        }
+
+        [Test]
+        [Category(TestCategory.DataAccess)]
         public void OpenWavmFileAndCheckGridAndFunctions()
         {
             string wavmPath = TestHelper.GetTestFilePath("output_wavm/wavm-wave.nc");
