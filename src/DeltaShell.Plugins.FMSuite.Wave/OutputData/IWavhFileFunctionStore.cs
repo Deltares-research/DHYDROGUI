dbee5252fc11410e9a3cf5089d5793201dcde493
Carlos Lubbers <carlos.lubbers@deltares.nl>
2020-12-10 14:24:22 +0100

    Improve xmldoc D3DFMIQ-2386
    Former-commit-id: e38ceb93d140739b9f98c583838a8611bbbfb199

--- src/DeltaShell.Plugins.FMSuite.Wave/OutputData/IWavhFileFunctionStore.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/OutputData/IWavhFileFunctionStore.cs
@@ -2,5 +2,8 @@ using DeltaShell.Plugins.FMSuite.Common.FunctionStores;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.OutputData
 {
+    /// <summary>
+    /// Interface for the wave his file function store.
+    /// </summary>
     public interface IWavhFileFunctionStore : IFMNetCdfFileFunctionStore {}
 }
--- src/DeltaShell.Plugins.FMSuite.Wave/OutputData/IWavmFileFunctionStore.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/OutputData/IWavmFileFunctionStore.cs
@@ -3,10 +3,13 @@ using NetTopologySuite.Extensions.Grids;
 
 namespace DeltaShell.Plugins.FMSuite.Wave.OutputData
 {
+    /// <summary>
+    /// Interface for the wave map file function store.
+    /// </summary>
     public interface IWavmFileFunctionStore : IFMNetCdfFileFunctionStore
     {
         /// <summary>
-        /// Gets the grid of this <see cref="WavmFileFunctionStore"/>.
+        /// Gets the grid of this <see cref="IWavmFileFunctionStore"/>.
         /// </summary>
         CurvilinearGrid Grid { get; }
     }
