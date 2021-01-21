10d2e644c64c688693e2728bb29d1e045a16fcbe
tegelaer <maarten.tegelaers@deltares.nl>
2021-01-21 06:39:25 +0100

    Add new Pump implementation D3DFMIQ-288
    Former-commit-id: d42d99d710fad99c84c5ac976083c773693cca8b

---
+++ src/DelftTools.Hydro/Area/Objects/IPump.cs
@@ -0,0 +1,30 @@
+﻿using DelftTools.Functions;
+using DelftTools.Utils;
+
+namespace DelftTools.Hydro.Area.Objects
+{
+    /// <summary>
+    /// <see cref="IPump"/> defines a single pump located in an <see cref="HydroArea"/>.
+    /// </summary>
+    /// <remarks>
+    /// Deriving classes of <see cref="IPump"/> are expected to have
+    /// a <see cref="Utils.Aop.EntityAttribute"/>.
+    /// </remarks>
+    public interface IPump : INameable, IGroupableFeature
+    {
+        /// <summary>
+        /// If true, <see cref="CapacityTimeSeries"/> is used. Otherwise <see cref="Capacity"/> is used.
+        /// </summary>
+        bool UseCapacityTimeSeries { get; set; }
+
+        /// <summary>
+        /// Gets or sets the capacity of this <see cref="IPump"/>.
+        /// </summary>
+        double Capacity { get; set; }
+
+        /// <summary>
+        /// Gets the capacity time series of this <see cref="IPump"/>.
+        /// </summary>
+        TimeSeries CapacityTimeSeries { get; }
+    }
+}
---
+++ src/DelftTools.Hydro/Area/Objects/Pump.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using DelftTools.Functions;
+using DelftTools.Hydro.Structures;
+using DelftTools.Utils.Aop;
+using DelftTools.Utils.Data;
+using DelftTools.Utils.Guards;
+using GeoAPI.Extensions.Feature;
+using GeoAPI.Geometries;
+
+namespace DelftTools.Hydro.Area.Objects
+{
+    /// <summary>
+    /// <see cref="Pump"/> implements a 2D pump which is
+    /// contained in the <see cref="HydroArea"/>.
+    /// </summary>
+    /// <seealso cref="Unique{T}" />
+    /// <seealso cref="IPump" />
+    [Entity]
+    public sealed class Pump : Unique<long>, IPump
+    {
+        private string groupName;
+
+        /// <summary>
+        /// Creates a new <see cref="Pump"/>.
+        /// </summary>
+        public Pump() : this(HydroTimeSeriesFactory.CreateTimeSeries("Capacity", 
+                                                                     "Capacity", 
+                                                                     "m3/s")) { }
+
+        private Pump(TimeSeries capacityTimeSeries)
+        {
+            Ensure.NotNull(capacityTimeSeries, nameof(capacityTimeSeries));
+            CapacityTimeSeries = capacityTimeSeries;
+        }
+
+        public object Clone()
+        {
+            return new Pump((TimeSeries) CapacityTimeSeries.Clone())
+            {
+                GroupName = GroupName,
+                Geometry = (IGeometry) Geometry.Clone(),
+                Attributes = (IFeatureAttributeCollection) Attributes.Clone(),
+                Name = Name,
+                IsDefaultGroup = IsDefaultGroup,
+                UseCapacityTimeSeries = UseCapacityTimeSeries,
+                Capacity = Capacity,
+            };
+        }
+        public IGeometry Geometry { get; set; }
+
+        [DisplayName("Group name")]
+        [FeatureAttribute(Order = 1)]
+        public string GroupName
+        {
+            get => groupName;
+            set => groupName = GroupableFeatureHelper.SetGroupableFeatureGroupName(value);
+        }
+
+        public bool IsDefaultGroup { get; set; } = false;
+
+        [DisplayName("Name")]
+        [FeatureAttribute(Order = 2)]
+
+        public string Name { get; set; } = "Pump";
+
+        public bool UseCapacityTimeSeries { get; set; } = false;
+
+        [DisplayName("Capacity")]
+        [FeatureAttribute(Order = 3)]
+        public double Capacity { get; set; }
+
+        public TimeSeries CapacityTimeSeries { get; }
+
+        [NoNotifyPropertyChange]
+        public IFeatureAttributeCollection Attributes { get; set; }
+    }
+}
