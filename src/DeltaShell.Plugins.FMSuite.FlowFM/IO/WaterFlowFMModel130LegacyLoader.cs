5b13a36ec51cbc57d4876a7c74efaa5d3a171855
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-01-28 09:12:35 +0100

    Add LegacyLoader for D-Flow FM plugin version 1.3.0 D3DFMIQ-2504
    Former-commit-id: a1cbb69faf9fa63708fe0b71f7458e7ab9631789

---
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/WaterFlowFMModel130LegacyLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using DelftTools.Shell.Core;
+using DelftTools.Shell.Core.Dao;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
+{
+    /// <summary>
+    /// Legacy loader for <see cref="FlowFMApplicationPlugin"/> version 1.3.0.
+    /// This legacy loader removes the data items for the spatial coverages from the data base.
+    /// </summary>
+    /// <seealso cref="LegacyLoader"/>
+    public class WaterFlowFMModel130LegacyLoader : LegacyLoader
+    {
+        public override void OnAfterInitialize(object entity, IDbConnection dbConnection)
+        {
+            base.OnAfterInitialize(entity, dbConnection);
+        }
+
+        public override void OnAfterProjectMigrated(Project project)
+        {
+            base.OnAfterProjectMigrated(project);
+        }
+    }
+}
