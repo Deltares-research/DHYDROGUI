48b78139327047847ae9bfe31741842620a97c36
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-01-28 10:21:46 +0100

    Move methods from WaveDirectoryStructureMigrationHelper to MigrationHelper D3DFMIQ-2504
    Former-commit-id: 01c8686db75fba7a8fdc6d76c35dc24c5289f382

--- src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/WaveDirectoryStructureMigrationHelper.cs
+++ src/DeltaShell.Plugins.FMSuite.Wave/Migrations/1.1.0.0/WaveDirectoryStructureMigrationHelper.cs
@@ -22,38 +22,6 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0
         private static readonly ILog log = LogManager.GetLogger(typeof(WaveDirectoryStructureMigrationHelper));
 
         /// <summary>
-        /// Try and parse the database path from the provided <paramref name="connectionString"/>.
-        /// </summary>
-        /// <param name="connectionString">The connection string.</param>
-        /// <param name="databasePath">The database path.</param>
-        /// <returns>
-        /// <c>True</c> if a database path could be retrieved and stored in
-        /// <paramref name="databasePath"/>; <c>False</c> if no database path
-        /// could be retrieved and null is stored in <paramref name="databasePath"/>.
-        /// </returns>
-        public static bool TryParseDatabasePath(string connectionString, out string databasePath)
-        {
-            databasePath = null;
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                return false;
-            }
-
-            string[] keyValuePairs = connectionString.Split(';');
-
-            foreach (string keyValueString in keyValuePairs)
-            {
-                if (TryParseDataSource(keyValueString, out string parsedPath))
-                {
-                    databasePath = parsedPath;
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        /// <summary>
         /// Gets an unique temporary migration directory name for the provided
         /// <paramref name="srcDirectory"/>.
         /// </summary>
@@ -122,23 +90,6 @@ namespace DeltaShell.Plugins.FMSuite.Wave.Migrations._1._1._0._0
             RenameNewModelDirectory(temporaryDirectory, origModelDirectoryInfo.Name);
 
             RemoveOldExplicitWorkingDirectory(origModelDirectoryInfo);
-        }
-
-        private static bool TryParseDataSource(string keyValueString, out string parsedDataSourcePath)
-        {
-            parsedDataSourcePath = null;
-
-            string[] keyValuePair = keyValueString.Split('=');
-            string key = keyValuePair[0].Trim();
-            string value = keyValuePair[1].Trim();
-
-            if (key != "Data Source")
-            {
-                return false;
-            }
-
-            parsedDataSourcePath = value;
-            return true;
         }
 
         private static DirectoryInfo CreateToTemporaryDirectory(DirectoryInfo oldModelDirectory)
