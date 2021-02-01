acce22ea657d0e8f2121781597f01e7c9b64d105
Tim van den Aardweg <Tim.vandenAardweg@deltares.nl>
2021-02-01 13:52:17 +0000

    FM1D2D-1117 Added testcase to reproduce issue.
    Former-commit-id: a5e997005c7039e61c9bf092af398db90e599326
    Former-commit-id: f26fb85d9d16017918b047f44ab3c440ed2fb8f3

---
+++ test/DeltaShell.Plugins.ImportExport.GWSW.Tests/NwrwSurfaceDataTest.cs
@@ -0,0 +1,33 @@
+﻿using DelftTools.Hydro;
+using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain.Concepts.Nwrw;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.ImportExport.GWSW.Tests
+{
+    [TestFixture]
+    public class NwrwSurfaceDataTest
+    {
+        [Test]
+        public void GivenCatchmentWithSurfaceAreaForSpecificSurfaceType_WhenAddingAdditionalSurfaceAreaForSameSurfaceType_ThenCorrectlySetsNewSurfaceArea()
+        {
+            // Given
+            var nwrwData = new NwrwData(new Catchment());
+
+            nwrwData.SurfaceLevelDict.Add(NwrwSurfaceType.ClosedPavedFlat, 123.456);
+
+            var surfaceData = new NwrwSurfaceData()
+            {
+                NwrwSurfaceType = NwrwSurfaceType.ClosedPavedFlat,
+                SurfaceArea = 456.123
+            };
+
+            // When
+            surfaceData.InitializeNwrwCatchmentModelData(nwrwData);
+
+            // Then
+            var actualSurfaceArea = nwrwData.SurfaceLevelDict[NwrwSurfaceType.ClosedPavedFlat];
+            var expectedSurfaceArea = 123.456 + 456.123;
+            Assert.That(actualSurfaceArea, Is.EqualTo(expectedSurfaceArea));
+        }
+    }
+}
