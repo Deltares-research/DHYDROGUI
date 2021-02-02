7f399c2f18d39edd5a6e3fd25a823f2e0203a45b
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-02-02 10:49:34 +0100

    Add TracerVariable D3DFMIQ-1486
    Former-commit-id: 4584e4afb53bb10dd4966d744357464ce8a46fc6

---
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/FeatureData/SourcesAndSinks/TracerVariableTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DeltaShell.Plugins.FMSuite.FlowFM.FeatureData.SourcesAndSinks;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.FeatureData.SourcesAndSinks
+{
+    [TestFixture]
+    public class TracerVariableTest
+    {
+        [Test]
+        public void Constructor_InitializesInstanceCorrectly()
+        {
+            // Call
+            var variable = new TracerVariable("Some name");
+
+            // Assert
+            Assert.That(variable.Name, Is.EqualTo("Some name"));
+            Assert.That(variable.Unit.Name, Is.EqualTo("kilograms per cubic meter"));
+            Assert.That(variable.Unit.Symbol, Is.EqualTo("kg/m3"));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void Constructor_ArgumentNullOrEmpty_ThrowsArgumentException(string name)
+        {
+            // Call
+            void Call() => new TracerVariable(name);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("name"));
+        }
+    }
+}
