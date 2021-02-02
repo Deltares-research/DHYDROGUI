d167647b37c64fcddd093b789ec56c909cba3dbd
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-02-02 11:04:10 +0100

    Add SourceAndSinkFunction D3DFMIQ-1486
    Former-commit-id: ae538ccd6b9ee708ac179ab61e309936928ed880

---
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/FeatureData/SourcesAndSinks/SourceAndSinkFunction.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using DelftTools.Functions;
+using DelftTools.Functions.Generic;
+using DelftTools.Units;
+using DelftTools.Utils.Guards;
+using DeltaShell.NGHS.Common.Utils;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.FeatureData.SourcesAndSinks
+{
+    /// <summary>
+    /// This represent the function of the <see cref="SourceAndSink"/>.
+    /// The variable order of the function always remains the same:
+    /// Discharge | Salinity | Temperature | Sediment Fractions | Secondary Flow | Tracers
+    /// </summary>
+    /// <remarks>
+    /// This variables of this function should not be changed from outside this class.
+    /// </remarks>
+    public sealed class SourceAndSinkFunction : Function, ISourceAndSinkFunction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SourceAndSinkFunction"/> class.
+        /// </summary>
+        public SourceAndSinkFunction()
+        {
+            Arguments.Add(new Variable<DateTime>(SourceSinkVariableInfo.TimeVariableName) {DefaultValue = DateTime.Today});
+
+            AddVariable(SourceSinkVariableInfo.DischargeVariableName, SourceSinkVariableInfo.DischargeUnitDescription,
+                        SourceSinkVariableInfo.DischargeUnitSymbol);
+            AddVariable(SourceSinkVariableInfo.SalinityVariableName, SourceSinkVariableInfo.SalinityUnitDescription,
+                        SourceSinkVariableInfo.SalinityUnitSymbol);
+            AddVariable(SourceSinkVariableInfo.TemperatureVariableName, SourceSinkVariableInfo.TemperatureUnitDescription,
+                        SourceSinkVariableInfo.TemperatureUnitSymbol);
+            AddVariable(SourceSinkVariableInfo.SecondaryFlowVariableName, SourceSinkVariableInfo.SecondaryFlowUnitDescription,
+                        SourceSinkVariableInfo.SecondaryFlowUnitSymbol);
+        }
+
+        /// <summary>
+        /// Removes the sediment fraction variable with the specified <paramref name="name"/> from the function.
+        /// </summary>
+        /// <param name="name">The name of the sediment fraction.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="name"/> is <c>null</c> or empty.
+        /// </exception>
+        public void RemoveSedimentFraction(string name)
+        {
+            Ensure.NotNullOrEmpty(name, nameof(name));
+
+            var variable = GetVariable<SedimentFractionVariable>(name);
+            Components.Remove(variable);
+        }
+
+        /// <summary>
+        /// Removes the tracer variable with the specified <paramref name="name"/> from the function.
+        /// </summary>
+        /// <param name="name">The name of the tracer.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="name"/> is <c>null</c> or empty.
+        /// </exception>
+        public void RemoveTracer(string name)
+        {
+            Ensure.NotNullOrEmpty(name, nameof(name));
+
+            var variable = GetVariable<TracerVariable>(name);
+            Components.Remove(variable);
+        }
+
+        /// <summary>
+        /// Adds a new tracer variable to the function.
+        /// </summary>
+        /// <param name="name">The name of the tracer.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="name"/> is <c>null</c> or empty.
+        /// </exception>
+        public void AddTracer(string name)
+        {
+            Components.Add(new TracerVariable(name));
+        }
+
+        /// <summary>
+        /// Adds a new sediment fraction variable to the function.
+        /// </summary>
+        /// <param name="name">The name of the sediment fraction.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="name"/> is <c>null</c> or empty.
+        /// </exception>
+        public void AddSedimentFraction(string name)
+        {
+            int index = Components.FindIndex(c => c.Name == SourceSinkVariableInfo.SecondaryFlowVariableName);
+
+            Components.Insert(index, new SedimentFractionVariable(name));
+        }
+
+        private T GetVariable<T>(string name) where T : IVariable
+        {
+            return Components.OfType<T>().FirstOrDefault(v => v.Name == name);
+        }
+
+        private void AddVariable(string name, string unitDescription, string unitSymbol)
+        {
+            Components.Add(CreateVariable(name, unitDescription, unitSymbol));
+        }
+
+        private static IVariable CreateVariable(string name, string unitDescription, string unitSymbol)
+        {
+            return new Variable<double>(name) {Unit = new Unit(unitDescription, unitSymbol)};
+        }
+    }
+}
---
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/FeatureData/SourcesAndSinks/SourceAndSinkFunctionTest.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using DelftTools.Functions;
+using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Plugins.FMSuite.FlowFM.FeatureData.SourcesAndSinks;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests.FeatureData.SourcesAndSinks
+{
+    [TestFixture]
+    public class SourceAndSinkFunctionTest
+    {
+        [Test]
+        public void Constructor_InitializesInstanceCorrectly()
+        {
+            // Call
+            var function = new SourceAndSinkFunction();
+
+            // Assert
+            IEventedList<IVariable> arguments = function.Arguments;
+            Assert.That(arguments, Has.Count.EqualTo(1));
+            Assert.That(arguments[0].Name, Is.EqualTo("Time"));
+            Assert.That(arguments[0].DefaultValue, Is.EqualTo(DateTime.Today));
+
+            IEventedList<IVariable> components = function.Components;
+            Assert.That(components, Has.Count.EqualTo(4));
+            AssertComponent(components[0], "Discharge", "cubic meters per second", "m3/s");
+            AssertComponent(components[1], "Salinity", "parts per trillion", "ppt");
+            AssertComponent(components[2], "Temperature", "degree celsius", "°C");
+            AssertComponent(components[3], "Secondary Flow", "meters per second", "m/s");
+        }
+
+        [Test]
+        public void AddTracer_AddsCorrectVariable()
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+
+            // Call
+            function.AddTracer("Some Tracer 1");
+            function.AddTracer("Some Tracer 2");
+            function.AddTracer("Some Tracer 3");
+
+            // Assert
+            Assert.That(function.Components, Has.Count.EqualTo(7));
+            AssertComponent(function.Components[4], "Some Tracer 1", "kilograms per cubic meter", "kg/m3");
+            AssertComponent(function.Components[5], "Some Tracer 2", "kilograms per cubic meter", "kg/m3");
+            AssertComponent(function.Components[6], "Some Tracer 3", "kilograms per cubic meter", "kg/m3");
+        }
+
+        [Test]
+        public void AddSedimentFraction_AddsCorrectVariable()
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+
+            // Call
+            function.AddSedimentFraction("Some Sediment Fraction 1");
+            function.AddSedimentFraction("Some Sediment Fraction 2");
+            function.AddSedimentFraction("Some Sediment Fraction 3");
+
+            // Assert
+            Assert.That(function.Components, Has.Count.EqualTo(7));
+            AssertComponent(function.Components[3], "Some Sediment Fraction 1", "", "");
+            AssertComponent(function.Components[4], "Some Sediment Fraction 2", "", "");
+            AssertComponent(function.Components[5], "Some Sediment Fraction 3", "", "");
+        }
+
+        [Test]
+        public void RemoveTracer_RemovesCorrectVariable()
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+            function.AddTracer("Some Tracer 1");
+            function.AddTracer("Some Tracer 2");
+            function.AddTracer("Some Tracer 3");
+
+            // Call
+            function.RemoveTracer("Some Tracer 2");
+
+            // Assert
+            Assert.That(function.Components, Has.Count.EqualTo(6));
+            AssertComponent(function.Components[4], "Some Tracer 1", "kilograms per cubic meter", "kg/m3");
+            AssertComponent(function.Components[5], "Some Tracer 3", "kilograms per cubic meter", "kg/m3");
+        }
+
+        [Test]
+        public void RemoveSedimentFraction_RemovesCorrectVariable()
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+            function.AddSedimentFraction("Some Sediment Fraction 1");
+            function.AddSedimentFraction("Some Sediment Fraction 2");
+            function.AddSedimentFraction("Some Sediment Fraction 3");
+
+            // Call
+            function.RemoveSedimentFraction("Some Sediment Fraction 2");
+
+            // Assert
+            Assert.That(function.Components, Has.Count.EqualTo(6));
+            AssertComponent(function.Components[3], "Some Sediment Fraction 1", "", "");
+            AssertComponent(function.Components[4], "Some Sediment Fraction 3", "", "");
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void AddTracer_ArgumentNullOrEmpty_ThrowsArgumentException(string name)
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+
+            // Call
+            void Call() => function.AddTracer(name);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("name"));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void AddSedimentFraction_ArgumentNullOrEmpty_ThrowsArgumentException(string name)
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+
+            // Call
+            void Call() => function.AddSedimentFraction(name);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("name"));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void RemoveTracer_ArgumentNullOrEmpty_ThrowsArgumentException(string name)
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+
+            // Call
+            void Call() => function.RemoveTracer(name);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("name"));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void RemoveSedimentFraction_ArgumentNullOrEmpty_ThrowsArgumentException(string name)
+        {
+            // Setup
+            var function = new SourceAndSinkFunction();
+
+            // Call
+            void Call() => function.RemoveSedimentFraction(name);
+
+            // Assert
+            var e = Assert.Throws<ArgumentException>(Call);
+            Assert.That(e.ParamName, Is.EqualTo("name"));
+        }
+
+        private static void AssertComponent(IVariable component, string name, string unitDescription, string unitSymbol)
+        {
+            Assert.That(component.Name, Is.EqualTo(name));
+            Assert.That(component.Unit.Name, Is.EqualTo(unitDescription));
+            Assert.That(component.Unit.Symbol, Is.EqualTo(unitSymbol));
+        }
+    }
+}
