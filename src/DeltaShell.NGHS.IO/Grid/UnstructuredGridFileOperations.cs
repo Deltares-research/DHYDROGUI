41c4392a7fbeff165b0a565bb3f7a22fe8e4e93c
Dennis Tang <dennis.tang@deltares.nl>
2021-02-05 13:54:29 +0100

    D3DFMIQ-2128 Proof of concept to reduce loading times: - Introduce class that stores the retrieved GridApiDataSet.DataConventions
    Former-commit-id: 9481150773aa20053c8b862dc25c13f9389d1df8

---
+++ src/DeltaShell.NGHS.IO/Grid/UnstructuredGridFileOperations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using DeltaShell.NGHS.IO.Adapters;
+using DeltaShell.Plugins.SharpMapGis.ImportExport;
+using GeoAPI.Extensions.CoordinateSystems;
+using NetTopologySuite.Extensions.Grids;
+
+namespace DeltaShell.NGHS.IO.Grid
+{
+    public class UnstructuredGridFileOperations
+    {
+        private readonly string filePath;
+        private readonly GridApiDataSet.DataSetConventions dataSetConventions;
+        
+        public UnstructuredGridFileOperations(string filePath)
+        {
+            this.filePath = filePath;
+            dataSetConventions = GetConvention(filePath);
+        }
+
+        public UnstructuredGrid GetGrid(bool loadFlowLinksAndCells = false,
+                                        bool callCreateCells = false)
+        {
+            switch (dataSetConventions)
+            {
+                case GridApiDataSet.DataSetConventions.CONV_UGRID:
+                    using (var fmUGridAdapter = new UGridToUnstructuredGridAdapter(filePath))
+                    {
+                        return fmUGridAdapter.GetUnstructuredGridFromUGridMeshId(1, callCreateCells: callCreateCells);
+                    }
+                case GridApiDataSet.DataSetConventions.CONV_OTHER:
+                    return loadFlowLinksAndCells
+                               ? NetFileImporter.ImportModelGrid(filePath)
+                               : NetFileImporter.ImportGrid(filePath);
+                default:
+                    return null;
+            }
+        }
+        
+        public void DoIfUgrid(Action<UGridToUnstructuredGridAdapter> ugridAction)
+        {
+            if (dataSetConventions != GridApiDataSet.DataSetConventions.CONV_UGRID)
+            {
+                return;
+            }
+
+            using (var uGridAdaptor = new UGridToUnstructuredGridAdapter(filePath))
+            {
+                ugridAction(uGridAdaptor);
+            }
+        }
+        
+        public ICoordinateSystem GetCoordinateSystem()
+        {
+            switch (dataSetConventions)
+            {
+                case GridApiDataSet.DataSetConventions.CONV_UGRID:
+                    using (var uGrid = new UGrid(filePath))
+                    {
+                        if (!uGrid.IsInitialized())
+                        {
+                            uGrid.Initialize();
+                        }
+
+                        return uGrid.CoordinateSystem;
+                    }
+                case GridApiDataSet.DataSetConventions.CONV_OTHER:
+                    return NetFile.ReadCoordinateSystem(filePath);
+                default:
+                    return null;
+            }
+        }
+        
+        private static GridApiDataSet.DataSetConventions GetConvention(string path)
+        {
+            IUGridApi gridApi = GridApiFactory.CreateNew();
+            if (gridApi == null)
+            {
+                return GridApiDataSet.DataSetConventions.CONV_NULL;
+            }
+
+            using (gridApi)
+            {
+                GridApiDataSet.DataSetConventions convention;
+                int ierr = gridApi.GetConvention(path, out convention);
+                if (ierr != GridApiDataSet.GridConstants.NOERR)
+                {
+                    throw new Exception("Couldn't get the grid convention because of error number: " + ierr);
+                }
+
+                return convention;
+            }
+        }
+    }
+}
