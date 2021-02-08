b3500f2a55b50a9b7e92c2f6144ea66953f56464
Tim van den Aardweg <Tim.vandenAardweg@deltares.nl>
2021-02-08 09:27:42 +0000

    FM1D2D-1126 Fixed a bug where the log was filled with warnings.
    Former-commit-id: 5b33e8daff230e2d46fe13a48190a57d92e5f4b3
    Former-commit-id: ff04742bedc8927a2e915d1a305c47486230cfb5

--- src/DeltaShell.Plugins.GWSWImporter/SewerOutletCompartmentGenerator.cs
+++ src/DeltaShell.Plugins.GWSWImporter/SewerOutletCompartmentGenerator.cs
@@ -11,5 +11,23 @@ namespace DeltaShell.Plugins.ImportExport.GWSW
                 ? CreateCompartment<GwswStructureOutletCompartment>(gwswElement) 
                 : CreateCompartment<GwswPointOutletCompartment>(gwswElement);
         }
+
+        protected override void SetCompartmentProperties(Compartment compartment, GwswElement gwswElement)
+        {
+            if (!gwswElement.IsValidGwswCompartment()) return;
+
+            compartment.ManholeLength = 0.8d;
+            compartment.ManholeWidth = 0.8d;
+            compartment.FloodableArea = 100;
+            
+            double auxDouble;
+            var outletCompartment = compartment as OutletCompartment;
+            if (outletCompartment != null)
+            {
+                var surfaceWaterLevelAttribute = gwswElement.GetAttributeFromList(SewerStructureMapping.PropertyKeys.SurfaceWaterLevel);
+                if (surfaceWaterLevelAttribute.TryGetValueAsDouble(out auxDouble))
+                    outletCompartment.SurfaceWaterLevel = auxDouble;
+            }
+        }
     }
 }
