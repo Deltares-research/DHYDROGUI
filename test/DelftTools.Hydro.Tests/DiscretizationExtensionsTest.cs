19ea55deaf6d5ea66879a7ab929e970f8ee6fbc9
Ralph Peelen <ralph.peelen@deltares.nl>
2021-03-25 16:13:12 +0000

    FM1D2D-1329 : processed review remarks, added tests
    Former-commit-id: fae6d7089a2adf73bf990c863c7438ac7eb022fa
    Former-commit-id: 934a41090e6e078f6a0a61b617270da4fe0cef7b

--- test/DelftTools.Hydro.Tests/DiscretizationExtensionsTest.cs
+++ test/DelftTools.Hydro.Tests/DiscretizationExtensionsTest.cs
@@ -9,7 +9,7 @@ namespace DelftTools.Hydro.Tests
     public class DiscretizationExtensionsTest
     {
         [Test]
-        public void GivenDiscretization_GetDuplicatePointsOnManholes_ShouldReturnTheCorrectDuplicates()
+        public void GivenDiscretization_WhenRemoveDuplicatePointsOnManholes_ShouldReturnTheUniquePointsOnly()
         {
 
             // c1 (m1)
@@ -74,13 +74,92 @@ namespace DelftTools.Hydro.Tests
             }
 
             // Act
-            var duplicates = discretization.GetDuplicatePointsOnManholes().ToList();
-
+            discretization.UpdateNetworkLocations(discretization.Locations.Values, false);
+            
             // Assert
-            Assert.AreEqual(3, duplicates.Count);
-            Assert.AreEqual(new NetworkLocation(connection2, 0), duplicates[0]);
-            Assert.AreEqual(new NetworkLocation(connection3, 0), duplicates[1]);
-            Assert.AreEqual(new NetworkLocation(connection4, 0), duplicates[2]);
+            Assert.AreEqual(2, discretization.Locations.Values.Count);
+            Assert.AreEqual(new NetworkLocation(connection1, 0), discretization.Locations.Values[0]);
+            Assert.AreEqual(new NetworkLocation(connection1, connection1.Length), discretization.Locations.Values[1]);
+        }
+        
+        [Test]
+        public void GivenDiscretization_WhenRemoveDuplicatePointsOnManholesAndAddLaterExtraLocations_ShouldReturnTheUniquePointsOnly()
+        {
+
+            // c1 (m1)
+            //  |
+            //  |
+            //  |
+            //  V con 1
+            //  |
+            //  |
+            //  |
+            // c2 (m2) -------->-------- c4 (m3)
+            //  |           con2
+            //  V con 3
+            //  |
+            // c3 (m2)
+            //  |
+            //  |
+            //  |
+            //  V con 4
+            //  |
+            //  |
+            //  |
+            // c5 (m4)
+            //
+            // c = compartment
+            // m = manhole
+            // con = sewer connection
+
+            //Arrange
+            var manhole1 = new Manhole();
+            var manhole2 = new Manhole();
+            var manhole3 = new Manhole(); 
+            var manhole4 = new Manhole();
+
+            var compartment1 = new Compartment();
+            var compartment2 = new Compartment();
+            var compartment3 = new Compartment();
+            var compartment4 = new Compartment();
+            var compartment5 = new Compartment();
+
+            manhole1.Compartments.Add(compartment1);
+            manhole2.Compartments.Add(compartment2);
+            manhole2.Compartments.Add(compartment3);
+            manhole3.Compartments.Add(compartment4);
+            manhole4.Compartments.Add(compartment5);
+
+            var connection1 = new SewerConnection {Name = "Con1", SourceCompartment = compartment1, TargetCompartment = compartment2, Length = 10};
+            var connection2 = new SewerConnection {Name = "Con2", SourceCompartment = compartment2, TargetCompartment = compartment4, Length = 12};
+            var connection3 = new SewerConnection {Name = "Con3", SourceCompartment = compartment2, TargetCompartment = compartment3, Length = 1 };
+            var connection4 = new SewerConnection {Name = "Con4", SourceCompartment = compartment3, TargetCompartment = compartment5, Length = 14};
+
+            var network = new HydroNetwork();
+            network.Nodes.AddRange(new[] {manhole1, manhole2, manhole3, manhole4});
+            network.Branches.AddRange(new[] { connection1, connection2, connection3, connection4});
+
+            var discretization = new Discretization{Network = network};
+
+            foreach (var branch in network.Branches)
+            {
+                discretization.Locations.Values.Add(new NetworkLocation(branch, 0));
+                discretization.Locations.Values.Add(new NetworkLocation(branch, branch.Length));
+            }
+
+            var locations = discretization.Locations.Values.ToList();
+            locations.Add(new NetworkLocation(connection1, connection1.Length));
+            locations.Add(new NetworkLocation(connection1, connection1.Length/2));
+
+            // Act
+            discretization.UpdateNetworkLocations(locations);
+            
+            // Assert
+            Assert.AreEqual(3, discretization.Locations.Values.Count);
+            Assert.AreEqual(new NetworkLocation(connection1, 0), discretization.Locations.Values[0]);
+            Assert.AreEqual(new NetworkLocation(connection1, connection1.Length/2), discretization.Locations.Values[1]);
+            Assert.AreEqual(new NetworkLocation(connection1, connection1.Length), discretization.Locations.Values[2]);
+            
         }
     }
 }
