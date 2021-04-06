63cb3d13f8fd8f332d041b8f360b6b41de406aa5
Tim van den Aardweg <Tim.vandenAardweg@deltares.nl>
2021-04-06 06:53:45 +0000

    FM1D2D-1470: Added a RainfallRunoff file comparer Former-commit-id: 213360090b17425b1b1056d989b51bb8b87a42f2 Former-commit-id: 64ff8ca1f24049a84dd7ebfbf7ed6fdb8845072a

---
+++ test/DeltaShell.Plugins.DelftModels.HydroModel.Tests/Acceptance/Persistence/RainfallRunoffFileComparer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.DelftModels.HydroModel.Tests.Acceptance.Persistence
+{
+    /// <summary>
+    /// Helper class for comparing the contents of two Rainfall Runoff file directories.
+    /// </summary>
+    public static class RainfallRunoffFileComparer
+    {
+        /// <summary>
+        /// Compares the contents of two Rainfall Runoff file collections.
+        /// </summary>
+        /// <param name="expectedRainfallRunoffFiles">The file paths of the expected Rainfall Runoff files.</param>
+        /// <param name="actualRainfallRunoffFiles">The file paths of the actual Rainfall Runoff files.</param>
+        public static void Compare(string[] expectedRainfallRunoffFiles, string[] actualRainfallRunoffFiles)
+        {
+            var identical = true;
+            var overallErrorMessage = $"{Environment.NewLine}{FileComparerHelper.VerticalLine}";
+            
+            IEnumerable<string> actualRainfallRunoffFileNames = actualRainfallRunoffFiles.Select(Path.GetFileName);
+            IEnumerable<string> expectedFlowFmFileNames = expectedRainfallRunoffFiles.Select(Path.GetFileName);
+            string[] allFileNames = actualRainfallRunoffFileNames.Union(expectedFlowFmFileNames).ToArray();
+            
+            foreach (string fileName in allFileNames)
+            {
+                var linesToIgnore = new string[]
+                    {};
+
+                string expectedRainfallRunoffFile = expectedRainfallRunoffFiles.FirstOrDefault(f => Path.GetFileName(f).Equals(fileName));
+                string actualRainfallRunoffFile = actualRainfallRunoffFiles.FirstOrDefault(f => Path.GetFileName(f).Equals(fileName));
+
+                if (!FileComparerHelper.FileNameIsEqual(fileName, expectedRainfallRunoffFile, actualRainfallRunoffFile, ref overallErrorMessage))
+                {
+                    identical = false;
+                    continue;
+                }
+                
+                string errorMessage = string.Empty;
+
+                identical = FileComparerHelper.CompareFiles(expectedRainfallRunoffFile, actualRainfallRunoffFile, linesToIgnore, out errorMessage) && identical;
+                
+                if (!string.IsNullOrEmpty(errorMessage))
+                {
+                    overallErrorMessage += $"{errorMessage}{FileComparerHelper.VerticalLine}";
+                }
+            }
+            
+            if (!identical)
+            {
+                Assert.Fail(overallErrorMessage);
+            }
+        }
+    }
+}
