2d95f123c766928586fb1200fc98ed016f57580a
Tim van den Aardweg <Tim.vandenAardweg@deltares.nl>
2021-04-26 09:59:22 +0000

    FM1D2D-1500: Implemented review comments: Improved testcase. Former-commit-id: 79af893b36342b332b02b9db6ca5541077beab2f Former-commit-id: f047f558d655108730426c52bbd4316746adac49

--- test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/WaterFlowFMModelTest.cs
+++ test/DeltaShell.Plugins.FMSuite.FlowFM.Tests/WaterFlowFMModelTest.cs
@@ -2411,28 +2411,28 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.Tests
         public void GivenWaterFlowFMModel_WhenChangingRandomModelProperty_ThenAlwaysTriggersWaterFlowFMModelOnPropertyChanged()
         {
             // Setup
-            var random = new Random(707);
-            
             using (var model = new WaterFlowFMModel())
             {
                 WaterFlowFMModelDefinition modelDefinition = model.ModelDefinition;
-
-                int propertyCount = modelDefinition.Properties.Count;
-                int randomPropertyIndex = random.Next(propertyCount - 1);
-                WaterFlowFMProperty randomProperty = modelDefinition.Properties[randomPropertyIndex];
-                object randomPropertyValue = randomProperty.Value;
-
+                
                 int propertyChangedCounter = 0;
                 model.PropertyChanged += (sender, args) =>
                 {
                     propertyChangedCounter++;
                 };
 
-                // Call
-                modelDefinition.SetModelProperty(randomProperty.PropertyDefinition.MduPropertyName, randomPropertyValue.ToString());
-                
-                // Assert
-                Assert.That(propertyChangedCounter, Is.GreaterThan(0)); // Apparently, the event can be fired multiple times
+                foreach (WaterFlowFMProperty property in modelDefinition.Properties)
+                {
+                    string propertyName = property.PropertyDefinition.MduPropertyName;
+                    string defaultValue = property.PropertyDefinition.DefaultValueAsString;
+                    
+                    // Call
+                    modelDefinition.SetModelProperty(propertyName, defaultValue);
+                    
+                    // Assert
+                    Assert.That(propertyChangedCounter, Is.GreaterThan(0)); // Apparently, the event can be fired multiple times
+                    propertyChangedCounter = 0; // Reset counter
+                }
             }
         }
     }
