08e7528cc855b2e90bf26ed49a00b17006a2e23e
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-05-28 11:04:36 +0000

    FM1D2D-1602 Make sure output is not cleared when opening a project by setting the SuspendClearOutput to true..
    Former-commit-id: 9db75c8fc2cdb9875a4013e6fa85bee3a98b7395
    Former-commit-id: 59c825a395092a552366ff262ae189d44af3b352

--- src/DeltaShell.Plugins.DelftModels.HydroModel/HydroModelDataAccessListener.cs
+++ src/DeltaShell.Plugins.DelftModels.HydroModel/HydroModelDataAccessListener.cs
@@ -1,5 +1,8 @@
-﻿using DelftTools.Shell.Core.Dao;
+﻿using System.Collections.Generic;
+using DelftTools.Shell.Core.Dao;
+using DelftTools.Shell.Core.Workflow;
 using DeltaShell.NGHS.Common.Extensions;
+using DeltaShell.NGHS.Common.Utils;
 
 namespace DeltaShell.Plugins.DelftModels.HydroModel
 {
@@ -10,17 +13,42 @@ namespace DeltaShell.Plugins.DelftModels.HydroModel
             if (!(entity is HydroModel hydroModel))
                 return;
 
-            // Relink after loading
-            using (hydroModel.InEditMode("Linking items after loading"))
+            IList<bool> suspendClearOutput = new List<bool>();
+            
+            try
             {
-                hydroModel.RelinkDataItems();
-                hydroModel.RelinkHydroRegionLinks();
+                SuspendClearOutput(hydroModel, suspendClearOutput);
+
+                // Relink after loading
+                using (hydroModel.InEditMode("Linking items after loading"))
+                {
+                    hydroModel.RelinkDataItems();
+                    hydroModel.RelinkHydroRegionLinks();
+                }
+            }
+            finally
+            {
+                ResetSuspendClearOutput(hydroModel, suspendClearOutput);
             }
         }
-
+        
         public override object Clone()
         {
             return new HydroModelDataAccessListener { ProjectRepository = ProjectRepository };
+        }
+        
+        private static void SuspendClearOutput(HydroModel hydroModel, ICollection<bool> suspendClearOutput)
+        {
+            foreach (IModel model in hydroModel.Models)
+            {
+                suspendClearOutput.Add(model.SuspendClearOutputOnInputChange);
+                model.SuspendClearOutputOnInputChange = true;
+            }
+        }
+        
+        private static void ResetSuspendClearOutput(HydroModel hydroModel, IList<bool> suspendClearOutput)
+        {
+            (hydroModel.Models, suspendClearOutput).ForEach((m, s) => m.SuspendClearOutputOnInputChange = s);
         }
     }
 }
