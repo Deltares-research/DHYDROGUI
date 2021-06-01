09df027e2e46219af57683362ce5bc998bcfd13b
Hidde Elzinga <hidde.elzinga@deltares.nl>
2021-06-01 13:57:36 +0000

    FM1D2D-1400 Made ManholeView a reusable view
    Former-commit-id: f7d6e8917a55f2bd78cc02b5274f1add98c45310
    Former-commit-id: 0d877ec15df8492df0fefc2660d8ad1eb119f301

--- src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/SewerFeatureViews/ManholeView.xaml.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/Forms/SewerFeatureViews/ManholeView.xaml.cs
@@ -17,7 +17,7 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.SewerFeatureViews
     /// <summary>
     /// Interaction logic for ManholeView.xaml
     /// </summary>
-    public partial class ManholeView : UserControl, IView
+    public partial class ManholeView : UserControl, IReusableView
     {
         private Point startPoint;
         private Cursor customCursor;
@@ -28,13 +28,19 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.SewerFeatureViews
             ViewModel.DeselectItem = () => ManholeVisualisationControl.DeselectItem();
         }
 
-        #region IView implementation
-
         public object Data
         {
             get { return ViewModel.Manhole; }
             set { ViewModel.Manhole = (Manhole) value; }
         }
+
+        public bool Locked { get; set; }
+
+        public string Text { get; set; }
+
+        public Image Image { get; set; }
+        
+        public ViewInfo ViewInfo { get; set; }
 
         public void Dispose()
         {
@@ -46,15 +52,7 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.Forms.SewerFeatureViews
             ViewModel.SelectedItem = item;
         }
 
-        public string Text { get; set; }
-
-        public Image Image { get; set; }
-
-        public bool Visible { get; }
-
-        public ViewInfo ViewInfo { get; set; }
-
-        #endregion
+        public event EventHandler LockedChanged;
 
         private void ToolBoxMouseDown(object sender, MouseButtonEventArgs e)
         {
