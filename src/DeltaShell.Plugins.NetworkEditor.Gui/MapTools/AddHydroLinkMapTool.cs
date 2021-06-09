84558ff90e4ddf9e728745f2682a77fc903f746c
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-06-09 12:08:17 +0000

    FM1D2D-1487 Refactor: AddHydroLinkMapTool
    Former-commit-id: 0581767dfde75191fa5c9a42c0725d606ef04110
    Former-commit-id: e33bcc15a7f9231a5b1682e915eca60b5c33de7a

--- src/DeltaShell.Plugins.NetworkEditor.Gui/MapTools/AddHydroLinkMapTool.cs
+++ src/DeltaShell.Plugins.NetworkEditor.Gui/MapTools/AddHydroLinkMapTool.cs
@@ -2,11 +2,13 @@ using System;
 using System.Linq;
 using System.Windows.Forms;
 using DelftTools.Hydro;
+using DelftTools.Utils.Guards;
 using DeltaShell.NGHS.Common.Gui.Modals.Helpers;
 using DeltaShell.Plugins.NetworkEditor.Gui.Properties;
 using GeoAPI.CoordinateSystems.Transformations;
 using GeoAPI.Extensions.CoordinateSystems;
 using GeoAPI.Geometries;
+using SharpMap.Api.Editors;
 using SharpMap.Api.Layers;
 using SharpMap.CoordinateSystems.Transformations;
 using SharpMap.Extensions.CoordinateSystems;
@@ -40,7 +42,7 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.MapTools
         /// The name of the tool.
         /// </summary>
         public const string ToolName = "add hydro link";
-        
+
         private static readonly IRequestUserInputService<UserInput> userInputService = new RequestUserInputService<UserInput>();
         private static readonly Cursor cursor = MapCursors.CreateArrowOverlayCuror(Resources.Link);
 
@@ -48,38 +50,55 @@ namespace DeltaShell.Plugins.NetworkEditor.Gui.MapTools
         /// Initializes a new instance of the <see cref="AddHydroLinkMapTool"/> class.
         /// </summary>
         /// <param name="layerFilter"> The layer filter. </param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <paramref name="layerFilter"/> is <c>null</c>.
+        /// </exception>
         public AddHydroLinkMapTool(Func<ILayer, bool> layerFilter) : base(layerFilter, ToolName)
         {
-            AddNewFeature = (g, cs, sourecSr, targetSr, tool) =>
-            {
-                if (sourecSr.SnappedFeature is Catchment && targetSr.SnappedFeature is LateralSource)
-                {
-                    UserInput? result = userInputService.RequestUserInput(
-                        Resources.HydroRegionEditorMapTool_Overwriting_existing_later_source_flow_data,
-                        Resources.HydroRegionEditorMapTool_Connecting_hydro_link_removes_existing_data + Environment.NewLine +
-                        Resources.HydroRegionEditorMapTool_Do_you_want_to_continue);
+            Ensure.NotNull(layerFilter, nameof(layerFilter));
 
-                    if (result != UserInput.Continue)
-                    {
-                        return;
-                    }
-                }
-
-                // Find the correct link layer to add to
-                IHydroRegion region = HydroRegion.GetCommonRegion((IHydroObject) sourecSr.SnappedFeature, (IHydroObject) targetSr.SnappedFeature);
-                ILayer layer = tool.Layers.FirstOrDefault(l => Equals(l.DataSource.Features, region.Links));
-                if (layer == null)
-                {
-                    return;
-                }
-
-                layer.DataSource.Add(GetLocalGeometry(g, cs, layer.CoordinateSystem));
-                layer.RenderRequired = true;
-            };
+            AddNewFeature = AddNewHydroLink;
             Cursor = cursor;
         }
 
-        private static IGeometry GetLocalGeometry(IGeometry geometry, ICoordinateSystem sourceCoordinateSystem, ICoordinateSystem targetCoordinateSystem)
+        private static void AddNewHydroLink(IGeometry geometry, ICoordinateSystem coordinateSystem, SnapResult snappedSource, SnapResult snappedTarget, NewArrowLineTool tool)
+        {
+            if (snappedSource.SnappedFeature is Catchment && snappedTarget.SnappedFeature is LateralSource)
+            {
+                if (UserCancels())
+                {
+                    return;
+                }
+            }
+
+            if (!TryGetLinksLayer(snappedSource, snappedTarget, tool, out ILayer layer))
+            {
+                return;
+            }
+
+            IGeometry transformedGeometry = GetTransformedGeometry(geometry, coordinateSystem, layer.CoordinateSystem);
+            layer.DataSource.Add(transformedGeometry);
+            layer.RenderRequired = true;
+        }
+
+        private static bool TryGetLinksLayer(SnapResult snappedSource, SnapResult snappedTarget, IMapTool tool, out ILayer layer)
+        {
+            IHydroRegion region = HydroRegion.GetCommonRegion((IHydroObject) snappedSource.SnappedFeature, (IHydroObject) snappedTarget.SnappedFeature);
+            layer = tool.Layers.FirstOrDefault(l => Equals(l.DataSource.Features, region.Links));
+            return layer != null;
+        }
+
+        private static bool UserCancels()
+        {
+            UserInput? userInput = userInputService.RequestUserInput(
+                Resources.HydroRegionEditorMapTool_Overwriting_existing_later_source_flow_data,
+                Resources.HydroRegionEditorMapTool_Connecting_hydro_link_removes_existing_data + Environment.NewLine +
+                Resources.HydroRegionEditorMapTool_Do_you_want_to_continue);
+
+            return userInput != UserInput.Continue;
+        }
+
+        private static IGeometry GetTransformedGeometry(IGeometry geometry, ICoordinateSystem sourceCoordinateSystem, ICoordinateSystem targetCoordinateSystem)
         {
             if (sourceCoordinateSystem == null || targetCoordinateSystem == null ||
                 sourceCoordinateSystem == targetCoordinateSystem)
