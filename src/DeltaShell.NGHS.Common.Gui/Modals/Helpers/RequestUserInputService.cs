2ed7eacfb94f296217b95f9b466edf50435d326c
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-06-09 09:51:43 +0000

    FM1D2D-1487 Add implementation for requesting user input (RequestUserInputService, UserInputModelView)
    Former-commit-id: ee94bda6111131f4cfba15481b13bce6ddced6a7
    Former-commit-id: 5ae7f5ed7dfb16d9e04f732a290e60195bcc9294

---
+++ src/DeltaShell.NGHS.Common.Gui/Modals/Helpers/RequestUserInputService.cs
@@ -0,0 +1,36 @@
+using DelftTools.Utils.Guards;
+using DeltaShell.NGHS.Common.Gui.Modals.ViewModels;
+using DeltaShell.NGHS.Common.Gui.Modals.Views;
+
+namespace DeltaShell.NGHS.Common.Gui.Modals.Helpers
+{
+    /// <summary>
+    /// <see cref="RequestUserInputService{TEnum}"/> implements the interface with which
+    /// user input can be requested.
+    /// </summary>
+    /// <typeparam name="TEnum">The type of the enum.</typeparam>
+    /// <remarks>
+    /// <see cref="TEnum"/> is expected to be an enum.
+    /// </remarks>
+    /// <seealso cref="IRequestUserInputService{TEnum}" />
+    public sealed class RequestUserInputService<TEnum> : IRequestUserInputService<TEnum>
+        where TEnum : struct
+    {
+        public TEnum? RequestUserInput(string title, string text)
+        {
+            Ensure.NotNull(title, nameof(title));
+            Ensure.NotNull(text, nameof(text));
+
+            var modalData = new UserInputModalViewModel<TEnum>
+            {
+                Title = title,
+                Text = text,
+            };
+
+            var modal = new UserInputModalView(modalData);
+            modal.ShowDialog();
+
+            return modalData.Result;
+        }
+    }
+}
---
+++ src/DeltaShell.NGHS.Common.Gui/Modals/ViewModels/UserInputModalViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DeltaShell.NGHS.Common.Gui.Modals.ViewModels
+{
+    /// <summary>
+    /// <see cref="UserInputModalViewModel{TEnum}"/> implements the <see cref="UserInputModalViewModelBase"/>
+    /// as a generic class specifying the <typeparamref name="TEnum"/> used to define the options.
+    /// </summary>
+    /// <typeparam name="TEnum">The type of the enum.</typeparam>
+    /// <seealso cref="UserInputModalViewModelBase" />
+    public sealed class UserInputModalViewModel<TEnum> : UserInputModalViewModelBase
+        where TEnum : struct
+    {
+        public override Array UserInputOptions => Enum.GetValues(typeof(TEnum));
+
+        /// <summary>
+        /// Gets the <see cref="UserInputModalViewModelBase.InternalResult"/> as
+        /// the actual <typeparamref name="TEnum"/>.
+        /// </summary>
+        public TEnum? Result => (TEnum?)InternalResult;
+    }
+}
