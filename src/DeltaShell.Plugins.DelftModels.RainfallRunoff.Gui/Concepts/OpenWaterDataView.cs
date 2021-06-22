e83d653d302d5329b751a56daadf556aa2f4de12
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-06-22 09:12:18 +0000

    FM1D2D-1632 Add area textbox OpenWaterDataView and view model.
    Former-commit-id: 3a6c35869d6e230b77296a115c907e86612bccc8
    Former-commit-id: 9490867b84c78292477716c730d8f93a55dcc436

--- src/DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui/Concepts/OpenWaterDataView.cs
+++ src/DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui/Concepts/OpenWaterDataView.cs
@@ -2,49 +2,72 @@
 using System.Windows.Forms;
 using DelftTools.Controls;
 using DelftTools.Utils.Collections.Generic;
+using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain;
 using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain.Concepts;
 using DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Controls;
 
 namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 {
-    public partial class OpenWaterDataView : UserControl, IView, IRRMeteoStationAwareView
+    public partial class OpenWaterDataView : UserControl, IView, IRRMeteoStationAwareView, IRRUnitAwareView
     {
         private OpenWaterData data;
+        private OpenWaterDataViewModel viewModel;
+        private RainfallRunoffEnums.AreaUnit areaUnit;
 
         public OpenWaterDataView()
         {
             InitializeComponent();
+            RainfallRunoffFormsHelper.ApplyRealNumberFormatToDataBinding(this);
+        }
+
+        public RainfallRunoffEnums.AreaUnit AreaUnit
+        {
+            get => areaUnit;
+            set
+            {
+                viewModel.AreaUnit = value;
+                areaUnit = value;
+            }
+        }
+
+        public bool UseMeteoStations
+        {
+            set => catchmentMeteoStationSelection1.UseMeteoStations = value;
+        }
+
+        public IEventedList<string> MeteoStations
+        {
+            set => catchmentMeteoStationSelection1.MeteoStations = value;
         }
 
         public object Data
         {
-            get { return data; }
-            set 
+            get => data;
+            set
             {
                 if (data != null)
                 {
+                    openWaterDataViewModelBindingSource.DataSource = typeof(OpenWaterDataViewModel);
+
                     catchmentMeteoStationSelection1.CatchmentModelData = null;
                     catchmentMeteoStationSelection1.MeteoStations = null;
                 }
+
                 data = (OpenWaterData) value;
                 if (data != null)
                 {
                     Text = "Open water data: " + data.Name;
                     catchmentMeteoStationSelection1.CatchmentModelData = data;
+                    viewModel = new OpenWaterDataViewModel(data, AreaUnit);
+                    openWaterDataViewModelBindingSource.DataSource = viewModel;
                 }
             }
         }
 
         public Image Image { get; set; }
 
-        public void EnsureVisible(object item)
-        {
-        }
-
         public ViewInfo ViewInfo { get; set; }
 
-
-        public bool UseMeteoStations { set { catchmentMeteoStationSelection1.UseMeteoStations = value; } }
-        public IEventedList<string> MeteoStations { set { catchmentMeteoStationSelection1.MeteoStations = value; } }
+        public void EnsureVisible(object item) {}
     }
 }
