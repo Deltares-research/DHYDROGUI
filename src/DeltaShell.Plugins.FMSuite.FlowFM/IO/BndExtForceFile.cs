ad84ea695b5ae19c2ca3c472433046eccdbde2fd
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-06-23 09:06:11 +0000

    FM1D2D-1626 Remove now unused method parameters.
    Former-commit-id: 882cc20baa8f3191db5e3347d0261579d9d0144c
    Former-commit-id: a754235d7e95cb2d9df1117963a845cdeae6f288

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/BndExtForceFile.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/BndExtForceFile.cs
@@ -297,14 +297,7 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
             foreach (var fmMeteoField in fmMeteoFields)
             {
                 var quantityName = ExtForceQuantNames.MeteoQuantityNames[fmMeteoField.Quantity];
-
-                var pliFileName = fmMeteoField.FeatureData?.Feature is Feature2D
-                    ? existingPolylineFiles[(Feature2D)fmMeteoField.FeatureData.Feature]
-                    : null;
-
-                var fmMeteoLocationType =
-                    BcMeteoFileDataBuilder.FmMeteoLocationKernelNames[fmMeteoField.FmMeteoLocationType];
-                var bndBlock = CreateMeteoBlock(quantityName, fmMeteoLocationType, pliFileName, path);
+                var bndBlock = CreateMeteoBlock(quantityName, path);
 
                 resultingItems.Add(bndBlock);
             }
@@ -318,7 +311,7 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
 
             return resultingItems;
         }
-        private static DelftIniCategory CreateMeteoBlock(string quantity, string fmMeteoLocationType, string locationFilePath, string forcingFilePath)
+        private static DelftIniCategory CreateMeteoBlock(string quantity, string forcingFilePath)
         {
             var block = new DelftIniCategory(MeteoBlockKey);
             if (!string.IsNullOrEmpty(quantity))
