981e5fa84bcbc93fe8411fd403e8078a7b33ab27
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-06-25 11:21:16 +0000

    FM1D2D-1563 Move ImportSamplesSpatialOperationExtension to separate file.
    Former-commit-id: 3dff736709fa490ac61eddf8b9ac5d50423037ac
    Former-commit-id: e1a58b6feda710c9077f95329cba253b166e4cd7

--- src/DeltaShell.Plugins.FMSuite.FlowFM/IO/ExtForceFileHelper.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/IO/ExtForceFileHelper.cs
@@ -18,61 +18,11 @@ using log4net;
 using NetTopologySuite.Extensions.Features;
 using SharpMap;
 using SharpMap.Api.SpatialOperations;
-using SharpMap.Data.Providers;
 using SharpMap.SpatialOperations;
 
 namespace DeltaShell.Plugins.FMSuite.FlowFM.IO
 {
     // Hacky, but it does the job...
-    public class ImportSamplesSpatialOperationExtension : ImportSamplesOperation
-    {
-        public virtual double RelativeSearchCellSize { get; set; }
-        public virtual GridCellAveragingMethod AveragingMethod { get; set; }
-        public virtual int MinSamplePoints { get; set; }
-        public virtual SpatialInterpolationMethod InterpolationMethod { get; set; }
-        public virtual PointwiseOperationType Operand { get; set; }
-        
-        public ImportSamplesSpatialOperationExtension() : base(false)
-        {
-            RelativeSearchCellSize = 1;
-            MinSamplePoints = 1;
-            AveragingMethod = GridCellAveragingMethod.ClosestPoint;
-            InterpolationMethod = SpatialInterpolationMethod.Averaging;
-        }
-
-        public virtual DelftTools.Utils.Tuple<ImportSamplesOperation, InterpolateOperation> CreateOperations()
-        {
-            var importSamplesOperation = CreateImportSamplesOperation();
-
-            var interpolateOperation = new InterpolateOperation
-            {
-                Name = "Interpolate",
-                CoordinateSystem = CoordinateSystem,
-                Dirty = true,
-                Enabled = Enabled,
-                RelativeSearchCellSize = RelativeSearchCellSize,
-                MinNumSamples = MinSamplePoints,
-                GridCellAveragingMethod = AveragingMethod,
-                InterpolationMethod = InterpolationMethod,
-                OperationType = Operand
-            };
-            interpolateOperation.Mask.Provider = new FeatureCollection(new List<Feature>(), typeof(Feature));
-            interpolateOperation.LinkInput(InterpolateOperation.InputSamplesName, importSamplesOperation.Output);
-            return new DelftTools.Utils.Tuple<ImportSamplesOperation, InterpolateOperation>(importSamplesOperation, interpolateOperation);
-        }
-
-        protected virtual ImportSamplesOperation CreateImportSamplesOperation()
-        {
-            return new ImportSamplesOperation(false)
-            {
-                Name = Name,
-                CoordinateSystem = CoordinateSystem,
-                Dirty = true,
-                Enabled = Enabled,
-                FilePath = FilePath,
-            };
-        }
-    }
 
     // YAGNI (GvdO) merge into extforce file, or strip extforce file from all path/file logic, but now it is not clear who is doing what
     public static class ExtForceFileHelper
