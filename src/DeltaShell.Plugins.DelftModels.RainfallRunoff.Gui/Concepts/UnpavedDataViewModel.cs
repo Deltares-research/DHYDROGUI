776a82b317452e264b7c863d2b8674e892a7a654
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-06-28 18:50:39 +0000

    FM1D2D-1658 Cleanup UnpavedDataViewModel
    Former-commit-id: 06f2dfab37b7053c68ea5554134c2e8bcced98d5
    Former-commit-id: 53077eebe06a286313c6a3155d2273c8a3411991

--- src/DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui/Concepts/UnpavedDataViewModel.cs
+++ src/DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui/Concepts/UnpavedDataViewModel.cs
@@ -8,7 +8,7 @@ using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain.Concepts;
 
 namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 {
-    [Entity(FireOnCollectionChange=false)]
+    [Entity(FireOnCollectionChange = false)]
     public class UnpavedDataViewModel
     {
         private RainfallRunoffEnums.AreaUnit areaUnit;
@@ -23,7 +23,7 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public bool GroundWaterLevelIsConstant
         {
-            get { return Data.InitialGroundWaterLevelSource == UnpavedEnums.GroundWaterSourceType.Constant; }
+            get => Data.InitialGroundWaterLevelSource == UnpavedEnums.GroundWaterSourceType.Constant;
             set
             {
                 if (value)
@@ -35,29 +35,13 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public bool ModelRunningParallelWithFlow { get; set; }
 
-        private bool LinkedToRunoffBoundary
-        {
-            get { return Data.Catchment.Links.Any() && Data.Catchment.Links.First().Target is RunoffBoundary; }
-        }
+        public bool UseWaterLevelFromLinkedNode => LinkedToRunoffBoundary || LinkedToFlowNode && ModelRunningParallelWithFlow;
 
-        private bool LinkedToFlowNode
-        {
-            get { return Data.Catchment.Links.Any() && !LinkedToRunoffBoundary; }
-        }
-        
-        public bool UseWaterLevelFromLinkedNode
-        {
-            get { return LinkedToRunoffBoundary || (LinkedToFlowNode && ModelRunningParallelWithFlow); }
-        }
-
-        public bool OverwriteWaterLevelBoundaryEnabled
-        {
-            get { return !UseWaterLevelFromLinkedNode; }
-        }
+        public bool OverwriteWaterLevelBoundaryEnabled => !UseWaterLevelFromLinkedNode;
 
         public bool GroundWaterLevelIsFromLinkedNode
         {
-            get { return Data.InitialGroundWaterLevelSource == UnpavedEnums.GroundWaterSourceType.FromLinkedNode; }
+            get => Data.InitialGroundWaterLevelSource == UnpavedEnums.GroundWaterSourceType.FromLinkedNode;
             set
             {
                 if (value)
@@ -69,7 +53,7 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public bool GroundWaterLevelIsSeries
         {
-            get { return Data.InitialGroundWaterLevelSource == UnpavedEnums.GroundWaterSourceType.Series; }
+            get => Data.InitialGroundWaterLevelSource == UnpavedEnums.GroundWaterSourceType.Series;
             set
             {
                 if (value)
@@ -81,7 +65,7 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public bool SeepageIsConstant
         {
-            get { return Data.SeepageSource == UnpavedEnums.SeepageSourceType.Constant; }
+            get => Data.SeepageSource == UnpavedEnums.SeepageSourceType.Constant;
             set
             {
                 if (value)
@@ -93,7 +77,7 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public bool SeepageIsSeries
         {
-            get { return Data.SeepageSource == UnpavedEnums.SeepageSourceType.Series; }
+            get => Data.SeepageSource == UnpavedEnums.SeepageSourceType.Series;
             set
             {
                 if (value)
@@ -105,7 +89,7 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public bool SeepageIsH0Series
         {
-            get { return Data.SeepageSource == UnpavedEnums.SeepageSourceType.H0Series; }
+            get => Data.SeepageSource == UnpavedEnums.SeepageSourceType.H0Series;
             set
             {
                 if (value)
@@ -117,7 +101,7 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public RainfallRunoffEnums.AreaUnit AreaUnit
         {
-            get { return areaUnit; }
+            get => areaUnit;
             set
             {
                 areaUnit = value;
@@ -127,92 +111,69 @@ namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts
 
         public RainfallRunoffEnums.StorageUnit StorageUnit
         {
-            get { return Data.LandStorageUnit; }
-            set { Data.LandStorageUnit = value; }
+            get => Data.LandStorageUnit;
+            set => Data.LandStorageUnit = value;
         }
 
         public RainfallRunoffEnums.RainfallCapacityUnit InfiltrationCapacityUnit
         {
-            get { return Data.InfiltrationCapacityUnit; }
-            set { Data.InfiltrationCapacityUnit = value; }
+            get => Data.InfiltrationCapacityUnit;
+            set => Data.InfiltrationCapacityUnit = value;
         }
 
         public string AreaUnitLabel { get; private set; }
 
         public double TotalAreaForGroundWaterCalculations
         {
-            get
-            {
-                return RainfallRunoffUnitConverter.ConvertArea(RainfallRunoffEnums.AreaUnit.m2, AreaUnit,
-                                                               Data.TotalAreaForGroundWaterCalculations);
-            }
-            set
-            {
-                Data.TotalAreaForGroundWaterCalculations = RainfallRunoffUnitConverter.ConvertArea(AreaUnit,
-                                                                                                   RainfallRunoffEnums.
-                                                                                                       AreaUnit.m2,
-                                                                                                   value);
-            }
+            get => RainfallRunoffUnitConverter.ConvertArea(RainfallRunoffEnums.AreaUnit.m2, AreaUnit,
+                                                           Data.TotalAreaForGroundWaterCalculations);
+            set => Data.TotalAreaForGroundWaterCalculations = RainfallRunoffUnitConverter.ConvertArea(AreaUnit,
+                                                                                                      RainfallRunoffEnums.AreaUnit.m2,
+                                                                                                      value);
         }
 
         public double MaximumLandStorage
         {
-            get
-            {
-                return RainfallRunoffUnitConverter.ConvertStorage(RainfallRunoffEnums.StorageUnit.mm, StorageUnit,
-                                                                  Data.MaximumLandStorage,
-                                                                  Data.TotalAreaForGroundWaterCalculations);
-            }
-            set
-            {
-                Data.MaximumLandStorage = RainfallRunoffUnitConverter.ConvertStorage(StorageUnit,
-                                                                                     RainfallRunoffEnums.StorageUnit.mm,
-                                                                                     value,
-                                                                                     Data.
-                                                                                         TotalAreaForGroundWaterCalculations);
-            }
+            get => RainfallRunoffUnitConverter.ConvertStorage(RainfallRunoffEnums.StorageUnit.mm, StorageUnit,
+                                                              Data.MaximumLandStorage,
+                                                              Data.TotalAreaForGroundWaterCalculations);
+            set => Data.MaximumLandStorage = RainfallRunoffUnitConverter.ConvertStorage(StorageUnit,
+                                                                                        RainfallRunoffEnums.StorageUnit.mm,
+                                                                                        value,
+                                                                                        Data.TotalAreaForGroundWaterCalculations);
         }
 
         public double InitialLandStorage
         {
-            get
-            {
-                return RainfallRunoffUnitConverter.ConvertStorage(RainfallRunoffEnums.StorageUnit.mm, StorageUnit,
-                                                                  Data.InitialLandStorage,
-                                                                  Data.TotalAreaForGroundWaterCalculations);
-            }
-            set
-            {
-                Data.InitialLandStorage = RainfallRunoffUnitConverter.ConvertStorage(StorageUnit,
-                                                                                     RainfallRunoffEnums.StorageUnit.mm,
-                                                                                     value,
-                                                                                     Data.
-                                                                                         TotalAreaForGroundWaterCalculations);
-            }
+            get => RainfallRunoffUnitConverter.ConvertStorage(RainfallRunoffEnums.StorageUnit.mm, StorageUnit,
+                                                              Data.InitialLandStorage,
+                                                              Data.TotalAreaForGroundWaterCalculations);
+            set => Data.InitialLandStorage = RainfallRunoffUnitConverter.ConvertStorage(StorageUnit,
+                                                                                        RainfallRunoffEnums.StorageUnit.mm,
+                                                                                        value,
+                                                                                        Data.TotalAreaForGroundWaterCalculations);
         }
 
         public double InfiltrationCapacity
         {
-            get
-            {
-                return RainfallRunoffUnitConverter.ConvertRainfall(RainfallRunoffEnums.RainfallCapacityUnit.mm_hr,
-                                                                   InfiltrationCapacityUnit, Data.InfiltrationCapacity);
-            }
-            set
-            {
-                Data.InfiltrationCapacity = RainfallRunoffUnitConverter.ConvertRainfall(InfiltrationCapacityUnit,
-                                                                                        RainfallRunoffEnums.RainfallCapacityUnit.mm_hr, 
-                                                                                        value);
-            }
+            get => RainfallRunoffUnitConverter.ConvertRainfall(RainfallRunoffEnums.RainfallCapacityUnit.mm_hr,
+                                                               InfiltrationCapacityUnit, Data.InfiltrationCapacity);
+            set => Data.InfiltrationCapacity = RainfallRunoffUnitConverter.ConvertRainfall(InfiltrationCapacityUnit,
+                                                                                           RainfallRunoffEnums.RainfallCapacityUnit.mm_hr,
+                                                                                           value);
         }
 
         public string AreaPerCropTypeLabel
         {
             get
             {
-                var converter = TypeDescriptor.GetConverter(typeof (RainfallRunoffEnums.AreaUnit));
+                TypeConverter converter = TypeDescriptor.GetConverter(typeof(RainfallRunoffEnums.AreaUnit));
                 return "Area per crop type in " + converter.ConvertToString(areaUnit);
             }
         }
+
+        private bool LinkedToRunoffBoundary => Data.Catchment.Links.Any() && Data.Catchment.Links.First().Target is RunoffBoundary;
+
+        private bool LinkedToFlowNode => Data.Catchment.Links.Any() && !LinkedToRunoffBoundary;
     }
 }
