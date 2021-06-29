aea8272eb7bd10b12f3e577023f35b70a91c1df6
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-06-29 09:44:25 +0000

    FM1D2D-1658 Add tests for GreenHouseDataViewModel
    Former-commit-id: f85acd658c08629d223fc78ac7b0b5653e1e6789
    Former-commit-id: 8b4815ffd1b21cf41f5ae598005edea5da2e8fe1

---
+++ test/DeltaShell.Plugins.DelftModels.RainfallRunoff.Tests/Domain/Concepts/Greenhouse/GreenHouseDataViewModelTest.cs
@@ -0,0 +1,88 @@
+using DelftTools.Hydro;
+using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain;
+using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain.Concepts;
+using DeltaShell.Plugins.DelftModels.RainfallRunoff.Gui.Concepts;
+using GeoAPI.Geometries;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeltaShell.Plugins.DelftModels.RainfallRunoff.Tests.Domain.Concepts.Greenhouse
+{
+    [TestFixture]
+    public class GreenHouseDataViewModelTest
+    {
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 10, 7, 700d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 1, 7, 7000d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 10, 70, 7000d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 1, 70, 70000d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 10, 7, 7)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 1, 7, 7)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 10, 70, 70)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 1, 70, 70)]
+        public void SetMaximumRoofStorage_SetsCorrectValueOnData(RainfallRunoffEnums.StorageUnit storageUnit, double area, double setValue, double expValue)
+        {
+            // Setup
+            GreenhouseData data = GetData(area);
+            var viewModel = new GreenhouseDataViewModel(data, RainfallRunoffEnums.AreaUnit.km2) {StorageUnit = storageUnit};
+
+            // Call
+            viewModel.MaximumRoofStorage = setValue;
+
+            // Assert
+            Assert.That(viewModel.MaximumRoofStorage, Is.EqualTo(setValue));
+            Assert.That(data.MaximumRoofStorage, Is.EqualTo(expValue));
+        }
+
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 10, 7, 700d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 1, 7, 7000d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 10, 70, 7000d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.m3, 1, 70, 70000d)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 10, 7, 7)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 1, 7, 7)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 10, 70, 70)]
+        [TestCase(RainfallRunoffEnums.StorageUnit.mm, 1, 70, 70)]
+        public void SetInitialRoofStorage_SetsCorrectValueOnData(RainfallRunoffEnums.StorageUnit storageUnit, double area, double setValue, double expValue)
+        {
+            // Setup
+            GreenhouseData data = GetData(area);
+            var viewModel = new GreenhouseDataViewModel(data, RainfallRunoffEnums.AreaUnit.m2) {StorageUnit = storageUnit};
+
+            // Call
+            viewModel.InitialRoofStorage = setValue;
+
+            // Assert
+            Assert.That(viewModel.InitialRoofStorage, Is.EqualTo(setValue));
+            Assert.That(data.InitialRoofStorage, Is.EqualTo(expValue));
+        }
+
+        [TestCase(RainfallRunoffEnums.AreaUnit.m2, 1, 1)]
+        [TestCase(RainfallRunoffEnums.AreaUnit.ha, 1, 10000)]
+        [TestCase(RainfallRunoffEnums.AreaUnit.km2, 1, 1000000)]
+        [TestCase(RainfallRunoffEnums.AreaUnit.m2, 0.123, 0.123)]
+        [TestCase(RainfallRunoffEnums.AreaUnit.ha, 0.123, 1230)]
+        [TestCase(RainfallRunoffEnums.AreaUnit.km2, 0.123, 123000)]
+        public void SetSubSoilStorageArea_SetsCorrectValueOnData(RainfallRunoffEnums.AreaUnit areaUnit, double setValue, double expValue)
+        {
+            // Setup
+            GreenhouseData data = GetData(10);
+            var viewModel = new GreenhouseDataViewModel(data, areaUnit);
+
+            // Call
+            viewModel.SubSoilStorageArea = setValue;
+
+            // Assert
+            Assert.That(viewModel.SubSoilStorageArea, Is.EqualTo(setValue));
+            Assert.That(data.SubSoilStorageArea, Is.EqualTo(expValue));
+        }
+
+        private static GreenhouseData GetData(double area)
+        {
+            var geometry = Substitute.For<IGeometry>();
+            geometry.Area.Returns(area);
+            var catchment = new Catchment {Geometry = geometry};
+            var data = new GreenhouseData(catchment);
+
+            return data;
+        }
+    }
+}
