2cd30cee036c429cb0bd431caf6cff319a5555ad
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-07-09 06:53:20 +0000

    FM1D2D-1342 Adjust error message to include category name and category line number.
    Former-commit-id: 1d159d4bc267e1176301e3071d77aced5dcbe6d2
    Former-commit-id: d4ef4a125aecb18ea91c244ad6daa2b211e86111

--- test/DeltaShell.NGHS.IO.Tests/Helpers/DelftIniCategoryExtensionMethodsTest.cs
+++ test/DeltaShell.NGHS.IO.Tests/Helpers/DelftIniCategoryExtensionMethodsTest.cs
@@ -22,5 +22,19 @@ namespace DeltaShell.NGHS.IO.Tests.Helpers
             string error = TestHelper.GetAllRenderedMessages(Call, Level.Error).Single();
             Assert.That(error, Is.EqualTo("Property 'some_property' is not found in the file for category 'some_name' on line 7"));
         }
+
+        [Test]
+        public void ReadPropertiesToListOfType_IsOptionalFalse_PropertyNotFound_LogsError()
+        {
+            // Setup
+            var category = new DelftIniCategory("some_name") {LineNumber = 7};
+
+            // Call
+            void Call() => category.ReadPropertiesToListOfType<double>("some_property", isOptional: false);
+
+            // Assert
+            string error = TestHelper.GetAllRenderedMessages(Call, Level.Error).Single();
+            Assert.That(error, Is.EqualTo("Property 'some_property' is not found in the file for category 'some_name' on line 7"));
+        }
     }
 }
