b36408dc34582fc96982c7429605ba369aed282a
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-07-15 14:27:19 +0000

    FM1D2D-1456 Rewrite test to add cases later.
    Former-commit-id: ed91e25d3c282ed01cdb4f0e40918f4ce6d26018
    Former-commit-id: c0ba5e130dde71fd1530a6eb7334c514a499986a

--- test/DeltaShell.NGHS.IO.Tests/FileWriters/Structures/StructureRegionTest.cs
+++ test/DeltaShell.NGHS.IO.Tests/FileWriters/Structures/StructureRegionTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DeltaShell.NGHS.IO.FileWriters.Structure;
 using DeltaShell.NGHS.IO.Helpers;
 using NUnit.Framework;
@@ -8,16 +9,19 @@ namespace DeltaShell.NGHS.IO.Tests.FileWriters.Structures
     public class StructureRegionTest
     {
         [Test]
-        public void Capacity_IsCorrectConfigurationSetting()
+        [TestCaseSource(nameof(ConfigurationSettingsCases))]
+        public void ConfigurationSetting_IsCorrect(ConfigurationSetting setting, string expKey, string expDefault, string expDescription, string expFormat)
         {
-            // Call
-            ConfigurationSetting capacitySettings = StructureRegion.Capacity;
-
             // Assert
-            Assert.That(capacitySettings.Key, Is.EqualTo("capacity"));
-            Assert.That(capacitySettings.DefaultValue, Is.Null);
-            Assert.That(capacitySettings.Description, Is.EqualTo("Pump capacity (m3/s)"));
-            Assert.That(capacitySettings.Format, Is.EqualTo("F4"));
+            Assert.That(setting.Key, Is.EqualTo(expKey));
+            Assert.That(setting.DefaultValue, Is.EqualTo(expDefault));
+            Assert.That(setting.Description, Is.EqualTo(expDescription));
+            Assert.That(setting.Format, Is.EqualTo(expFormat));
+        }
+
+        private static IEnumerable<TestCaseData> ConfigurationSettingsCases()
+        {
+            yield return new TestCaseData(StructureRegion.Capacity, "capacity", null, "Pump capacity (m3/s)", "F4");
         }
     }
 }
