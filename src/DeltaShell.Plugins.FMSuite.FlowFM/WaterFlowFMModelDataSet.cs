b95eb8a0f6504c0460186230dd919c69544d645c
Stef Hummel <Stef.Hummel@deltares.nl>
2021-07-19 18:30:41 +0000

    FM1D2D-1663 Fix in adding discharge.
    Former-commit-id: fc6343950e29806c187d942a18ae2a64a5ad3109
    Former-commit-id: 891df4ae25b74dc72f23c31708908ff236580985

--- src/DeltaShell.Plugins.FMSuite.FlowFM/WaterFlowFMModelDataSet.cs
+++ src/DeltaShell.Plugins.FMSuite.FlowFM/WaterFlowFMModelDataSet.cs
@@ -182,7 +182,7 @@ namespace DeltaShell.Plugins.FMSuite.FlowFM
             {
                 {Model1DParameterNames.ObservationPointWaterLevel, "water_level"},
                 {Model1DParameterNames.ObservationPointWaterDepth, "water_depth"},
-                {Model1DParameterNames.ObservationPointWaterDepth, "discharge"},
+                {Model1DParameterNames.ObservationPointDischarge, "discharge"},
                 {Model1DParameterNames.ObservationPointVelocity, "velocity"},
                 {Model1DParameterNames.ObservationPointSaltConcentration, "salinity"},
                 {Model1DParameterNames.ObservationPointSaltDispersion, "salinity"},
