7391588e4c686f3aa42bb0658bf12584c9d741bf
Prisca van der Sluis <36264671+priscavdsluis@users.noreply.github.com>
2021-10-11 13:14:22 +0200

    Feature/fm1 d2 d 1784 silo definition error (#18)
    * FM1D2D-1784 Improve error message
    
    * FM1D2D-1784 Give error only if silo's are used (silo area > 0)
    
    * FM1D2D-1784 Use TryGetValue
    
    * FM1D2D-1784 Add tests for the SobekRRGreenhouseImporter
    
    * FM1D2D-1784 Cleanup and reformat class, add docs
    
    Former-commit-id: 3920bbb1dc649f1a231002d365ab4aea4c140d73

--- src/DeltaShell.Plugins.ImportExport.Sobek/PartialSobekImporter/SobekRRGreenhouseImporter.cs
+++ src/DeltaShell.Plugins.ImportExport.Sobek/PartialSobekImporter/SobekRRGreenhouseImporter.cs
@@ -1,96 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using DeltaShell.NGHS.Utils;
 using DeltaShell.Plugins.DelftModels.RainfallRunoff;
 using DeltaShell.Plugins.DelftModels.RainfallRunoff.Domain.Concepts;
 using DeltaShell.Sobek.Readers.Readers.SobekRrReaders;
+using DeltaShell.Sobek.Readers.SobekDataObjects;
 using log4net;
 
 namespace DeltaShell.Plugins.ImportExport.Sobek.PartialSobekImporter
 {
-    public class SobekRRGreenhouseImporter: PartialSobekImporterBase
+    /// <summary>
+    /// Sobek importer for the greenhouse data.
+    /// </summary>
+    public class SobekRRGreenhouseImporter : PartialSobekImporterBase
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(SobekRRGreenhouseImporter));
 
-        public override string DisplayName
-        {
-            get { return "Rainfall Runoff greenhouse data"; }
-        }
+        public override string DisplayName => "Rainfall Runoff greenhouse data";
 
-        public override SobekImporterCategories Category { get; } = SobekImporterCategories.RainfallRunoff;
+        public override SobekImporterCategories Category => SobekImporterCategories.RainfallRunoff;
 
         protected override void PartialImport()
         {
             log.DebugFormat("Importing greenhouse data ...");
 
             var rainfallRunoffModel = GetModel<RainfallRunoffModel>();
-            var catchmentModelData = rainfallRunoffModel.GetAllModelData()
-                                                        .OfType<GreenhouseData>()
-                                                        .ToDictionary(rra => rra.Name);
-            ReadAndAddOrUpdateGreenhouseData(GetFilePath(SobekFileNames.SobekRRGreenhouseFileName), catchmentModelData);
+            Dictionary<string, GreenhouseData> catchmentModelData = rainfallRunoffModel.GetAllModelData()
+                                                                                       .OfType<GreenhouseData>()
+                                                                                       .ToDictionary(rra => rra.Name);
+            ImportGreenhouseData(GetFilePath(SobekFileNames.SobekRRGreenhouseFileName), catchmentModelData);
         }
 
-        private void ReadAndAddOrUpdateGreenhouseData(string filePath, Dictionary<string, GreenhouseData> catchmentModelData)
+        private static void ImportGreenhouseData(string filePath, Dictionary<string, GreenhouseData> catchmentModelData)
         {
-            var pathRf = filePath.Replace(".3B", ".RF");
-            var pathSil = filePath.Replace(".3B", ".SIL");
+            string storageFilePath = filePath.Replace(".3B", ".RF");
+            string siloFilePath = filePath.Replace(".3B", ".SIL");
 
-            var dicRf = new SobekRRStorageReader().Read(pathRf).ToDictionaryWithErrorDetails(pathRf, item => item.Id, item => item);
-            var dicSil = new SobekRRGreenhouseSiloDefinitionReader().Read(pathSil).ToDictionaryWithErrorDetails(pathSil, item => item.Id, item => item);
+            Dictionary<string, SobekRRStorage> storageData = new SobekRRStorageReader().Read(storageFilePath).ToDictionaryWithErrorDetails(storageFilePath, item => item.Id, item => item);
+            Dictionary<string, SobekRRGreenhouseSiloDefinition> siloData = new SobekRRGreenhouseSiloDefinitionReader().Read(siloFilePath).ToDictionaryWithErrorDetails(siloFilePath, item => item.Id, item => item);
 
-            foreach (var greenhouse in new SobekRRGreenhouseReader().Read(filePath))
+            foreach (SobekRRGreenhouse greenhouse in new SobekRRGreenhouseReader().Read(filePath))
             {
-                if (catchmentModelData.ContainsKey(greenhouse.Id))
-                {
-                    var greenhouseData = catchmentModelData[greenhouse.Id];
-
-                    greenhouseData.CalculationArea = greenhouse.Areas.Sum();
-
-                    for (var i = 0; i < greenhouse.Areas.Length; i++)
-                    {
-                        greenhouseData.AreaPerGreenhouse[
-                            (GreenhouseEnums.AreaPerGreenhouseType)Enum.ToObject(typeof(GreenhouseEnums.AreaPerGreenhouseType), i)] =
-                            greenhouse.Areas[i];
-                    }
-
-                    greenhouseData.SurfaceLevel = greenhouse.SurfaceLevel;
-
-                    greenhouseData.MeteoStationName = greenhouse.MeteoStationId;
-                    greenhouseData.AreaAdjustmentFactor = greenhouse.AreaAjustmentFactor;
-
-                    // Roof storage
-                    if (dicRf.ContainsKey(greenhouse.StorageOnRoofsId))
-                    {
-                        var storage = dicRf[greenhouse.StorageOnRoofsId];
-                        greenhouseData.InitialRoofStorage = storage.InitialRoofStorage;
-                        greenhouseData.MaximumRoofStorage = storage.MaxRoofStorage;
-                    }
-                    else
-                    {
-                        log.ErrorFormat("Storage definition {0} has not been found.", greenhouse.StorageOnRoofsId);
-                    }
-
-                    // Silo definition
-                    if (dicSil.ContainsKey(greenhouse.SiloId))
-                    {
-                        var silo = dicSil[greenhouse.SiloId];
-                        greenhouseData.SiloCapacity = silo.SiloCapacity;
-                        greenhouseData.PumpCapacity = silo.SiloPumpCapacity;
-                    }
-                    else
-                    {
-                        log.ErrorFormat("Silo definition {0} has not been found.", greenhouse.SiloId);
-                    }
-
-
-                    greenhouseData.SubSoilStorageArea = greenhouse.SiloArea;
-                    greenhouseData.UseSubsoilStorage = greenhouse.SiloArea > 0;
-                }
-                else
+                if (!catchmentModelData.TryGetValue(greenhouse.Id, out GreenhouseData greenhouseData))
                 {
                     log.WarnFormat("Rainfall runoff area with id {0} has not been found. Item has been skipped...", greenhouse.Id);
+                    continue;
                 }
+
+                greenhouseData.CalculationArea = greenhouse.Areas.Sum();
+
+                for (var i = 0; i < greenhouse.Areas.Length; i++)
+                {
+                    greenhouseData.AreaPerGreenhouse[(GreenhouseEnums.AreaPerGreenhouseType)i] = greenhouse.Areas[i];
+                }
+
+                greenhouseData.SurfaceLevel = greenhouse.SurfaceLevel;
+                greenhouseData.MeteoStationName = greenhouse.MeteoStationId;
+                greenhouseData.AreaAdjustmentFactor = greenhouse.AreaAjustmentFactor;
+
+                ImportStorageDefinition(greenhouseData, greenhouse, storageData, storageFilePath);
+                ImportSiloDefinition(greenhouseData, greenhouse, siloData, siloFilePath);
+            }
+        }
+
+        private static void ImportStorageDefinition(GreenhouseData greenhouseData, SobekRRGreenhouse greenhouse, Dictionary<string, SobekRRStorage> data, string filePath)
+        {
+            if (data.TryGetValue(greenhouse.StorageOnRoofsId, out SobekRRStorage storage))
+            {
+                greenhouseData.InitialRoofStorage = storage.InitialRoofStorage;
+                greenhouseData.MaximumRoofStorage = storage.MaxRoofStorage;
+            }
+            else
+            {
+                log.ErrorFormat($"Storage definition with id '{greenhouse.StorageOnRoofsId}' has not been found: {filePath}");
+            }
+        }
+
+        private static void ImportSiloDefinition(GreenhouseData greenhouseData, SobekRRGreenhouse greenhouse, Dictionary<string, SobekRRGreenhouseSiloDefinition> data, string filePath)
+        {
+            greenhouseData.SubSoilStorageArea = greenhouse.SiloArea;
+            greenhouseData.UseSubsoilStorage = greenhouse.SiloArea > 0;
+
+            if (data.TryGetValue(greenhouse.SiloId, out SobekRRGreenhouseSiloDefinition silo))
+            {
+                greenhouseData.SiloCapacity = silo.SiloCapacity;
+                greenhouseData.PumpCapacity = silo.SiloPumpCapacity;
+            }
+            else if (greenhouseData.UseSubsoilStorage)
+            {
+                log.ErrorFormat($"Silo definition with id '{greenhouse.SiloId}' has not been found: {filePath}");
             }
         }
     }
