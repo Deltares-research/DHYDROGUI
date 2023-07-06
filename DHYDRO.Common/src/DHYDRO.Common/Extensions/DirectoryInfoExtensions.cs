feee7be6e293f76fe126264bf374304d142d92c6
Tim van den Aardweg <71257004+tim-vd-aardweg@users.noreply.github.com>
2023-07-06 12:44:17 +0200

    Move extension classes from 1D2D to Common (#300)
    * Moved StringExtensions from 1D2D to Common.
    
    * Fixed namespace
    
    * Moved DictionaryExtensions from 1D2D to Common.
    
    * Moved DirectoryInfoExtensions from 1D2D to Common.
    
    * Moved EnumerableExtensions from 1D2D to Common.
    
    Former-commit-id: a95354db2fef575476c4b642ebe197a73c75cf88

---
+++ DHYDRO.Common/src/DHYDRO.Common/Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace DHYDRO.Common.Extensions
+{
+    /// <summary>
+    /// Contains extensions methods for <see cref="DirectoryInfo"/>.
+    /// </summary>
+    public static class DirectoryInfoExtensions
+    {
+        /// <summary>
+        /// Determines whether to instances of <see cref="DirectoryInfo"/> point to the same directory.
+        /// </summary>
+        /// <param name="directory1"> The first directory info. </param>
+        /// <param name="directory2"> The second directory info. </param>
+        /// <returns>
+        /// A boolean indicating whether the two instances of <see cref="DirectoryInfo"/> point to the same directory or are both
+        /// <c>null</c>.
+        /// </returns>
+        /// <remarks>
+        /// This method is only guaranteed to work for absolute paths.
+        /// </remarks>
+        public static bool EqualsDirectory(this DirectoryInfo directory1, DirectoryInfo directory2)
+        {
+            if (directory1 == null || directory2 == null)
+            {
+                return directory1 == directory2;
+            }
+
+            return directory1.FullName.EqualsCaseInsensitive(directory2.FullName);
+        }
+    }
+}
