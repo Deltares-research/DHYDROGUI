a67fac4a5a3f70c270036e7fd23b7396cba4448b
RStolkerDeltares <122269898+RStolkerDeltares@users.noreply.github.com>
2023-12-07 10:18:58 +0100

    FM1D2D-2742: Use generic object properties data (#409)
    Former-commit-id: 234501a7cb3164e24b0d25454aba378c6889908f

--- src/DeltaShell.Plugins.DelftModels.RealTimeControl.Gui/Forms/Properties/MathematicalExpressionProperties.cs
+++ src/DeltaShell.Plugins.DelftModels.RealTimeControl.Gui/Forms/Properties/MathematicalExpressionProperties.cs
@@ -11,13 +11,6 @@ namespace DeltaShell.Plugins.DelftModels.RealTimeControl.Gui.Forms.Properties
     [DisplayName("Mathematical Expression")]
     public class MathematicalExpressionProperties : ObjectProperties<MathematicalExpression>
     {
-        [Browsable(false)]
-        public override object Data
-        {
-            get => data;
-            set => data = (MathematicalExpression) value;
-        }
-
         [ResourcesDisplayName(typeof(Resources), "Common_Name_DisplayName")]
         [ResourcesCategory(typeof(Resources), "Categories_General")]
         [PropertyOrder(1)]
